1dfa848ad1ee8ae4f3b3fd8988851c946b77ff23
jdickens <jdickens@280ce282-5f28-0410-b095-e054cb237d46>
2008-06-14 03:59:35 +0000

    sproutcore now treats space as a printable key instead of a function key.
    git-svn-id: http://is-svn/svn/uieng/frameworks/sproutcore_1b/trunk@24891 280ce282-5f28-0410-b095-e054cb237d46

--- foundation/input_manager.js
+++ foundation/input_manager.js
@@ -50,7 +50,8 @@ SC.InputManager = SC.Object.extend(
         return responder[methodName]( event );
       }
     } 
-    if (chr && responder.respondsTo('insertText'))
+    // if(cmd == "space") chr = " ";
+    if ( chr && responder.respondsTo('insertText'))
     {
       // if we haven't returned yet and there is plain text, then do an insert of the text.
       return responder.insertText(chr);
@@ -132,7 +133,7 @@ SC.MODIFIER_KEYS = {
 
 SC.FUNCTION_KEYS = {
   8: 'backspace',  9: 'tab',  13: 'return',  19: 'pause',  27: 'escape',  
-  32: 'space',  33: 'pageup', 34: 'pagedown', 35: 'end', 36: 'home', 
+  33: 'pageup', 34: 'pagedown', 35: 'end', 36: 'home', 
   37: 'left', 38: 'up', 39: 'right', 40: 'down', 44: 'printscreen', 
   45: 'insert', 46: 'delete', 112: 'f1', 113: 'f2', 114: 'f3', 115: 'f4', 
   116: 'f5', 117: 'f7', 119: 'f8', 120: 'f9', 121: 'f10', 122: 'f11', 
@@ -140,12 +141,12 @@ SC.FUNCTION_KEYS = {
 } ;
 
 SC.PRINTABLE_KEYS = {
-  48:"0", 49:"1", 50:"2", 51:"3", 52:"4", 53:"5", 54:"6", 55:"7", 56:"8",
-  57:"9", 59:";", 61:"=", 65:"a", 66:"b", 67:"c", 68:"d", 69:"e", 70:"f",
-  71:"g", 72:"h", 73:"i", 74:"j", 75:"k", 76:"l", 77:"m", 78:"n", 79:"o",
-  80:"p", 81:"q", 82:"r", 83:"s", 84:"t", 85:"u", 86:"v", 87:"w", 88:"x",
-  89:"y", 90:"z", 107:"+", 109:"-", 110:".", 188:",", 190:".", 191:"/",
-  192:"`", 219:"[", 220:"\\", 221:"]", 222:"\""
+  32: ' ', 48:"0", 49:"1", 50:"2", 51:"3", 52:"4", 53:"5", 54:"6", 55:"7",
+  56:"8", 57:"9", 59:";", 61:"=", 65:"a", 66:"b", 67:"c", 68:"d", 69:"e",
+  70:"f", 71:"g", 72:"h", 73:"i", 74:"j", 75:"k", 76:"l", 77:"m", 78:"n",
+  79:"o", 80:"p", 81:"q", 82:"r", 83:"s", 84:"t", 85:"u", 86:"v", 87:"w",
+  88:"x", 89:"y", 90:"z", 107:"+", 109:"-", 110:".", 188:",", 190:".",
+  191:"/", 192:"`", 219:"[", 220:"\\", 221:"]", 222:"\""
 } ;
 
 // make reverse keycode lookup for using in unit tests...
