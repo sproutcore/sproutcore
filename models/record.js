8ca12d48c2aece2ce0dbe7a85d95ad3d782fa3d6
Charles Jolley <charles@sproutit.com>
2008-06-24 09:25:25 -0700

    use more modern date parsing

--- models/record.js
+++ models/record.js
@@ -593,7 +593,7 @@ SC.Record.Date = function(value,direction) {
   } else if (typeof(value) == "string") {
     // try to parse date. trim any decimal numbers at end since Rails sends
     // this sometimes.
-    var ret = Date.parseDate(value.replace(/\.\d+$/,'')) ;
+    var ret = Date.parse(value.replace(/\.\d+$/,'')) ;
     if (ret) value = ret ;
   }
   return value ;
