e73ea85e933a18c39725e84ec673c83b5eee1061
pinzon <pinzon@280ce282-5f28-0410-b095-e054cb237d46>
2008-07-04 00:04:49 +0000

    -<rdar://problem/6045791> IE7 memory leak improvements
    git-svn-id: http://is-svn/svn/uieng/frameworks/sproutcore_1b/trunk@27052 280ce282-5f28-0410-b095-e054cb237d46

--- foundation/page.js
+++ foundation/page.js
@@ -48,6 +48,7 @@ SC.Page = SC.Object.extend(
     SC.callOnLoad(function() {
       if (el && el.parentNode) el.parentNode.removeChild(el) ;
     }) ;
+	el=null;
   },
   
   // returns the property, but only if it is already configured.
