40760753b60753f36eb7ca03e8026f4b1c7b919e
Charles Jolley <charles@sproutit.com>
2008-07-10 13:27:19 -0700

    Fix documentation bug

--- core.js
+++ core.js
@@ -39,9 +39,16 @@
 //
 // ==========================================================================
 
-// All objects live in the SproutCore namespace, which is also availabe in the
-// abreviation SC.
-if (!window.SC) SC = {};  SproutCore = SC ;
+if (!window.SC) {
+  /**
+    @namespace
+    
+    All objects live in the SproutCore namespace, which is also availabe in the
+    abbreviation SC.
+  */
+  SC = {};
+  SproutCore = SC ;
+}
 
 // this makes for some nicer to read code
 var YES = true ; var NO = false ;
