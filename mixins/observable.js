b2a76c224de4825b4e82c1e5458275a2fe9fbdb9
Charles Jolley <charles@sproutit.com>
2008-08-04 03:27:20 -0700

    Moved obverser set to its own file

--- mixins/observable.js
+++ mixins/observable.js
@@ -923,7 +923,7 @@ SC.Observable = {
 // FUNCTION ENHANCEMENTS
 //
 // Enhance function.
-Object.extend(Function.prototype,
+SC.mixin(Function.prototype,
 /** @scope Function.prototype */ {
   
   /**
@@ -1038,8 +1038,10 @@ Object.extend(Function.prototype,
     this.isProperty = true; return this; 
   },
   
-  // Declare that a function should observe an object at the named path.  Note
-  // that the path is used only to construct the observation one time.
+  /**  
+    Declare that a function should observe an object at the named path.  Note
+    that the path is used only to construct the observation one time.
+  */
   observes: function(propertyPaths) { 
     this.propertyPaths = $A(arguments); 
     return this;
@@ -1077,87 +1079,6 @@ Object.extend(Function.prototype,
   }    
   
 }) ;
-
-// ........................................................................
-// OBSERVER Set
-//
-// This private class is used to store information about obversers on a 
-// particular key.  Note that this object is not observable.
-SC._ObserverSet = {
-
-  // the number of targets in the set.
-  targets: 0,
-  
-  _membersCacheIsValid: NO,
-  
-  // adds the named target/method observer to the set.  The method must be
-  // a function, not a string.
-  add: function(target, method) {
-    var targetGuid = SC.guidFor(target) ;
-    
-    // get the set of methods
-    var methods = this[targetGuid] ;
-    if (!methods) {
-      methods = this[targetGuid] = SC.Set.create() ;
-      methods.target = target ;
-      methods.isTargetSet = YES ; // used for getMembers().
-      this.targets++ ;
-    }
-    
-    methods.add(method) ;
-    this._membersCacheIsValid = NO ;
-  },
-  
-  // removes the named target/method observer from the set.  If this is the
-  // last method for the named target, then the number of targets will also
-  // be reduced.
-  //
-  // returns YES if the items was removed, NO if it was not found.
-  remove: function(target, method) {
-    var targetGuid = SC.guidFor(target) ;
-    
-    // get the set of methods
-    var methods = this[targetGuid] ;    
-    if (!methods) return NO ;
-    
-    methods.remove(method) ;
-    if (methods.length <= 0) {
-      methods.target = null;
-      methods.isTargetSet = NO ;
-      delete this[targetGuid] ;
-      this.targets-- ;
-    }
-    
-    this._membersCacheIsValid = NO;
-    
-    return YES ;
-  },
-  
-  // Returns an array of target/method pairs.  This is cached.
-  getMembers: function() {
-    if (this._membersCacheIsValid) return this._members ;
-    
-    // need to recache, reset the array...
-    if (!this._members) {
-      this._members = [] ;
-    } else this._members.length = 0 ; // reset
-    var ret = this._members ;
-
-    // iterate through the set, look for sets.
-    for(var key in this) {
-      if (!this.hasOwnProperty()) continue ;
-      var value = this[key] ;
-      if (value && value.isTargetSet) {
-        var idx = value.length;
-        var target = value.target ;
-        while(--idx>=0) ret.push([target, value[idx]]) ;
-      }
-    }
-    
-    return ret ;
-  }
-  
-} ;
 
 // ........................................................................
 // OBSERVER QUEUE
@@ -1217,33 +1138,3 @@ SC.Observers = {
   }
 } ;
 
-
-// ........................................................................
-// NOTIFCATION QUEUE
-//
-// Property notifications are placed into this queue first and then processed
-// to keep the stack size down.
-SC.NotificationQueue = {
-  queue: [],
-  maxFlush: 5000, // max time you can spend flushing before we reschedule.
-  _flushing: false,
-  add: function(target, func, args) { this.queue.push([target, func, args]);},
-  flush: function(force) {
-    if (this._flushing && !force) return ;
-    this._flushing = true ;
-    
-    var start = new Date().getTime() ;
-    var now = start ;
-    var n = null ;
-    while(((now - start) < this.maxFlush) && (n = this.queue.pop())) { 
-      var t = n[0] || n[1] ;
-      n[1].apply(t,n[2]) ;
-      now = Date.now() ;
-    }
-    this._flushing = false ;
-    
-    if (this.queue.length > 0) { 
-      SC.NotificationQueue.flush.invokeLater(SC.NotificationQueue, 1) ;
-    }
-  }
-} ;
