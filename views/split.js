a70c4fad89e86944cb967d2cc75bbad82dcd5e37
Lawrence Pit <lawrence.pit@gmail.com>
2008-08-19 11:34:39 +1000

    Re-implementation of SC.SplitView and SC.SplitDividerView. See tickets #128 and #129.

--- views/split.js
+++ views/split.js
@@ -9,49 +9,70 @@ require('mixins/delegate_support');
 SC.HORIZONTAL = 'horizontal' ;
 SC.VERTICAL = 'vertical' ;
 
-/** 
+/**
   @class
 
   A split view is used to show views that the user can resize or collapse.
-  To use the split view, you need to add the child views you want layed out
-  separated by divider views (instances of SC.SplitDividerView).
+  To use the split view, you need to add a top left view followed by an
+  instance of SC.SplitDividerView followed by a bottom right view.
+
+  For example:
   
-  When the user clicks and drags on a divider view, it will automatically
-  resize the views immediately before and after the view.  You can constrain
-  the resizing allowed by the split view either by setting a minThickness and
-  maxThickness property on the views themselves or by implementing methods
-  on a delegate object.
+  {{{
+    <% view :workspace_container, :class => 'workspace_container' do %>
+      <% split_view :workspace, :class => 'sc-app-workspace', :direction => :vertical do %>
+        <% view :top_view, :can_collapse => true, :min_thickness => 50 do %>
+          <p>My top view</p>
+        <% end %>
+        <%= split_divider_view %>
+        <% view :bottom_view, :can_collapse => false, :max_thickness => 100 do %>
+          <p>My bottom view</p>
+        <% end %>
+      <% end%>
+    <% end %>
+  }}}
+
+  When the user clicks and drags on a split divider view, it will
+  automatically resize the views immediately before and after the split
+  divider view. You can constrain the resizing allowed by the split view
+  either by setting a minThickness and maxThickness property on the views
+  themselves or by implementing the method splitViewConstrainThickness on
+  a delegate object.
 
   In addition to resizing views, users can also collapse views by double
-  clicking on a divider view.  When a view is collapsed, it's isVisible 
+  clicking on a split divider view.  When a view is collapsed, it's isVisible
   property is set to NO and its space it removed from the view.  Double
-  clicking on a divider again will restore a collapse view.  A user can also
+  clicking on a divider again will restore a collapsed view.  A user can also
   start to drag the divider to show the collapsed view.
-  
-  You can programmatically control collapsing behavior by setting the
-  canCollapseViews property on the SplitView, a canCollapse property on each
-  child view, or by implementing the appropriate delegate method.
-  
-  Finally, SplitViews can layout their child views either horizontally or 
+
+  You can programmatically control collapsing behavior by setting a
+  canCollapse property on either of the top/left or right/bottom view
+  (if both are set it will take the top/left), and/or by setting a
+  canCollapseViews property on this split view, or by implementing the
+  method splitViewCanCollapse on a delegate object.
+
+  Finally, SplitViews can layout their child views either horizontally or
   vertically.  To choose the direction of layout set the layoutDirection
-  property on the view.  This property should be set when the view is created.
-  Changing it dynamically will have an unknown effect.
-  
+  property on the view (or the :direction option in the view helper).
+  This property should be set when the view is created. Changing it
+  dynamically will have an unknown effect.
+
   @extends SC.View
   @extends SC.DelegateSupport
-  
+
   @author Charles Jolley
+  @author Lawrence Pit
 */
 SC.SplitView = SC.View.extend(SC.DelegateSupport,
 /** @scope SC.SplitView.prototype */ {
 
   emptyElement: '<div class="sc-split-view"></div>',
-  
+
   /**
     delegate for controlling split view behavior.
   */
   delegate: null,
-  
+
   /**
     Direction of layout.  Must be SC.HORIZONTAL || SC.VERTICAL.
   */
@@ -66,214 +87,187 @@ SC.SplitView = SC.View.extend(SC.DelegateSupport,
     Used by split divider to decide if the view can be collapsed.
   */
   canCollapseView: function(view) {
-    if (!this.get('canCollapseViews')) return NO ;
-    if (view.get('canCollapse') === NO) return NO ;
     return this.invokeDelegateMethod(this.delegate, 'splitViewCanCollapse', this, view) ;
-  },
-  
-  /**
-    One view in your array must have a flexible width to allow proper
-    resizing.  You can set this view manually with this outlet or you can
-    just let the split view choose the center-most view.
-    
-    Views to the left/top of this view will be anchored to the left/top of
-    the parent view.  Views to the right/bottom of this view will be anchored
-    to the right/bottom.
-  */
-  flexibleView: null,
-  
-  /**
-    Sets the thickness of the named view and then lays out the rest of the
-    views.
-    
-    @param {SC.View} view the view to adjust. Must not be a divider.
-    @param {Number} offset the new desired offset
-    @returns the actual allowed offset
-  */
-  setThicknessForView: function(view, thickness) {
-    if (view.get('parentNode') != this) {
-      throw "view must belong to reciever (view: %@)".fmt(view);
-    }
-
-    var direction = this.get('layoutDirection') ;
-    
-    // constrain to thickness set on view.
-    var max = view.get('maxThickness') ;
-    var min = view.get('minThickness') ;
-    if (max != null) thickness = Math.min(max, thickness) ; 
-    if (min != null) thickness = Math.max(min, thickness) ;
-    
-    // constrain to thickness determined by delegate.
-    thickness = this.invokeDelegateMethod(this.delegate, 'splitViewConstrainThickness', this, view, thickness) ;
-    
-    // thickness cannot be greater than the total of all the other views (
-    // except for the flexibleView) added together.
-  
-    // available = total minus thickness of all views except "view" and 
-    // "flexible view" that is the same as thickness 'view' plus thickness 
-    // 'flexible view'
-    var flexibleView = this.get('flexibleView');
-    var available = this.thicknessForView(view) +   
-        this.thicknessForView(flexibleView);
-  
-    thickness = Math.min(thickness, available) ;
-    
-    // cannot be less than 0
-    thickness = Math.max(0, thickness) ;
-    
-    // now apply constrained value
-    if (thickness != this.thicknessForView(view)) {
-      
-      // un-collapse if needed.
-      view.set('isCollapsed', (thickness <= 0)) ;
-      
-      // set new frame
-      var f = (direction === SC.HORIZONTAL) ? { width: thickness } : { height: thickness } ;
-      view.set('frame', f) ;
-      
-      // and layout
-      this.layout() ;
-    }
-    
   },
 
   /**
     Returns the thickness for a given view.
-    
+
     @param {SC.View} view the view to get.
     @returns the view with the width.
   */
-  thicknessForView: function(view) {
+  getThicknessForView: function(view) {
     var direction = this.get('layoutDirection') ;
     var ret = view.get('frame') ;
-    return (direction === SC.HORIZONTAL) ? ret.width : ret.height ; 
+    return (direction === SC.HORIZONTAL) ? ret.width : ret.height ;
   },
-  
+
   /**
-    Finds the flexible view.
-    
-    This will use the flexibleView property if you set it or use the center
-    most view that is not a divider.
+    Sets the thickness of the named view and then lays out the rest of the
+    views.
+
+    @param {SC.View} view the view to adjust. Must not be a divider.
+    @param {Number} proposedThickness the new desired thickness
+    @returns the actual thickness
   */
-  computeFlexibleView: function() {
-    var flexibleView = this.get('flexibleView') ;
-    var originalFlexibleView = flexibleView ;
-    if (!flexibleView) {
-      var views = this.get('childNodes') ;
-      flexibleView = views[Math.ceil(views.length/2)] ;
+  setThicknessForView: function(view, proposedThickness) {
+    if (view.get('parentNode') != this) {
+      throw "view must belong to receiver (view: %@)".fmt(view);
     }
+
+    var views = this.get('childNodes') ;
+    var tl_view = views[0] ;  // top/left view
+    var br_view = views[2] ;  // bottom/right view
+    var tl_view_thickness = this.getThicknessForView(tl_view);
+    var br_view_thickness = this.getThicknessForView(br_view);
     
-    // If the flexible view is a divider, find the first non-g.
-    while(flexibleView && (flexibleView instanceof SC.SplitDividerView)) {
-      flexibleView = flexibleView.get('nextSibling') ;
+    var minAvailable = this.getThicknessForView(views[1]) ;  // thickness of divider
+    var maxAvailable = 0;
+    if (!tl_view.get("isCollapsed")) maxAvailable += this.getThicknessForView(tl_view) ;
+    if (!br_view.get("isCollapsed")) maxAvailable += this.getThicknessForView(br_view) ;
+
+    if (view == br_view) {
+      proposedThickness = maxAvailable - proposedThickness ;
+      view = tl_view ;
+    } else if (view != tl_view) {
+      throw "You can only set the thickness for the top/left or bottom/right views"
     }
-    
-    // save new flexible view if we had to fix it up.
-    if (originalFlexibleView !== flexibleView) {
-      this.set('flexibleView', flexibleView);
+
+    var thickness = proposedThickness;
+    var direction = this.get('layoutDirection') ;
+    var bottomRightCanCollapse = this.canCollapseView(br_view);
+
+    // constrain to thickness set on top/left
+    var max = tl_view.get('maxThickness') ;
+    var min = tl_view.get('minThickness') ;
+    if (max != null) thickness = Math.min(max, thickness) ;
+    if (min != null) thickness = Math.max(min, thickness) ;
+
+    // constrain to thickness set on bottom/right
+    max = br_view.get('maxThickness') ;
+    min = br_view.get('minThickness') ;
+    bottomRightThickness = maxAvailable - thickness ;
+    if (max != null) bottomRightThickness = Math.min(max, bottomRightThickness) ;
+    if (min != null) bottomRightThickness = Math.max(min, bottomRightThickness) ;
+    thickness = maxAvailable - bottomRightThickness ;
+
+    // constrain to thickness determined by delegate.
+    thickness = this.invokeDelegateMethod(this.delegate, 'splitViewConstrainThickness', this, tl_view, thickness) ;
+
+    // cannot be more than what's available
+    thickness = Math.min(thickness, maxAvailable) ;
+
+    // cannot be less than zero
+    thickness = Math.max(0, thickness) ;
+
+    if ((proposedThickness <= 0) && this.canCollapseView(tl_view)) {
+      // want to collapse top/left, check if this doesn't violate the max thickness of bottom/right
+      max = br_view.get('maxThickness');
+      if (!max || (minAvailable + maxAvailable) <= max) {
+        // collapse top/left view, even if it has a minThickness
+        thickness = 0 ;
+      }
+    } else if (proposedThickness >= maxAvailable && this.canCollapseView(br_view)) {
+      // want to collapse bottom/right, check if this doesn't violate the max thickness of top/left
+      max = tl_view.get('maxThickness');
+      if (!max || (minAvailable + maxAvailable) <= max) {
+        // collapse bottom/right view, even if it has a minThickness
+        thickness = maxAvailable;
+      }
     }
-    
-    return flexibleView;
-  }, 
-  
+
+    // now apply constrained value
+    if (thickness != this.getThicknessForView(tl_view)) {
+
+      // un-collapse if needed.
+      tl_view.set('isCollapsed', thickness == 0) ;
+      br_view.set('isCollapsed', thickness >= maxAvailable) ;
+
+      // set new frame
+      var f = (direction === SC.HORIZONTAL) ? { width: thickness } : { height: thickness } ;
+      tl_view.set('frame', f) ;
+
+      // and layout
+      this.layout() ;
+    }
+
+    console.log(proposedThickness + " --> " + thickness + " isCollapsed[0]="+tl_view.get('isCollapsed')+ ", isCollapsed[2]="+br_view.get('isCollapsed'));
+
+  },
+
+
+
   /**
     Layout the views.
-    
+
     This method needs to be called anytime you change the view thicknesses
     to make sure they are arranged properly.  This will setup the views so
     that they can resize appropriately.
   */
   layout: function() {
-
     var views = this.get('childNodes') ;
-    
-    // find the flexible view, if it is not set.  
-    var flexibleView = this.computeFlexibleView();
-
-    // everything before the flexible view is anchored to the left/top
+    var topLeftThickness = this.getThicknessForView(views[0]) ;
+    var dividerThickness = this.getThicknessForView(views[1]) ;
     var direction = this.get('layoutDirection') ;
+
+    // top/left view
     var view = views[0];
-    var offset = 0 ;
-    while(view && (view !== flexibleView)) {
-      var isCollapsed = view.get('isCollapsed') || NO ;
-      view.setIfChanged('isVisible', !isCollapsed) ;
-      if (!isCollapsed) {
-        view.viewFrameWillChange() ;
-        if (direction == SC.HORIZONTAL) {
-          view.setIfChanged('styleLeft', offset) ;
-          view.setIfChanged('styleRight', null) ;
-        } else {
-          view.setIfChanged('styleTop', offset) ;
-          view.setIfChanged('styleBottom', null) ;
-        }
-        view.viewFrameDidChange() ;
+    var isCollapsed = view.get('isCollapsed') || NO ;
+    view.setIfChanged('isVisible', !isCollapsed) ;
 
-        offset += this.thicknessForView(view) ;
-      }
-      view = view.get('nextSibling') ;
+    // split divider view
+    view = views[1] ;
+    view.viewFrameWillChange() ;
+    if (direction == SC.HORIZONTAL) {
+      view.setIfChanged('styleLeft', topLeftThickness) ;
+    } else {
+      view.setIfChanged('styleTop', topLeftThickness) ;
     }
+    view.viewFrameDidChange() ;
 
-    var flexHead = offset ;
-    
-    // everything after the flexible view is anchored to the right/bottom.
-    var view = views.last() ;
-    var offset = 0;
-    while(view && (view !== flexibleView)) {
-      var isCollapsed = view.get('isCollapsed') || NO ;
-      view.setIfChanged('isVisible', !isCollapsed) ;
-      if (!isCollapsed) {
-        view.viewFrameWillChange() ;
-        if (direction == SC.HORIZONTAL) {
-          view.setIfChanged('styleLeft', null) ;
-          view.setIfChanged('styleRight', offset) ;
-        } else {
-          view.setIfChanged('styleTop', null) ;
-          view.setIfChanged('styleBottom', offset) ;
-        }
-        view.viewFrameDidChange() ;
-
-        offset += this.thicknessForView(view) ;
-      }
-      view = view.get('previousSibling') ;
-    }
-    
-    var flexTail = offset ;
-    
-    if (flexibleView) {
-      view = flexibleView ;
+    // bottom/right view
+    view = views[2] ;
+    var isCollapsed = view.get('isCollapsed') || NO ;
+    view.setIfChanged('isVisible', !isCollapsed) ;
+    if (!isCollapsed) {
       view.viewFrameWillChange() ;
       if (direction == SC.HORIZONTAL) {
-        view.setIfChanged('styleLeft', flexHead) ;
-        view.setIfChanged('styleRight', flexTail) ;
+        view.setIfChanged('styleLeft', topLeftThickness + dividerThickness) ;
+        view.setIfChanged('styleRight', 0) ;
         view.setIfChanged('styleWidth', null) ;
       } else {
-        view.setIfChanged('styleTop', flexHead) ;
-        view.setIfChanged('styleBottom', flexTail) ;
+        view.setIfChanged('styleTop', topLeftThickness + dividerThickness) ;
+        view.setIfChanged('styleBottom', 0) ;
         view.setIfChanged('styleHeight', null) ;
       }
       view.viewFrameDidChange() ;
     }
-    
   },
 
   /**
     (DELEGATE) Control whether a view can be collapsed.
-    
-    The default implemention returns YES.
-    
+
+    The default implemention returns NO if the split view property
+    canCollapseViews is set to NO or when the given view has
+    property canCollapse set to NO, otherwise it returns YES.
+
     @param {SC.SplitView} splitView the split view
     @param {SC.View} view the view we want to collapse.
     @returns {Boolean} YES to allow collapse.
   */
-  splitViewCanCollapse: function(splitView, view) { return YES; },
+  splitViewCanCollapse: function(splitView, view) {
+    if (splitView.get('canCollapseViews') === NO) return NO ;
+    if (view.get('canCollapse') === NO) return NO ;
+    return YES ;
+  },
 
   /**
     (DELEGATE) Constrain a views allowed thickness.
-    
+
     The default implementation allows any thickness.  The view will
-    automatically constrain the view to not allow views to overflow the 
+    automatically constrain the view to not allow views to overflow the
     visible area.
-    
+
     @param {SC.SplitView} splitView the split view
     @param {SC.View} view the view in question
     @param {Number} proposedThickness the proposed thickness.
@@ -282,14 +276,14 @@ SC.SplitView = SC.View.extend(SC.DelegateSupport,
   splitViewConstrainThickness: function(splitView, view, proposedThickness) {
     return proposedThickness;
   },
-  
+
   /** @private */
   init: function() {
     sc_super() ;
     this.addClassName(this.get('layoutDirection')) ;
   }
-  
-  
+
+
 }) ;
 
 
--- views/split_divider.js
+++ views/split_divider.js
@@ -6,104 +6,92 @@
 require('views/view') ;
 require('views/split');
 
-/** 
+/**
   @class
 
-  A SplitDividerView displays a divider between two split views.  Clicking
-  and dragging the divider will change the thickness of the view either to 
-  the left or right of the divider, depending on which side of the flexible
-  view the divider is on.
-  
-  Double-clicking will try to collapse the same view so it is not visible
-  unless you have canCollapse disabled on the SplitView.
-  
-  This view must be a direct child of the split view it works with.
-  
+  A SplitDividerView displays a divider between two views within a SplitView.
+  Clicking and dragging the divider will change the thickness of each view
+  either to the top/left or bottom/right of the divider.
+
+  Double-clicking on the SplitDividerView will try to collapse the first
+  view within the SplitView that has property canCollapse set to true,
+  so it is not visible, unless you have canCollapse disabled on the SplitView.
+
+  This view must be a direct child of the split view it works with. It must
+  be surrounded by two other views.
+
   @extends SC.View
-  
+
   @author Charles Jolley
+  @author Lawrence Pit
 */
 SC.SplitDividerView = SC.View.extend(
 /** @scope SC.SplitDividerView.prototype */ {
-  
+
   emptyElement: '<div class="sc-split-divider-view"></div>',
 
-  /**
-    Returns the view to be managed by the divider view.
-  */
-  targetView: function() {  
-    var splitView = this.get('parentNode') ;
-    if (!splitView) return null ;
-
-    var flexibleView = splitView.computeFlexibleView() ;
-    var views = splitView.get('childNodes') ;
-    var myIndex = views.indexOf(this) ;
-    var flexibleIndex = views.indexOf(flexibleView) ;
-    
-    if (myIndex < 0) throw "SplitDividerView must belong to the SplitView";
-    
-    return (myIndex <= flexibleIndex) ? this.get('previousSibling') : this.get('nextSibling') ;
-
-  }.property(),
-  
   mouseDown: function(evt) {
-
-    var splitView = this.get('parentNode') ;
-    if (!splitView) return ;
-    
     // cache some info for later use.
     this._mouseDownLocation = Event.pointerLocation(evt) ;
+    this._splitView = this.get('parentNode') ;
+    this._topLeftView = this.get('previousSibling') ;
+    this._originalTopLeftThickness = this._splitView.getThicknessForView(this._topLeftView) ;
+    this._direction = this._splitView.get('layoutDirection') ;
 
-    this._targetView = this.get('targetView') ;
-    
-    // determine the view to change.
-    this._originalThickness = splitView.thicknessForView(this._targetView);
-    
-    this._direction = splitView.get('layoutDirection') ;
-    
     // return true so we can track mouse dragged.
     return true ;
   },
-  
+
   mouseDragged: function(evt) {
-    
-    // calculate new thickness
+    // calculate new thickness requested by mouse
     var loc = Event.pointerLocation(evt) ;
-    
+
     if (this._direction == SC.HORIZONTAL) {
       var offset = loc.x - this._mouseDownLocation.x ;
     } else {
       var offset = loc.y - this._mouseDownLocation.y ;
     }
 
-    var thickness = this._originalThickness + offset ;
-    var splitView = this.get('parentNode') ;
-    splitView.setThicknessForView(this._targetView, thickness) ;
-    
+    var proposedThickness = this._originalTopLeftThickness + offset ;
+    this._splitView.setThicknessForView(this._topLeftView, proposedThickness) ;
+
     return true ;
   },
-  
+
   // clear left overs.
   mouseUp: function(evt) {
-    this._targetView = this._originalThickness = this._direction = this._mouseDownLocation = null ;
+    this._mouseDownLocation = this._splitView = this._topLeftView =
+    this._originalTopLeftThickness = this._direction = null ;
   },
-  
+
   doubleClick: function(evt) {
     var splitView = this.get('parentNode') ;
-    if (!splitView) return; // nothing to do.
+
+    var view = this.get('previousSibling') ;
+    var isCollapsed = view.get('isCollapsed') || NO;
+    if (!isCollapsed && !splitView.canCollapseView(view)) {
+      view = this.get('nextSibling') ;
+      isCollapsed = view.get('isCollapsed') || NO;
+      if (!isCollapsed && !splitView.canCollapseView(view)) return;
+    }
     
-    // try to collapse or un-collapse.
-    var targetView = this.get('targetView');
-    var isCollapsed = targetView.get('isCollapsed') || NO;
-    
-    // do not collapse if not allowed.
-    if (!isCollapsed && !splitView.canCollapseView(targetView)) return; 
-    
-    // now set the collapsed state and layout.
-    targetView.set('isCollapsed', !isCollapsed) ;
-    splitView.layout() ;
-    
+    if (!isCollapsed) {
+      // remember thickness in it's uncollapsed state
+      view._uncollapsedThickness = splitView.getThicknessForView(view)  ;
+      // and collapse
+      splitView.setThicknessForView(view, 0) ;
+      // if however the splitview decided not to collapse, clear:
+      if (!view.get("isCollapsed")) {
+        view._uncollapsedThickness = null;
+      }
+    } else {
+      // uncollapse to the last thickness in it's uncollapsed state
+      console.log("Un: " + this._uncollapsedThickness);
+      splitView.setThicknessForView(view, view._uncollapsedThickness) ;
+      view._uncollapsedThickness = null ;
+    }
+
     return true ;
   }
-  
+
 });
