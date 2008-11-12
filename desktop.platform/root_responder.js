fbb089f1348677ccfc36c0147e91170d34947729
Charles Jolley <charles@sproutit.com>
2008-11-11 21:54:05 -0800

    Add platform dirs

---
+++ desktop.platform/root_responder.js
@@ -0,0 +1,33 @@
+// ========================================================================
+// SproutCore
+// copyright 2006-2008 Sprout Systems, Inc.
+// ========================================================================
+
+require('foundation/application/root_responder');
+
+// Swap in platform-specific subclass.  Class-cluster FTW!
+SC.RootResponder = SC.RootResponder.extend({
+
+  platform: 'desktop',
+  
+  // setup any event listeners on the window.  This can be customized per
+  // platform.  For example, phones only deal w/ touch events.
+  setupEventListeners: function() {
+    var win = this.domWindow, doc = win.document;
+    
+    var wevents = 'resize';
+    var devents = 'mousedown mouseup mousemoved mouseover mouseout';
+    
+    wevents.w().forEach(function(key){
+      SC.Event.add(win, key, this, this[key]);
+    },this);
+
+    devents.w().forEach(function(key){
+      SC.Event.add(doc, key, this, this[key]);
+    },this);
+    
+    delete win; delete doc; 
+    return this ;
+  }
+  
+}) ;
