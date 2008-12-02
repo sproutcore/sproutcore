bf766ad398faee93819b760a1fde86a2ab9aa161
Charles Jolley <charles@sproutit.com>
2008-12-02 11:27:12 -0800

    Convert SC.ImageView.  Also improve image cache management slightly

---
+++ views/controls/image.js
@@ -0,0 +1,153 @@
+// ========================================================================
+// SproutCore
+// copyright 2006-2008 Sprout Systems, Inc.
+// ========================================================================
+
+require('views/view') ;
+require('mixins/control') ;
+
+SC.IMAGE_STATE_NONE = 'none';
+SC.IMAGE_STATE_LOADING = 'loading';
+SC.IMAGE_STATE_LOADED = 'loaded';
+SC.IMAGE_STATE_FAILED = 'failed';
+SC.IMAGE_STATE_SPRITE = 'sprite';
+
+/**
+  URL to a transparent GIF.  Used for spriting.
+*/
+SC.BLANK_IMAGE_URL = static_url('blank.gif');
+
+/**
+  @class
+
+  Displays an image in the browser.  
+  
+  The ImageView can be used to efficiently display images in the browser.
+  It includes a built in support for a number of features that can improve
+  your page load time if you use a lot of images including a image loading
+  queue and automatic support for CSS spriting.
+
+  Note that there are actually many controls that will natively include 
+  images using an icon property name.
+  
+  @extends SC.View
+  @extends SC.Control
+  @since SproutCore 1.0
+*/
+SC.ImageView = SC.View.extend(SC.Control, 
+/** @scope SC.ImageView.prototype */ {
+  
+  /** Image views contain an img tag. */
+  emptyElement: '<%@1 src="'+SC.BLANK_IMAGE_URL+'" />',
+  styleClass: 'sc-image-view',
+  tagName: 'img',
+  
+  /**
+    Current load status of the image.
+    
+    This status changes as an image is loaded from the server.  If spriting
+    is used, this will always be loaded.  Must be one of the following
+    constants: SC.IMAGE_STATE_NONE, SC.IMAGE_STATE_LOADING, 
+    SC.IMAGE_STATE_LOADED, SC.IMAGE_STATE_FAILED, SC.IMAGE_STATE_SPRITE
+    
+    @property {String}
+  */
+  status: SC.IMAGE_STATE_NONE,
+  
+  /**
+    A url or CSS class name.
+    
+    This is the image you want the view to display.  It should be either a
+    url or css class name.  You can also set the content and 
+    contentValueKey properties to have this value extracted 
+    automatically.
+    
+    If you want to use CSS spriting, set this value to a CSS class name.  If
+    you need to use multiple class names to set your icon, separate them by
+    spaces.
+    
+    Note that if you provide a URL, it must contain at least one '/' as this
+    is how we autodetect URLs.
+    
+    @property {String}
+  */
+  value: null,
+
+  /**
+    If YES, image view will use the imageCache to control loading.  This 
+    setting is generally preferred.
+    
+    @property {String}
+  */
+  useImageCache: YES,
+  
+  /**
+    If YES, this image can load in the background.  Otherwise, it is treated
+    as a foreground image.  If the image is not visible on screen, it will
+    always be treated as a background image.
+  */
+  canLoadInBackground: NO,
+  
+  displayProperties: 'status'.w(),
+  
+  updateDisplay: function() {
+    // the image source is the value if the status is LOADED or blank
+    var status = this.get('status'), value = this.get('value');
+    var src = (status === SC.IMAGE_STATE_LOADED) ? value : SC.BLANK_IMAGE_URL;
+    var className = ['sc-view',this.get('styleClass')];
+    if (status === SC.IMAGE_STATE_SPRITE) className.push(value);
+    this.$().attr('src', src).attr('class', className.join(' '));
+  },
+  
+  /** @private - 
+    Whenever the value changes, update the image state and possibly schedule
+    an image to load.
+  */
+  _image_valueDidChange: function() {
+    var value = this.get('value'), isUrl = SC.View.valueIsUrl(value);
+    
+    // if the old image is still loading, cancel it
+    if (this._loadingUrl) SC.imageCache.abortImage(this._loadingUrl);
+    
+    // now update local state as needed....
+    if (isUrl && this.get('useImageCache')) {
+      var isBackground = this.get('isVisibleInWindow') || this.get('canLoadInBackground');
+      SC.imageCache.loadImage(value, this, this.imageDidLoad, isBackground);
+
+      this.set('status', SC.IMAGE_STATE_LOADING);
+      this._loadingUrl = value ;
+      
+    // otherwise, just set state immediately
+    } else {
+      this.set('status', (isUrl) ? SC.IMAGE_STATE_LOADED : SC.IMAGE_STATE_SPRITE);
+      this.displayDidChange(); // call manually in case status did not change
+      // (e.g value changes from one sprite to another)
+    }
+    
+  }.observes('value'),
+
+  /** 
+    Called when the imageCache indicates that the image has loaded. 
+    Changing the image state will update the display.
+  */
+  imageDidLoad: function(url, imageOrError) {
+    if (url === this._loadingUrl) this._loadingUrl = null;
+
+    // do nothing if we get this notification by the value of the image has 
+    // since changed.
+    if (this.get('value') === url) {
+      this.set('status', SC.$ok(imageOrError) ? SC.IMAGE_STATE_LOADED : SC.IMAGE_STATE_FAILED);
+      this.displayDidChange();
+    }
+  }
+  
+}) ;
+
+/**
+  Returns YES if the passed value looks like an URL and not a CSS class
+  name.
+*/
+SC.ImageView.valueIsUrl = function(value) {
+  return value.indexOf('/') >= 0 ;
+} ;
+
