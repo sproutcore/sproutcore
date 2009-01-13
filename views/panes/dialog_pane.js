f5ad09badd3a46a4ca1863c1aee9f53e568ab2ea
Erich Ocean <erich@atlasocean.com>
2009-01-12 19:53:32 -0800

    added dialog pane

---
+++ views/panes/dialog_pane.js
@@ -0,0 +1,89 @@
+// ========================================================================
+// SproutCore
+// copyright 2006-2008 Sprout Systems, Inc.
+// ========================================================================
+
+require('views/panes/pane');
+
+/** @class
+
+  Most SproutCore applications need modal dialogs. The default way to use the 
+  dialog pane is to simply add it to your page like this:
+  
+  {{{
+    SC.DialogPane.create({
+      dialogView: SC.View.extend({
+        layout: { width: 400, height: 200, centerX: 0, centerY: 0 }
+      })
+    }).append();
+  }}}
+  
+  This will cause your dialog to display.  The default layout for a DialogPane 
+  is to cover the entire document window with a semi-opaque background, and to 
+  resize with the window.
+  
+  @extends SC.Pane
+  @author Erich Ocean
+  @since SproutCore 1.0
+*/
+SC.DialogPane = SC.Pane.extend({
+
+  emptyElement: '<div class="sc-pane sc-dialog-pane"></div>',
+
+  layout: { left:0, right:0, top:0, bottom:0 },
+  
+  acceptsKeyFocus: YES,
+  
+  /**
+    Set this to the view you want to act as the dialog within the dialog pane.
+    
+    @type {SC.View}
+  */
+  dialogView: null,
+  
+  /** @private */
+  dialogViewBindingDefault: SC.Binding.single(),
+  
+  /** @private - extends SC.Pane's method */
+  paneDidAttach: function() {
+    var ret = sc_super();
+    var responder = this.rootResponder;
+    responder.makeMainPane(this);
+    responder.makeKeyPane(this);
+    return ret ;
+  },
+  
+  /**
+    Replaces any child views with the passed new content.  
+    
+    This method is automatically called whenever your dialogView property 
+    changes.  You can override it if you want to provide some behavior other
+    than the default.
+    
+    @param {SC.View} newContent the new dialog view or null.
+  */
+  replaceContent: function(newContent) {
+    this.removeAllChildren() ;
+    if (newContent) this.appendChild(newContent) ;
+  },
+
+  /** @private */
+  createChildViews: function() {
+    // if dialogView is defined, then create the content
+    var view = this.dialogView ;
+    if (view) {
+      view = this.dialogView = this.createChildView(view) ;
+      this.childViews = [view] ;
+    }
+  },
+  
+  /**
+    Invoked whenever the content property changes.  This method will simply
+    call replaceContent.  Override replaceContent to change how the view is
+    swapped out.
+  */
+  dialogViewDidChange: function() {
+    this.replaceContent(this.get('dialogView'));
+  }.observes('dialogView')
+  
+});
