cb263916e4cb5ef96900bc739d8aa1407e0cb142
Erich Ocean <erich@atlasocean.com>
2009-01-19 11:30:56 -0800

    remove untestable test

--- tests/debug/qunit.js
+++ tests/debug/qunit.js
@@ -8,10 +8,6 @@
   set up properly before any QUnit tests are run. Therefore...
 */
 
-var mainRan = NO ;
-
-main = function() { mainRan = YES; } ;
-
 module("QUnit") ;
 
 test("tests should not run until SproutCore is ready", function() {
@@ -21,8 +17,4 @@ test("tests should not run until SproutCore is ready", function() {
 test("tests should not run until the root responder has been setup", function() {
   expect(1);
   ok(SC.RootResponder.responder, "SC.RootResponder.responder");
-});
-
-test("main() function should be overridable in a unit test", function() {
-  ok(mainRan, "main function defined in unit test did not run");
 });
