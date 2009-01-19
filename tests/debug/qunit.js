8cbd46f002f77dee94159433b6fdebbb6e7b8d29
Erich Ocean <erich@atlasocean.com>
2009-01-19 11:02:59 -0800

    new qUnit test: main() function should be overridable in a unit test

--- tests/debug/qunit.js
+++ tests/debug/qunit.js
@@ -8,6 +8,10 @@
   set up properly before any QUnit tests are run. Therefore...
 */
 
+var mainRan = NO ;
+
+main = function() { mainRan = YES; } ;
+
 module("QUnit") ;
 
 test("tests should not run until SproutCore is ready", function() {
@@ -17,4 +21,8 @@ test("tests should not run until SproutCore is ready", function() {
 test("tests should not run until the root responder has been setup", function() {
   expect(1);
   ok(SC.RootResponder.responder, "SC.RootResponder.responder");
+});
+
+test("main() function should be overridable in a unit test", function() {
+  ok(mainRan, "main function defined in unit test did not run");
 });
