9e720864e78830a4456c209cda17359b49ea3b76
Erich Ocean <erich@atlasocean.com>
2009-01-20 13:17:26 -0800

    ported SC.Request to qUnit; all tests now pass

--- tests/system/request.js
+++ tests/system/request.js
@@ -1,86 +1,84 @@
-Test.context("requesttest", {
+// ========================================================================
+// SC.Request Base Tests
+// ========================================================================
+/*globals module test ok isObj equals expects */
+
+var url, request, contents ;
+
+module("SC.Request", {
   
-    "Test Asynchronous GET Request": function() {
-        var url = static_url("file_exists.json");
-        assertNotEqual("", url);    
+  setup: function() {
+    url = "/static/sproutcore/en/desktop/_src/desktop.platform/english.lproj/file_exists.json" ; // static_url("file_exists.json") ;
+    request = SC.Request.getUrl(url) ;
+    contents = null ;
+  },
+  
+  teardown: function() {
+    delete url ;
+    delete request ;
+    delete contents ;
+  }
+  
+});
 
-        var request = SC.Request.getUrl(url);
-        assertNotNull(request);
-        
-        var contents = null;
-        request.addObserver("response", function(response){
-            contents = request.get("response");
-        });
+test("Basic Requirements", function() {
+  ok(SC.Request, "SC.Request is defined") ;
+  ok("" != url, "url variable is not empty") ;
+  ok(request !== null, "request object is not null") ;
+  ok(contents === null, "contents is null" ) ;
+});
 
-        request.send();
-        
-        var self = this;
-        this.wait(1000,
-            function(){ with(self){
-                assertNotNull(contents);
-                assertEqual('{"message": "Yay!"}', contents);            
-            } }
-        );
-    },
+test("Test Asynchronous GET Request", function() {
+  request.addObserver("response", function(response) {
+      contents = request.get("response") ;
+  });
+  
+  request.send() ;
+  
+  stop() ; // stops the test runner
+  setTimeout( function(){
+    ok(contents !== null) ;
+    equals('{"message": "Yay!"}', contents) ;
+    window.start() ; // starts the test runner
+  }, 1000);
+});
 
-    "Test Synchronous GET Request": function() {
-      var url = static_url("file_exists.json");
-      assertNotEqual("", url);    
+test("Test Synchronous GET Request", function() {
+  request.set("isAsynchronous", NO);
+  
+  request.send();
+  
+  contents = request.get("response");
+  
+  ok(contents !== null) ;
+  equals('{"message": "Yay!"}', contents) ;
+});
 
-      var request = SC.Request.getUrl(url);
-      assertNotNull(request);
+test("Test Asynchronous GET Request, auto-deserializing JSON", function() {
+  request.set("isJSON", YES);
+  
+  request.addObserver("response", function(response){
+      contents = request.get("response");
+  });
+  
+  request.send();
+  
+  stop() ; // stops the test runner
+  setTimeout( function(){
+    ok(contents !== null) ;
+    same({"message": "Yay!"}, contents) ;
+    window.start() ; // starts the test runner
+  }, 1000);
+});
 
-      request.set("isAsynchronous", false);
-
-      request.send();
-      
-      var contents = request.get("response");
-
-      assertNotNull(contents);
-      assertEqual('{"message": "Yay!"}', contents);
-    },
-
-    "Test Asynchronous GET Request, auto-deserializing JSON": function(){
-        var url = static_url("file_exists.json");
-        assertNotEqual("", url);    
-
-        var request = SC.Request.getUrl(url);
-        assertNotNull(request);
-        
-        request.set("isJSON", true);
-
-        var contents = null;
-        request.addObserver("response", function(response){
-            contents = request.get("response");
-        });
-
-        request.send();
-        
-        var self = this;
-        this.wait(1000,function(){with(self){
-            assertNotNull(contents);
-            if(contents != null){
-                assertEqual("Yay!",contents["message"])
-            }
-        }});
-    },
-    "Test Synchronous GET Request, auto-deserializing JSON": function() {
-      var url = static_url("file_exists.json");
-      assertNotEqual("", url);    
-
-      var request = SC.Request.getUrl(url);
-      assertNotNull(request);
-
-      request.set("isAsynchronous", false);
-      request.set("isJSON", true);
-
-      request.send();
-      
-      var contents = request.get("response");
-      
-      assertNotNull(contents);
-      if(contents != null){
-          assertEqual("Yay!",contents["message"])
-      }
-    }  
+test("Test Synchronous GET Request, auto-deserializing JSON", function() {
+  request.set("isAsynchronous", false);
+  request.set("isJSON", true);
+  
+  request.send();
+  
+  var contents = request.get("response");
+  
+  ok(contents !== null) ;
+  same({"message": "Yay!"}, contents) ;
 });
