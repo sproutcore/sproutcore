f6aee7d7b52c311c70effe08e7dfdd12ad7b4dc2
Peter Bergstrom <pbergstr@Peters-Pewter-2.local>
2009-02-18 17:02:24 -0800

    Adding empty unit test for chaining stores.

---
+++ frameworks/datastore/tests/integration/chaining_stores.js
@@ -0,0 +1,13 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Apple, Inc. and contributors.
+// License:   Licened under MIT license (see license.js)
+// ==========================================================================
+
+/*global module test equals context ok same htmlbody */
+
+module("Chaining Store");
+
+test("chaining stores", function() {
+  
+});
