0df5a75f90338d6a0c3ca1077788268f8e35d319
Erich Ocean <erich@atlasocean.com>
2009-02-23 11:08:54 -0800

    perf tweaks; now use render() method

--- frameworks/desktop/views/collection.js
+++ frameworks/desktop/views/collection.js
@@ -641,7 +641,10 @@ SC.CollectionView = SC.View.extend(SC.CollectionViewDelegate,
     }
   }.observes('isDirty', 'isVisibleInWindow'),
   
-  _fullUpdateChildren: function() { return this.updateChildren(YES); },
+  _fullUpdateChildren: function() {
+    // return this.updateChildren(YES) ;
+    this.displayDidChange() ;
+  },
   
   // ..........................................................
   // NOW SHOWING RANGE
@@ -709,7 +712,8 @@ SC.CollectionView = SC.View.extend(SC.CollectionViewDelegate,
     if (!old || !SC.rangesEqual(range, old)) {
       this._collection_nowShowingRange = range ;
       if (this.get('isVisibleInWindow')) {
-        this.invokeOnce(this.updateChildren); // 'fast' update
+        // this.invokeOnce(this.updateChildren); // 'fast' update
+        this.displayDidChange() ;
       } else this.set('isDirty', YES);
     }
   }.observes('nowShowingRange'),
@@ -838,78 +842,50 @@ SC.CollectionView = SC.View.extend(SC.CollectionViewDelegate,
   // GENERATING CHILDREN
   //
   
-  // render: function(context, firstTime) {
-  //   // console.log('updateChildren invoked on %@, fullUpdate is %@'.fmt(this, fullUpdate));
-  //   var f ;
-  //   
-  //   // force fullUpdate if we are currently dirty.
-  //   if (this.get('isDirty')) fullUpdate = YES;
-  //   
-  //   if (SC.BENCHMARK_RENDER) {
-  //     var bkey = '%@.render'.fmt(this) ;
-  //     SC.Benchmark.start(bkey);
-  //   }
-  //   
-  //   this.beginPropertyChanges() ; // avoid sending notifications
-  //   
-  //   var content = SC.makeArray(this.get('content'));
-  //   
-  //   // get the target nowShowingRange and the current range.  Save the value
-  //   var range = this.get('nowShowingRange') ;
-  //   var curRange = this._currentNowShowingRange ; 
-  //   this._currentNowShowingRange = range ;
-  //   
-  //   var groupBy = this.get('groupBy'), didChange = false ;
-  //   var key, itemView, c, start, length;
-  //   var itemViewsByContent = {} ; // this will replace the current hash.
-  //   var curItemViewsByContent = this._itemViewsByContent ;
-  //   
-  //   // iterate through all of the views and insert them.  If the view 
-  //   // already exists, it will simply be reused.
-  //   var idx = SC.maxRange(range) ;
-  //   
-  //   // console.log('range is {%@, %@}'.fmt(range.start, range.length));
-  //   while (--idx >= range.start) {
-  //     c = content.objectAt(idx) ;
-  //     key = SC.guidFor(c) ;
-  //     itemView = this._insertItemViewFor(c, groupBy, idx) ;
-  //     
-  //     if (itemView) {
-  //       // add item view to new hash and remove from old hash.
-  //       itemViewsByContent[key] = itemView;
-  //       delete curItemViewsByContent[key];
-  //     }
-  //   }
-  //   
-  //   // Now iterate through the old hash.  Any left over item views should
-  //   // be removed.
-  //   for(key in curItemViewsByContent) {
-  //     if (!curItemViewsByContent.hasOwnProperty(key)) continue ;
-  //     itemView = curItemViewsByContent[key] ;
-  //     this._removeItemView(itemView, groupBy) ;
-  //   }
-  //   
-  //   // Swap out remaining content items.
-  //   curItemViewsByContent = null ; // release memory...
-  //   this._itemViewsByContent = itemViewsByContent ;
-  //   didChange = true;
-  //   
-  //   // Clear dirty state
-  //   this.set('isDirty', NO);
-  //   
-  //   // Clean out some cached items and notify their changes.
-  //   // NOTE: This must be called after isDirty is cleared or 
-  //   // updateSelectionStates() may not run.
-  //   if (didChange) {
-  //     this._flushZombieGroupViews() ;
-  //     this.updateSelectionStates() ;
-  //     this.notifyPropertyChange('itemViews') ;
-  //     this.notifyPropertyChange('groupViews') ;
-  //   }
-  //   
-  //   this.endPropertyChanges() ;
-  //   if (SC.BENCHMARK_RENDER) SC.Benchmark.end(bkey);    
-  // },
+  render: function(context, firstTime) {
+    if (SC.BENCHMARK_RENDER) {
+      var bkey = '%@.render'.fmt(this) ;
+      SC.Benchmark.start(bkey);
+    }
+    this.beginPropertyChanges() ; // avoid sending notifications
+    
+    var content = SC.makeArray(this.get('content'));
+    var range = this.get('nowShowingRange') ;
+    var key, itemView, c ;
+    var idx = SC.maxRange(range) ;
+    var exampleViewKey = this.get('contentExampleViewKey') ;
+    var ExampleView ;
+    if (exampleViewKey) {
+      ExampleView = content.get(exampleViewKey)
+    } else {
+      ExampleView = this.get('exampleView') ;
+    }
+    
+    if (ExampleView) {
+      itemView = ExampleView.create({
+        classNames: ['sc-collection-item'],
+        owner: this,
+        displayDelegate: this,
+        parentView: this,
+        isVisible: YES
+      });
+    } else throw "You must define an exampleView class to render collection items with" ;
+    
+    while (--idx >= range.start) {
+      c = content.objectAt(idx) ;
+      key = SC.guidFor(c) ;
+      itemView.set('content', c) ;
+      itemView.set('layerId', key) ;
+      this.layoutItemView(itemView, idx, YES) ;
+      context = context.begin(itemView.get('tagName')) ;
+      itemView.prepareContext(context, YES) ;
+      context = context.end() ;
+    }
+    
+    this.set('isDirty', NO);
+    this.endPropertyChanges() ;
+    if (SC.BENCHMARK_RENDER) SC.Benchmark.end(bkey);    
+  },
   
   /**
     Update the itemViews in the receiver to match the currently visible 
@@ -929,106 +905,106 @@ SC.CollectionView = SC.View.extend(SC.CollectionViewDelegate,
     @param {Bool} fullUpdate (Optional) if set to true, assumes content has changed and will perform a full update.
     @returns {SC.CollectionView} reciever
   */
-  updateChildren: function(fullUpdate) {
-    // console.log('updateChildren invoked on %@, fullUpdate is %@'.fmt(this, fullUpdate));
-    var f ;
-    
-    // force fullUpdate if we are currently dirty.
-    if (this.get('isDirty')) fullUpdate = YES;
-    
-    if (SC.BENCHMARK_UPDATE_CHILDREN) {
-      var bkey = '%@.updateChildren(%@)'.fmt(this, (fullUpdate) ? 'FULL' : 'FAST') ;
-      SC.Benchmark.start(bkey);
-    }
-    
-    this.beginPropertyChanges() ; // avoid sending notifications
-    
-    var content = SC.makeArray(this.get('content'));
-    
-    // get the target nowShowingRange and the current range.  Save the value
-    var range = this.get('nowShowingRange') ;
-    var curRange = this._currentNowShowingRange ; 
-    this._currentNowShowingRange = range ;
-    
-    var groupBy = this.get('groupBy'), didChange = false ;
-    var key, itemView, c, start, length;
-    
-    // If this is a fullUpdate, then rebuild the itemViewsByContent hash
-    // from scratch.  This is necessary if the content or the visible range
-    // might have changed.
-    // if (fullUpdate) {
-      var itemViewsByContent = {} ; // this will replace the current hash.
-      var curItemViewsByContent = this._itemViewsByContent ;
-      
-      // iterate through all of the views and insert them.  If the view 
-      // already exists, it will simply be reused.
-      var idx = SC.maxRange(range) ;
-      
-      // console.log('range is {%@, %@}'.fmt(range.start, range.length));
-      while(--idx >= range.start) {
-        c = content.objectAt(idx) ;
-        key = SC.guidFor(c) ;
-        itemView = this._insertItemViewFor(c, groupBy, idx) ;
-        
-        if (itemView) {
-          // add item view to new hash and remove from old hash.
-          itemViewsByContent[key] = itemView;
-          delete curItemViewsByContent[key];
-        }
-      }
-      
-      // Now iterate through the old hash.  Any left over item views should
-      // be removed.
-      for(key in curItemViewsByContent) {
-        if (!curItemViewsByContent.hasOwnProperty(key)) continue ;
-        itemView = curItemViewsByContent[key] ;
-        this._removeItemView(itemView, groupBy) ;
-      }
-      
-      // Swap out remaining content items.
-      curItemViewsByContent = null ; // release memory...
-      this._itemViewsByContent = itemViewsByContent ;
-      didChange = true;
-      
-    // If a fullUpdate is not required, then we assume no content has changed
-    // and we just need to add or remove some views to bring the ranges up
-    // to date.
-    // } else {
-    //   // Find changed range at the top.  Note that the length here may be 
-    //   // negative.  Negative means views should be removed.
-    //   start = range.start ;
-    //   length = (curRange.start - start) ;
-    //   if (length !== 0) {
-    //     this._insertOrRemoveItemViewsInRange(start, length, groupBy) ;
-    //     didChange = true ;
-    //   }
-    //   
-    //   // Find the changed range at the bottom.  Note that the length here may
-    //   // also be negative. Negative means views should be removed.
-    //   start = SC.maxRange(curRange) ;
-    //   length = SC.maxRange(range) - start ;
-    //   if (length !== 0) {
-    //     this._insertOrRemoveItemViewsInRange(start, length, groupBy) ;
-    //     didChange = true ;
-    //   }
-    // }
-    
-    // Clear dirty state
-    this.set('isDirty', NO);
-    
-    // Clean out some cached items and notify their changes.
-    // NOTE: This must be called after isDirty is cleared or 
-    // updateSelectionStates() may not run.
-    if (didChange) {
-      this._flushZombieGroupViews() ;
-      this.updateSelectionStates() ;
-      this.notifyPropertyChange('itemViews') ;
-      this.notifyPropertyChange('groupViews') ;
-    }
-    
-    this.endPropertyChanges() ;
-    if (SC.BENCHMARK_UPDATE_CHILDREN) SC.Benchmark.end(bkey);    
-  },
+  // updateChildren: function(fullUpdate) {
+  //   // console.log('updateChildren invoked on %@, fullUpdate is %@'.fmt(this, fullUpdate));
+  //   var f ;
+  //   
+  //   // force fullUpdate if we are currently dirty.
+  //   if (this.get('isDirty')) fullUpdate = YES;
+  //   
+  //   if (SC.BENCHMARK_UPDATE_CHILDREN) {
+  //     var bkey = '%@.updateChildren(%@)'.fmt(this, (fullUpdate) ? 'FULL' : 'FAST') ;
+  //     SC.Benchmark.start(bkey);
+  //   }
+  //   
+  //   this.beginPropertyChanges() ; // avoid sending notifications
+  //   
+  //   var content = SC.makeArray(this.get('content'));
+  //   
+  //   // get the target nowShowingRange and the current range.  Save the value
+  //   var range = this.get('nowShowingRange') ;
+  //   var curRange = this._currentNowShowingRange ; 
+  //   this._currentNowShowingRange = range ;
+  //   
+  //   var groupBy = this.get('groupBy'), didChange = false ;
+  //   var key, itemView, c, start, length;
+  //   
+  //   // If this is a fullUpdate, then rebuild the itemViewsByContent hash
+  //   // from scratch.  This is necessary if the content or the visible range
+  //   // might have changed.
+  //   // if (fullUpdate) {
+  //     var itemViewsByContent = {} ; // this will replace the current hash.
+  //     var curItemViewsByContent = this._itemViewsByContent ;
+  //     
+  //     // iterate through all of the views and insert them.  If the view 
+  //     // already exists, it will simply be reused.
+  //     var idx = SC.maxRange(range) ;
+  //     
+  //     // console.log('range is {%@, %@}'.fmt(range.start, range.length));
+  //     while(--idx >= range.start) {
+  //       c = content.objectAt(idx) ;
+  //       key = SC.guidFor(c) ;
+  //       itemView = this._insertItemViewFor(c, groupBy, idx) ;
+  //       
+  //       if (itemView) {
+  //         // add item view to new hash and remove from old hash.
+  //         itemViewsByContent[key] = itemView;
+  //         delete curItemViewsByContent[key];
+  //       }
+  //     }
+  //     
+  //     // Now iterate through the old hash.  Any left over item views should
+  //     // be removed.
+  //     for(key in curItemViewsByContent) {
+  //       if (!curItemViewsByContent.hasOwnProperty(key)) continue ;
+  //       itemView = curItemViewsByContent[key] ;
+  //       this._removeItemView(itemView, groupBy) ;
+  //     }
+  //     
+  //     // Swap out remaining content items.
+  //     curItemViewsByContent = null ; // release memory...
+  //     this._itemViewsByContent = itemViewsByContent ;
+  //     didChange = true;
+  //     
+  //   // If a fullUpdate is not required, then we assume no content has changed
+  //   // and we just need to add or remove some views to bring the ranges up
+  //   // to date.
+  //   // } else {
+  //   //   // Find changed range at the top.  Note that the length here may be 
+  //   //   // negative.  Negative means views should be removed.
+  //   //   start = range.start ;
+  //   //   length = (curRange.start - start) ;
+  //   //   if (length !== 0) {
+  //   //     this._insertOrRemoveItemViewsInRange(start, length, groupBy) ;
+  //   //     didChange = true ;
+  //   //   }
+  //   //   
+  //   //   // Find the changed range at the bottom.  Note that the length here may
+  //   //   // also be negative. Negative means views should be removed.
+  //   //   start = SC.maxRange(curRange) ;
+  //   //   length = SC.maxRange(range) - start ;
+  //   //   if (length !== 0) {
+  //   //     this._insertOrRemoveItemViewsInRange(start, length, groupBy) ;
+  //   //     didChange = true ;
+  //   //   }
+  //   // }
+  //   
+  //   // Clear dirty state
+  //   this.set('isDirty', NO);
+  //   
+  //   // Clean out some cached items and notify their changes.
+  //   // NOTE: This must be called after isDirty is cleared or 
+  //   // updateSelectionStates() may not run.
+  //   if (didChange) {
+  //     this._flushZombieGroupViews() ;
+  //     this.updateSelectionStates() ;
+  //     this.notifyPropertyChange('itemViews') ;
+  //     this.notifyPropertyChange('groupViews') ;
+  //   }
+  //   
+  //   this.endPropertyChanges() ;
+  //   if (SC.BENCHMARK_UPDATE_CHILDREN) SC.Benchmark.end(bkey);    
+  // },
   
   /**
     Rebuild all the child item views in the collection view.
@@ -1044,7 +1020,10 @@ SC.CollectionView = SC.View.extend(SC.CollectionViewDelegate,
     
     @returns {void}
   */
-  rebuildChildren: function() { this.updateChildren(YES); },
+  rebuildChildren: function() {
+    // this.updateChildren(YES) ;
+    this.displayDidChange() ;
+  },
   
   /**
     Update the selection state for the item views to reflect the selection 
