33c0683790ac45f940cf08ee8a0dba3a3d1b4846
Charles Jolley <charles@sproutit.com>
2009-02-24 09:45:46 -0800

    Further improved ControlTestPane

--- frameworks/desktop/tests/views/checkbox/ui.js
+++ frameworks/desktop/tests/views/checkbox/ui.js
@@ -5,42 +5,94 @@
 // License:   Licened under MIT license (see license.js)
 // ==========================================================================
 
-/*global module test htmlbody */
-
-module("SC.CheckboxView UI");
+/*global module test htmlbody ok equals same */
 
 htmlbody('<style> .sc-static-layout { border: 1px red dotted; } </style>');
 
-test("various UI states of SC.CheckboxView", function() {
+var pane = SC.ControlTestPane.design()
+  .add("basic", SC.CheckboxView, { 
+    value: NO, isEnabled: YES, title: "Hello World" 
+  })
+  
+  .add("selected", SC.CheckboxView, { 
+    value: YES, title: "Hello World" 
+  })
+  
+  .add("disabled", SC.CheckboxView, { 
+    isEnabled: NO, title: "Hello World" 
+  })
+  
+  .add("disabled - selected", SC.CheckboxView, { 
+    isEnabled: NO, value: YES, title: "Hello World" 
+  })
 
-  var pane = SC.ControlTestPane.design()
-    .add("basic", SC.CheckboxView, { 
-      value: NO, isEnabled: YES, title: "Hello World" 
-    })
-    
-    .add("selected", SC.CheckboxView, { 
-      value: YES, title: "Hello World" 
-    })
-    
-    .add("disabled", SC.CheckboxView, { 
-      isEnabled: NO, title: "Hello World" 
-    })
-    
-    .add("disabled - selected", SC.CheckboxView, { 
-      isEnabled: NO, value: YES, title: "Hello World" 
-    })
+  .add("static layout 1", SC.CheckboxView, {
+    useStaticLayout: YES, 
+    layout: { width: 'auto', right: 'auto' },
+    title: 'Static Layout'
+  })
 
-    .add("static layout 1", SC.CheckboxView, {
-      useStaticLayout: YES, 
-      layout: { width: 'auto', right: 'auto' },
-      title: 'Static Layout'
-    })
+  .add("static layout 2", SC.CheckboxView, {
+    useStaticLayout: YES, 
+    layout: { width: 'auto', right: 'auto' },
+    title: 'Different Length Title'
+  });
+  
+pane.show(); // add a test to show the test pane
 
-    .add("static layout 2", SC.CheckboxView, {
-      useStaticLayout: YES, 
-      layout: { width: 'auto', right: 'auto' },
-      title: 'Different Length Title'
-    });
+// ..........................................................
+// TEST VIEWS
+// 
+module('SC.CheckboxView ui', pane.standardSetup());
 
-  pane = pane.create(); // show on screen    
+test("basic", function() {
+  var view = pane.view('basic');
+  ok(!view.$().hasClass('disabled'), 'should not have disabled class');
+  ok(!view.$().hasClass('sel'), 'should not have sel class');
+  
+  var input = view.$('input');
+  ok(!input.attr('checked'), 'input should not be checked');
+  ok(!input.attr('disabled'), 'input should not be disabled');
+  
+  var label = view.$('span.label');
+  equals(label.text(), 'Hello World', 'should have label');
+});
+
+test("selected", function() {
+  var view = pane.view('selected');
+  ok(!view.$().hasClass('disabled'), 'should not have disabled class');
+  ok(view.$().hasClass('sel'), 'should have sel class');
+  
+  var input = view.$('input');
+  ok(input.attr('checked'), 'input should be checked');
+  ok(!input.attr('disabled'), 'input should not be disabled');
+  
+  var label = view.$('span.label');
+  equals(label.text(), 'Hello World', 'should have label');
+});
+
+test("disabled", function() {
+  var view = pane.view('disabled');
+  ok(view.$().hasClass('disabled'), 'should have disabled class');
+  ok(!view.$().hasClass('sel'), 'should not have sel class');
+  
+  var input = view.$('input');
+  ok(!input.attr('checked'), 'input should not be checked');
+  ok(input.attr('disabled'), 'input should be disabled');
+  
+  var label = view.$('span.label');
+  equals(label.text(), 'Hello World', 'should have label');
+});
+
+test("disabled - selected", function() {
+  var view = pane.view('disabled - selected');
+  ok(view.$().hasClass('disabled'), 'should have disabled class');
+  ok(view.$().hasClass('sel'), 'should have sel class');
+  
+  var input = view.$('input');
+  ok(input.attr('checked'), 'input should be checked');
+  ok(input.attr('disabled'), 'input should be disabled');
+  
+  var label = view.$('span.label');
+  equals(label.text(), 'Hello World', 'should have label');
 });
--- frameworks/foundation/debug/control_test_pane.js
+++ frameworks/foundation/debug/control_test_pane.js
@@ -7,21 +7,86 @@
 
 sc_require('views/pane');
 
-SC.ControlTestPane = SC.Pane.extend({
+/*global test */
+
+/** @class
+  Generates a pane that will display vertically stacked views for testing.
+  You can use this class in test mode to easily create a palette with views
+  configured in different ways.
+  
+  h1. Example
+  
+  {{{
+    var pane = SC.ControlTestPane.design()
+      .add('basic', SC.CheckboxView.design({ title: "Hello World" }))
+      .add('disabled', SC.CheckboxView.design({
+        title: "Hello World", isEnabled: NO
+      }));
+      
+    module("CheckboxView UI", pane);
+    
+    test("basic", function() {
+      var view = pane.view('basic');
+      ok(view.get('isEnabled'), 'should be enabled');
+    });
+  }}}
+  
+  @extends SC.Pane
+  @since SproutCore 1.0
+*/
+SC.ControlTestPane = SC.Pane.extend(
+/** @scope SC.ControlTestPane.prototype */ {
+  
   classNames: ['sc-control-test-pane'],
   layout: { right: 20, width: 480, top: 75, bottom: 20 },
 
+  /**
+    The starting top location for the first row.  This will increment as 
+    views are added to the pane.
+  */
   top:       0,
+  
+  /**
+    The default height of each row.  This will be used for a view unless you
+    manually specify a height in the view's layout.
+  */
   height:    20,
+  
+  /**
+    The default padding added to the edges and between each row.
+  */
   padding:   4,
-  defaultLayout: { },  
+
+  /**
+    Retrieves the test sample view that was added with the passed key name.
+    
+    @param {String} keyName the key used to register the view.
+    @returns {SC.View} view instance
+  */
+  view: function(keyName) { 
+    var idx = this._views[keyName];
+    if (!idx) throw "SC.ControlTestPane does not have a view named %@".fmt(keyName);
+    return this.childViews[idx].childViews[0]; 
+  },
   
   init: function() {
     sc_super();
+    if (!this._views) this._views = {};
     this.append(); // auto-add to screen
   }
 });
 
+/**
+  Adds a test view to the control pane design.  The passed label will be used
+  as the key which you can use to find the view layer.  You can either pass
+  a view that is already designed or pass an array of attributes that will be
+  used to create a design on the view.
+  
+  @param {String} label the view key name
+  @param {SC.View} view a view class or view design
+  @param {Hash} attrs optional attrs to use when designing the view
+  @returns {SC.ControlTestPane} receiver
+*/
 SC.ControlTestPane.add = function(label, view, attrs) {
   if (attrs === undefined) attrs = {};
   if (!view.isDesign) view = view.design(attrs);
@@ -34,17 +99,57 @@ SC.ControlTestPane.add = function(label, view, attrs) {
   
   // calculate labelView and add it
   layout = { left: padding, width: 150, top: top, height: height };
-  label = SC.LabelView.design({
+  var labelView = SC.LabelView.design({
     value: label + ':', layout: layout, textAlign: SC.ALIGN_RIGHT, fontWeight: SC.BOLD_WEIGHT 
   });
-  this.childView(label);
+  this.childView(labelView);
   
   // now layout view itself...
   var wrapper = SC.View.design({
+    classNames: ['wrapper'],
     layout: { left: 150+padding*2, top: top, right: padding, height: height },
     childViews: [view]
   });
+  var idx = this.prototype.childViews.length ;
   this.childView(wrapper);
+  
+  var views = this.prototype._views;
+  if (!views) views = this.prototype._views = {};
+  views[label] = idx ;
   
   return this;
 };
+
+/**
+  Returns a standard setup/teardown object for use by the module() method.
+*/
+SC.ControlTestPane.standardSetup = function() {
+  var pane = this ;
+  return {
+    setup: function() { pane._pane = pane.create(); },
+    teardown: function() {
+      if (pane._pane) pane._pane.remove();
+      pane._pane = null ;
+    }
+  } ;
+};
+
+/**
+  Convenience method.  Returns the view with the given name on the current
+  pane instance if there is one.
+*/
+SC.ControlTestPane.view = function(viewKey) {
+  if (!this._pane) throw "view() cannot be called on a class";
+  return this._pane.view(viewKey);
+};
+
+/**
+  Registers a final test that will instantiate the control test pane and 
+  display it.  This allows the developer to interact with the controls once
+  the test has completed.
+*/
+SC.ControlTestPane.show = function() {
+  var pane = this ;
+  test("show control test pane", function() { pane.create(); });
+};
+
