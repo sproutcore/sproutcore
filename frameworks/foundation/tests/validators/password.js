4f1c85f3b93b2a32636d0f4989c68dbeeacf0085
nirumalthomas <Nirumal_Thomas@infosys.com>
2009-02-26 18:15:40 +0530

    Added new files

---
+++ frameworks/foundation/tests/validators/password.js
@@ -0,0 +1,13 @@
+// ========================================================================
+// SC.Validator.Password Base Tests
+// ========================================================================
+/*globals module test ok isObj equals expects */
+// htmlbody('<!-- Test Styles -->\
+// <form id="form" action="formaction"><input type="password" name="action" value="Test" id="field" maxlength="30"/></form>\
+// ');
+// module("SC.Validator.password");
+// 
+// test("Attaching the field to the form", function() {
+// 	var a = SC.Validator.Password.attachTo(SC.$("#form"), SC.$('#field'));
+// 	alert(a);
+// });
