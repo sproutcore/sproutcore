5cdb148668abed24006c80e1ae73670dbaddcd61
Charles Jolley <charles@sproutit.com>
2009-03-01 12:34:22 -0800

    Initial list item tests

--- frameworks/desktop/tests/views/list_item/ui.js
+++ frameworks/desktop/tests/views/list_item/ui.js
@@ -7,9 +7,44 @@
 
 /*global module test htmlbody ok equals same stop start */
 
-var pane = SC.ControlTestPane.design()
+var pane = SC.ControlTestPane.design({ height: 32 })
   .add("basic", SC.ListItemView.design({ 
-    content: "Basic List Item"
+    content: "List Item"
+  }))
+
+  .add("full", SC.ListItemView.design({ 
+    content: SC.Object.create({ 
+      icon: "sc-icon-folder-16",
+      title: "List Item", 
+      checkbox: YES,
+      count: 23,
+      branch: YES 
+    }),
+    
+    hasContentIcon:  YES,
+    hasContentBranch: YES,
+
+    contentValueKey: "title",
+    contentCheckboxKey: 'checkbox',
+    contentIconKey:  "icon",
+    contentUnreadCountKey: 'count',
+    contentIsBranchKey: 'branch'
+
+  }))
+  
+  .add("icon", SC.ListItemView.design({ 
+    content: { title: "List Item", icon: "sc-icon-folder-16" },
+    contentValueKey: "title",
+
+    contentIconKey:  "icon",
+    hasContentIcon:  YES
+
+  }))
+
+  .add("checkbox - YES", SC.ListItemView.design({ 
+    content: { title: "List Item", checkbox: YES },
+    contentValueKey: "title",
+    contentCheckboxKey:  "checkbox"
   })) ;
 
 pane.show();
