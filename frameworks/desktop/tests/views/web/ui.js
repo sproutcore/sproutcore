9a4fdf59cfb82fc2133f910faa5a993bf84a1a16
sudarshanbhat <sudarshan.bhat@gmail.com>
2009-03-04 14:11:39 -0800

    1. basic working version of sproutcore 1.0 based web view,  some issues with setting the content property.

--- frameworks/desktop/tests/views/web/ui.js
+++ frameworks/desktop/tests/views/web/ui.js
@@ -7,4 +7,37 @@
 
 /*global module test htmlbody ok equals same stop start */
 
-module("TODO: Test SC.WebView UI");
+
+(function() {
+  var pane = SC.ControlTestPane.design()
+
+    .add("basic", SC.WebView, {
+      content : "http://www.sproutcore.com",
+      layout:{width: 250, height: 150}
+    });
+  
+  pane.show(); // add a test to show the test pane
+
+  // ..........................................................
+  // TEST VIEWS
+  // 
+  module('SC.WebView UI', pane.standardSetup());
+
+  test("basic", function() {
+    var view = pane.view('basic');
+    var iFrame = view.$('iframe');
+    equals(iFrame.attr('src'),"http://www.sproutcore.com", "should have source as http://www.sproutcore.com");
+  });
+  
+  test('set content to google.com', function() {
+    var view = pane.view('basic');
+    var iFrame = view.$('iframe');
+    console.log(iFrame.attr('src'));
+    console.log('setting content to google.com, current value: '+ view.get('content'));
+    SC.RunLoop.begin();
+    view.set('content', "http://www.google.com");
+    SC.RunLoop.end();
+    console.log(iFrame.attr('src'));
+    equals(iFrame.attr('src'),"http://www.google.com", "should have source as http://www.google.com");
+  });
+})();
--- frameworks/desktop/views/web.js
+++ frameworks/desktop/views/web.js
@@ -4,96 +4,107 @@
 //            Portions ©2008-2009 Apple, Inc. All rights reserved.
 // License:   Licened under MIT license (see license.js)
 // ==========================================================================
-
 /** @class
 
   Used to display an iframe.
-  
-  
+
+
   @extends SC.View
   @since SproutCore 1.0
 */
 SC.WebView = SC.View.extend({
-  
-  emptyElement: '<div><iframe style="position: absolute; width: 100%; height: 100%; border: 0px; margin: 0px; padding: 0p;"></iframe></div>',
+
+  // emptyElement: '<div><iframe style="position: absolute; width: 100%; height: 100%; border: 0px; margin: 0px; padding: 0p;"></iframe></div>',
   classNames: 'sc-iframe-view',
 
   content: null,
   contentBindingDefault: SC.Binding.single().notEmpty(),
-  
+
   contentUrlKey: null,
   contentUrlKeyBindingDefault: SC.Binding.single(),
-  
-  displayProperties: ['content'],
-  
-  $iframe: function() { return this.$('iframe') ; },
-  
+
+  displayProperties: ['content', 'contentUrlKey'],
+
+  $iframe: function() {
+    return this.$('iframe');
+  },
+
   delegate: null,
-  
-  iFrameViewWillLoadUrl: function(view, url) { return url; },
-  iFrameViewDidLoadUrl: function(view, url) {},
-  
-  updateDisplay: function() {
-    var content = this.get('content') ;
-    var urlKey = this.get('contentUrlKey') ;
+
+  iFrameViewWillLoadUrl: function(view, url) {
+    return url;
+  },
+  iFrameViewDidLoadUrl: function(view, url) {
+    // TODO: logic to propogate the 
+  },
+
+  render: function(context, firstTime) {
+    var content = this.get('content');
+    var urlKey = this.get('contentUrlKey');
     var src = '';
-    
     switch (content) {
-      case SC.MULTIPLE_PLACEHOLDER:
-      case SC.EMPTY_PLACEHOLDER:
-      case SC.NULL_PLACEHOLDER:
-        src = static_url('blank');
-        break;
-      default:
-        src = (urlKey) ? content.get(urlKey) : content ;
-        break;
+    case SC.MULTIPLE_PLACEHOLDER:
+    case SC.EMPTY_PLACEHOLDER:
+    case SC.NULL_PLACEHOLDER:
+      src = static_url('blank');
+      break;
+    default:
+      src = (urlKey) ? content.get(urlKey) : content;
+      break;
     }
-    
-    src = this.invokeDelegateMethod(this.delegate, 'iFrameViewWillLoadUrl', this, src) || '' ;
-    var iframe = this.$iframe() ;
-    iframe.attr('src', 'javascript:;') ; // clear out the previous src, to force a reload
-    iframe.attr('src', src) ;
-    this.invokeDelegateMethod(this.delegate, 'iFrameViewDidLoadUrl', this, src) ;
+
+    src = this.invokeDelegateMethod(this.delegate, 'iFrameViewWillLoadUrl', this, src) || '';
+
+    if (firstTime) {
+      context.push('<iframe src="' + src + '" style="position: absolute; width: 100%; height: 100%; border: 0px; margin: 0px; padding: 0p;"></iframe>');
+    } else {
+      var iframe = this.$iframe();
+      iframe.attr('src', 'javascript:;'); // clear out the previous src, to force a reload
+      iframe.attr('src', src);
+    }
+
+    this.invokeDelegateMethod(this.delegate, 'iFrameViewDidLoadUrl', this, src);
   },
 
   /** @private */
-  _contentDidChange: function(target,key,value,propertyRevision) {
+  _contentDidChange: function(target, key, value, propertyRevision) {
+
     // handle changes to the content...
     if ((value = this.get('content')) != this._field_content) {
 
       // get the handler method
-      var f = this._contentPropertyDidChange ;
-      
+      var f = this._contentPropertyDidChange;
+
       // stop listening to old content.
       if (this._field_content) {
         if (SC.isArray(this._field_content)) {
-          this._field_content.invoke('removeObserver', '*', this, f) ;
+          this._field_content.invoke('removeObserver', '*', this, f);
         } else if (this._field_content.removeObserver) {
-          this._field_content.removeObserver('*', this, f) ;
+          this._field_content.removeObserver('*', this, f);
         }
       }
-      
+
       // start listening for changes on the new content object.
-      this._field_content = value ;
+      this._field_content = value;
       if (value) {
         if (SC.isArray(value)) {
-          value.invoke('addObserver', '*', this, f) ;
+          value.invoke('addObserver', '*', this, f);
         } else if (value.addObserver) {
-          value.addObserver('*', this, f) ;
+          value.addObserver('*', this, f);
         }
       }
 
       // notify everyone that everything is different now.
-      this.allPropertiesDidChange() ;
-      this.endPropertyChanges() ;
+      this.allPropertiesDidChange();
+      this.endPropertyChanges();
     }
   }.observes('content'),
-  
+
   /** @private Invoked when properties on the content object change. */
-  _contentPropertyDidChange: function(target,key,value, propertyRevision) {
+  _contentPropertyDidChange: function(target, key, value, propertyRevision) {
     if (key === '*' || key === this.get('contentUrlKey')) {
-      this.displayDidChange() ;
+      this.displayDidChange();
     }
   }
-  
-}) ;
+
+});
