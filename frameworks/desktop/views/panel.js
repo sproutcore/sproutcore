7b8535b322aaa3460b3b19f296ed46e95c8b37d0
teresa <teresa.tsui@gmail.com>
2009-03-05 14:26:28 -0800

    Renamed SC.DialogPane to SC.Panel

--- frameworks/desktop/views/panel.js
+++ frameworks/desktop/views/panel.js
@@ -28,7 +28,7 @@
 SC.Panel = SC.Pane.extend({
 
   layout: { left:0, right:0, top:0, bottom:0 },
-  classNames: ['sc-dialog-pane'],
+  classNames: ['sc-panel'],
   acceptsKeyPane: YES,
 
   // ..........................................................
