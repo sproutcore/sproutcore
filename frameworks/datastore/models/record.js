6c3bbd85b45ce33f1fdcb04d9a0c6f5053049336
Charles Jolley <charles@sproutit.com>
2009-03-17 19:22:42 -0500

    Latest tweaks to SC.Record

--- frameworks/datastore/models/record.js
+++ frameworks/datastore/models/record.js
@@ -65,54 +65,39 @@ SC.Record = SC.Object.extend(
     When a new empty record is created, this will be set to true.  It will be
     set to NO again the first time the record is committed.
     
-    @property
-    @type {Boolean}
+    @property {Boolean}
   */
   newRecord: NO,
   
   /**
     The record's status changes as it is loaded from the server.
     
-    @property
-    @type {Number}
+    @property {Number}
   */
   status: SC.RECORD_EMPTY,
-  
-  /**
-    @private
 
-    The edit level needs to be 0 in order to commit a record.
+  /**
+    The store that owns this record.  All changes will be buffered into this
+    store and committed to the rest of the store chain through here.
     
-    @property
-    @type {Integer}
+    @property {SC.Store | SC.Server}
   */
-  _editLevel: 0,
+  store: null,
 
   /**
-    @private
-
     This is the store key for the record, it is used to link it back to the 
     dataHash. If a record is reused, this value will be replaced.
     
-    @property
-    @type {Integer}
+    You should not edit this store key but you may sometimes need to refer to
+    this store key when implementing a Server object.
+    
+    @property {Integer}
   */
-  _storeKey: null,
-
+  storeKey: null,
   
   // ...............................
   // CRUD OPERATIONS
   //
-
-  /**
-    The item providing the data for this.  Set to either the store or a
-    Server.  Setting it to the Store will make refresh and commit effectively
-    null-ops.
-    
-    @property
-    @type {SC.Store or SC.Server}
-  */
-  store: SC.Store,
 
   /**
     Invoked by the UI to request the model object be updated from the server.
@@ -139,10 +124,40 @@ SC.Record = SC.Object.extend(
   },
   
   /**
-    This is automatically called when using set on a property. However, if you
-    want to prevent the store from being updated with every set, you can get 
-    edits from being propagated by calling beginEditing() first then ending 
-    the editing session with endEditing().
+    You can invoke this method anytime you need to make the record as dirty.
+    This will cause the record to be commited when you commitChanges()
+    on the underlying store.
+    
+    If you use the writeAttribute() primitive, this method will be called for 
+    you.
+    
+    @returns {SC.Record} reciever
+  */
+  recordDidChange: function() {
+    var store = this.get('store');
+    if(store) {
+      store.recordDidChange(this);
+    }
+    return this ;
+  },
+  
+  // ...............................
+  // ATTRIBUTES
+  //
+
+  /** @private
+    Current edit level.  Used to defer editing changes. 
+  */
+  _editLevel: 0 ,
+  
+  /**
+    Defers notification of record changes until you call a matching 
+    endEditing() method.  This method is called automatically whenever you
+    set an attribute, but you can call it yourself to group multiple changes.
+    
+    Calls to beginEditing() and endEditing() can be nested.
+    
+    @returns {SC.Record} receiver
   */
   beginEditing: function() {
     if(this._editLevel === 0) {
@@ -152,107 +167,73 @@ SC.Record = SC.Object.extend(
       }
     }
     this._editLevel++;
+    return this ;
   },
 
   /**
-    This is automatically called when using set on a property. However, if you
-    want to prevent the store from being updated with every set, you can get 
-    edits from being propagated by calling beginEditing() function first then 
-    ending the editing session with endEditing().
+    Notifies the store of record changes if this matches a top level call to
+    beginEditing().  This method is called automatically whenever you set an
+    attribute, but you can call it yourself to group multiple changes.
+    
+    Calls to beginEditing() and endEditing() can be nested.
+    
+    @returns {SC.Record} receiver
   */
   endEditing: function() {
-    this._editLevel--;
-    if(this._editLevel <= 0) {
-      this._editLevel = 0;
+    if(--this._editLevel <= 0) {
+      this._editLevel = 0; 
       this.recordDidChange();
     }
+    return this ;
   },
   
-  // ...............................
-  // ATTRIBUTES
-  //
-  // The core attributes hash is used to store the values of a record in a 
-  // format that can be easily passed to/from the server.  The values should 
-  // generally be stored in their raw string form.  References to external 
-  // records should be stored as primary keys.
-  //
-  // Normally you do not need to work with the attributes hash directly.  
-  // Instead you should use get/set on normal record properties.  If the 
-  // property is not defined on the object, then the record will check the 
-  // attributes hash instead.
-  // 
-  // You can bulk update attributes from the server using the 
-  // updateAttributes() method.
-  
   /**
-    Gets an attribute, converting it to the proper format.
+    Reads the raw attribute from the underlying data hash.  This method does
+    not transform the underlying attribute at all.
   
     @param {string} key the attribute you want to read
     @returns {value} the value of the key, or null if it doesn't exist
-  **/
+  */
   readAttribute: function(key) {
-    var store = this.get('store'), storeKey = this._storeKey;
-    var cached = store.cachedAttributes[storeKey];
+    var store = this.get('store'), storeKey = this.storeKey;
     var attr = store.dataHashes[storeKey];
-    if(cached === undefined) cached = {};
-    if (cached[key] !== undefined) return cached[key];
-
-    var ret = (attr) ? attr[key] : undefined ;
-    ret = ret || this[key] ; // also check properties...
-    if (ret !== undefined) {
-      var recordType = this.recordType(key+'Type') ;
-      ret = this._propertyFromAttribute(ret, recordType) ;
-    }
-    store.cachedAttributes[storeKey] = cached[key] = ret;
-    return (ret === undefined) ? null : ret;
+    var ret = attr[key] ;
+    return (ret === undefined) ? null : ret ;
   },
 
   /**
-    Updates the attribute, converting it back to the property format.
+    Updates the passed attribute with the new value.  This method does not 
+    transform the value at all.  If instead you want to modify an array or 
+    hash already defined on the underlying json, you should instead get 
+    an editable version of the attribute using readEditableAttribute()
   
     @param {String} key the attribute you want to read
     @param {Object} value the attribute you want to read
-    @returns {Object} the value of the key, or null if it doesn't exist
+    @returns {SC.Record} receiver
   **/
   writeAttribute: function(key, value) {
     this.beginEditing();
-    var recordType = this.recordType(key+'Type') ;
-    var ret = this._attributeFromProperty(value, recordType) ;
-
-    var store = this.get('store'), storeKey = this._storeKey;
-    var cached = store.cachedAttributes[storeKey];
+    var store = this.get('store'), storeKey = this.storeKey;
     var attr = store.dataHashes[storeKey];
-    
-    if(!attr) attr = {};
-    if(!cached) cached = {};
-    
-    attr[key] = ret ;
-    if (cached) delete cached[key];  // clear cache.
+    if (!attr) store.dataHashes[storeKey] = attr = {}; 
+
+    attr[key] = value ;
     this.endEditing();
-    return value ;  
+    return this ;  
   },
   
   /**
-    You can invoke this method anytime you need to make the record as dirty
-    and needing a commit to the server.
-  */
-  recordDidChange: function() {
-    var store = this.get('store');
-    if(store) {
-      store.recordDidChange(this);
-    }
-  },
-  
-  /**
-    This will return the current set of attributes as a hash you can send 
-    back to the server.
+    This will return the raw attributes that you can edit directly.  If you 
+    make changes to this hash, be sure to call beginEditing() before you get
+    the attributes and endEditing() aftwards.
   
     @returns {Object} the current attributes of the receiver
   **/
   attributes: function() {
-    var store = this.get('store'), storeKey = this._storeKey;
-    var attr = store.dataHashes[storeKey];
-    return SC.clone(attr);
+    var store = this.get('store'), storeKey = this.storeKey;
+    var attrs = store.dataHashes[storeKey];
+    if (!attrs) attrs = store.dataHashes[storeKey] = {} ;
+    return attrs ;
   }.property(),
   
   /**
@@ -291,132 +272,15 @@ SC.Record = SC.Object.extend(
     return value;
   },
   
-  _attributeFromProperty: function(value,recordType) {
-    if (value && value instanceof Array) {
-      var that = this;
-      return value.map(function(v) { 
-        return that._attributeFromProperty(v,recordType); 
-      }) ;
-    } else {
-      var typeConverter = this._pickTypeConverter(recordType) ;
-      if (typeConverter) return typeConverter(value,'out') ;
-      if (recordType) {
-        return (value) ? value.get(recordType.primaryKey()) : null ;
-      } else return value ;
-    }
-  },
-  
-  _propertyFromAttribute: function(value,recordType) {
-    if (value && value instanceof Array) {
-      var max = value.get('length') ;
-      var ret = new Array(max) ; 
-      for(var idx=0;idx<max;idx++) {
-        var v = value.objectAt(idx) ; 
-        ret[idx] = this._propertyFromAttribute(v, recordType) ; 
-      }
-      ret.ownerRecord = this ;
-      return ret ;
-      
-    } else {
-      var typeConverter = this._pickTypeConverter(recordType) ;
-      if (typeConverter) return typeConverter(value,'in') ;
-      if (recordType) {
-        if (!value) return null ;
-        return SC.Store.getRecordFor(value,recordType) ;
-      } else return value ;
-    }
-  },
-  
-  recordType: function(recordTypeKey) {
-    var type = this[recordTypeKey] ;
-    if (type && (typeof(type) == "string")) {
-      type = eval(type) ; // look up type.
-      if (type) this[recordTypeKey] = type ;
-    }
-    return type ;
-  },
-
   // ...............................
   // PRIVATE
   //
   
   toString: function() {
-    var that = this ;  
-    var store = this.get('store'), storeKey = this._storeKey;
-    var cached = store.cachedAttributes[storeKey];
-
-    var ret = cached.map(function(key) {
-      var value = that.get(key) ; 
-      if (typeof(value) == "string") value = '"' + value + '"' ;
-      if (value === undefined) value = "(undefined)" ;
-      if (value === null) value = "(null)" ;
-      return [key,value].join('=') ;
-    }) ;
-    return this.constructor.toString() + '({ ' + ret.join(', ') + ' })' ;
-  },
-    
-  concatenatedProperties: ['properties'],
-  
-  // this is used for the update.  It should return a hash with current state
-  // of the record.  This uses the types to automatically marshall properties.
-  getPropertyData: function() {
-    var ret = {} ;
-    var properties = this.get('properties') || []; var loc = properties.length;
-    while(--loc >= 0) {
-      var key = properties[loc] ;
-      var value = this.get(key) ;
-      var recordType = this[key + 'Type'] ;
-      var typeConverter = this._pickTypeConverter(recordType) ;
-      if (typeConverter) recordType = null ;
-      
-      // if there is a type, use that to make the conversion.
-      if (value instanceof Array) {
-        var ary = [] ;
-        for(var vloc=0;vloc<value.length;vloc++) {
-          var v = value[vloc] ;
-          ary.push(this._convertValueOut(v,typeConverter,recordType));          
-        }
-        value = ary ;
-      } else value = this._convertValueOut(value,typeConverter,recordType);
-      
-      // set key
-      ret[key] = value ;
-    }
-    return ret ;
-  },
-  
-  _pickTypeConverter: function(recordType) {
-    var typeConverter = null ;
-    if (recordType && recordType.isTypeConverter) {
-      typeConverter = recordType; recordType = null ;
-    } else if(recordType) switch(recordType) {
-      case Date:
-        typeConverter = SC.Record.Date; recordType = null ;
-        break ;
-      case Number:
-        typeConverter = SC.Record.Number; recordType = null;
-        break;
-      case String:
-        typeConverter = null; recordType = null ;
-        break ;
-    }          
-    return typeConverter;
-  },
-  
-  _convertValueOut: function(value,typeConverter,recordType) {
-    if (typeConverter) return typeConverter(value,'out') ;
-    if (recordType) {
-      return (value) ? value.get(recordType.primaryKey()) : null ;
-    } else return value ;
-  },
-  
-  _convertValueIn: function(value,typeConverter,recordType) {
-    if (typeConverter) return typeConverter(value,'in') ;
-    if (recordType) {
-      return SC.Store.getRecordFor(value,recordType) ;
-    } else return value ;
+    var attrs = this.get('attributes');
+    return "%@(%@)".fmt(this.constructor.toString(), SC.inspect(attrs));
   }
-  
+    
 }) ;
 
 // Class Methods
