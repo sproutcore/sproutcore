7dece033a8cb83c09824163eb12b5803ea638f7a
sudarshanbhat <sudarshan.bhat@gmail.com>
2009-03-18 14:03:48 -0700

    1.cleaned up code.. removed all delegates and observers.. 2. removed content related properties.. will use value instead
    3. added SC.Control mixin to view
    4. updated unit tests.

--- frameworks/desktop/tests/views/web/ui.js
+++ frameworks/desktop/tests/views/web/ui.js
@@ -12,10 +12,22 @@
   var pane = SC.ControlTestPane.design()
 
     .add("basic", SC.WebView, {
-      content : "/clock",
+      value : "/clock",
       layout:{width: 250, height: 150}
-      
-    });
+    })
+
+
+    .add("/clock to /view_builder", SC.WebView, {
+      value : "/clock",
+      layout:{width: 250, height: 150}
+    })
+    
+    
+    // .add("autoResizeView", SC.WebView,{
+    //   value : "/clock",
+    //   shouldAutoResize: NO,
+    //   layout: {width:250, height: 150}
+    // });
   
   pane.show(); // add a test to show the test pane
 
@@ -28,26 +40,32 @@
     var view = pane.view('basic');
     var iframe = view.$iframe();
     ok(view,'view should exist');
-    ok(view.get('content'),'should have content property');
+    ok(view.get('value'),'should have value property');
     equals(view.get('shouldAutoResize'), NO, 'should have autoresize off by default, shouldAutoResize');
     var rootElement = view.$();
     ok(rootElement.hasClass('sc-view'), 'should have sc-view css class');
     ok(rootElement.hasClass('sc-iframe-view'), 'should have sc-iframe-view css class');
     equals(rootElement.height(), 150, 'should have height');
     equals(rootElement.width(), 250, 'should have width');
-
+  
     ok(iframe,'should have iframe element');
     equals(iframe.attr('src'),"/clock", "should have source as /clock");
     ok(iframe[0].contentDocument, 'should have content document');
-
+  
   });
   
-  // test('set content to google.com', function() {
-  //   var view = pane.view('basic');
-  //   var iFrame = view.$iframe();
-  //   SC.RunLoop.begin();
-  //   view.set('content', "http://www.google.com");
-  //   SC.RunLoop.end();
-  //   equals(iFrame.attr('src'),"http://www.google.com", "should have source as http://www.google.com");
+  // test('auto resize tests', function(){
+  //   var view = pane.view('autoResizeView');
+  //   
   // });
+  
+  test('change src/value ', function() {
+    var view = pane.view('/clock to /view_builder');
+    var iFrame = view.$iframe();
+    SC.RunLoop.begin();
+    view.set('value', "/view_builder");
+    SC.RunLoop.end();
+    equals(view.get('value'),"/view_builder", "should have value property in view as \'\\view_builder\'");
+    equals(iFrame.attr('src'),"/view_builder", "should have source in the iframe as \'\\view_builder\'");
+  });
 })();
--- frameworks/desktop/views/web.js
+++ frameworks/desktop/views/web.js
@@ -14,17 +14,10 @@
   @extends SC.View
   @since SproutCore 1.0
 */
-SC.WebView = SC.View.extend({
+SC.WebView = SC.View.extend(SC.Control,{
 
   classNames: 'sc-iframe-view',
-  content: null,
-  contentBindingDefault: SC.Binding.single().notEmpty(),
-  contentUrlKey: null,
-  contentUrlKeyBindingDefault: SC.Binding.single(),
-  displayProperties: ['content', 'contentUrlKey', 'shouldAutoResize'],
-  delegate: null,
-  
-  
+  displayProperties: ['value', 'shouldAutoResize'],
 
   /**
   The content of the iframe can be bigger than the size specifed when creating
@@ -38,173 +31,90 @@ SC.WebView = SC.View.extend({
   */
   shouldAutoResize: NO,
 
+
+  /**
   
-  
-
-
-
+  */
   $iframe: function() {
     return this.$('iframe');
   },
 
 
-
-
-
-
-  iFrameViewWillLoadUrl: function(view, url) {
-    return url;
-  },
-  iFrameViewDidLoadUrl: function(view, url) {
-    console.log('iFrameViewDidLoadUrl%@'.fmt(url));
-    
-    console.log(this.$iframe()[0]);
-    
-    
-    /**
-    resize the view automatically based on the content size, if the flag
-    shouldAutoResize is set to YES
-    */
-    // if ( this.get('shouldAutoResize')){
-    //   var newSize = this._computeContentSize();
-    //   this.height(newSize.height);
-    //   this.width(newSize.width);
-    // } 
-  },
-
-
-
-
-
-
+  tempRenderCounter: 0,
 
   render: function(context, firstTime) {
-    var content = this.get('content');
-    var urlKey = this.get('contentUrlKey');
-    var src = '';
-    switch (content) {
-    case SC.MULTIPLE_PLACEHOLDER:
-    case SC.EMPTY_PLACEHOLDER:
-    case SC.NULL_PLACEHOLDER:
-      src = static_url('blank');
-      break;
-    default:
-      src = (urlKey) ? content.get(urlKey) : content;
-      break;
-    }
-
-    src = this.invokeDelegateMethod(this.delegate, 'iFrameViewWillLoadUrl', 
-    this, src) || '';
-
+    var src = this.get('value');
+    this.tempRenderCounter++;
     if (firstTime) {
-      context.push('<iframe src="' + src + 
-      '" style="position: absolute; width: 100%; height: 100%; border: 0px; margin: 0px; padding: 0p;"></iframe>');
+      context.push('<iframe src="' + src + '" style="position: absolute; width: 100%; height: 100%; border: 0px; margin: 0px; padding: 0p;"></iframe>');
+      console.log('firsttime, renderCounter:%@'.fmt(this.tempRenderCounter));
+
     } else {
-      var iframe = this.$iframe();
+      console.log('not firsttime, renderCounter:%@'.fmt(this.tempRenderCounter));
+            var iframe = this.$iframe();
       // clear out the previous src, to force a reload
-      iframe.attr('src', 'javascript:;'); 
+      iframe.attr('src', 'javascript:;');
       iframe.attr('src', src);
     }
+  },
 
-    this.invokeDelegateMethod(this.delegate, 'iFrameViewDidLoadUrl', this, src);
-    
-    
+  
+  
+  
+  didCreateLayer: function() {
+    var f = this.$iframe()[0];
+    SC.Event.add(f,'load',this, this._autoResizeView);
   },
   
   
   
   
   
-  
-  
 
-  /** @private */
-  _contentDidChange: function(target, key, value, propertyRevision) {
-
-    // handle changes to the content...
-    if ((value = this.get('content')) != this._field_content) {
-
-      // get the handler method
-      var f = this._contentPropertyDidChange;
-
-      // stop listening to old content.
-      if (this._field_content) {
-        if (SC.isArray(this._field_content)) {
-          this._field_content.invoke('removeObserver', '*', this, f);
-        } else if (this._field_content.removeObserver) {
-          this._field_content.removeObserver('*', this, f);
-        }
-      }
-
-      // start listening for changes on the new content object.
-      this._field_content = value;
-      if (value) {
-        if (SC.isArray(value)) {
-          value.invoke('addObserver', '*', this, f);
-        } else if (value.addObserver) {
-          value.addObserver('*', this, f);
-        }
-      }
-
-      // notify everyone that everything is different now.
-      this.allPropertiesDidChange();
-      this.endPropertyChanges();
-    }
-  }.observes('content'),
-  
-  
-  
-  
-  
-  
-
-  /** @private Invoked when properties on the content object change. */
-  _contentPropertyDidChange: function(target, key, value, propertyRevision) {
-    if (key === '*' || key === this.get('contentUrlKey')) {
-      this.displayDidChange();
-    }
-  }
-  
-  
-  
-  
-  
-  
-  
-
-  
   /** Computes the size of the contents of the iframe from the DOM 
   @returns {Object} a JSON object representing the size in terms of height
   and width
-  */  
-  // _computeContentSize: function() {
-  //   var size ;
-  //   debugger;
-  //   var iframeElt = this.$iframe();
-  //   var contentDoc =  iframeElt?iframeElt[0].contentDocument: null;
-  //   if (!contentDoc){
-  //     size = {
-  //       width: this.width(),
-  //       height: this.height()
-  //     };
-  //   } else if (window.innerHeight) {
-  //     size = { 
-  //       width: window.innerWidth, 
-  //       height: window.innerHeight 
-  //     } ;
-  // 
-  //   } else if (document.documentElement && document.documentElement.clientHeight) {
-  //     size = { 
-  //       width: document.documentElement.clientWidth, 
-  //       height: document.documentElement.clientHeight 
-  //     } ;
-  // 
-  //   } else if (document.body) {
-  //     size = { 
-  //       width: document.body.clientWidth, 
-  //       height: document.body.clientHeight 
-  //     } ;
-  //   }
-  //   return size;
-  // }
+  */
+
+  _autoResizeView: function() {
+    
+    // if (this.get('shouldAutoResize')) {
+    //   var size;
+    //   var iframeElt = this.$iframe();
+    //   var contentDoc = iframeElt ? iframeElt[0].contentDocument: null;
+    //   debugger;
+    //   if (contentDoc) {
+    //     if (window.innerHeight) {
+    //       size = {
+    //         width: window.innerWidth,
+    //         height: window.innerHeight
+    //       };
+    //     } else if (document.documentElement && document.documentElement.clientHeight) {
+    //       size = {
+    //         width: document.documentElement.clientWidth,
+    //         height: document.documentElement.clientHeight
+    //       };
+    //     } else if (document.body) {
+    //       size = {
+    //         width: document.body.clientWidth,
+    //         height: document.body.clientHeight
+    //       };
+    //     }
+    //   }
+    // }
+    
+
+
+    
+    // else do nothing.
+    // else {
+    // }
+    
+    
+    
+    
+    
+    // this.height(size.height);
+    // this.width(size.width);
+  }
 });
