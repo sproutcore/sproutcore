b90ffa0bc923d047c9e0a3d2e621e7389a64a4c3
sudarshanbhat <sudarshan.bhat@gmail.com>
2009-03-19 18:24:07 -0700

    added fixture data (sample sources for iframe ), first working prototype of sizetofit iframe

--- frameworks/desktop/tests/views/web/ui.js
+++ frameworks/desktop/tests/views/web/ui.js
@@ -4,68 +4,65 @@
 //            portions copyright @2009 Apple, Inc.
 // License:   Licened under MIT license (see license.js)
 // ==========================================================================
-
 /*global module test htmlbody ok equals same stop start v*/
-
 
 (function() {
   var pane = SC.ControlTestPane.design()
 
-    .add("basic", SC.WebView, {
-      value : "/clock",
-      layout:{width: 250, height: 150}
-    })
+  // .add("basic", SC.WebView, {
+  //   value : "/clock",
+  //   layout:{width: 250, height: 150}
+  // })
+  // .add("/clock to /view_builder", SC.WebView, {
+  //   value : "/clock",
+  //   layout:{width: 250, height: 150}
+  // })
+  .add("autoResize", SC.WebView, {
+    value: sc_static('iframe'),
+    shouldAutoResize: YES,
+    layout: {
+      width: 250,
+      height: 150
+    }
+  });
 
-
-    .add("/clock to /view_builder", SC.WebView, {
-      value : "/clock",
-      layout:{width: 250, height: 150}
-    })
-    
-    
-    // .add("autoResizeView", SC.WebView,{
-    //   value : "/clock",
-    //   shouldAutoResize: NO,
-    //   layout: {width:250, height: 150}
-    // });
-  
   pane.show(); // add a test to show the test pane
-
   // ..........................................................
   // TEST VIEWS
   // 
   module('SC.WebView UI');
 
-  test("attributes tests", function() {
-    var view = pane.view('basic');
-    var iframe = view.$iframe();
-    ok(view,'view should exist');
-    ok(view.get('value'),'should have value property');
-    equals(view.get('shouldAutoResize'), NO, 'should have autoresize off by default, shouldAutoResize');
-    var rootElement = view.$();
-    ok(rootElement.hasClass('sc-view'), 'should have sc-view css class');
-    ok(rootElement.hasClass('sc-iframe-view'), 'should have sc-iframe-view css class');
-    equals(rootElement.height(), 150, 'should have height');
-    equals(rootElement.width(), 250, 'should have width');
-  
-    ok(iframe,'should have iframe element');
-    equals(iframe.attr('src'),"/clock", "should have source as /clock");
-    ok(iframe[0].contentDocument, 'should have content document');
-  
-  });
-  
-  // test('auto resize tests', function(){
-  //   var view = pane.view('autoResizeView');
-  //   
+  // test("attributes tests", function() {
+  //   var view = pane.view('basic');
+  //   var iframe = view.$('iframe');
+  //   ok(view,'view should exist');
+  //   ok(view.get('value'),'should have value property');
+  //   equals(view.get('shouldAutoResize'), NO, 'should have autoresize off by default, shouldAutoResize');
+  //   var rootElement = view.$();
+  //   ok(rootElement.hasClass('sc-view'), 'should have sc-view css class');
+  //   ok(rootElement.hasClass('sc-web-view'), 'should have sc-web-view css class');
+  //   equals(rootElement.height(), 150, 'should have height');
+  //   equals(rootElement.width(), 250, 'should have width');
+  // 
+  //   ok(iframe,'should have iframe element');
+  //   equals(iframe.attr('src'),"/clock", "should have source as /clock");
+  //   ok(iframe[0].contentDocument, 'should have content document');
+  // 
   // });
-  
-  test('change src/value ', function() {
-    var view = pane.view('/clock to /view_builder');
-    var iFrame = view.$iframe();
-    SC.RunLoop.begin();
-    view.set('value', "/view_builder");
-    SC.RunLoop.end();
-    equals(view.get('value'),"/view_builder", "should have value property in view as \'\\view_builder\'");
-    equals(iFrame.attr('src'),"/view_builder", "should have source in the iframe as \'\\view_builder\'");
+  // 
+  // test('change src/value ', function() {
+  //   var view = pane.view('/clock to /view_builder');
+  //   var iFrame = view.$('iframe');
+  //   SC.RunLoop.begin();
+  //   view.set('value', "/view_builder");
+  //   SC.RunLoop.end();
+  //   equals(view.get('value'),"/view_builder", "should have value property in view as \'\\view_builder\'");
+  //   equals(iFrame.attr('src'),"/view_builder", "should have source in the iframe as \'\\view_builder\'");
+  // });
+
+  test('auto resize tests',
+  function() {
+    view = pane.view('autoResize');
   });
+
 })();
--- frameworks/desktop/views/web.js
+++ frameworks/desktop/views/web.js
@@ -6,115 +6,65 @@
 // ==========================================================================
 /** @class
 
-  Used to display an iframe. The contents of the iFrame should be from the 
-  same domain. (i.e. the src / content should be from the same domain)
-  if you want to access the contents of the iframe.
+  Used to display an iframe. The source, (specified by the value property) of 
+  the iFrame should be from the same domain. (i.e. the src / value should be 
+  from the same domain) if you want to access the contents of the iframe.
 
-
-  @extends SC.View
+  @extends SC.View, SC.Control
   @since SproutCore 1.0
 */
-SC.WebView = SC.View.extend(SC.Control,{
+SC.WebView = SC.View.extend(SC.Control, {
 
-  classNames: 'sc-iframe-view',
+  classNames: 'sc-web-view',
   displayProperties: ['value', 'shouldAutoResize'],
 
   /**
   The content of the iframe can be bigger than the size specifed when creating
   the view. If you want the view to be auto-resized to the dimensions of the 
   iframe, then set the value of this property to YES.
-  
-  
   The web view can be auto resized only if the contents are from the same
   domain as the parent domain.
   @property{Boolean}
   */
   shouldAutoResize: NO,
 
-
-  /**
-  
-  */
-  $iframe: function() {
-    return this.$('iframe');
-  },
-
-
-  tempRenderCounter: 0,
-
   render: function(context, firstTime) {
     var src = this.get('value');
-    this.tempRenderCounter++;
     if (firstTime) {
       context.push('<iframe src="' + src + '" style="position: absolute; width: 100%; height: 100%; border: 0px; margin: 0px; padding: 0p;"></iframe>');
-      console.log('firsttime, renderCounter:%@'.fmt(this.tempRenderCounter));
-
     } else {
-      console.log('not firsttime, renderCounter:%@'.fmt(this.tempRenderCounter));
-            var iframe = this.$iframe();
+      var iframe = this.$('iframe');
       // clear out the previous src, to force a reload
       iframe.attr('src', 'javascript:;');
       iframe.attr('src', src);
     }
   },
 
-  
-  
-  
+  /**
+  */
   didCreateLayer: function() {
-    var f = this.$iframe()[0];
-    SC.Event.add(f,'load',this, this._autoResizeView);
+    var f = this.$('iframe');
+    SC.Event.add(f, 'load', this, this.iframeDidLoad);
   },
-  
-  
-  
-  
-  
+
 
   /** Computes the size of the contents of the iframe from the DOM 
   @returns {Object} a JSON object representing the size in terms of height
   and width
   */
-
-  _autoResizeView: function() {
-    
-    // if (this.get('shouldAutoResize')) {
-    //   var size;
-    //   var iframeElt = this.$iframe();
-    //   var contentDoc = iframeElt ? iframeElt[0].contentDocument: null;
-    //   debugger;
-    //   if (contentDoc) {
-    //     if (window.innerHeight) {
-    //       size = {
-    //         width: window.innerWidth,
-    //         height: window.innerHeight
-    //       };
-    //     } else if (document.documentElement && document.documentElement.clientHeight) {
-    //       size = {
-    //         width: document.documentElement.clientWidth,
-    //         height: document.documentElement.clientHeight
-    //       };
-    //     } else if (document.body) {
-    //       size = {
-    //         width: document.body.clientWidth,
-    //         height: document.body.clientHeight
-    //       };
-    //     }
-    //   }
-    // }
-    
-
-
-    
-    // else do nothing.
-    // else {
-    // }
-    
-    
-    
-    
-    
-    // this.height(size.height);
-    // this.width(size.width);
+  iframeDidLoad: function() {
+    //fit the iframe to size of the contents.
+    if (this.get('shouldAutoResize') === YES){
+      var contentWindow;
+      var iframeElt = this.$('iframe')[0];
+      if(iframeElt && iframeElt.contentWindow){
+        contentWindow = iframeElt.contentWindow;
+        if(contentWindow && contentWindow.document && contentWindow.document.documentElement){
+          var docElement = contentWindow.document.documentElement;
+          this.$().height(docElement.scrollHeight);
+          this.$().width(docElement.scrollWidth);
+        }
+      }
+    }
   }
 });
