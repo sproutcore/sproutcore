fe6e1b820ea490196821f59dc4634f83e8c7b80e
Charles Jolley <charles@sproutit.com>
2009-03-23 22:31:55 -0700

    Add initial data source

---
+++ frameworks/datastore/models/data_source.js
@@ -0,0 +1,42 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2009 Apple, Inc. All rights reserved.
+// License:   Licened under MIT license (see license.js)
+// ==========================================================================
+
+
+/** @class
+
+  TODO: Describe
+  
+  @extend SC.Object
+  @since SproutCore 1.0
+*/
+SC.DataSource = SC.Object.extend( /** SC.DataSource.prototype */ {
+
+  retrieveRecords: function(storeKeys) {
+    
+  },
+  
+  refreshRecords: function(storeKeys) {
+    
+  },
+  
+  commitRecords: function(storeKeys) {
+    
+  },
+  
+  updateRecords: function() {
+    
+  },
+  
+  createRecords: function() {
+    
+  },
+  
+  destroyRecords: function() {
+    
+  }
+  
+});
