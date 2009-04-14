9e1bde5e0c8a748a91fed64c88a99dfad9669b8d
Evin Grano <etgryphon@cox.net>
2009-04-14 10:27:58 -0400

    added error throwing to the foundational and logs to image preview

--- frameworks/foundation/views/image.js
+++ frameworks/foundation/views/image.js
@@ -107,8 +107,11 @@ SC.ImageView = SC.View.extend(SC.Control,
     an image to load.
   */
   _image_valueDidChange: function() {
-    var value = this.get('value'), isUrl = SC.ImageView.valueIsUrl(value);
-    
+    //var value = this.get('value'), isUrl = SC.ImageView.valueIsUrl(value);
+    var value = this.get('value');
+    console.log('Image Value Changed...%@'.fmt(value));
+    if(value instanceof Array) value = value[0];
+    var isUrl = SC.ImageView.valueIsUrl(value);
     // if the old image is still loading, cancel it
     // if (this._loadingUrl) SC.imageCache.abortImage(this._loadingUrl);
     
