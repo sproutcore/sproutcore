c774a10c833e50e63745dd544d175396dd869aba
Mike Ball <mike.ball3@gmail.com>
2009-04-29 13:52:32 -0400

    Added failing test for select_field

--- frameworks/desktop/tests/views/select_field/ui.js
+++ frameworks/desktop/tests/views/select_field/ui.js
@@ -10,30 +10,34 @@
 htmlbody('<style> .sc-static-layout { border: 1px red dotted; } </style>');
 (function() {
 var pane = SC.ControlTestPane.design()
-  .add("basic", SC.SelectFieldView, { 
-     objects:["1","2","3","4","5"]
-  })
-
-  .add("disabled", SC.SelectFieldView, { 
-    isEnabled: NO, objects:["1","2","3","4","5"]
-  })
-
-  .add("Not Selected", SC.SelectFieldView, { 
-    isSelected: NO, objects:["1","2","3","4","5"]
-  })
-
-  .add("Not Visible", SC.SelectFieldView, { 
-    isVisible: NO, objects:["1","2","3","4","5"]
-  })
-
-  .add("sortedStringOptions", SC.SelectFieldView, { 
-    objects:["Apple","Sproutcore 1.0","Development","Charles"],
-	useStaticLayout: YES, 
-    layout: { width: 'auto', right: 'auto' }
-  })
-
-  .add("Width 150 Right 0", SC.SelectFieldView, { 
-    objects: [1,6,11,2,8],
+    .add("basic", SC.SelectFieldView, { 
+       objects:["1","2","3","4","5"]
+    })
+  
+    .add("disabled", SC.SelectFieldView, { 
+      isEnabled: NO, objects:["1","2","3","4","5"]
+    })
+  
+    .add("Not Selected", SC.SelectFieldView, { 
+      isSelected: NO, objects:["1","2","3","4","5"]
+    })
+  
+    .add("Not Visible", SC.SelectFieldView, { 
+      isVisible: NO, objects:["1","2","3","4","5"]
+    })
+  
+    .add("sortedStringOptions", SC.SelectFieldView, { 
+      objects:["Apple","Sproutcore 1.0","Development","Charles"],
+  useStaticLayout: YES, 
+      layout: { width: 'auto', right: 'auto' }
+    })
+  
+    .add("Width 150 Right 0", SC.SelectFieldView, { 
+      objects: [1,6,11,2,8],
+      useStaticLayout: YES, 
+      layout: { width: '150', right: '0' }
+    })
+  .add("redraw", SC.SelectFieldView, { 
     useStaticLayout: YES, 
     layout: { width: '150', right: '0' }
   });
@@ -79,6 +83,13 @@ test("Width 150 Right 0", function() {
   var view = pane.view('Width 150 Right 0');
   ok(!view.$().hasClass('disabled'), 'should not have disabled class');
   ok(!view.$().hasClass('sel'), 'should not have sel class');
+});
+
+test("redrawMenu", function(){
+  var view = pane.view('redraw');
+  ok(view.get('objects').length === 0, "objects should be empty");
+  view.set('objects', [1,6,11,2,8]);
+  ok(view.$('options').length === 5, "objects length should be 5");
 });
 
 
