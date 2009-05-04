fb5c424281a382f18266b380e9eb527411139bb2
teresa <teresa.tsui@gmail.com>
2009-05-04 11:19:13 -0700

    fine turn alert panel btn location

--- frameworks/desktop/panes/alert.js
+++ frameworks/desktop/panes/alert.js
@@ -220,7 +220,7 @@ SC.AlertPane = SC.PanelPane.extend({
       }),
 
       SC.View.extend({
-        layout: { bottom: 13, height: 30, right: 18, width: 200 },
+        layout: { bottom: 13, height: 24, right: 18, width: 200 },
         childViews: ['cancelButton', 'okButton'],
         classNames: ['textAlignRight'],
         cancelButton : SC.ButtonView.extend({
@@ -249,7 +249,7 @@ SC.AlertPane = SC.PanelPane.extend({
       }),
       
       SC.View.extend({
-        layout: { bottom: 13, height: 30, left: 18, width: 100 },
+        layout: { bottom: 13, height: 24, left: 18, width: 100 },
         childViews: [
           SC.ButtonView.extend({
             useStaticLayout: YES,
