2057a203fe705986a9c032abbe81e3f9d7537dbb
teresa <teresa.tsui@gmail.com>
2009-05-04 15:59:53 -0700

    fix space

--- frameworks/foundation/system/utils.js
+++ frameworks/foundation/system/utils.js
@@ -364,46 +364,46 @@ SC.mixin( /** @scope SC */ {
   PARSE_COLOR_HEXRE: /^\#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/,
 
   // return an array of r,g,b colour
-	expandColor: function(color) {
-		var hexColor, red, green, blue;
-		hexColor = this.parseColor(color);
-		if (hexColor) {
-			red = parseInt(hexColor.slice(1, 3), 16);
-			green = parseInt(hexColor.slice(3, 5), 16);
-			blue = parseInt(hexColor.slice(5, 7), 16);
-			return [red,green,blue];
-		}
-	},
+  expandColor: function(color) {
+    var hexColor, red, green, blue;
+    hexColor = this.parseColor(color);
+    if (hexColor) {
+      red = parseInt(hexColor.slice(1, 3), 16);
+      green = parseInt(hexColor.slice(3, 5), 16);
+      blue = parseInt(hexColor.slice(5, 7), 16);
+      return [red,green,blue];
+    }
+  },
 
   // parse rgb color or 3-digit hex color to return a properly formatted 6-digit hex colour spec, or false
   parseColor: function(string) {
-  	var color = '#', match;
-  	if(match = this.PARSE_COLOR_RGBRE.exec(string)) {
-  		var part;
-  		for (var i=1; i<=3; i++) {
-  			part = Math.max(0, Math.min(255, parseInt(match[i],0)));
-  			color += this.toColorPart(part);
-  		}
-  		return color;
-  	}
-  	if (match = this.PARSE_COLOR_HEXRE.exec(string)) {
-  		if(match[1].length == 3) {
-  			for (var i=0; i<3; i++) {
-  				color += match[1].charAt(i) + match[1].charAt(i);
-  			}
-  			return color;
-  		}
-  		return '#' + match[1];
-  	}
-  	return false;
+    var color = '#', match;
+    if(match = this.PARSE_COLOR_RGBRE.exec(string)) {
+      var part;
+      for (var i=1; i<=3; i++) {
+        part = Math.max(0, Math.min(255, parseInt(match[i],0)));
+        color += this.toColorPart(part);
+      }
+      return color;
+    }
+    if (match = this.PARSE_COLOR_HEXRE.exec(string)) {
+      if(match[1].length == 3) {
+        for (var i=0; i<3; i++) {
+          color += match[1].charAt(i) + match[1].charAt(i);
+        }
+        return color;
+      }
+      return '#' + match[1];
+    }
+    return false;
   },
 
   // convert one r,g,b number to a 2 digit hex string
   toColorPart: function(number) {
-  	if (number > 255) number = 255;
-  	var digits = number.toString(16);
-  	if (number < 16) return '0' + digits;
-  	return digits;
+    if (number > 255) number = 255;
+    var digits = number.toString(16);
+    if (number < 16) return '0' + digits;
+    return digits;
   }
 
 }) ;
