c48cebd70239328f62cf5348db7fcfe164780541
teresa <teresa.tsui@gmail.com>
2009-05-04 15:54:23 -0700

    add color conversion (rgb, hex and hsv) functions into utility

--- frameworks/foundation/system/utils.js
+++ frameworks/foundation/system/utils.js
@@ -325,6 +325,85 @@ SC.mixin( /** @scope SC */ {
     if (r1 == null) return r2.length < 0 ;
     if (r2 == null) return r1.length < 0 ;
     return (r1.start == r2.start) && (r1.length == r2.length) ;
+  },
+
+  /** Returns hex color from hsv value */
+  convertHsvToHex: function (h, s, v) {
+    var r = 0;
+    var g = 0;
+    var b = 0;
+
+    if (v > 0) {
+      var i = (h == 1) ? 0 : Math.floor(h * 6);
+      var f = (h == 1) ? 0 : (h * 6) - i;
+      var p = v * (1 - s);
+      var q = v * (1 - (s * f));
+      var t = v * (1 - (s * (1 - f)));
+      var rgb = [[v,t,p],[q,v,p],[p,v,t],[p,q,v],[t,p,v],[v,p,q]];
+      r = Math.round(255 * rgb[i][0]);
+      g = Math.round(255 * rgb[i][1]);
+      b = Math.round(255 * rgb[i][2]);
+    }
+    return this.parseColor('rgb(' + r + ',' + g + ',' + b + ')');
+  },  
+
+  /** Returns hsv color from hex value */
+  convertHexToHsv: function (hex) {
+    var rgb = this.expandColor(hex);
+    var max = Math.max(Math.max(rgb[0], rgb[1]), rgb[2]);
+    var min = Math.min(Math.min(rgb[0], rgb[1]), rgb[2]);
+
+    var h = (max == min) ? 0 : ((max == rgb[0]) ? ((rgb[1]-rgb[2])/(max-min)/6) : ((max == rgb[1]) ? ((rgb[2]-rgb[0])/(max-min)/6+1/3) : ((rgb[0]-rgb[1])/(max-min)/6+2/3)));
+    var s = (max == 0) ? 0 : (1 - min/max);
+    var v = max/255;
+    return [h, s, v];
+  },
+
+  /** regular expression for parsing color: rgb, hex */
+  PARSE_COLOR_RGBRE: /^rgb\(\s*(\d+)\s*,\s*(\d+)\s*,\s*(\d+)\s*\)$/i,
+  PARSE_COLOR_HEXRE: /^\#([0-9a-fA-F]{3}|[0-9a-fA-F]{6})$/,
+
+  // return an array of r,g,b colour
+	expandColor: function(color) {
+		var hexColor, red, green, blue;
+		hexColor = this.parseColor(color);
+		if (hexColor) {
+			red = parseInt(hexColor.slice(1, 3), 16);
+			green = parseInt(hexColor.slice(3, 5), 16);
+			blue = parseInt(hexColor.slice(5, 7), 16);
+			return [red,green,blue];
+		}
+	},
+
+  // parse rgb color or 3-digit hex color to return a properly formatted 6-digit hex colour spec, or false
+  parseColor: function(string) {
+  	var color = '#', match;
+  	if(match = this.PARSE_COLOR_RGBRE.exec(string)) {
+  		var part;
+  		for (var i=1; i<=3; i++) {
+  			part = Math.max(0, Math.min(255, parseInt(match[i],0)));
+  			color += this.toColorPart(part);
+  		}
+  		return color;
+  	}
+  	if (match = this.PARSE_COLOR_HEXRE.exec(string)) {
+  		if(match[1].length == 3) {
+  			for (var i=0; i<3; i++) {
+  				color += match[1].charAt(i) + match[1].charAt(i);
+  			}
+  			return color;
+  		}
+  		return '#' + match[1];
+  	}
+  	return false;
+  },
+
+  // convert one r,g,b number to a 2 digit hex string
+  toColorPart: function(number) {
+  	if (number > 255) number = 255;
+  	var digits = number.toString(16);
+  	if (number < 16) return '0' + digits;
+  	return digits;
   }
-  
+
 }) ;
