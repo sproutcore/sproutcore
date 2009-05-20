7cd34b3099c96c4cc4abc88cc7447e9281c51b07
Erich Ocean <erich.ocean@me.com>
2009-05-20 03:01:55 -0700

    added unit tests for improved String.prototype.w() behavior

---
+++ frameworks/foundation/tests/mixins/string.js
@@ -0,0 +1,17 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Apple, Inc. and contributors.
+// License:   Licened under MIT license (see license.js)
+// ==========================================================================
+/*global module test equals context ok same */
+
+module('String.prototype.w()');
+
+test("'one two three'.w() => ['one','two','three']", function() {
+  same('one two three'.w(), ['one','two','three'], "should be equal");
+});
+
+test("'one    two    three'.w() with extra spaces between words => ['one','two','three']", function() {
+  same('one    two    three'.w(), ['one','two','three'], "should be equal");
+});
+
