bbaa530d3d688500ea3286909940c56aecad16b4
Peter Bergstrom <pbergstr@Galactica.local>
2009-05-19 20:46:18 -0700

    Added correct license to file.

--- frameworks/foundation/system/benchmark.js
+++ frameworks/foundation/system/benchmark.js
@@ -1,7 +1,9 @@
-// ========================================================================
-// SproutCore
-// copyright 2006-2008 Sprout Systems, Inc.
-// ========================================================================
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2009 Apple, Inc. All rights reserved.
+// License:   Licened under MIT license (see license.js)
+// ==========================================================================
 
 require('core') ;
 require('foundation/date');
