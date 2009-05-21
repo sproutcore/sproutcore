285957245e5336b0a7861a509ee4778ae4fa698b
Onar Vikingstad <onar@onarvikingstad.apple.com>
2009-05-21 16:33:15 -0700

    Only write defaultValue returned if there is a data hash

--- frameworks/datastore/models/record_attribute.js
+++ frameworks/datastore/models/record_attribute.js
@@ -196,7 +196,7 @@ SC.RecordAttribute = SC.Object.extend(
         if (typeof value === SC.T_FUNCTION) {
           value = this.defaultValue(record, key, this);
           // write default value so it doesn't have to be executed again
-          record.writeAttribute(attrKey, value);
+          if(record.attributes()) record.writeAttribute(attrKey, value);
         }
       } else value = this.toType(record, key, value);
     }
