58e8ec4dc5c839a9685b27eb6b69ce6520794ab9
Charles Jolley <charles@sproutit.com>
2009-05-28 16:53:02 -0700

    SC.RecordAttribute should return null for empty toOne

--- frameworks/datastore/models/record_attribute.js
+++ frameworks/datastore/models/record_attribute.js
@@ -333,11 +333,12 @@ SC.RecordAttribute.registerTransform(SC.Record, {
   /** @private - convert a record id to a record instance */
   to: function(id, attr, recordType, parentRecord) {
     var store = parentRecord.get('store');
-    return store.find(recordType, id);
+    if (SC.none(id) || (id==="")) return null;
+    else return store.find(recordType, id);
   },
   
   /** @private - convert a record instance to a record id */
-  from: function(record) { return record.get('id'); }
+  from: function(record) { return record ? record.get('id') : null; }
 });
 
 /** @private - generic converter for transforming computed record attributes */
