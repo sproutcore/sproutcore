441308033bc032e40ff499bad538b8d40fe0680d
Peter Bergstrom <pbergstr@galactica.apple.com>
2009-07-23 21:42:27 -0700

    Uncomment check for target being visible in the window in the _findDropTarget function in SC.Drag. Non-visible targets should be skipped.

--- frameworks/desktop/system/drag.js
+++ frameworks/desktop/system/drag.js
@@ -543,7 +543,8 @@ SC.Drag = SC.Object.extend(
     for (var idx=0, len=ary.length; idx<len; idx++) {
       target = ary[idx] ;
       
-      // FIXME if (!target.get('isVisibleInWindow')) continue ;
+      // If the target is not visible, it is not valid.
+      if (!target.get('isVisibleInWindow')) continue ;
       
       // get clippingFrame, converted to the pane.
       frame = target.convertFrameToView(target.get('clippingFrame'), null) ;
