a8929083c41243f760f8b736cb0a59e12b53317b
Mohammed Ashik <mohammedashik_kp@infosys.com>
2009-07-30 22:22:44 +0530

    changes for setting prefMatrix and checkbox for selected Menu item

--- frameworks/desktop/panes/drop_down.js
+++ frameworks/desktop/panes/drop_down.js
@@ -12,20 +12,25 @@
 */
 sc_require('views/button');
 
-SC.DropDownView = SC.ButtonView.extend(
+SC.DropDownMenu = SC.ButtonView.extend(
 /** @scope SC.DropDownView.prototype */ {
   
   /**
     This property shows whether the menu is currently displayed or not
   */
   keyEquivalent: null,
+  
+  /**
+    className - sc-popup-button
+  */
   classNames: ['sc-popup-button'],
   
   /** 
     Menu item list
+    @Array
   */
   itemList: [],
-    
+  
   /**private */
   acceptsFirstResponder: YES,
   /**
@@ -52,13 +57,22 @@ SC.DropDownView = SC.ButtonView.extend(
   menu : null,
   
   /**
-      Prefer matrix to position the color menu on the button such that the 
-      selected color swatch from color menu will appear alligned to the 
-      swtach image on the button.
-
+    To set the menu item index
+    
+    @property
+  */
+  itemIdx: null,
+  
+  // flag: 0,
+  
+  /**
+      Prefer matrix to position the drop down menu the button such that the 
+      selected item for the menu item will appear aligned to the 
+      icon on the button.
+      
       @property
   */  
-    preferMatrix: [-13, 0, 2],
+  preferMatrix: [-13, 0, 2],
   
   /**
     Binds the button's selection state to the menu's visibility.
@@ -70,23 +84,56 @@ SC.DropDownView = SC.ButtonView.extend(
     Method to set the Menu Items from the user defined objects
   */
   fetchMenuItems: function() {
-    console.log(this.get('preferMatrix')) ;
     var items = this.get('objects') ;
     var titleValueKey = this.get('titleValueKey') ;
     var iconValueKey = this.get('iconValueKey') ;
+    var selectionValueKey = this.get('selectionValueKey') ;
     var itemList = [];
+    
+    
+    // var flag = this.get('flag') ;
+    
+    //to set the 'checkbox' property of menu items
+    var isChecked = YES ;
+    
     items.forEach(function(object) {
       if (object) {
-        // Get the value key. If value key is not specified convert obj 
+        
+        //Get the title value key. If value key is not specified convert obj 
         //to string
-        var titleValue = titleValueKey ? object[titleValueKey] : object.toString() ;
-        var iconURL = object[iconValueKey] ;
+        var titleValue = 
+          titleValueKey ? object[titleValueKey] : object.toString() ;
+        
+        //get selection value
+        var selectionValue = 
+          selectionValueKey ? object[selectionValueKey] : null ;
+          
+        //Check if the item is currentSelectedItem or not
+        if(titleValue === this.title) {
+          isChecked = YES ;
+        }
+        // else if (selectionValue === YES) {
+        //          if (flag === 0 ) {
+        //            isChecked = YES
+        //            this.set('flag', 1) ;
+        //          }
+        //          else {
+        //            isChecked = NO ;
+        //          }
+        //        }
+        else {
+          isChecked = NO ;
+        }
+        
+        //Get the iconVlaue key
         var iconValue = iconValueKey ? object[iconValueKey] : object ;
+        
+        //Set the items in the itemList array
         itemList.push({
           title: titleValue,
-          icon: iconURL,
+          icon: iconValue,
           isEnabled: YES,
-          // checkbox: YES,
+          checkbox: isChecked,
           action: this.displaySelectedItem
         });
        }
@@ -95,19 +142,24 @@ SC.DropDownView = SC.ButtonView.extend(
   },
 
   /**
+    render method 
     @private
   */
   render: function(context,firstTime) {
     sc_super() ;
     
+    //Fetch the menu items
     this.fetchMenuItems() ;
-    this.changeDropDownPreferMatrix();
+    
+    //Set the preference matrix for the menu pane
+    this.changeDropDownPreferMatrix(this.itemIdx);
     
     var menu = this.get('menu') ;
     if(firstTime && menu) {
       menu.createLayer() ;
     }
   },
+  
   /**
     Button action handler
     @param {DOMMouseEvent} evt mouseup event that triggered the action
@@ -117,8 +169,18 @@ SC.DropDownView = SC.ButtonView.extend(
     var menu = this.get('menu') ;
     var itemList = this.get('itemList') ;
     var pane = SC.MenuPane.create({
+      
+      /**
+        Class name - drop-down-menu-item
+
+      */
       classNames: ['drop-down-menu-item'],
-            
+      
+      /**
+        The menu items are set from the itemList property of DropDownMenu
+
+        @property
+      */
       items: itemList,
       
       /**
@@ -127,6 +189,7 @@ SC.DropDownView = SC.ButtonView.extend(
         @property
       */
       isEnabled: YES,
+      
       /**
         This property points to the key setting the enable/disable configuration.
 
@@ -169,7 +232,6 @@ SC.DropDownView = SC.ButtonView.extend(
       })
     }) ;
     
-    
     pane.popup(this, this.preferMatrix) ;
     
     // no menu to toggle... bail...
@@ -186,43 +248,55 @@ SC.DropDownView = SC.ButtonView.extend(
   theme: 'popup',
   
   /**
-    Trigger the render function when these properties change
+    Render method gets triggered when these properties change
   */
   displayProperties: ['icon', 'title'],
   
   /** 
-     @param view - the selected MenuItem view
+     Action method for the drop down menu items
      
   */  
    displaySelectedItem: function() {
-     console.log(this) ;
-     var menuView = this.get('parentView') ;
-     console.log('menuView =>'+menuView) ;
      
-     // var menuIdx = idx = (menuItemViews.indexOf(menuItem)
-     //set the title and icon of the here. This in turn
-     //triggers the render method
+     //Get the menu pane
+     var menuView = this.get('parentView') ;
+     
+     //Current selected menu item
+     var currSel = menuView.get('currentSelectedMenuItem') ;
+     
+     // MenuItemViews
+     var itemViews = menuView.menuItemViews ;
+     
+     /**
+       Fetch the index of the current selected item
+     */
+     var itemIdx = itemViews.indexOf(currSel) ; 
+     
+     //Get the drop down View
      var button = this.getPath('parentView.anchor') ;
      
+     //Set the button title and icon
      button.set('title', this.get('value')) ;
      button.set('icon', this.get('icon')) ;
+     
+     //Set the value of 'itemIdx' property of the DropDownMenu 
+     button.set('itemIdx', itemIdx) ;
    },
    
    /**
      Set the "top" attribute in the prefer matrix property which will position 
-     menu such that the selected color swatch in the menu will be place alligned 
-     to the swatch on the button when menu is opened.
+     menu such that the selected item in the menu will be place aligned 
+     to the item on the button when menu is opened.
    */
    changeDropDownPreferMatrix: function() {
      var pxOffset = 0;
-     var preferMatrixAttributeTop = 20;
+     var preferMatrixAttributeTop = 0;
      
-     /** 
-       Have to fetch the selected menuItem index. Once this is done.
-        the menu pane aligment is complete.
-     */
-     // if(selectedItem) {}
-     //   preferMatrixAttributeTop = index * 20;
+     var itemIdx = this.get('itemIdx') ;
+     
+     //Set the preferMatrixAttribute Top
+     if(itemIdx) 
+     preferMatrixAttributeTop = itemIdx * 20;
      
      if(this.get('preferMatrix'))
        this.get('preferMatrix')[1] = -preferMatrixAttributeTop + pxOffset;
