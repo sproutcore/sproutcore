c49b7cb516d171a32bf0a7070409fe378a31086e
Charles Jolley <charles@sproutit.com>
2009-08-11 14:40:54 -0700

    Now any object can be an SC.Error and have a value

--- frameworks/foundation/system/error.js
+++ frameworks/foundation/system/error.js
@@ -19,7 +19,15 @@
   
   You can pass error objects to various UI elements to display the error in
   the interface. You can easily determine if the value returned by some API is 
-  an error or not using the helper SC.$ok(value).
+  an error or not using the helper SC.ok(value).
+  
+  h2. Faking Error Objects
+  
+  You can actually make any object you want to be treated like an Error object
+  by simply implementing two properties: isError and errorValue.  If you 
+  set isError to YES, then calling SC.ok(obj) on your object will return NO.
+  If isError is YES, then SC.val(obj) will return your errorValue property 
+  instead of the receiver.
   
   @extends SC.Object
   @since SproutCore 1.0
@@ -36,7 +44,13 @@ SC.Error = SC.Object.extend(
     Human readable description of the error.  This can also be a non-localized
     key.
   */
-  description: '',
+  message: '',
+  
+  /**
+    The value the error represents.  This is used when wrapping a value inside
+    of an error to represent the validation failure.
+  */
+  errorValue: null,
   
   /**
     Human readable name of the item with the error.
@@ -44,9 +58,16 @@ SC.Error = SC.Object.extend(
   label: null,
   
   toString: function() {
-    return "SC.Error:%@:%@ (%@)".fmt(SC.guidFor(this), this.description, this.code);
-  }
+    return "SC.Error:%@:%@ (%@)".fmt(SC.guidFor(this), this.get('message'), this.get('code'));
+  },
+  
+  /**
+    Walk like a duck.
+  */
+  isError: YES
 }) ;
+
+Error.prototype.isError = YES ; // also walk like a duck
 
 /**
   Creates a new SC.Error instance with the passed description, label, and
@@ -57,10 +78,11 @@ SC.Error = SC.Object.extend(
   @param code {Number} an error code to use for testing.
   @returns {SC.Error} new error instance.
 */
-SC.Error.desc = function(description, label, code) {
-  var opts = { description: description } ;
+SC.Error.desc = function(description, label, value, code) {
+  var opts = { message: description } ;
   if (label !== undefined) opts.label = label ;
   if (code !== undefined) opts.code = code ;
+  if (value !== undefined) opts.errorValue = value ;
   return this.create(opts) ;
 } ;
 
@@ -72,18 +94,29 @@ SC.Error.desc = function(description, label, code) {
   @param code {Number} an error code to use for testing.
   @returns {SC.Error} new error instance.
 */
-SC.$error = function(description, label, c) { 
-  return SC.Error.desc(description,label, c); 
+SC.$error = function(description, label, value, c) { 
+  return SC.Error.desc(description,label, value, c); 
 } ;
-var $error = SC.$error ; // export globally
 
 /**
-  Returns YES if the passed value is an error object, otherwise NO.
+  Returns YES if the passed value is an error object or false.
 */
-SC.$ok = function(ret) {
-  return (ret !== false) && (SC.typeOf(ret) != SC.T_ERROR) ;
+SC.ok = SC.$ok = function(ret) {
+  return (ret !== false) && !(ret && ret.isError);
 };
-var $ok = SC.$ok ; // export globally.
+
+/**
+  Returns the value of an object.  If the passed object is an error, returns
+  the value associated with the error; otherwise returns the receiver itself.
+  
+  @param {Object} obj the object
+  @returns {Object} value 
+*/
+SC.val = SC.$val = function(obj) {
+  if (obj && obj.isError) {
+    return obj.get ? obj.get('errorValue') : null ; // Error has no value
+  } else return obj ;
+};
 
 // STANDARD ERROR OBJECTS
 
--- frameworks/foundation/tests/system/error.js
+++ frameworks/foundation/tests/system/error.js
@@ -6,36 +6,42 @@
 module("SC.ERROR");
 
 test("SC.Error.desc creates an error instance with description,label and code", function() {
-  c = SC.Error.desc('This is an error instance','Error Instance',99999);
+  var c = SC.Error.desc('This is an error instance','Error Instance', "FOO", 99999);
   equals(SC.T_ERROR,SC.typeOf(c),'Error instance');
-  equals('This is an error instance',c.description,'Description');
+  equals('This is an error instance',c.message,'Description');
   equals('Error Instance',c.label,'Label');
+  equals(c.get('errorValue'), "FOO", 'error value should be set');
   equals(99999,c.code,'Code');
 });
 
 test("SC.$error creates an error instance with description,label and code",function(){
-  d = SC.$error('This is a new error instance','New Error Instance',99999);
+  var d = SC.$error('This is a new error instance','New Error Instance', "FOO", 99999);
   equals(SC.T_ERROR,SC.typeOf(d),'New Error instance');
-  equals('This is a new error instance',d.description,'Description');
+  equals('This is a new error instance',d.message,'Description');
   equals('New Error Instance',d.label,'Label');
+  equals(d.get('errorValue'), "FOO", 'error value should be set');
   equals(99999,d.code,'Code');
 });
 
-test("SC.$ok should return YES if the passed value is an error object", function() {
-	var denominator = [1,2,0];
-	var numerator =3;
-	var result = [];
-	for(i=0; i<denominator.length ;i++)
-	{
-		if(denominator[i]!=0)
-		{
-			result[i]= numerator/denominator[i];
-		}
-		else
-		{
-			// err = SC.Error.desc('divide by zero error','Divde by zero error',99999);
-			// 			equals(YES,SC.$ok(err),'Is an error');
-			// 			break;
-		}
-	}
+test("SC.$ok should return YES if the passed value is an error object or false", function() {
+  ok(SC.$ok(true), '$ok(true) should be YES');
+  ok(!SC.$ok(false), '$ok(false) should be NO');
+  ok(SC.$ok(null), '$ok(null) should be YES');
+  ok(SC.$ok(undefined), '$ok(undefined) should be YES');
+  ok(SC.$ok("foo"), '$ok(foo) should be YES');
+  ok(!SC.$ok(SC.$error("foo")), '$ok(SC.Error) should be NO');
+  ok(!SC.$ok(new Error()), '$ok(Error) should be NO');
+  ok(!SC.$ok(SC.Object.create({ isError: YES })), '$ok({ isError: YES }) should be NO');
+});
+
+test("SC.$val should return the error value if it has one", function() {
+  equals(SC.val(true), true, 'val(true) should be true');
+  equals(SC.val(false), false, 'val(false) should be false');
+  equals(SC.val(null), null, 'val(null) should be YES');
+  equals(SC.val(undefined), undefined, '$ok(undefined) should be YES');
+  equals(SC.val("foo"), "foo", 'val(foo) should be YES');
+  equals(SC.val(SC.$error("foo", "FOO", "BAZ")), "BAZ", 'val(SC.Error) should be BAZ');
+  equals(SC.val(SC.$error("foo", "FOO")), undefined, 'val(SC.Error) should be undefined');
+  equals(SC.val(new Error()), null, 'val(Error) should be null');
+  equals(SC.val(SC.Object.create({ isError: YES, errorValue: "BAR" })), "BAR", 'val({ isError: YES, errorValue: BAR }) should be BAR');
 });
