7a1d2cd91920634a7371796645eeb441ffef337b
Charles Jolley <charles@sproutit.com>
2009-09-22 18:34:03 -0700

    Should use CoreSet (no KVO)

--- frameworks/runtime/system/object.js
+++ frameworks/runtime/system/object.js
@@ -121,7 +121,7 @@ SC._object_extend = function _object_extend(base, ext) {
         while(--pathLoc >= 0) {
           local = base._kvo_for(SC.keyFor('_kvo_local', paths[pathLoc]), SC.Set);
           local.add(key);
-          base._kvo_for('_kvo_observed_keys', SC.Set).add(paths[pathLoc]) ;
+          base._kvo_for('_kvo_observed_keys', SC.CoreSet).add(paths[pathLoc]);
         }
 
       // handle computed properties
