60d2180caa9d5a36c72b4283a0952ae6d34d3e85
Christopher Swasey <christopher.swasey@gmail.com>
2009-10-27 17:58:01 -0400

    Fix to rectsEqual falsely returning true due to comparison to null instead of SC.none

--- frameworks/foundation/system/utils.js
+++ frameworks/foundation/system/utils.js
@@ -133,12 +133,12 @@ SC.mixin( /** @scope SC */ {
   rectsEqual: function(r1, r2, delta) {
     if (!r1 || !r2) return (r1 == r2) ;
     
-    if (delta === null) delta = 0.1;
+    if (SC.none(delta)) delta = 0.1;
     if ((r1.y != r2.y) && (Math.abs(r1.y - r2.y) > delta)) return NO ; 
     if ((r1.x != r2.x) && (Math.abs(r1.x - r2.x) > delta)) return NO ; 
     if ((r1.width != r2.width) && (Math.abs(r1.width - r2.width) > delta)) return NO ; 
     if ((r1.height != r2.height) && (Math.abs(r1.height - r2.height) > delta)) return NO ; 
-    return true ;
+    return YES ;
   },
   
   /** Returns the insersection between two rectangles. 
