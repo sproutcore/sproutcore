1aa91bbace02cd357484bdc3b0a2d8a26e3ff79a
Juan Pinzon <juanpin@Juan-Pinzons-MacBook-Pro.local>
2009-11-06 17:37:53 -0800

    WellView with updated style

---
+++ frameworks/desktop/views/well.js
@@ -0,0 +1,80 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2009 Apple Inc. All rights reserved.
+// License:   Licened under MIT license (see license.js)
+// ==========================================================================
+
+/** @class
+
+  A WellView is a ContainerView with a border. It's useful when you want to
+  group a set of views. It allows you to easily switch its contents too.
+  
+  It has a default contentLayout that will replace the layout of the contentView.
+  
+  @extends SC.ContainerView
+  @since SproutCore 1.0
+  @test in progress
+*/
+SC.WellView = SC.ContainerView.extend(
+/** @scope SC.WellView.prototype */ {
+  
+  classNames: 'sc-well-view',
+  
+  /**
+    Layout for the content of the container view.
+    @property {Object}
+  */
+  contentLayout: {top:10, bottom:10, left:10, right:10},
+  
+  
+  /**
+     Overrides createChildViews and replaces the layout of the contentView
+     with the one in contentLayout.
+   */
+  
+  createChildViews: function() {
+    // if contentView is defined, then create the content
+    var view = this.get('contentView') ;
+    if (view) {
+      view = this.contentView = this.createChildView(view) ;
+      view.set('layout', this.contentLayout);
+      this.childViews = [view] ;
+    } 
+  },
+  
+  /**
+     The render method for the WellView simply add the html necessary for
+     the border.
+     
+   */
+  
+  render: function(context, firstTime) {
+    sc_super();
+    if(firstTime){
+     context.push("<div class='top-left-edge'></div>",
+       "<div class='top-edge'></div>",
+       "<div class='top-right-edge'></div>",
+       "<div class='right-edge'></div>",
+       "<div class='bottom-right-edge'></div>",
+       "<div class='bottom-edge'></div>",
+       "<div class='bottom-left-edge'></div>",
+       "<div class='left-edge'></div>",
+       "<div class='content-background'></div>");
+     }    
+  },
+  
+  /**
+     Invoked whenever the content property changes.  This method will simply
+     call replaceContent and set the contentLayout in the new contentView.
+     
+     Override replaceContent to change how the view is
+     swapped out.
+   */
+  contentViewDidChange: function() {
+    var view = this.get('contentView');
+    view.set('layout', this.contentLayout);
+    this.replaceContent(view);
+  }.observes('contentView')
+  
+}) ;
