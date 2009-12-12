85048c596770cf79140fb0ef0bcfd6b8c41fcb4d
dcporter <dcporter@gmail.com>
2009-12-11 19:28:12 -0700

    Taught toFormattedString to handle characters %h (unpadded 24-hour time, 1-24) and %i (unpadded 12-hour time, 1-12).  Fixed its documentation to fluff the wall of text out into a nice list.

--- frameworks/foundation/system/datetime.js
+++ frameworks/foundation/system/datetime.js
@@ -252,32 +252,34 @@ SC.DateTime = SC.Object.extend(SC.Freezable, SC.Copyable,
     like the C strftime function.
   
     The format parameter can contain the following characters:
-      %a - The abbreviated weekday name (``Sun'')
-      %A - The full weekday name (``Sunday'')
-      %b - The abbreviated month name (``Jan'')
-      %B - The full month name (``January'')
-      %c - The preferred local date and time representation
-      %d - Day of the month (01..31)
-      %H - Hour of the day, 24-hour clock (00..23)
-      %I - Hour of the day, 12-hour clock (01..12)
-      %j - Day of the year (001..366)
-      %m - Month of the year (01..12)
-      %M - Minute of the hour (00..59)
-      %p - Meridian indicator (``AM'' or ``PM'')
-      %S - Second of the minute (00..60)
-      %U - Week number of the current year,
+      - %a - The abbreviated weekday name (``Sun'')
+      - %A - The full weekday name (``Sunday'')
+      - %b - The abbreviated month name (``Jan'')
+      - %B - The full month name (``January'')
+      - %c - The preferred local date and time representation
+      - %d - Day of the month (01..31)
+      - %h - Hour of the day, 24-hour clock (0..23)
+      - %H - Hour of the day, 24-hour clock (00..23)
+      - %i - Hour of the day, 12-hour clock (1..12)
+      - %I - Hour of the day, 12-hour clock (01..12)
+      - %j - Day of the year (001..366)
+      - %m - Month of the year (01..12)
+      - %M - Minute of the hour (00..59)
+      - %p - Meridian indicator (``AM'' or ``PM'')
+      - %S - Second of the minute (00..60)
+      - %U - Week number of the current year,
           starting with the first Sunday as the first
           day of the first week (00..53)
-      %W - Week number of the current year,
+      - %W - Week number of the current year,
           starting with the first Monday as the first
           day of the first week (00..53)
-      %w - Day of the week (Sunday is 0, 0..6)
-      %x - Preferred representation for the date alone, no time
-      %X - Preferred representation for the time alone, no date
-      %y - Year without a century (00..99)
-      %Y - Year with century
-      %Z - Time zone (ISO 8601 formatted)
-      %% - Literal ``%'' character
+      - %w - Day of the week (Sunday is 0, 0..6)
+      - %x - Preferred representation for the date alone, no time
+      - %X - Preferred representation for the time alone, no date
+      - %y - Year without a century (00..99)
+      - %Y - Year with century
+      - %Z - Time zone (ISO 8601 formatted)
+      - %% - Literal ``%'' character
     
     @param {String} format the format string
     @return {String} the formatted string
@@ -783,7 +785,11 @@ SC.DateTime.mixin(SC.Comparable,
       case 'B': return this.monthNames[this._get('month')-1];
       case 'c': return this._date.toString();
       case 'd': return this._pad(this._get('day'));
+      case 'h': return this._get('hour');
       case 'H': return this._pad(this._get('hour'));
+      case 'i':
+        var hour = this._get('hour');
+        return (hour === 12 || hour === 0) ? 12 : (hour + 12) % 12;
       case 'I': 
         var hour = this._get('hour');
         return this._pad((hour === 12 || hour === 0) ? 12 : (hour + 12) % 12);
