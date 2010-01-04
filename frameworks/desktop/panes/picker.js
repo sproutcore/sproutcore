4db2225b67df57ee2d61491b454698d6bc348884
Tom Dale <tomdale@67-218-104-198.cust.layer42.net>
2010-01-04 09:14:15 -0800

    Position nested menu items to the left of their root menu item if they exceed the width of the viewport.

--- frameworks/desktop/panes/picker.js
+++ frameworks/desktop/panes/picker.js
@@ -219,7 +219,7 @@ SC.PickerPane = SC.PalettePane.extend({
   computeAnchorRect: function(anchor) {
     var ret = SC.viewportOffset(anchor); // get x & y
     var cq = SC.$(anchor);
-    var wsize = this.get('currentWindowSize') || SC.RootResponder.responder.computeWindowSize() ;
+    var wsize = SC.RootResponder.responder.computeWindowSize() ;
     ret.width = cq.outerWidth();
     ret.height = (wsize.height-ret.y) < cq.outerHeight() ? (wsize.height-ret.y) : cq.outerHeight();
     return ret ;
@@ -299,11 +299,11 @@ SC.PickerPane = SC.PalettePane.extend({
   },
 
   /** @private
-    re-position rule optimized for Menu to enforce min left(7px)/right(20px) padding to the window
+    re-position rule optimized for Menu to enforce min left(7px)/right(8px) padding to the window
   */
   fitPositionToScreenMenu: function(w, f) {
     // min left/right padding to the window
-    if( (f.x + f.width) > (w.width-20) ) f.x = w.width - f.width - 20;
+    if( (f.x + f.width) > (w.width-9) ) f.x = f.x - (f.width*2);
     if( f.x < 7 ) f.x = 7;
     
     // if the height of the menu is bigger than the window height resize it.
