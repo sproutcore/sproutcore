sproutcore/sproutcore

commit 3bd03d368dfe153ea189ef684c9622672952fe8a

Implement SC.get().  Use it to retrieve a property from an object if it may or may not implement SC.Observable.

frameworks/runtime/mixins/observable.js
@@ -1315,5 +1315,20 @@ SC.logChange = function logChange(target, key, value) {
   console.log("CHANGE: %@[%@] => %@".fmt(target, key, target.get(key))) ;
 };
 
+/**
+  Retrieves a property from an object, using get() if the
+  object implements SC.Observable.
+
+  @param  {Object}  object  the object to query
+  @param  {String}  key the property to retrieve
+*/
+SC.mixin(SC, {
+  get: function(object, key) {
+    if (!object) return undefined;
+    if (object.get) return object.get(key);
+    return object[key];
+  }
+});
+
 // Make all Array's observable
 SC.mixin(Array.prototype, SC.Observable) ;

frameworks/runtime/tests/mixins/observable/observable.js
@@ -56,6 +56,75 @@ test("should call unknownProperty when value is undefined", function() {
   equals(object.lastUnknownProperty, "unknown") ;
 });
 
+// ..........................................................
+// SC.GET()
+//
+module("SC.get()", {
+  setup: function() {
+    objectA = SC.Object.create({
+
+      normal: 'value',
+      numberVal: 24,
+      toggleVal: true,
+
+      computed: function() { return 'value'; }.property(),
+
+      method: function() { return "value"; },
+
+      nullProperty: null,
+
+      unknownProperty: function(key, value) {
+        this.lastUnknownProperty = key ;
+        return "unknown" ;
+      }
+
+    });
+
+    objectB = {
+      normal: 'value',
+
+      nullProperty: null
+    };
+  }
+});
+
+test("should get normal properties on SC.Observable", function() {
+  equals(SC.get(objectA, 'normal'), 'value') ;
+});
+
+test("should call computed properties on SC.Observable and return their result", function() {
+  equals(SC.get(objectA, "computed"), "value") ;
+});
+
+test("should return the function for a non-computed property on SC.Observable", function() {
+  var value = SC.get(objectA, "method") ;
+  equals(SC.typeOf(value), SC.T_FUNCTION) ;
+});
+
+test("should return null when property value is null on SC.Observable", function() {
+  equals(SC.get(objectA, "nullProperty"), null) ;
+});
+
+test("should call unknownProperty when value is undefined on SC.Observable", function() {
+  equals(SC.get(object, "unknown"), "unknown") ;
+  equals(object.lastUnknownProperty, "unknown") ;
+});
+
+test("should get normal properties on standard objects", function() {
+  equals(SC.get(objectB, 'normal'), 'value');
+});
+
+test("should return null when property is null on standard objects", function() {
+  equals(SC.get(objectB, 'nullProperty'), null);
+});
+
+test("should return undefined if the provided object is null", function() {
+  equals(SC.get(null, 'key'), undefined);
+});
+
+test("should return undefined if the provided object is undefined", function() {
+  equals(SC.get(undefined, 'key'), undefined);
+});
 
 // ..........................................................
 // SET()

commit dbd1a7cb414827a0931c5d9f849baee9e1e1a361

Fix for the bug introduced by 3bd03d368dfe153ea189ef684c9622672952fe8a, makes SC.get('foo') and SC.objectForPropertyPath('SC.foo') work.

frameworks/runtime/mixins/observable.js
@@ -1328,6 +1328,7 @@ SC.logChange = function logChange(target, key, value) {
 SC.mixin(SC, {
   get: function(object, key) {
     if (!object) return undefined;
+    if (key === undefined) return this[object];
     if (object.get) return object.get(key);
     return object[key];
   }

frameworks/runtime/tests/mixins/observable/observable.js
@@ -126,6 +126,12 @@ test("should return undefined if the provided object is undefined", function() {
   equals(SC.get(undefined, 'key'), undefined);
 });
 
+test("should work when object is SC (used in SC.objectForPropertyPath)", function() {
+  equals(SC.objectForPropertyPath('SC.RunLoop'), SC.RunLoop);
+  equals(SC.get('RunLoop'), SC.RunLoop);
+  equals(SC.get(SC, 'RunLoop'), SC.RunLoop);
+});
+
 // ..........................................................
 // SET()
 // 