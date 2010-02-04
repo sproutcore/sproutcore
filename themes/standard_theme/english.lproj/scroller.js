35ffdfd370109eb3fb675ee61a9286cd54d96e19
Tom Dale <tdale@apple.com>
2010-02-04 15:28:42 -0800

    Added custom scrollers to standard theme.  Artwork by Alex Iskander. Only vertical is there for now.

---
+++ themes/standard_theme/english.lproj/scroller.js
@@ -0,0 +1,13 @@
+/**
+  Define the metrics for scrollbar display
+*/
+
+SC.mixin(SC.ScrollerView.prototype, {
+  scrollbarThickness: 14,
+  capLength: 18,
+  capOverlap: 14,
+  buttonOverlap: 11,
+  buttonLength: 41,
+  thumbTopLength: 10,
+  thumbBottomLength: 10
+});
