d230186c76ce28da208116242d2b7aa92198c34b
Tom Dale <tdale@apple.com>
2010-02-04 15:25:46 -0800

    Started to revamp unit tests for SC.ScrollerView.

--- frameworks/desktop/tests/views/scroller/methods.js
+++ frameworks/desktop/tests/views/scroller/methods.js
@@ -12,25 +12,24 @@ module("SC.ScrollerView",{
     SC.RunLoop.begin();
 	pane = SC.MainPane.create({
 	  childViews: [
-	    SC.ScrollerView.extend({		     
+	    SC.ScrollerView.extend({
 		}),
-		SC.ScrollerView.extend({		     
+		SC.ScrollerView.extend({
 		  minimum:10,
 		  maximum:100,
 		  isEnabled:NO,
-		  value:15,
 		  layoutDirection: SC.LAYOUT_HORIZONTAL
 		}),
-		SC.ScrollerView.extend({	
-      layout:{ top: 0, bottom: 0, right: 0, width: 20 },	     
+		SC.ScrollerView.extend({
+      layout:{ top: 0, bottom: 0, right: 0, width: 20 },
 		  minimum:0,
 		  maximum:100
 		})
 
-	  ]	
+	  ]
 	});
 	pane.append(); // make sure there is a layer...
-	SC.RunLoop.end();	
+	SC.RunLoop.end();
 	view = pane.childViews[0];
 	view1= pane.childViews[1];
   view2= pane.childViews[2];
@@ -42,31 +41,39 @@ module("SC.ScrollerView",{
   }
 });
 
-test("listing the displayProperties",function(){
-  var obj = view.get('displayProperties');
-  equals(obj.length, 5,'the number of display properties');
-  equals(obj[0],'isFirstResponder','the offset value property');
-  equals(obj[1],'isVisible','the isVisible value property');
-  equals(obj[2],'maximum','the maximum value property');	  
-  equals(obj[3],'isEnabled','the isEnabled offset value property');	  
-  equals(obj[4],'layoutDirection','the layoutDirection property');	  
-
+test('value', function() {
+  equals(view1.get('maximum'), 100, 'precond - view has maximum of 100');
+  equals(view1.get('minimum'), 10, 'precond - view has a minimum of 10');
+  view1.set('value', 300);
+  equals(view1.get('value'), view1.get('maximum'), 'value is set to maximum if attempting to set higher than maximum');
+  view1.set('maximum', 50);
+  equals(view1.get('value'), view1.get('maximum'), 'value should change if maximum changes');
+  view1.set('value', 0);
+  equals(view1.get('value'), view1.get('minimum'), 'value is set to minimum if attempt to set lower than minimum');
+  view1.set('minimum', 15);
+  equals(view1.get('value'), view1.get('minimum'), 'value should change if minimum changes');
 });
 
-test("testing properties of a scrollerview", function(){
-  equals(10,view1.get('minimum'),'Minimum offset value for the scroller')	;
-  equals(100,view1.get('maximum'),'Maximum offset value for the scroller');
-  equals(NO,view1.get('isEnabled'),'The scroller should be not enabled');
-  equals(SC.LAYOUT_VERTICAL,view.get('layoutDirection'),'The default scroller direction should be vertical');
-  equals(SC.LAYOUT_HORIZONTAL,view1.get('layoutDirection'),'The scroller direction set as vertical during view creation');
-  equals(0,view.get('value'),'The default offset value set during creation for a scroller should be 0');
-  equals(15,view1.get('value'),'The offset value should be 15');
-  // view1.set('layoutDirection',SC.LAYOUT_VERTICAL);
-  // equals(SC.LAYOUT_VERTICAL,view1.get('layoutDirection'),'The scroller direction set after view creation');
+test('isEnabled', function() {
+  ok(!view.$().hasClass('disabled'), 'scrollers should be enabled by default');
+  view.set('isEnabled', NO);
+  SC.RunLoop.begin().end();
+  ok(view.$().hasClass('disabled'), 'scrollers should have disabled class set if isEnabled is set to NO');
+
+  var evt = SC.Object.create({
+    target: { className: 'button-top' }
+  });
+  var scrollerHeight = view.get('frame').height;
+  view.set('maximum', scrollerHeight+100);
+  view.set('value', 500);
+  view.mouseDown(evt);
+  equals(view.get('value'), 500, 'scrollers should not respond to mouse events if they are disabled');
+  view.set('isEnabled', YES);
+  view.mouseDown(evt);
+  ok(view.get('value') < (scrollerHeight+100), 'scrollers should respond to mouse events if they are not disabled');
 });
 
-test("ownerScrollValueKey() function of the scroller view",function(){
-  equals('verticalScrollOffset',view.ownerScrollValueKey(),'should have a vertical scroll offset');
-  equals('horizontalScrollOffset',view1.ownerScrollValueKey(),'should have a vertical scroll offset');
+test('maximum', function() {
+  var scrollerHeight = view.get('frame').height;
+  view.set('maximum', scrollerHeight + 100);
 });
-
