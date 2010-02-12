68a792c9c2f369e89f5807fcb09ccfff5cf44f1d
Tom Dale <tdale@apple.com>
2010-02-12 13:24:12 -0800

    Fixed some styling issues with new menu pane.

--- frameworks/desktop/views/menu_item.js
+++ frameworks/desktop/views/menu_item.js
@@ -205,7 +205,7 @@ SC.MenuItemView = SC.View.extend( SC.ContentDisplay,
   */
 
   renderBranch: function(context) {
-    context.push('<span class= "hasBranch"></span>') ;
+    context.push('<span class="has-branch"></span>') ;
   },
 
   /** 
