2d146d91169d9f1dd33e7f57ecc5d958f9d608de
Mike Ball <mike.ball3@gmail.com>
2010-02-21 18:51:09 -0500

    added pane support to design controller

--- frameworks/designer/controllers/design.js
+++ frameworks/designer/controllers/design.js
@@ -13,5 +13,23 @@ SC.designController = SC.ObjectController.create(
 /** @scope SC.designController.prototype */ {
 
   contentBinding: 'SC.designsController.selection',
-  contentBindingDefault: SC.Binding.single()
+  contentBindingDefault: SC.Binding.single(),
+  
+  _dc_content_is_pane: function(){
+    var c = this.get('content'), pane;
+    if(c){
+      pane = c.get('view');
+      //remove any existing panes
+      if(this._currentPane && pane !== this._currentPane){
+        this._currentPane.remove();
+        this._currentPane = null;
+      }
+      //append if necessary..
+      if(c.get('type') === 'pane' && this._currentPane !== pane){
+        pane.append();
+        pane.adjust({bottom: 60}); //TODO: figure out how to reverse this....
+        this._currentPane = pane;
+      }
+    }
+  }.observes('content')
 }) ;
--- frameworks/designer/controllers/designs.js
+++ frameworks/designer/controllers/designs.js
@@ -20,15 +20,17 @@ SC.designsController = SC.ArrayController.create(
     for(var v in page){
       if(page.hasOwnProperty(v)){
         if(page[v] && page[v].kindOf){
-          if(page[v].kindOf(iframe.SC.View)){
+          
+          if(page[v].kindOf(iframe.SC.Pane)){
+            designs.push(SC.Object.create({type: 'pane', view: page.get(v), name: v}));
+          }
+          else if(page[v].kindOf(iframe.SC.View)){
             designs.push(SC.Object.create({type: 'view', view: page.get(v), name: v}));
           }
           else if(page[v].kindOf(iframe.SC.Page)){
             designs.push(SC.Object.create({type: 'page', view: page.get(v), name: v}));
           }
-          else if(page[v].kindOf(iframe.SC.Pane)){
-            designs.push(SC.Object.create({type: 'pane', view: page.get(v), name: v}));
-          }
+
         }
       }
     }
