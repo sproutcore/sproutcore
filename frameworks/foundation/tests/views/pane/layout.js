8f4b31710fd3ec3997e90aae54e9847d2cd55578
Martin Ottenwaelter <martin.ottenwaelter@gmail.com>
2010-02-26 12:05:16 +0100

    Fix for panes not updating their layout when modified.

---
+++ frameworks/foundation/tests/views/pane/layout.js
@@ -0,0 +1,31 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Apple Inc. and contributors.
+// License:   Licened under MIT license (see license.js)
+// ==========================================================================
+
+/*global module test equals context ok same Q$ htmlbody */
+
+var pane;
+
+module("SC.Pane#layout", {
+  setup: function() {
+    pane = SC.Pane.create({
+      layout: { top: 0, left: 0, width: 1, height: 1}
+    });
+    pane.append();
+  },
+  
+  teardown: function() {
+    pane.remove();
+  }
+});
+
+test("make sure that a call to adjust actually adjusts the view's size", function() {  
+  SC.run(function() {
+    pane.adjust({ width: 100, height: 50 });
+  });
+  
+  equals(pane.$()[0].style.width, '100px', 'width should have been adjusted');
+  equals(pane.$()[0].style.height, '50px', 'height should have been adjusted');
+});
