beac812be1a64916e74b75dde45a66821efbe752
Tom Dale <tdale@apple.com>
2010-03-03 16:47:45 -0800

    These menu item unit tests were bogus anyway, so putting a placeholder for now.

--- frameworks/desktop/tests/views/menu_item/ui.js
+++ frameworks/desktop/tests/views/menu_item/ui.js
@@ -1,44 +1,10 @@
 // ==========================================================================
-// Project: SproutCore - JavaScript Application Framework
+// Project:   SproutCore - JavaScript Application Framework
 // Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
-// portions copyright @2009 Apple Inc.
-// License: Licensed under MIT license (see license.js)
+//            portions copyright @2010 Apple Inc.
+// License:   Licensed under MIT license (see license.js)
 // ==========================================================================
+
 /*global module test htmlbody ok equals same stop start */
 
-htmlbody('<style> .sc-static-layout { border: 1px red dotted; } </style>');
-(function() {
-  var method = function() { console.log("done"); };
-var pane = SC.ControlTestPane.design({ width:200,height: 32 })
-  .add("full", SC.MenuItemView.design({ 
-    content: SC.Object.create({ 
-      icon: "sc-icon-folder-16",
-      title1: "List Item 1",
-      checkbox: YES,
-      shortcut: "Text",
-      keyEquivalent: "alt_shift_z"
-   }),
-  isAnOption: YES,
-  contentValueKey: "title1",
-  contentIconKey: "icon",
-  shortCutKey: "shortcut",
-  contentCheckboxKey: "checkbox",
-  keyEquivalent: "shift_>",
-  action: method,
-  isEnabled: NO
-    // contentIsBranchKey: 'branch'
-  }))
-  
-  .add("disabled menu item with no content", SC.MenuItemView.design({ 
-    content: SC.Object.create(),
-    isSeparator: YES,
-    isEnabled: NO
-  }));
- 
-pane.show();
-
-module("Menu Item View");
-test("Changing the properties", function() {
-  pane.view('full').set('isEnabledKey', YES);
-});
-})();
+module("TODO: Test SC.MenuItemView UI");
