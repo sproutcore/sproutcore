c35e11b89f056a383bfd2ba54ef3a56c5719f39b
William Kakes <bkakes@funke.apple.com>
2010-03-11 15:06:03 -0800

    In Firefox only, have 'input' elements manually dispatch their 'keypress' events to the root responder, because there are certain cases -- such as the "down" key when the cursor is at the end of the field -- where the 'keypress' event will not bubble up to the document, and that's where SproutCore listens for it.

--- frameworks/foundation/views/text_field.js
+++ frameworks/foundation/views/text_field.js
@@ -475,8 +475,23 @@ SC.TextFieldView = SC.FieldView.extend(SC.StaticLayout, SC.Editable,
     // For some strange reason if we add focus/blur events to textarea
     // inmediately they won't work. However if I add them at the end of the
     // runLoop it works fine.
-    if(this.get('isTextArea')) this.invokeLast(this._addTextAreaEvents);
-    else this._addTextAreaEvents();
+    if(this.get('isTextArea')) {
+      this.invokeLast(this._addTextAreaEvents);
+    }
+    else {
+      this._addTextAreaEvents();
+      
+      // In Firefox, for input fields only (that is, not textarea elements),
+      // if the cursor is at the end of the field, the "down" key will not
+      // result in a "keypress" event for the document (only for the input
+      // element), although it will be bubbled up in other contexts.  Since
+      // SproutCore's event dispatching requires the document to see the
+      // event, we'll manually forward the event along.
+      if (SC.browser.mozilla) {
+        var input = this.$input();
+        SC.Event.add(input, 'keypress', this, this._firefox_dispatch_keypress);
+      }
+    }
   },
   
   _addTextAreaEvents: function() {
@@ -500,9 +515,10 @@ SC.TextFieldView = SC.FieldView.extend(SC.StaticLayout, SC.Editable,
     sc_super();
 
     var input = this.$input();
-    SC.Event.remove(input, 'focus', this, this._textField_fieldDidFocus);
-    SC.Event.remove(input, 'blur',  this, this._textField_fieldDidBlur);
-    SC.Event.remove(input, 'select',  this, this._textField_selectionDidChange);
+    SC.Event.remove(input, 'focus',  this, this._textField_fieldDidFocus);
+    SC.Event.remove(input, 'blur',   this, this._textField_fieldDidBlur);
+    SC.Event.remove(input, 'select', this, this._textField_selectionDidChange);
+    SC.Event.remove(input, 'focus',  this, this._firefox_dispatch_keypress);
   },
 
   _textField_fieldDidFocus: function(evt) {
@@ -573,6 +589,21 @@ SC.TextFieldView = SC.FieldView.extend(SC.StaticLayout, SC.Editable,
       }
     }
     return this ;
+  },
+  
+  
+  /**
+    In Firefox, as of 3.6 -- including 3.0 and 3.5 -- for input fields only
+    (that is, not textarea elements), if the cursor is at the end of the
+    field, the "down" key will not result in a "keypress" event for the
+    document (only for the input element), although it will be bubbled up in
+    other contexts.  Since SproutCore's event dispatching requires the
+    document to see the event, we'll manually forward the event along.
+  */
+  _firefox_dispatch_keypress: function(evt) {
+    var input = this.$input();
+    var responder = SC.RootResponder.responder;
+    responder.keypress.call(responder, evt);
   },
   
   
