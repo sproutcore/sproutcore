4b55a0a6d708385953d32f7003461e3b53fa1239
Juan Pinzon <juanpin@Juan-Pinzons-MacBook-Pro.local>
2010-03-25 14:35:19 -0700

    Add some validator to avoid textfield regression in IE

--- frameworks/desktop/system/root_responder.js
+++ frameworks/desktop/system/root_responder.js
@@ -522,7 +522,7 @@ SC.RootResponder = SC.RootResponder.extend(
   
   mousedown: function(evt) {
     try {
-      evt.target.focus();
+      if(evt.target && evt.target.focus)  evt.target.focus();
       // First, save the click count. The click count resets if the mouse down
       // event occurs more than 200 ms later than the mouse up event or more
       // than 8 pixels away from the mouse down event.
