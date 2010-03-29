54d0739950dd78643699dc1630c7e33fc1d1c69d
Mike Ball <mike.ball3@gmail.com>
2010-03-29 10:14:36 -0400

    failing picker unit test

--- frameworks/desktop/tests/panes/picker/ui.js
+++ frameworks/desktop/tests/panes/picker/ui.js
@@ -16,6 +16,7 @@ function evaluatePicker(pane) {
   ok(pane.$().hasClass('sc-picker'), 'pane should have sc-picker class');
   ok(pane.childViews[0].get('isVisibleInWindow'), 'pane.div.isVisibleInWindow should be YES');
   ok(pane.childViews[0].$().hasClass('sc-view'), 'pane.div should have sc-view class');
+  equals(pane.$('.sc-empty').length, 1, 'pane should have only one content view');
   
   var ret = pane.layoutStyle();
 
