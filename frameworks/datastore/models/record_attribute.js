9f58047d3459adbf94ff5569db05c0abe283b36e
William Kakes <bkakes@funke.apple.com>
2010-04-01 14:17:20 -0700

    Don't needless lock the record (by calling writeAttribute) and write a duplicitous value when the defaultValue() function returns the same value (null/undefined) that caused us to call it in the first place.

--- frameworks/datastore/models/record_attribute.js
+++ frameworks/datastore/models/record_attribute.js
@@ -226,9 +226,10 @@ SC.RecordAttribute = SC.Object.extend(
     nvalue = value = record.readAttribute(attrKey);
     if (SC.none(value) && (value = this.get('defaultValue'))) {
        if (typeof value === SC.T_FUNCTION) {
-        value = this.defaultValue(record, key, this);
+        nvalue = this.defaultValue(record, key, this);
         // write default value so it doesn't have to be executed again
         if ((nvalue !== value)  &&  record.get('store').readDataHash(record.get('storeKey'))) {
+          value = nvalue;
           record.writeAttribute(attrKey, value, true);
         }
       }
