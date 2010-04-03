0be82e9bc4ab3760f25f81cfa4d9a1245e2dbae7
SantoshShan <sshanbogue@apple.com>
2010-04-02 17:03:39 -0700

    Made changes to override the _button_isSelectedDidChange function of SC.Button mixin. Ideally this observer should be used only in case it is toggle Button not otherwise

--- frameworks/desktop/panes/select_button.js
+++ frameworks/desktop/panes/select_button.js
@@ -225,7 +225,7 @@ SC.SelectButtonView = SC.ButtonView.extend(
 
     @private
   */
-  isSelectedBinding: '*menu.isVisibleInWindow',
+  isSelectedBinding: '*menu.isVisible',
 
   /**
     If this property is set to 'YES', the menu pane will be positioned
@@ -293,19 +293,14 @@ SC.SelectButtonView = SC.ButtonView.extend(
     switch (this.get('controlSize')) {
       case SC.TINY_CONTROL_SIZE:
         return SC.SelectButtonView.TINY_OFFSET_X;
-        break;
       case SC.SMALL_CONTROL_SIZE:
         return SC.SelectButtonView.SMALL_OFFSET_X;
-        break;
       case SC.REGULAR_CONTROL_SIZE:
         return SC.SelectButtonView.REGULAR_OFFSET_X;
-        break;
       case SC.LARGE_CONTROL_SIZE:
         return SC.SelectButtonView.LARGE_OFFSET_X;
-        break;
       case SC.HUGE_CONTROL_SIZE:
         return SC.SelectButtonView.HUGE_OFFSET_X;
-        break;
     }
     return 0;
   }.property('controlSize'),
@@ -579,8 +574,8 @@ SC.SelectButtonView = SC.ButtonView.extend(
 
     // no menu to toggle... bail...
     if (!menu) return NO ;
-    this.set('menu', menu);
     menu.popup(this, this.preferMatrix) ;
+    this.set('menu', menu);
     menu.set('currentMenuItem', menu.menuItemViewForContentIndex(this.get('itemIdx'))) ;
     return YES ;
   },
@@ -693,7 +688,15 @@ SC.SelectButtonView = SC.ButtonView.extend(
   /** Function overridden - tied to the isEnabled state */
   acceptsFirstResponder: function() {
     return this.get('isEnabled');
-  }.property('isEnabled')
+  }.property('isEnabled'),
+  
+  /** @private
+    Override the button isSelectedDidChange function in order to not perform any action
+    on selecting the select_button
+  */
+  _button_isSelectedDidChange: function() {
+    
+  }.observes('isSelected')
 
 }) ;
 
