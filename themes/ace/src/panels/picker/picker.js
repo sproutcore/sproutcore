cf1f3a5890ff564368c3a1019cf9c1e329533041
Alex Iskander <alex@tpsitulsa.com>
2010-04-10 13:25:00 -0500

    Add very basic picker renderer.

---
+++ themes/ace/src/panels/picker/picker.js
@@ -0,0 +1,14 @@
+require("src/theme");
+
+SC.AceTheme.renderers.Picker = SC.Renderer.extend({
+  POINTER_DISTANCE_FROM_EDGE: 50,
+  render: function(context){
+    
+  },
+  
+  update: function() {
+    
+  }
+});
+
+SC.AceTheme.renderers.picker = SC.AceTheme.renderers.Picker.create();
