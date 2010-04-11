48f1ab3779b0e4caee2792a560e0eaae695f4022
Alex Iskander <alex@tpsitulsa.com>
2010-04-10 22:24:36 -0500

    Start theming popover.

---
+++ themes/ace/src/panels/picker/popover/workspace.js
@@ -0,0 +1,50 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2009 Apple Inc. All rights reserved.
+// License:   Licened under MIT license (see license.js)
+// ==========================================================================
+
+/** @class
+  @extends SC.EmptyTheme.renderers.Workspace
+  @since SproutCore 1.1
+*/
+
+// requires popover theme.
+require("src/panels/picker/popover/popover");
+
+var theme = SC.AceTheme.Popover;
+
+SC.AceTheme.Popover.renderers.Workspace = SC.EmptyTheme.renderers.Workspace.extend({
+  computeClassNames: function() {
+    var cn = this._TMP_CLASS_NAMES || {};
+    cn["top-toolbar"] = this.hasTopToolbar;
+    cn["bottom-toolbar"] = this.hasBottomToolbar;
+    this._TMP_CLASS_NAMES = cn;
+    return cn;
+  },
+  
+  render: function(context) {
+    if (this.contentProvider) this.contentProvider.renderContent(context);
+    
+    context.setClass(this.computeClassNames());
+    
+    context.push("<div class='sc-workspace-overlay'>",
+      "<div class='middle'></div>",
+      "<div class='top-left-edge'></div>",
+      "<div class='top-edge'></div>",
+      "<div class='top-right-edge'></div>",
+      "<div class='right-edge'></div>",
+      "<div class='bottom-right-edge'></div>",
+      "<div class='bottom-edge'></div>",
+      "<div class='bottom-left-edge'></div>",
+      "<div class='left-edge'></div>",
+    "</div>");
+  },
+  
+  update: function() {
+    this.$().setClass(this.computeClassNames());
+  }
+});
+
+SC.AceTheme.Popover.renderers.workspace = SC.AceTheme.Popover.renderers.Workspace.create();
