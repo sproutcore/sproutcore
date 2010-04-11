e27a806ef52f73dec64cbb2de9cef6146ed50d4e
Alex Iskander <alex@tpsitulsa.com>
2010-04-10 22:21:47 -0500

    Convert PickerPane to use a renderer.

---
+++ themes/empty_theme/renderers/picker.js
@@ -0,0 +1,51 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2009 Apple Inc. All rights reserved.
+// License:   Licened under MIT license (see license.js)
+// ==========================================================================
+
+/** @class
+  @extends SC.Renderer
+  @since SproutCore 1.1
+*/
+
+require("theme");
+SC.EmptyTheme.renderers.Picker = SC.Renderer.extend({
+  init: function(attr) {
+    this.attr(attr);
+    
+    this.panelRenderer = this.theme.panel();
+  },
+  render: function(context) {
+    this.panelRenderer.attr("contentProvider", this.contentProvider);
+    this.panelRenderer.render(context);
+    
+    if (this.preferType == SC.PICKER_POINTER || this.preferType == SC.PICKER_MENU_POINTER) {
+      context.push('<div class="sc-pointer '+this.pointerPos+'" style="margin-top: '+this.pointerPosY+'px"></div>');
+      context.addClass(this.pointerPos);
+    }
+  },
+  
+  update: function() {
+    this.panelRenderer.attr("contentProvider", this.contentProvider);
+    this.panelRenderer.update();
+    
+    if (this.preferType == SC.PICKER_POINTER || this.preferType == SC.PICKER_MENU_POINTER) {
+      var el = this.$('.sc-pointer');
+      el.attr('class', "sc-pointer "+this.pointerPos);
+      el.attr('style', "margin-top: "+this.pointerPosY+"px");
+      this.$().addClass(this.pointerPos);
+    }
+  },
+  
+  didAttachLayer: function(l) {
+    this.panelRenderer.attachLayer(l);
+  },
+  
+  willDetachLayer: function() {
+    this.panelRenderer.detachLayer();
+  }
+});
+
+SC.EmptyTheme.renderers.picker = SC.EmptyTheme.renderers.Picker.create();
