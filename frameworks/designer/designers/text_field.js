169a4439cfd402149e19b3b59925cc0fefea7a78
Mike Ball <mike.ball3@gmail.com>
2010-04-14 00:25:08 -0400

    text field view designer

---
+++ frameworks/designer/designers/text_field.js
@@ -0,0 +1,15 @@
+// ========================================================================
+// SproutCore -- JavaScript Application Framework
+// Copyright ©2006-2008, Sprout Systems, Inc. and contributors.
+// Portions copyright ©2008 Apple Inc.  All rights reserved.
+// ========================================================================
+
+sc_require('designers/view_designer');
+
+SC.TextFieldView.Designer = SC.ViewDesigner.extend(
+/** @scope SC.TabView.Designer.prototype */ {
+  
+  encodeChildViews: NO,
+  
+  designProperties: 'isPassword isTextArea hint'.w()
+});
