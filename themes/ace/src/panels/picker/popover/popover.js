9dc961d6c463972949f7a9c71737579b2520b578
Alex Iskander <alex@tpsitulsa.com>
2010-04-15 19:15:56 -0500

    add empty popover theme.

--- themes/ace/src/panels/picker/popover/popover.js
+++ themes/ace/src/panels/picker/popover/popover.js
@@ -1,1 +1,3 @@
 SC.AceTheme.Popover = SC.AceTheme.subtheme("popover", "popover");
+SC.AceTheme.SolidPopover = SC.AceTheme.Popover.subtheme("solid", "solid");
+SC.AceTheme.register("solid-popover", SC.AceTheme.SolidPopover);
