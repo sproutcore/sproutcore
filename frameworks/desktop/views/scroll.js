f9ddb271e956563400df0c7322e0d74c20b360a8
Alex Iskander <alex@tpsitulsa.com>
2010-04-28 18:16:09 -0500

    Make bouncing/scale settings come from SC.platform.

--- frameworks/desktop/views/scroll.js
+++ frameworks/desktop/views/scroll.js
@@ -1006,7 +1006,7 @@ SC.ScrollView = SC.View.extend(SC.Border, {
       },
       scrolling: { x: NO, y: NO },
       
-      enableBouncing: SC.browser.mobileSafari,
+      enableBouncing: SC.platform.bounceOnScroll,
       
       // offsets and velocities
       startClipOffset: { x: startClipOffsetX, y: startClipOffsetY },
@@ -1021,7 +1021,7 @@ SC.ScrollView = SC.View.extend(SC.Border, {
       
       startScale: this._scale,
       startDistance: avg.d,
-      canScale: this.get("canScale") && SC.browser.mobileSafari,
+      canScale: this.get("canScale") && SC.platform.pinchToScale,
       minimumScale: this.get("minimumScale"),
       maximumScale: this.get("maximumScale"),
       
