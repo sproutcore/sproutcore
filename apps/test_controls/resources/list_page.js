f3111084622dac7a6818f41812b17ad61ede372b
Alex Iskander <alex@tpsitulsa.com>
2010-04-28 18:14:02 -0500

    Make ListView test include icons, checkboxes, etc. (even though Ace 2.0 doesn't yet support them)

--- apps/test_controls/resources/list_page.js
+++ apps/test_controls/resources/list_page.js
@@ -4,12 +4,26 @@ TestControls.listPage = SC.View.design({
   scroll: SC.ScrollView.design({
     backgroundColor: "white",
     contentView: SC.ListView.design({
+      hasContentIcon: YES,
+      hasContentRightIcon: YES,
+      contentIconKey: "icon",
+      contentRightIconKey: "rightIcon",
+      contentCheckboxKey: "isChecked",
+      contentUnreadCountKey: "unread",
+      
+      
       classNames: ['big-list'],
       rowHeight: 44,
       content: function() {
         var idx = 0, ret = [];
         for (idx = 0; idx < 1000; idx++) {
-          ret.push(SC.Object.create({"title": "Item " + idx}));
+          ret.push(SC.Object.create({
+            "title": "Item " + idx,
+            "icon": "sc-icon-document-16",
+            "unread": idx,
+            rightIcon: "sc-icon-info-16",
+            isChecked: YES
+          }));
         }
         return ret;
       }.property().cacheable(),
