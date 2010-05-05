5021e2f5ef4b7ab9c5f8da9f53f6e4e8970bde34
Alex Iskander <alex@tpsitulsa.com>
2010-05-04 20:48:35 -0500

    Add a gesture recognizer and a swipe gesture.

---
+++ frameworks/foundation/gestures/swipe.js
@@ -0,0 +1,106 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2010 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+sc_require("system/gesture");
+
+SC.SWIPE_HORIZONTAL = "X";
+SC.SWIPE_VERTICAL = "Y";
+SC.SWIPE_LEFT = "LEFT";
+SC.SWIPE_RIGHT = "RIGHT";
+SC.SWIPE_UP = "UP";
+SC.SWIPE_DOWN = "DOWN";
+
+SC.SwipeGesture = SC.Gesture.extend({
+  name: "swipe",
+  acceptsMultitouch: YES,
+  
+  direction: SC.SWIPE_HORIZONTAL,
+  startDistance: 5,
+  swipeDistance: 40,
+  
+  tolerance: 0.5, // we accept .5 the distance in the other direction as a swipe
+  
+  touchIsInGesture: function(touch, status) {
+    // if we have not "flunked" the touch before, and it has moved 
+    if (!status.flunked) {
+      var d = this.get("direction"), 
+          o = (d === SC.SWIPE_HORIZONTAL ? "Y" : "X"),
+          delta = touch["page" + d] - touch["start" + d],
+          oDelta = touch["page" + o] - touch["start" + o];
+      
+      if (
+        Math.abs(delta) > this.get("startDistance") &&
+        Math.abs(delta) * this.get("tolerance") > Math.abs(oDelta)
+      ) {
+        return YES;
+      }
+      
+    }
+    return NO;
+  },
+  
+  touchStart: function(touch) {
+    var d = this.get("direction"), 
+        delta = touch["page" + d] - touch["start" + d],
+        swipeDirection;
+    
+    if (delta < 0) swipeDirection = (d === SC.SWIPE_HORIZONTAL) ? SC.SWIPE_LEFT : SC.SWIPE_UP;
+    else swipeDirection = (d === SC.SWIPE_HORIZONTAL) ? SC.SWIPE_RIGHT : SC.SWIPE_DOWN;
+    
+    this.start(touch, swipeDirection, delta);
+    return YES;
+  },
+  
+  touchesDragged: function(evt, touches) {
+    var touch = touches.firstObject();
+    var d = this.get("direction"), 
+        o = (d === SC.SWIPE_HORIZONTAL ? "Y" : "X"),
+        delta = touch["page" + d] - touch["start" + d],
+        oDelta = touch["page" + o] - touch["start" + o],
+        swipeDirection;
+    
+    if (delta < 0) swipeDirection = (d === SC.SWIPE_HORIZONTAL) ? SC.SWIPE_LEFT : SC.SWIPE_UP;
+    else swipeDirection = (d === SC.SWIPE_HORIZONTAL) ? SC.SWIPE_RIGHT : SC.SWIPE_DOWN;
+    
+    if (
+      Math.abs(delta) < this.get("startDistance") ||
+      Math.abs(delta) * this.get("tolerance") < Math.abs(oDelta)
+    ) {
+      // does not qualify anymore
+      this.release(touch);
+    } else {
+      this.change(touch, swipeDirection, delta);
+    }
+  },
+  
+  touchEnd: function(touch) {
+    var d = this.get("direction"), 
+        o = (d === SC.SWIPE_HORIZONTAL ? "Y" : "X"),
+        delta = touch["page" + d] - touch["start" + d],
+        oDelta = touch["page" + o] - touch["start" + o],
+        swipeDirection;
+    
+    // determine swipe direction
+    if (delta < 0) swipeDirection = (d === SC.SWIPE_HORIZONTAL) ? SC.SWIPE_LEFT : SC.SWIPE_UP;
+    else swipeDirection = (d === SC.SWIPE_HORIZONTAL) ? SC.SWIPE_RIGHT : SC.SWIPE_DOWN;
+    
+    // trigger
+    if (
+      Math.abs(delta) > this.get("swipeDistance") ||
+      Math.abs(delta) * this.get("tolerance") < Math.abs(oDelta)
+    ) {
+      this.trigger(touch, swipeDirection);
+    }
+    
+    // and release all others
+    var touches = touch.touchesForResponder(this);
+    if (touches) {
+      touches.forEach(function(touch){
+        this.release(touch);
+      }, this);
+    }
+  }
+});
---
+++ frameworks/foundation/system/gesture.js
@@ -0,0 +1,228 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2010 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+
+/**
+  SC.Gesture is a simple object which can easily be extended, and which determines
+  if a touch or multiple touches match any given gesture.
+  
+  Gestures have names. These names can be customized at any point; they are merely
+  prefixes for the methods that will be called on the Gesturable view: 
+  
+  - [gesture]Start
+  - [gesture]Changed
+  - [gesture]End
+  - [gesture]
+
+  
+  When an SC.Gesturable talks with SC.Gesture, it will call touchStartHasPotential first.
+  
+  When a touch is moving, the Gesturable will call unassignedTouchesDidChange with the set of touches.
+  By default, this method calls touchIsInGesture(touch) for the touch.
+  
+  A gesture has two choices if it wants to take complete control of a touch. It can do so
+  in a one-off way--that is, the touch will then be ignored if anything further happens with it.
+  Or, it may take possession of the touch. The former will trigger [gestureName](gesture, args) on the view,
+  the latter will trigger [gestureName]Start(gesture). The gesture may then trigger, at any time,
+  change(arguments here) and end(arguments here).
+  
+  To trigger a one-off, you call trigger(touch). Otherwise, from touchIsInGesture you can return YES, or,
+  if not in touchIsInGesture, call take(touch) (returning YES calls take(touch) for you). 
+  
+  Gestures actually become the touch responder for a touch when they take posession of it. This means that
+  to release a touch, you must call touch.makeTouchResponder or touch.captureTouch(this.view) to release
+  the touch. Mostly, however, gestures themselves do not need to release touches. Still, just in case, there
+  is a convenience release(touch) function.
+  
+  Good places to call start() and end() (which call [gestureName]Start and [gestureName]End) from are
+  touchStart and touchEnd, respectively.
+  
+  Finally, if you have ANY interest in a touch, and don't want a view to relay it to some other view just
+  yet, you should call interestedInTouch(touch); if the touch is no longer interesting, call uninterestedInTouch(touch);
+  
+  Gestures are created by SC.Gesturable views as-needed, but last the lifetime of the view.
+*/
+SC.Gesture = SC.Object.extend({
+  /**
+    The gesture name
+  */
+  name: "gesture",
+  
+  /**
+    Return YES to take the touch, starting the gesture if it is not already started, or
+    trigger() to trigger the one-off gesture and call discardTouch(touch) to get rid of the touch.
+  */
+  touchIsInGesture: function(touch, status) {
+    return NO;
+  },
+  
+  /**
+    Called when a touch has started in the gesture (due to "take" being called).
+  */
+  touchStart: function(touch) {
+    
+  },
+  
+  /**
+    Called when touches have moved in the gesture.
+  */
+  touchesDragged: function(evt, touches) {
+    
+  },
+  
+  /**
+    Called whn a touch has ended in the gesture.
+  */
+  touchEnd: function(touch) {
+    
+  },
+  
+  /**
+    Starts the gesture, if it is not already started, 
+  */
+  start: function() {
+    if (!this.get("isActive")) {
+      this.set("isActive", YES);
+      
+      var args = SC.$A(arguments);
+      args.unshift(this);
+      
+      var act = this.name + "Start";
+      if (this.view[act]) this.view[act].apply(this.view, args);
+    }
+  },
+  
+  /**
+    Ends the gesture, if it is active.
+  */
+  end: function() {
+    if (this.get("isActive")) {
+      this.set("isActive", NO);
+
+      var args = SC.$A(arguments);
+      args.unshift(this);
+      
+      var act = this.name + "End";
+      if (this.view[act]) this.view[act].apply(this.view, args);
+    }
+  },
+  
+  /**
+    Alert that the gesture has changed.
+  */
+  change: function() {
+    var args = SC.$A(arguments);
+    args.unshift(this);
+    
+    var act = this.name + "Changed";
+    if (this.view[act]) this.view[act].apply(this.view, args);
+  },
+  
+  /**
+    Takes possession of a touch. This does not take effect immediately; it takes effect after
+    the run loop finishes to prevent it from being called during another makeTouchResponder.
+  */
+  take: function(touch) {
+    this.invokeLater("_take", 0, touch);
+  },
+  
+  _take: function(touch) {
+    touch.isTaken = YES; // because even changing responder won't prevent it from being used this cycle.
+    if (touch.touchResponder && touch.touchResponder !== this) touch.makeTouchResponder(this, YES);
+  },
+  
+  /**
+    Releases a touch. This takes effect immediately, because you would usually call this from
+    touchesDragged or such.
+  */
+  release: function(touch) {
+    touch.isTaken = NO;
+    touch.makeTouchResponder(touch.nextTouchResponder);
+  },
+  
+  /**
+    Triggers the gesture.
+  */
+  trigger: function() {
+    var args = SC.$A(arguments);
+    args.unshift(this);
+    
+    var act = this.name;
+    if (this.view[act]) this.view[act].apply(this.view, args);
+  },
+  
+  /**
+    Discards a touch, making its responder null.
+  */
+  discardTouch: function(touch) {
+    touch.isTaken = YES; // because even changing responder won't prevent it from being used this cycle.
+    touch.makeTouchResponder(null);
+  },
+  
+  /**
+    Returns a status hash (which gestures may and should modify) for a given touch, for tracking
+    whether it is a potential match, etc.
+  */
+  statusForTouch: function(touch) {
+    var key = SC.guidFor(touch.view) + this.name;
+    var status = touch[key];
+    if (!status) status = touch[key] = {};
+    return status;
+  },
+  
+  /**
+    Called when an unassigned touch has started. By default, this calls touchIsInGesture.
+  */
+  unassignedTouchDidStart: function(touch) {
+    if (touch.isTaken) return;
+    if (this.touchIsInGesture(touch, this.statusForTouch(touch))) {
+      this.take(touch);
+    }
+  },
+  
+  /**
+    This is called when the unassigned touches (touches not in a gesture) change or move
+    in some way. By default, this calls touchIsInGesture(touch, status) for each touch.
+  */
+  unassignedTouchesDidChange: function(evt, touches) {
+    touches.forEach(function(touch) {
+      if (touch.isTaken) return;
+      if (this.touchIsInGesture(touch, this.statusForTouch(touch))) {
+        this.take(touch);
+      }
+    }, this);
+  },
+  
+  /**
+    This is called when the unassigned touches (touches not in the gesture) have ended.
+    Default does nothing. Many gestures will want to implement this even if they don't implement
+    unassignedTouchesDidChange.
+  */
+  unassignedTouchDidEnd: function(touch) {
+    
+  },
+  
+  /**
+    Marks the touch as "interesting" to this gesture.
+  */
+  interestedInTouch: function(touch) {
+    var status = this.statusForTouch(touch);
+    if (status.isInterested) return;
+    status.isInterested = YES;
+    touch.isInteresting++;
+  },
+  
+  /**
+    Marks the touch as "uninteresting" to this gesture.
+  */
+  uninterestedInTouch: function(touch) {
+    var status = this.statusForTouch(touch);
+    if (!status.isInterested) return;
+    status.isInterested = NO;
+    touch.isInteresting--;
+  }
+});
