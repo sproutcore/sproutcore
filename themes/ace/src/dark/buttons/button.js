8ee3d561758184a9f81288026b57d9a7838317a6
Alex Iskander <alex@tpsitulsa.com>
2010-05-05 11:48:54 -0500

    Start on dark theme.

---
+++ themes/ace/src/dark/buttons/button.js
@@ -0,0 +1,3 @@
+SC.AceTheme.Dark.PointLeft = SC.AceTheme.Dark.subtheme("point-left", "point-left");
+SC.AceTheme.Dark.PointRight = SC.AceTheme.Dark.subtheme("point-right", "point-right");
+SC.AceTheme.Dark.PointRight = SC.AceTheme.Dark.subtheme("capsule", "capsule");
--- themes/ace/src/theme.js
+++ themes/ace/src/theme.js
@@ -1,10 +1,12 @@
 SC.AceTheme = SC.EmptyTheme.extend({
   name: "Ace",
   description: "Coolness.",
-  classNames: ["ace", "light"]
+  classNames: ["ace"]
 });
 
 SC.Theme.register("sc-ace", SC.AceTheme);
+
+SC.AceTheme.Dark = SC.AceTheme.subtheme("dark", "dark");
 
 // until SC build tools automatically do this:
 SC.Pane.prototype.baseTheme = "sc-ace";
