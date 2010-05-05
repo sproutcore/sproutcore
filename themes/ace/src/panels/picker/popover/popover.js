411af67bfe8b02bb00689c8efbeac744cc343b5e
Alex Iskander <alex@tpsitulsa.com>
2010-05-05 11:48:54 -0500

    Start on dark theme.

--- themes/ace/src/panels/picker/popover/popover.js
+++ themes/ace/src/panels/picker/popover/popover.js
@@ -1,3 +1,5 @@
-SC.AceTheme.Popover = SC.AceTheme.subtheme("popover", "popover");
+SC.AceTheme.Popover = SC.AceTheme.Dark.subtheme("popover", "popover");
+SC.AceTheme.register("popover", SC.AceTheme.Popover);
+
 SC.AceTheme.SolidPopover = SC.AceTheme.Popover.subtheme("solid", "solid");
 SC.AceTheme.register("solid-popover", SC.AceTheme.SolidPopover);
