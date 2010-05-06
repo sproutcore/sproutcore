63b9a336f6c7b3277df5aeede9b17d11687c5ee7
Alex Iskander <alex@tpsitulsa.com>
2010-05-06 00:47:01 -0500

    Add other slider sizes to test controls.

--- apps/test_controls/resources/sliders_page.js
+++ apps/test_controls/resources/sliders_page.js
@@ -11,18 +11,37 @@ TestControls.slidersPage = SC.View.design({
   form: SC.FormView.design({
     classNames: ["sample_controls"],
     layout: { left: 20, top: 40, right: 20, bottom: 40 },
-    childViews: "header normal disabled".w(),
+    childViews: "header small normal jumbo disabled".w(),
+    // Plain Views
     header: SC.LabelView.design({
-      classNames: "header".w(),
-      value: "Radio Views"
+      layout: { width: 250, height: 18 },
+      value: "Sliders",
+      classNames: "header".w()
     }),
     
-    normal: SC.FormView.row(SC.SliderView.design({
-      layout: { width: 150, height: 16},
+    small: SC.FormView.row(SC.SliderView.design({
+      layout: { width: 150, height: 14 },
+      controlSize: SC.SMALL_CONTROL_SIZE,
       minimum: 0,
       maximum: 10,
       value: 2
     })),
+    
+    normal: SC.FormView.row(SC.SliderView.design({
+      layout: { width: 150, height: 16 },
+      minimum: 0,
+      maximum: 10,
+      value: 2
+    })),
+    
+    jumbo: SC.FormView.row(SC.SliderView.design({
+      controlSize: SC.JUMBO_CONTROL_SIZE,
+      layout: { width: 150, height: 22 },
+      minimum: 0,
+      maximum: 10,
+      value: 2
+    })),
+    
     disabled: SC.FormView.row(SC.SliderView.design({
       layout: { width: 150, height: 16},
       isEnabled: NO,
