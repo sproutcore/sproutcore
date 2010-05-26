dd018d67bd3977776a57346a82d14ae28875db0a
Peter Wagenet <peter.wagenet@gmail.com>
2010-05-25 18:52:20 -0700

    Fixed ScrollView pinchToScale to the proper pinchToZoom

--- frameworks/desktop/views/scroll.js
+++ frameworks/desktop/views/scroll.js
@@ -1039,7 +1039,7 @@ SC.ScrollView = SC.View.extend(SC.Border, {
       
       startScale: this._scale,
       startDistance: avg.d,
-      canScale: this.get("canScale") && SC.platform.pinchToScale,
+      canScale: this.get("canScale") && SC.platform.pinchToZoom,
       minimumScale: this.get("minimumScale"),
       maximumScale: this.get("maximumScale"),
       
