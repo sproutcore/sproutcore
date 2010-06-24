29f162c9ce5d42f3ff680c5bf481762ac5ae5f00
William Kakes <bkakes@funke.apple.com>
2010-06-24 15:56:04 -0700

    Apply the optimizations from 751d8e40057d036547e3914429ff3d377754c242 to SC.Pane's overridden recomputeIsVisibleInWindow(), too.

--- frameworks/foundation/panes/pane.js
+++ frameworks/foundation/panes/pane.js
@@ -182,7 +182,7 @@ SC.Pane = SC.View.extend(SC.ResponderContext,
   /**
     Attempts to send the event down the responder chain for this pane.  If you 
     pass a target, this method will begin with the target and work up the 
-    responder chain.  Otherwise, it will begin with the current firstResponder 
+    responder chain.  Otherwise, it will begin with the current rr 
     and walk up the chain looking for any responder that implements a handler 
     for the passed method and returns YES when executed.
     
@@ -726,7 +726,14 @@ SC.Pane = SC.View.extend(SC.ResponderContext,
     // If our visibility has changed, then set the new value and notify our
     // child views to update their value.
     if (previous !== current) {
-      this.set('isVisibleInWindow', current) ;
+      this.set('isVisibleInWindow', current);
+
+      // Update the layer if we need to.  We'll pass in force=YES to bypass
+      // the "only update it if it's visible in the window" check, because we
+      // want to update it to reflect the new visibility (for example, if it
+      // was just marked as isVisible=NO, we need to add the 'hidden' class).
+      this.updateLayerIfNeeded(YES);
+
       
       var childViews = this.get('childViews'), len = childViews.length, idx;
       for(idx=0;idx<len;idx++) {
@@ -734,30 +741,18 @@ SC.Pane = SC.View.extend(SC.ResponderContext,
       }
 
 
-      // We'll also kick off some necessary work when the visibility changes.
-      // This more appropriately belongs in a 'isVisibleInWindow' observer or
-      // some such helper method because this work is not strictly related to
-      // computing the visibility, but view performance is critical, so
-      // avoiding the extra observer is worthwhile.
+      // For historical reasons, we'll also layout the child views if
+      // necessary.
       if (current) {
-        // If we just became visible, update layer + layout if needed...
-        this.displayDidChange();
-
-        if (this.get('childViewsNeedLayout')) {
-          this.invokeOnce(this.layoutChildViewsIfNeeded);
-        }
+        if (this.get('childViewsNeedLayout')) this.invokeOnce(this.layoutChildViewsIfNeeded);
       }
       else {
-        // We need to set some internal state to indicate that we went from
-        // visible to invisible.
-        //
-        // (Typically, invisible views are not updated for performance
-        // reasons, but if we went from visible to invisible, we need to
-        // update once.)
-        this._forceLayerUpdateDueToVisibilityChange = YES;
-        this.displayDidChange();
-
-        // Also, if we were previously 'firstResponder', resign it.
+        // Also, if we were previously visible and were the first responder,
+        // resign it.  This more appropriately belongs in a
+        // 'isVisibleInWindow' observer or some such helper method because
+        // this work is not strictly related to computing the visibility, but
+        // view performance is critical, so avoiding the extra observer is
+        // worthwhile.
         if (this.get('isFirstResponder')) this.resignFirstResponder();
       }
     }
