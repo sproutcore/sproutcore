fadd07bbb9809766708b1fe31086e9f5ff6cfa0f
Juan Pinzon <juanpin@me.com>
2010-07-27 11:07:04 -0700

    Add px units to only values that are numbers, if the value passed is a string it will be set as it comes

--- frameworks/foundation/system/render_context.js
+++ frameworks/foundation/system/render_context.js
@@ -388,8 +388,7 @@ SC.RenderContext = SC.Builder.create(/** SC.RenderContext.fn */ {
         if (!styles.hasOwnProperty(key)) continue ;
         value = styles[key];
         if (value === null) continue; // skip empty styles
-
-        if (!isNaN(value) && !(key === "zIndex" || key === "font-weight")) value += "px";
+        if (typeof start === SC.T_NUMBER && key !== "zIndex") value += "px";
         pair[0] = this._dasherizeStyleName(key);
         pair[1] = value;
         joined.push(pair.join(': '));
@@ -452,7 +451,7 @@ SC.RenderContext = SC.Builder.create(/** SC.RenderContext.fn */ {
           if(!styles.hasOwnProperty(key)) continue ;
           value = styles[key];
           if (value === null) continue; // skip empty styles
-          if (!isNaN(value) && !(key === "zIndex" || key === "font-weight")) value += "px";
+          if (!isNaN(value) && key !== "zIndex") value += "px";
 
           pair[0] = this._dasherizeStyleName(key);
           pair[1] = value;
