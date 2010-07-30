dc2a9a969887af1fff836fdda60f487308de4c8d
Alex Iskander <alex@sproutcore.com>
2010-07-29 18:23:15 -0700

    Fix a case in which we would not get a buffered jquery instance.

--- frameworks/foundation/renderers/renderer.js
+++ frameworks/foundation/renderers/renderer.js
@@ -91,7 +91,7 @@ SC.Renderer = {
     var ret, layer = this.layer();
     // note: SC.$([]) returns an empty CoreQuery object.  SC.$() would 
     // return an object selecting the document.
-    ret = !layer ? SC.$([]) : (sel === undefined) ? SC.$.buffer(layer) : SC.$.buffer(sel, layer) ;
+    ret = !layer ? SC.$.buffer([]) : (sel === undefined) ? SC.$.buffer(layer) : SC.$.buffer(sel, layer) ;
     layer = null ; // avoid memory leak
     return ret ;
   },
