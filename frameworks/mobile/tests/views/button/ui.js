0203fdc63b31a615210f4a2d6a3bb7e9e07bcb6a
Peter Wagenet <peter.wagenet@gmail.com>
2010-09-15 12:38:08 -0700

    Removed useless mobile framework

--- frameworks/mobile/tests/views/button/ui.js
+++
@@ -1,9 +0,0 @@
-// ==========================================================================
-// Project:   SproutCore - JavaScript Application Framework
-// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
-//            portions copyright @2009 Apple Inc.
-// License:   Licensed under MIT license (see license.js)
-// ==========================================================================
-
-/*global module test htmlbody ok equals same stop start */
-
