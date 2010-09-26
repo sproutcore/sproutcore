76a9b0d430b3d6a65910ac5d1c05c31e17d97dd4
Mike Ball <mike.ball3@gmail.com>
2010-09-25 23:51:07 -0400

    cleaning up old crap

--- apps/greenhouse/controllers/file.js
+++ apps/greenhouse/controllers/file.js
@@ -10,8 +10,6 @@
 
   @extends SC.ObjectController
 */
-Greenhouse.PAGE_DESIGNER = "pageDesigner";
-Greenhouse.BESPIN = "bespin";
 
 Greenhouse.fileController = SC.ObjectController.create(
 /** @scope Greenhouse.fileController.prototype */ {
@@ -33,47 +31,5 @@ Greenhouse.fileController = SC.ObjectController.create(
         Greenhouse.sendAction('fileSelectedIsNotAPage');
       });
     }
-  }.observes('*content.body'),
-  
-  state: null,
-  editorMode: '',
-  
-  // ..........................................................
-  // State information
-  // 
-  //TODO: Rip this crap out...
-  init: function(){
-    sc_super();
-    this.set('state', Greenhouse.PAGE_DESIGNER);
-    this.set('editorMode', "pageDesigner");
-    
-  },
-  
-  pageDesigner: function(){
-    var state = this.get('state');
-    switch(state){
-      case Greenhouse.BESPIN:
-        this.set('state', Greenhouse.PAGE_DESIGNER);
-        this.set('editorMode', "pageDesigner");
-        break;
-      default:
-        console.log("RedBull.fileController#pageDesigner not handled in current state %@".fmt(state));
-        break;
-    }
-  },
-  
-  bespinEditor: function(){
-    var state = this.get('state');
-    switch(state){
-      case Greenhouse.PAGE_DESIGNER:
-        this.set('state', Greenhouse.BESPIN);
-        this.set('editorMode', "bespinEditor");
-        break;
-      default:
-        console.log("RedBull.fileController#bespinEditor not handled in current state %@".fmt(state));
-        break;
-    }
-  }
-  
-  
+  }.observes('*content.body')  
 }) ;
