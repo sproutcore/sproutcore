6ac08f0287425dbc3e4bb22124a8646af0a889f6
Robert Buchholz <rbu@goodpoint.de>
2010-10-13 14:06:13 +0200

    Fix spelling in license header

--- frameworks/foundation/validators/date_time.js
+++ frameworks/foundation/validators/date_time.js
@@ -2,7 +2,7 @@
 // Project:   SproutCore - JavaScript Application Framework
 // Copyright: ©2006-2010 Sprout Systems, Inc. and contributors.
 //            Portions ©2008-2010 Apple Inc. All rights reserved.
-// License:   Licened under MIT license (see license.js)
+// License:   Licensed under MIT license (see license.js)
 // ==========================================================================
 
 require('validators/validator');
