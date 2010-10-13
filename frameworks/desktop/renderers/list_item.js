202f98eb7ebcc0e9acf5b934375f6582c77cbcbb
Robert Buchholz <rbu@goodpoint.de>
2010-10-13 14:06:13 +0200

    Fix spelling in license header

--- frameworks/desktop/renderers/list_item.js
+++ frameworks/desktop/renderers/list_item.js
@@ -2,7 +2,7 @@
 // Project:   SproutCore - JavaScript Application Framework
 // Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
 //            Portions ©2008-2009 Apple Inc. All rights reserved.
-// License:   Licened under MIT license (see license.js)
+// License:   Licensed under MIT license (see license.js)
 // ==========================================================================
 
 SC.LIST_ITEM_COUNT_DIGITS = ['zero', 'one', 'two', 'three', 'four', 'five'];
