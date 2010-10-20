e07e8b6a6e454a0148198da5711c7f329bf394dc
Yehuda Katz <wycats@Yehuda-Katzs-MacBook-Pro-2.local>
2010-10-19 20:12:05 -0400

    Whitespace and small style tweaks

--- frameworks/runtime/protocols/observable_protocol.js
+++ frameworks/runtime/protocols/observable_protocol.js
@@ -11,21 +11,21 @@
   work.
 */
 SC.ObservableProtocol = {
-  
-  /**  
-    Generic property observer called whenever a property on the receiver 
+
+  /**
+    Generic property observer called whenever a property on the receiver
     changes.
-    
+
     If you need to observe a large number of properties on your object, it
     is sometimes more efficient to implement this observer only and then to
-    handle requests yourself.  Although this observer will be triggered 
+    handle requests yourself.  Although this observer will be triggered
     more often than an observer registered on a specific property, it also
-    does not need to be registered which can make it faster to setup your 
+    does not need to be registered which can make it faster to setup your
     object instance.
-    
+
     You will often implement this observer using a switch statement on the
-    key parameter, taking appropriate action. 
-    
+    key parameter, taking appropriate action.
+
     @param observer {null} no longer used; usually null
     @param target {Object} the target of the change.  usually this
     @param key {String} the name of the property that changed
@@ -34,7 +34,7 @@ SC.ObservableProtocol = {
     @returns {void}
   */
   propertyObserver: function(observer,target,key,value, revision) {
-    
+
   }
-  
+
 };
