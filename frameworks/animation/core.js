ab46c2969fa9c278169a6f906764b4e4e51ce057
Peter Wagenet <peter.wagenet@gmail.com>
2010-10-22 21:15:28 -0700

    Animatable should check that transitions exist before using special case

--- frameworks/animation/core.js
+++ frameworks/animation/core.js
@@ -387,7 +387,8 @@ SC.Animatable = {
       // NOTE: This needs to match exactly the conditions in layoutStyles
       if (
         (SC.empty(nT) || (!SC.isPercentage(nT) && !SC.empty(nH))) &&
-        (SC.empty(nL) || (!SC.isPercentage(nL) && !SC.empty(nW)))
+        (SC.empty(nL) || (!SC.isPercentage(nL) && !SC.empty(nW))) &&
+        (this.transition && (this.transition['top'] || this.transition['left']))
       ) {
         specialTransform = YES;
         this._useSpecialCaseTransform = YES;
