a776e61f2d4258d79c65d73c780c8f0432b76f9d
Jonathan Lewis <jslewis1@gmail.com>
2010-10-29 12:38:19 -0400

    SC.Observable.addProbe() fix for iPad/iPhone -- console.log currently only accepts one argument.

--- frameworks/runtime/mixins/observable.js
+++ frameworks/runtime/mixins/observable.js
@@ -1328,7 +1328,7 @@ SC.Observable = {
 
 /** @private used by addProbe/removeProbe */
 SC.logChange = function logChange(target, key, value) {
-  console.log("CHANGE: %@[%@] =>".fmt(target, key), target.get(key));
+  console.log("CHANGE: %@[%@] => %@".fmt(target, key, target.get(key)));
 };
 
 /**
