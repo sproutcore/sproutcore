2c582f1d48781ceef2bac8a11fe45566deaacfeb
Tom Dale <tomdale@Tom-Dales-MacBook-Pro.local>
2010-11-10 14:33:50 -0800

    Add documentation for ariaRole property.

--- frameworks/foundation/views/view.js
+++ frameworks/foundation/views/view.js
@@ -1400,9 +1400,20 @@ SC.View = SC.Responder.extend(SC.DelegateSupport,
     @property {String}
   */
   tagName: 'div',
-  
+
+  /**
+    The WAI-ARIA role of the control represented by this view. For example, a
+    button may have a role of type 'button', or a pane may have a role of
+    type 'alertdialog'. This property is used by assistive software to help
+    visually challenged users navigate rich web applications.
+
+    The full list of valid WAI-ARIA roles is available at:
+    http://www.w3.org/TR/wai-aria/roles#roles_categorization
+
+    @property {String}
+  */
   ariaRole: null,
-  
+
   /**
     Standard CSS class names to apply to the view's outer element.  This 
     property automatically inherits any class names defined by the view's
