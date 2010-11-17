9356051ea3dd72f49aa79a93b39d818c059a462a
Alex Iskander <alexiskander@me.com>
2010-11-16 22:38:41 -0600

    Add docs for SC.AutoMixin.

--- frameworks/foundation/mixins/auto_mixin.js
+++ frameworks/foundation/mixins/auto_mixin.js
@@ -8,16 +8,22 @@
 /**
   @namespace 
 
-  A mixin that automatically mixes in a set of mixins into the child views created
-  by this view (that go through createChildView).
+  Use this mixin to automatically mix in a a collection of mixins into all
+  child views created _by the view_ (that are created at view initialization).
+  
   @since SproutCore 1.0
 */
 SC.AutoMixin = {
   /**
     The mixins to automatically mix in.
+    @property
   */
   autoMixins: [],
   
+  /**
+    @private
+    Override createChildViews to mix in the mixins defined in autoMixins.
+  */
   createChildView: function(v, attrs) {
     if (!attrs) attrs = {};
     attrs.owner = attrs.parentView = this;
