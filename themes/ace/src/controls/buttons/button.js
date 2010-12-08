8d0a707a7b154dcdaaad132e0ba1d7ea263b7051
Alex Iskander <alexiskander@me.com>
2010-12-08 00:02:26 -0600

    Convert Ace Button Renderer -> RenderDelegate.

--- themes/ace/src/controls/buttons/button.js
+++ themes/ace/src/controls/buttons/button.js
@@ -1,23 +1,131 @@
+
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2009 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+
 require("src/theme");
 
 SC.AceTheme.PointLeft = SC.AceTheme.subtheme("point-left", "point-left");
 SC.AceTheme.PointRight = SC.AceTheme.subtheme("point-right", "point-right");
-SC.AceTheme.PointRight = SC.AceTheme.subtheme("capsule", "capsule");
+SC.AceTheme.Capsule = SC.AceTheme.subtheme("capsule", "capsule");
 
-SC.AceTheme.Button = SC.BaseTheme.Button.extend({
-  renderContents: function(context) {
-    // render background slices
-    context.push("<span class='button-left'></span>");
-    // render inner html
-    context.push("<span class='button-middle'>");
+/**
+  Renders and updates the HTML representation of SC.ButtonView.
+*/
+SC.AceTheme.buttonRenderDelegate = SC.Object.create({
+  name: 'button',
+  
+  /**
+    Called when we need to create the HTML that represents the button.
+
+    @param {SC.Object} dataSource the object containing the information on how to render the button
+    @param {SC.RenderContext} context the render context instance
+  */
+  render: function(dataSource, context) {
+    var displayProperties = dataSource.getDisplayProperties();
+    var minWidth          = displayProperties.titleMinWidth;
+    var labelContent;
     
-    context = context.begin("label").addClass("sc-button-label");
-    this._titleRenderer.render(context);
+    
+    context.setClass('def', displayProperties.isDefault);
+    context.setClass('cancel', displayProperties.isCancel);
+    
+    dataSource.get('theme').slicesRenderDelegate.render(SC.THREE_SLICE, context);
+
+    // Add an icon class name to the button if it contains an icon in its
+    // title.
+    context.setClass('icon', !!displayProperties.icon);
+
+    // Create the inner label element that contains the text and, optionally,
+    // an icon.
+    context = context.begin('label').addClass('sc-button-label');
+    labelContent = this._htmlForTitleAndIcon(displayProperties);
+    context.push(labelContent);
+
+    // By adding the 'ellipsis' class, the text-overflow: ellipsis CSS
+    // rule will be applied.
+    if (displayProperties.needsEllipsis){
+      context.addClass('ellipsis');
+    }
+
     context = context.end();
+
+    if (displayProperties.supportFocusRing) {
+      context.push('<div class="focus-ring">',
+                    '<div class="focus-left"></div>',
+                    '<div class="focus-middle"></div>',
+                    '<div class="focus-right"></div></div>');
+    }
+  },
+
+  /**
+    Called when one or more display properties have changed and we need to
+    update the HTML representation with the new values.
+
+    @param {SC.Object} dataSource the object containing the information on how to render the button
+    @param {SC.RenderContext} jquery the jQuery object representing the HTML representation of the button
+  */
+  update: function(dataSource, jquery) {
+    var displayProperties = dataSource.getChangedDisplayProperties(),
+        allDisplayProperties = dataSource.getDisplayProperties();
+
+    if (allDisplayProperties.isActive) {
+      jquery.addClass('active');
+    }
     
-    context.push('</span>');
-    context.push("<span class='button-right'></span>");
+    jquery.setClass('def', allDisplayProperties.isDefault);
+    jquery.setClass('cancel', allDisplayProperties.isCancel);
+
+    if (displayProperties.contains('title', 'isActive')) {
+      jquery.find('label').html(this._htmlForTitleAndIcon(allDisplayProperties));
+    }
+  },
+
+  /**
+    Returns the HTML for the button's label, which can include a title and
+    an icon.
+
+    @param {Object} displayProperties the object containing the properties needed to generate the label
+    @returns {String} a string of HTML
+  */
+  _htmlForTitleAndIcon: function(displayProperties) {
+    var title = displayProperties.title,
+        titleMinWidth = displayProperties.titleMinWidth,
+        hint = displayProperties.hint,
+        escapeHTML = displayProperties.escapeHTML,
+        icon = displayProperties.icon || '';
+
+    // Escape the title of the button if needed. This prevents potential
+    // XSS attacks.
+    if (title && escapeHTML) {
+      title = SC.RenderContext.escapeHTML(title) ;
+    }
+
+    if (hint && !title) {
+      if (escapeHTML) {
+        hint = SC.RenderContext.escapeHTML(hint);
+      }
+      title = "<span class='sc-hint'>" + hint + "</span>";
+    }
+
+    if (icon) {
+      // If the icon property is the path to an image, create an image tag
+      // that points to that URL.
+      if (icon.indexOf('/') >= 0) {
+        icon = '<img src="'+icon+'" alt="" class="icon" />';
+
+      // Otherwise, the icon property is a class name that should be added
+      // to the image tag. Display a blank image so that the user can add
+      // background image using CSS.
+      } else {
+        icon = '<img src="'+SC.BLANK_IMAGE_URL+'" alt="" class="'+icon+'" />';
+      }
+    }
+
+    return icon+title;
   }
 });
-
-SC.AceTheme.addRenderer(SC.AceTheme.Button);
