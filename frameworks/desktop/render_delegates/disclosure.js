dd072459780f6da60c1e09e87ac8a66cad583829
Alex Iskander <alexiskander@me.com>
2010-12-10 17:09:30 -0600

    Make real disclosure render delegate.

--- frameworks/desktop/render_delegates/disclosure.js
+++ frameworks/desktop/render_delegates/disclosure.js
@@ -5,7 +5,33 @@
 // License:   Licened under MIT license (see license.js)
 // ==========================================================================
 
-SC.BaseTheme.disclosureRenderDelegate = SC.RendererRenderDelegate.create({
-  rendererName: 'disclosure'
+SC.BaseTheme.disclosureRenderDelegate = SC.Object.create({
+  name: 'disclosure',
+  
+  render: function(dataSource, context) {
+    var theme = dataSource.get('theme');
+    
+    var state = '';
+    state += dataSource.get('isSelected') ? 'open' : 'closed';
+    if (dataSource.get('isActive')) state += ' active';
+    
+    context.push('<img src = "' + SC.BLANK_IMAGE_URL + '" class = "disclosure button ' + state + '" />');
+    
+    context = context.begin('span').addClass('sc-button-label');
+    theme.labelRenderDelegate.render(dataSource, context);
+    context = context.end();
+  },
+  
+  update: function(dataSource, jquery) {
+    var theme = dataSource.get('theme');
+
+    jquery.find('img').setClass({
+      open: dataSource.get('isSelected'),
+      closed: !dataSource.get('isSelected'),
+      active: dataSource.get('isActive')
+    });
+    
+    theme.labelRenderDelegate.update(dataSource, jquery.find('span.sc-button-label'));
+  }
 });
 
