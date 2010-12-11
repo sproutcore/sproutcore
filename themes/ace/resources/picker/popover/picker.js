b122c350513dded28d2786027855f8b51024e7ce
Alex Iskander <alexiskander@me.com>
2010-12-10 23:59:33 -0600

    Prepare to work on popover.

---
+++ themes/ace/resources/picker/popover/picker.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2009 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+sc_require("resources/picker/popover/popover");
+
+SC.AceTheme.Popover.pickerRenderDelegate = SC.Object.create({
+  name: 'picker',
+  
+  render: function() {
+    
+  },
+  
+  update: function() {
+    
+  }
+});
