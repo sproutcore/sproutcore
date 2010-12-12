db9a198ca085d64e72844af76b8ee7141132ac8a
Alex Iskander <alexiskander@me.com>
2010-12-11 22:47:14 -0600

    Convert well and tabview styling.

---
+++ themes/ace/resources/well/well.js
@@ -0,0 +1,19 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2009 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+SC.AceTheme.Well = SC.AceTheme.subtheme('well');
+
+SC.AceTheme.Well.containerRenderDelegate = SC.Object.create({
+
+  render: function(dataSource, context) {
+    
+  },
+  
+  update: function() {
+
+  }
+});
