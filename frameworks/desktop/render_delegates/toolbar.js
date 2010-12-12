005d787ce806784f02588a2c653e59b51c6a5663
Alex Iskander <alexiskander@me.com>
2010-12-11 22:25:17 -0600

    Add toolbar render delegate.

---
+++ frameworks/desktop/render_delegates/toolbar.js
@@ -0,0 +1,18 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2009 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2009 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+SC.BaseTheme.toolbarRenderDelegate = SC.Object.create({
+  name: 'toolbar',
+
+  render: function(dataSource, context) {
+    // toolbar has nothing in it
+  },
+  
+  update: function() {
+    // toolbar has nothing to update
+  }
+});
