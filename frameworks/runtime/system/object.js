e619701cf265fa19330829f490c18543d9184abb
Emrys Ingersoll <ingersoll@gmail.com>
2010-12-13 13:34:43 -0500

    [#128] BUGFIX: Disconnect bindings when object is destroyed

--- frameworks/runtime/system/object.js
+++ frameworks/runtime/system/object.js
@@ -534,6 +534,10 @@ SC.Object.prototype = {
     var idx, inits = this.destroyMixin, len = (inits) ? inits.length : 0 ;
     for(idx=0;idx < len; idx++) inits[idx].call(this);
 
+    // disconnect all bindings
+    this.bindings.invoke('disconnect');
+    this.bindings = null;
+
     return this ;
   },
 
