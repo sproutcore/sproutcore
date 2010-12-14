ff0c1550775f94616b26d49250d9366a9a5e77a8
William Kakes <wkakes@apple.com>
2010-12-13 16:11:25 -0800

    Enhance the logging system, with 2 major new benefits:
    1.  Recording.  All log messages can be optionally recorded.  (And the recorded messages can be output to the screen or stringified for reporting on demand.)  Recording is off by default.
    
    2.  Dynamic logging levels.  Before, each logging message could be specified as debug, info, warn, or error, but all would be output.  Now, there is a setting to determine which levels (and above) will be output/recorded.  So, for example, you could say that in development mode you want to see all 'debug' messages, but only show 'warn' or above in production mode.
    
    Miscellaneous notes:
    
      *  The debug/info/warn/error (and related) methods automatically call String.fmt() where appropriate for convenience, performance, and code size / readability.  (Current log statements tend to call String.fmt() themselves, but with the new levels, it’s possible for this to be a needless expense if the levels are such that the result will never be used.)
    
      *  The various logging methods no longer return YES/NO depending on whether anything was actually logged because that’s not a well-defined result when a message could be logged, recorded, or both.  (The biggest prior benefit seemed to be for unit testing.)  I did leave it in for the various profile-type functions, though, because I can conceive of code trying to use those but falling back to SC.Benchmark or something when they’re not available.
    
      *  'fallBackOnLog' has been removed, and the behavior is now always as if it were YES.
    
      *  'format' has been removed.  When stringifying recorded log messages, the behavior is always like when it was YES, and is like when it was NO elsewhere.
    
      *  'debugEnabled' is now deprecated, although for backwards compatibility it will set the 'logOutputLevel' to 'debug'.

--- frameworks/runtime/tests/system/logger.js
+++ frameworks/runtime/tests/system/logger.js
@@ -42,6 +42,38 @@ module("SC.Logger", {
   }
 });
 
+
+// We'll use these a lot.
+var debugMessage    = "Test debug message",
+    infoMessage     = "Test informational message",
+    warnMessage     = "Test warning message",
+    errorMessage    = "Test error message",
+    debugGroupTitle = "Test debug group title",
+    infoGroupTitle  = "Test informational group title",
+    warnGroupTitle  = "Test warning group title",
+    errorGroupTitle = "Test error group title";
+
+var outputAll = function() {
+  // Outputs a log message for each possible level.
+  SC.Logger.debugGroup(debugGroupTitle);
+  SC.Logger.debug(debugMessage);
+  SC.Logger.debugGroupEnd();
+
+  SC.Logger.infoGroup(debugGroupTitle);
+  SC.Logger.info(infoMessage);
+  SC.Logger.infoGroupEnd();
+
+  SC.Logger.warnGroup(debugGroupTitle);
+  SC.Logger.warn(warnMessage);
+  SC.Logger.warnGroupEnd();
+
+  SC.Logger.errorGroup(debugGroupTitle);
+  SC.Logger.error(errorMessage);
+  SC.Logger.errorGroupEnd();
+};
+    
+
+
 test("exists", function() {
   equals(SC.Logger.get('exists'), true, "Reporter does exist check");
   
@@ -49,124 +81,39 @@ test("exists", function() {
   equals(SC.Logger.get('exists'), false, "Reporter does not exist check");
 });
 
-test("log", function() {
-  equals(SC.Logger.log("test"), true, "Function is defined" );
-  
-  var reporter = SC.Logger.get('reporter');
-  reporter.log = null;
-  equals(SC.Logger.log("test"), false, "Function is null");
-  
-  SC.Logger.fallBackOnAlert = true;
-  equals(SC.Logger.log("test"), true, "Function is null -- fallBackOnAlert true");
-});
-
-test("debug", function() {
-  equals(SC.Logger.debug("Debug"), true, "Function is defined");
-
-  SC.Logger.get('reporter').debug = null;
-  equals(SC.Logger.debug("Debug"), true, "Function is null -- fallBackOnLog true");
-
-  SC.Logger.fallBackOnLog = false;
-  equals(SC.Logger.debug("Debug"), false, "Function is null -- fallBackOnLog false");
-});
-
-test("dir", function() {
-  equals(SC.Logger.dir({test:"string"}), true, "Function is defined");
-  
-  SC.Logger.get('reporter').dir = null;
-  equals(SC.Logger.dir({test:"string"}), true, "Function is null -- fallBackOnLog true");
-  
-  SC.Logger.fallBackOnLog = false;
-  equals(SC.Logger.dir({test:"string"}), false, "Function is null -- fallBackOnLog false");
-});
-
-test("dirxml", function() {
-  equals(SC.Logger.dirxml("<here id='2'>is some XML</here>"), true, "Function is defined");
-  
-  SC.Logger.get('reporter').dirxml = null;
-  equals(SC.Logger.dirxml("<here id='2'>is some XML</here>"), true, "Function is null -- fallBackOnLog true");
-  
-  SC.Logger.fallBackOnLog = false;
-  equals(SC.Logger.dirxml("<here id='2'>is some XML</here>"), false, "Function is null -- fallBackOnLog false");
-});
-
-test("error", function() {
-  equals(SC.Logger.error("Error"), true, "Function is defined");
-  
-  SC.Logger.get('reporter').error = null;
-  equals(SC.Logger.error("Error"), true, "Function is null -- fallBackOnLog true");
-
-  SC.Logger.fallBackOnLog = false;
-  equals(SC.Logger.error("Error"), false, "Function is null -- fallBackOnLog false");
-});
-
-test("group", function() {
-  equals(SC.Logger.group("mygroup"), true, "Function is defined");
-  
-  SC.Logger.get('reporter').group = null;
-  equals(SC.Logger.group("mygroup"), false, "Function is null");
-});
-
-test("groupEnd", function() {
-  equals(SC.Logger.groupEnd("mygroup"), true, "Function is defined");
-  
-  SC.Logger.get('reporter').groupEnd = null;
-  equals(SC.Logger.groupEnd("mygroup"), false, "Function is null");
-});
-
-test("info", function() {
-  equals(SC.Logger.info("Info"), true, "Function is defined");
-  
-  SC.Logger.get('reporter').info = null;
-  equals(SC.Logger.info("Info"), true, "Function is null -- fallBackOnLog true");
-
-  SC.Logger.fallBackOnLog = false;
-  equals(SC.Logger.info("Info"), false, "Function is null -- fallBackOnLog false");
-});
-
 test("profile", function() {
-  equals(SC.Logger.profile(), true, "Function is defined");
+  equals(SC.Logger.profile(), true, "profile() function is defined");
   
   SC.Logger.get('reporter').profile = null;
-  equals(SC.Logger.profile(), false, "Function is null");
+  equals(SC.Logger.profile(), false, "profile() function is null");
 });
 
 test("profileEnd", function() {
-  equals(SC.Logger.profileEnd(), true, "Function is defined");
+  equals(SC.Logger.profileEnd(), true, "profileEnd() function is defined");
   
   SC.Logger.get('reporter').profileEnd = null;
-  equals(SC.Logger.profileEnd(), false, "Function is null");
+  equals(SC.Logger.profileEnd(), false, "profileEnd() function is null");
 });
 
 test("time", function() {
-  equals(SC.Logger.time('mytime'), true, "Function is defined");
+  equals(SC.Logger.time('mytime'), true, "time() function is defined");
   
   SC.Logger.get('reporter').time = null;
-  equals(SC.Logger.time('mytime'), false, "Function is null");
+  equals(SC.Logger.time('mytime'), false, "time() function is null");
 });
 
 test("timeEnd", function() {
-  equals(SC.Logger.timeEnd('mytime'), true, "Function is defined");
+  equals(SC.Logger.timeEnd('mytime'), true, "timeEnd() function is defined");
   
   SC.Logger.get('reporter').timeEnd = null;
-  equals(SC.Logger.timeEnd('mytime'), false, "Function is null");
+  equals(SC.Logger.timeEnd('mytime'), false, "timeEnd() function is null");
 });
 
 test("trace", function() {
-  equals(SC.Logger.trace(), true, "Function is defined");
+  equals(SC.Logger.trace(), true, "trace() function is defined");
   
   SC.Logger.get('reporter').trace = null;
-  equals(SC.Logger.trace(), false, "Function is null");
-});
-
-test("warn", function() {
-  equals(SC.Logger.warn("Warn"), true, "Function is defined");
-  
-  SC.Logger.get('reporter').warn = null;
-  equals(SC.Logger.warn("Warn"), true, "Function is null -- fallBackOnLog true");
-
-  SC.Logger.fallBackOnLog = false;
-  equals(SC.Logger.warn("Warn"), false, "Function is null -- fallBackOnLog false");
+  equals(SC.Logger.trace(), false, "trace() function is null");
 });
 
 test("_argumentsToString", function() {
@@ -174,4 +121,88 @@ test("_argumentsToString", function() {
   
   SC.LOGGER_LOG_DELIMITER = "|";
   equals(SC.Logger._argumentsToString.apply(SC.Logger, ["test", "test2"]), "test|test2", "Formatting using custom delimiter");
+});
+
+
+// ..........................................................
+// LOG LEVELS
+//
+// Since we can't really test whether or not things are output to the console,
+// we'll do all of our log level testing based on the log recording mechanism.
+//
+// In case anybody else has recorded a log message, all of these tests will
+// start out by clearing the recorded log messages array.
+
+test("Ensure that log levels function properly:  none", function() {
+  SC.Logger.set('recordedLogMessages', null);
+
+  SC.Logger.set('logRecordingLevel', SC.LOGGER_LEVEL_NONE);
+  outputAll();
+
+  // If it was null before, it should be still be null, since no messages
+  // should have been logged.
+  equals(SC.Logger.get('recordedLogMessages'), null, "recordedLogMessages remains null");
+
+  // If it was an empty array before, it should still be an empty array.
+  SC.Logger.set('recordedLogMessages', []);
+  outputAll();
+  equals(SC.Logger.getPath('recordedLogMessages.length'), 0, "recordedLogMessages remains an empty array");
+});
+
+test("Ensure that log levels function properly:  debug", function() {
+  SC.Logger.set('recordedLogMessages', null);
+
+  SC.Logger.set('logRecordingLevel', SC.LOGGER_LEVEL_DEBUG);
+  outputAll();
+
+  // All four messages (plus group begin / end directives) should have been
+  // logged.
+  equals(SC.Logger.getPath('recordedLogMessages.length'), 12, "recordedLogMessages should have all twelve entries");
+
+  equals(SC.Logger.getPath('recordedLogMessages.1').message, debugMessage,  "recordedLogMessages[1] should be the debug message");
+  equals(SC.Logger.getPath('recordedLogMessages.4').message, infoMessage,   "recordedLogMessages[4] should be the info message");
+  equals(SC.Logger.getPath('recordedLogMessages.7').message, warnMessage,   "recordedLogMessages[7] should be the warn message");
+  equals(SC.Logger.getPath('recordedLogMessages.10').message, errorMessage, "recordedLogMessages[10] should be the error message");  
+});
+
+test("Ensure that log levels function properly:  info", function() {
+  SC.Logger.set('recordedLogMessages', null);
+
+  SC.Logger.set('logRecordingLevel', SC.LOGGER_LEVEL_INFO);
+  outputAll();
+
+  // Three messages (plus group begin / end directives) should have been
+  // logged.
+  equals(SC.Logger.getPath('recordedLogMessages.length'), 9, "recordedLogMessages should have nine entries");
+
+  equals(SC.Logger.getPath('recordedLogMessages.1').message, infoMessage,  "recordedLogMessages[1] should be the info message");
+  equals(SC.Logger.getPath('recordedLogMessages.4').message, warnMessage,  "recordedLogMessages[4] should be the warn message");
+  equals(SC.Logger.getPath('recordedLogMessages.7').message, errorMessage, "recordedLogMessages[7] should be the error message");
+});
+
+test("Ensure that log levels function properly:  warn", function() {
+  SC.Logger.set('recordedLogMessages', null);
+
+  SC.Logger.set('logRecordingLevel', SC.LOGGER_LEVEL_WARN);
+  outputAll();
+
+  // Two messages (plus group begin / end directives) should have been logged.
+  equals(SC.Logger.getPath('recordedLogMessages.length'), 6, "recordedLogMessages should have 6 entries");
+
+  equals(SC.Logger.getPath('recordedLogMessages.1').message, warnMessage,  "recordedLogMessages[1] should be the warn message");
+  equals(SC.Logger.getPath('recordedLogMessages.4').message, errorMessage, "recordedLogMessages[4] should be the error message");
+});
+
+test("Ensure that log levels function properly:  error", function() {
+  SC.Logger.set('recordedLogMessages', null);
+
+  SC.Logger.set('logRecordingLevel', SC.LOGGER_LEVEL_ERROR);
+  outputAll();
+
+  // Only the error message (plus group begin / end directives) should have
+  // been logged.
+  equals(SC.Logger.getPath('recordedLogMessages.length'), 3, "recordedLogMessages should have three entries");
+
+  // That message should be equal to the error message.
+  equals(SC.Logger.getPath('recordedLogMessages.1').message, errorMessage, "recordedLogMessages[1] should be the error message");
 });
