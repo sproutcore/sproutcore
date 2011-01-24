c25c99eed82346d7e0405f2c450568140bb2fa6f
wycats <wycats@gmail.com>
2011-01-24 15:22:28 -0800

    white space and style

--- frameworks/amber/tests/controllers/array/single_case.js
+++ frameworks/amber/tests/controllers/array/single_case.js
@@ -10,23 +10,23 @@
 var content, controller, extra;
 
 var TestObject = SC.Object.extend({
-  title: "test",  
+  title: "test",
   toString: function() { return "TestObject(%@)".fmt(this.get("title")); }
 });
 
 
 // ..........................................................
 // allowsSingleContent
-// 
+//
 
 module("SC.ArrayController - single_case - SINGLE", {
   setup: function() {
     content = TestObject.create({ title: "FOO" });
-    controller = SC.ArrayController.create({ 
+    controller = SC.ArrayController.create({
       content: content, allowsSingleContent: YES
     });
   },
-  
+
   teardown: function() {
     controller.destroy();
   }
@@ -50,9 +50,9 @@ test("removeObject - no destroyOnRemoval", function() {
   var callCount = 0;
   controller.set('destroyOnRemoval', NO);
   controller.addObserver('[]', function() { callCount++; });
-  
+
   controller.removeObject(content);
-  
+
   equals(controller.get('content'), null, 'removeObject(content) should set content to null');
   equals(callCount, 1, 'should notify observer since content did not change');
   equals(controller.get('length'), 0, 'should now have length of 0');
@@ -84,16 +84,16 @@ test("arrangedObjects", function() {
 
 // ..........................................................
 // allowsSingleContent=NO
-// 
+//
 
 module("SC.ArrayController - single_case - allowsSingleContent=NO", {
   setup: function() {
     content = TestObject.create({ title: "FOO" });
-    controller = SC.ArrayController.create({ 
+    controller = SC.ArrayController.create({
       content: content, allowsSingleContent: NO
     });
   },
-  
+
   teardown: function() {
     controller.destroy();
   }
