5c2ab813596e2eff6f33a4afa650df9313f140b2
Mike Ball <mike.ball3@gmail.com>
2011-02-02 22:40:02 -0500

    more work on list item

--- apps/greenhouse/views/plist_item.js
+++ apps/greenhouse/views/plist_item.js
@@ -8,45 +8,55 @@
   This class is here to receive custom editor events
   @extends SC.View
 */
-Greenhouse.PlistItemView = SC.View.extend(
+Greenhouse.PlistItemView = SC.View.extend(SC.Control,
 /** @scope Greenhouse.ListItem.prototype */ {
  
- // contentValueKey: 'key',
- // 
+  classNames: ['sc-list-item-view'],
+ 
+ 
+  contentValueKey: 'key',
+ 
+  _valueStyle: {position: 'absolute', right: 5, top: '50%', height: 18, marginTop: -9},
+ 
   render: function(context, firstTime){
     var content = this.get('content'),
-       key, value;
-    console.log(content);
+       key, propertyKey, value, displayValue;
+    
     if(firstTime){
       // handle label -- always invoke
       key = this.get('contentValueKey');
-      value = (key && content) ? (content.get ? content.get(key) : content[key]) : content ;
-
+      propertyKey = (key && content) ? (content.get ? content.get(key) : content[key]) : content ;
+      value = content.get('value');
+      
+      //do the label
+      context.begin('label').addStyle({paddingLeft: 5}).text(propertyKey).end();
       if(value){
        if(SC.typeOf(value) === SC.T_STRING){
- 
+          context.begin('span').addStyle(this._valueStyle).text(value).end();
        }
-       else if(SC.typeOf(value) === SC.T_BOOLEAN){
- 
+       else if(SC.typeOf(value) === SC.T_BOOL){
+         context.begin('span').addStyle(this._valueStyle).text("bool").end();
        }
        else{
          value = value.toString();
+         context.begin('span').addStyle(this._valueStyle).text(value).end();
+         
        }
       }
       else{
-       value = "";
+        context.begin('span').addStyle(this._valueStyle).text("unknown").end();
       }
-      value = value + ": " + content.get('value');
-      context.push(value);
-      //this.renderLabel(context, value);
+      
     }
   },
   
-  renderValue: function(context, content){
-
-    context.begin('span')
-      .addStyle({left: '50%'})
-      .push(content.get('value'))
-    .end();
+  mouseDown: function(evt){
+    
+    return NO;
+  },
+  
+  mouseUp: function(evt){
+    
+    return NO;
   }
 });
