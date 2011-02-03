beba4882fa3ad78fbaf37d89cf570628502a57b2
wycats <wycats@gmail.com>
2011-02-02 19:21:15 -0800

    Move manipulation to its own file

---
+++ frameworks/amber/views/manipulation.js
@@ -0,0 +1,349 @@
+SC.View.reopen({
+  /**
+    Insert the view into the the receiver's childNodes array.
+
+    The view will be added to the childNodes array before the beforeView.  If
+    beforeView is null, then the view will be added to the end of the array.
+    This will also add the view's rootElement DOM node to the receivers
+    containerElement DOM node as a child.
+
+    If the specified view already belongs to another parent, it will be
+    removed from that view first.
+
+    @param {SC.View} view
+    @param {SC.View} beforeView
+    @returns {SC.View} the receiver
+  */
+  insertBefore: function(view, beforeView) {
+    view.beginPropertyChanges(); // limit notifications
+
+    // remove view from old parent if needed.  Also notify views.
+    if (view.get('parentView')) { view.removeFromParent() ; }
+    if (this.willAddChild) { this.willAddChild(view, beforeView) ; }
+    if (view.willAddToParent) { view.willAddToParent(this, beforeView) ; }
+
+    // set parentView of child
+    view.set('parentView', this);
+
+    // add to childView's array.
+    var idx, childViews = this.get('childViews') ;
+    if (childViews.needsClone) { this.set(childViews = []); }
+    idx = (beforeView) ? childViews.indexOf(beforeView) : childViews.length;
+    if (idx<0) { idx = childViews.length ; }
+    childViews.insertAt(idx, view) ;
+
+    // The DOM will need some fixing up, note this on the view.
+    view.parentViewDidChange() ;
+    view.layoutDidChange() ;
+    var pane = view.get('pane');
+    if(pane && pane.get('isPaneAttached')) {
+      view._notifyDidAppendToDocument();
+    }
+
+    // notify views
+    if (this.didAddChild) { this.didAddChild(view, beforeView) ; }
+    if (view.didAddToParent) { view.didAddToParent(this, beforeView) ; }
+
+    view.endPropertyChanges();
+
+    return this ;
+  },
+
+  /**
+    Removes the child view from the parent view.
+
+    @param {SC.View} view
+    @returns {SC.View} receiver
+  */
+  removeChild: function(view) {
+    if (!view) { return this; } // nothing to do
+    if (view.parentView !== this) {
+      throw "%@.removeChild(%@) must belong to parent".fmt(this,view);
+    }
+    // notify views
+    if (view.willRemoveFromParent) { view.willRemoveFromParent() ; }
+    if (this.willRemoveChild) { this.willRemoveChild(view) ; }
+
+    // update parent node
+    view.set('parentView', null) ;
+
+    // remove view from childViews array.
+    var childViews = this.get('childViews'),
+        idx = childViews.indexOf(view) ;
+    if (idx>=0) { childViews.removeAt(idx); }
+
+    // The DOM will need some fixing up, note this on the view.
+    view.parentViewDidChange() ;
+
+    // notify views
+    if (this.didRemoveChild) { this.didRemoveChild(view); }
+    if (view.didRemoveFromParent) { view.didRemoveFromParent(this) ; }
+
+    return this ;
+  },
+
+  /**
+    Removes all children from the parentView.
+
+    @returns {SC.View} receiver
+  */
+  removeAllChildren: function() {
+    var childViews = this.get('childViews'), view ;
+    while (view = childViews.objectAt(childViews.get('length')-1)) {
+      this.removeChild(view) ;
+    }
+    return this ;
+  },
+
+  /**
+    Removes the view from its parentView, if one is found.  Otherwise
+    does nothing.
+
+    @returns {SC.View} receiver
+  */
+  removeFromParent: function() {
+    var parent = this.get('parentView') ;
+    if (parent) { parent.removeChild(this) ; }
+    return this ;
+  },
+
+  /**
+    Replace the oldView with the specified view in the receivers childNodes
+    array. This will also replace the DOM node of the oldView with the DOM
+    node of the new view in the receivers DOM.
+
+    If the specified view already belongs to another parent, it will be
+    removed from that view first.
+
+    @param view {SC.View} the view to insert in the DOM
+    @param view {SC.View} the view to remove from the DOM.
+    @returns {SC.View} the receiver
+  */
+  replaceChild: function(view, oldView) {
+    // suspend notifications
+    view.beginPropertyChanges();
+    oldView.beginPropertyChanges();
+    this.beginPropertyChanges();
+
+    this.insertBefore(view,oldView).removeChild(oldView) ;
+
+    // resume notifications
+    this.endPropertyChanges();
+    oldView.endPropertyChanges();
+    view.endPropertyChanges();
+
+    return this;
+  },
+
+  /**
+    Replaces the current array of child views with the new array of child
+    views.
+
+    @param {Array} views views you want to add
+    @returns {SC.View} receiver
+  */
+  replaceAllChildren: function(views) {
+    var len = views.get('length'), idx;
+
+    this.beginPropertyChanges();
+    this.destroyLayer().removeAllChildren();
+    for(idx=0;idx<len;idx++) { this.appendChild(views.objectAt(idx)); }
+    this.replaceLayer();
+    this.endPropertyChanges();
+
+    return this ;
+  },
+
+  /**
+    Appends the specified view to the end of the receivers childViews array.
+    This is equivalent to calling insertBefore(view, null);
+
+    @param view {SC.View} the view to insert
+    @returns {SC.View} the receiver
+  */
+  appendChild: function(view) {
+    return this.insertBefore(view, null);
+  },
+
+  ///
+  /// BUILDING IN/OUT
+  ///
+
+  /**
+    Call this to append a child while building it in. If the child is not
+    buildable, this is the same as calling appendChild.
+  */
+  buildInChild: function(view) {
+    view.willBuildInToView(this);
+    this.appendChild(view);
+    view.buildInToView(this);
+  },
+
+  /**
+    Call to remove a child after building it out. If the child is not buildable,
+    this will simply call removeChild.
+  */
+  buildOutChild: function(view) {
+    view.buildOutFromView(this);
+  },
+
+  /**
+    Called by child view when build in finishes. By default, does nothing.
+
+  */
+  buildInDidFinishFor: function(child) {
+  },
+
+  /**
+    @private
+    Called by child view when build out finishes. By default removes the child view.
+  */
+  buildOutDidFinishFor: function(child) {
+    this.removeChild(child);
+  },
+
+  /**
+    Whether the view is currently building in.
+  */
+  isBuildingIn: NO,
+
+  /**
+    Whether the view is currently building out.
+  */
+  isBuildingOut: NO,
+
+  /**
+    Implement this, and call didFinishBuildIn when you are done.
+  */
+  buildIn: function() {
+    this.buildInDidFinish();
+  },
+
+  /**
+    Implement this, and call didFinsihBuildOut when you are done.
+  */
+  buildOut: function() {
+    this.buildOutDidFinish();
+  },
+
+  /**
+    This should reset (without animation) any internal states; sometimes called before.
+
+    It is usually called before a build in, by the parent view.
+  */
+  resetBuild: function() {
+
+  },
+
+  /**
+    Implement this if you need to do anything special when cancelling build out;
+    note that buildIn will subsequently be called, so you usually won't need to do
+    anything.
+
+    This is basically called whenever build in happens.
+  */
+  buildOutDidCancel: function() {
+
+  },
+
+  /**
+    Implement this if you need to do anything special when cancelling build in.
+    You probably won't be able to do anything. I mean, what are you gonna do?
+
+    If build in was cancelled, it means build out is probably happening.
+    So, any timers or anything you had going, you can cancel.
+    Then buildOut will happen.
+  */
+  buildInDidCancel: function() {
+
+  },
+
+  /**
+    Call this when you have built in.
+  */
+  buildInDidFinish: function() {
+    this.isBuildingIn = NO;
+    this._buildingInTo.buildInDidFinishFor(this);
+    this._buildingInTo = null;
+  },
+
+  /**
+    Call this when you have finished building out.
+  */
+  buildOutDidFinish: function() {
+    this.isBuildingOut = NO;
+    this._buildingOutFrom.buildOutDidFinishFor(this);
+    this._buildingOutFrom = null;
+  },
+
+  /**
+    Usually called by parentViewDidChange, this resets the build state (calling resetBuild in the process).
+  */
+  resetBuildState: function() {
+    if (this.isBuildingIn) {
+      this.buildInDidCancel();
+      this.isBuildingIn = NO;
+    }
+    if (this.isBuildingOut) {
+      this.buildOutDidCancel();
+      this.isBuildingOut = NO;
+    }
+
+    // finish cleaning up
+    this.buildingInTo = null;
+    this.buildingOutFrom = null;
+
+    this.resetBuild();
+  },
+
+  /**
+    @private (semi)
+    Called by building parent view's buildInChild method. This prepares
+    to build in, but unlike buildInToView, this is called _before_ the child
+    is appended.
+
+    Mostly, this cancels any build out _before_ the view is removed through parent change.
+  */
+  willBuildInToView: function(view) {
+    // stop any current build outs (and if we need to, we also need to build in again)
+    if (this.isBuildingOut) {
+      this.buildOutDidCancel();
+    }
+  },
+
+  /**
+    @private (semi)
+    Called by building parent view's buildInChild method.
+  */
+  buildInToView: function(view) {
+    // if we are already building in, do nothing.
+    if (this.isBuildingIn) { return; }
+
+    this._buildingInTo = view;
+    this.isBuildingOut = NO;
+    this.isBuildingIn = YES;
+    this.buildIn();
+  },
+
+  /**
+    @private (semi)
+    Called by building parent view's buildOutChild method.
+
+    The supplied view should always be the parent view.
+  */
+  buildOutFromView: function(view) {
+    // if we are already building out, do nothing.
+    if (this.isBuildingOut) { return; }
+
+    // cancel any build ins
+    if (this.isBuildingIn) {
+      this.buildInDidCancel();
+    }
+
+    // in any case, we need to build out
+    this.isBuildingOut = YES;
+    this.isBuildingIn = NO;
+    this._buildingOutFrom = view;
+    this.buildOut();
+  }
+});
--- frameworks/amber/views/view.js
+++ frameworks/amber/views/view.js
@@ -374,172 +374,6 @@ SC.View.reopen(
   childViews: SC.EMPTY_CHILD_VIEWS_ARRAY,
 
   /**
-    Insert the view into the the receiver's childNodes array.
-
-    The view will be added to the childNodes array before the beforeView.  If
-    beforeView is null, then the view will be added to the end of the array.
-    This will also add the view's rootElement DOM node to the receivers
-    containerElement DOM node as a child.
-
-    If the specified view already belongs to another parent, it will be
-    removed from that view first.
-
-    @param {SC.View} view
-    @param {SC.View} beforeView
-    @returns {SC.View} the receiver
-  */
-  insertBefore: function(view, beforeView) {
-    view.beginPropertyChanges(); // limit notifications
-
-    // remove view from old parent if needed.  Also notify views.
-    if (view.get('parentView')) view.removeFromParent() ;
-    if (this.willAddChild) this.willAddChild(view, beforeView) ;
-    if (view.willAddToParent) view.willAddToParent(this, beforeView) ;
-
-    // set parentView of child
-    view.set('parentView', this);
-
-    // add to childView's array.
-    var idx, childViews = this.get('childViews') ;
-    if (childViews.needsClone) this.set(childViews = []);
-    idx = (beforeView) ? childViews.indexOf(beforeView) : childViews.length;
-    if (idx<0) idx = childViews.length ;
-    childViews.insertAt(idx, view) ;
-
-    // The DOM will need some fixing up, note this on the view.
-    view.parentViewDidChange() ;
-    view.layoutDidChange() ;
-    var pane = view.get('pane');
-    if(pane && pane.get('isPaneAttached')) {
-      view._notifyDidAppendToDocument();
-    }
-
-    // notify views
-    if (this.didAddChild) this.didAddChild(view, beforeView) ;
-    if (view.didAddToParent) view.didAddToParent(this, beforeView) ;
-
-    view.endPropertyChanges();
-
-    return this ;
-  },
-
-  /**
-    Removes the child view from the parent view.
-
-    @param {SC.View} view
-    @returns {SC.View} receiver
-  */
-  removeChild: function(view) {
-    if (!view) return this; // nothing to do
-    if (view.parentView !== this) {
-      throw "%@.removeChild(%@) must belong to parent".fmt(this,view);
-    }
-    // notify views
-    if (view.willRemoveFromParent) view.willRemoveFromParent() ;
-    if (this.willRemoveChild) this.willRemoveChild(view) ;
-
-    // update parent node
-    view.set('parentView', null) ;
-
-    // remove view from childViews array.
-    var childViews = this.get('childViews'),
-        idx = childViews.indexOf(view) ;
-    if (idx>=0) childViews.removeAt(idx);
-
-    // The DOM will need some fixing up, note this on the view.
-    view.parentViewDidChange() ;
-
-    // notify views
-    if (this.didRemoveChild) this.didRemoveChild(view);
-    if (view.didRemoveFromParent) view.didRemoveFromParent(this) ;
-
-    return this ;
-  },
-
-  /**
-    Removes all children from the parentView.
-
-    @returns {SC.View} receiver
-  */
-  removeAllChildren: function() {
-    var childViews = this.get('childViews'), view ;
-    while (view = childViews.objectAt(childViews.get('length')-1)) {
-      this.removeChild(view) ;
-    }
-    return this ;
-  },
-
-  /**
-    Removes the view from its parentView, if one is found.  Otherwise
-    does nothing.
-
-    @returns {SC.View} receiver
-  */
-  removeFromParent: function() {
-    var parent = this.get('parentView') ;
-    if (parent) parent.removeChild(this) ;
-    return this ;
-  },
-
-  /**
-    Replace the oldView with the specified view in the receivers childNodes
-    array. This will also replace the DOM node of the oldView with the DOM
-    node of the new view in the receivers DOM.
-
-    If the specified view already belongs to another parent, it will be
-    removed from that view first.
-
-    @param view {SC.View} the view to insert in the DOM
-    @param view {SC.View} the view to remove from the DOM.
-    @returns {SC.View} the receiver
-  */
-  replaceChild: function(view, oldView) {
-    // suspend notifications
-    view.beginPropertyChanges();
-    oldView.beginPropertyChanges();
-    this.beginPropertyChanges();
-
-    this.insertBefore(view,oldView).removeChild(oldView) ;
-
-    // resume notifications
-    this.endPropertyChanges();
-    oldView.endPropertyChanges();
-    view.endPropertyChanges();
-
-    return this;
-  },
-
-  /**
-    Replaces the current array of child views with the new array of child
-    views.
-
-    @param {Array} views views you want to add
-    @returns {SC.View} receiver
-  */
-  replaceAllChildren: function(views) {
-    var len = views.get('length'), idx;
-
-    this.beginPropertyChanges();
-    this.destroyLayer().removeAllChildren();
-    for(idx=0;idx<len;idx++) this.appendChild(views.objectAt(idx));
-    this.replaceLayer();
-    this.endPropertyChanges();
-
-    return this ;
-  },
-
-  /**
-    Appends the specified view to the end of the receivers childViews array.
-    This is equivalent to calling insertBefore(view, null);
-
-    @param view {SC.View} the view to insert
-    @returns {SC.View} the receiver
-  */
-  appendChild: function(view) {
-    return this.insertBefore(view, null);
-  },
-
-  /**
     This method is called whenever the receiver's parentView has changed.
     The default implementation of this method marks the view's display
     location as dirty so that it will update at the end of the run loop.
@@ -983,7 +817,7 @@ SC.View.reopen(
       this._notifyWillDestroyLayer() ;
 
       // do final cleanup
-      if (layer.parentNode) layer.parentNode.removeChild(layer) ;
+      if (layer.parentNode) { layer.parentNode.removeChild(layer) ; }
       layer = null ;
     }
     return this ;
@@ -1371,13 +1205,13 @@ SC.View.reopen(
 
     // CASE 1: no new parentView.  just remove from parent (above).
     if (!parentView) {
-      if (node && node.parentNode) node.parentNode.removeChild(node);
+      if (node && node.parentNode) { node.parentNode.removeChild(node); }
 
     // CASE 2: parentView has no layer, view has layer.  destroy layer
     // CASE 3: parentView has no layer, view has no layer, nothing to do
     } else if (!parentNode) {
       if (node) {
-        if (node.parentNode) node.parentNode.removeChild(node);
+        if (node.parentNode) { node.parentNode.removeChild(node); }
         this.destroyLayer();
       }
 
@@ -1387,7 +1221,7 @@ SC.View.reopen(
       if (!node) {
         this.createLayer() ;
         node = this.get('layer') ;
-        if (!node) return; // can't do anything without a node.
+        if (!node) { return; } // can't do anything without a node.
       }
 
       var siblings = parentView.get('childViews'),
@@ -3026,188 +2860,6 @@ SC.View.reopen(
     if (x > 100 || y > 100) return NO;
     return YES;
   },
-
-  ///
-  /// BUILDING IN/OUT
-  ///
-
-  /**
-    Call this to append a child while building it in. If the child is not
-    buildable, this is the same as calling appendChild.
-  */
-  buildInChild: function(view) {
-    view.willBuildInToView(this);
-    this.appendChild(view);
-    view.buildInToView(this);
-  },
-
-  /**
-    Call to remove a child after building it out. If the child is not buildable,
-    this will simply call removeChild.
-  */
-  buildOutChild: function(view) {
-    view.buildOutFromView(this);
-  },
-
-  /**
-    Called by child view when build in finishes. By default, does nothing.
-
-  */
-  buildInDidFinishFor: function(child) {
-  },
-
-  /**
-    @private
-    Called by child view when build out finishes. By default removes the child view.
-  */
-  buildOutDidFinishFor: function(child) {
-    this.removeChild(child);
-  },
-
-  /**
-    Whether the view is currently building in.
-  */
-  isBuildingIn: NO,
-
-  /**
-    Whether the view is currently building out.
-  */
-  isBuildingOut: NO,
-
-  /**
-    Implement this, and call didFinishBuildIn when you are done.
-  */
-  buildIn: function() {
-    this.buildInDidFinish();
-  },
-
-  /**
-    Implement this, and call didFinsihBuildOut when you are done.
-  */
-  buildOut: function() {
-    this.buildOutDidFinish();
-  },
-
-  /**
-    This should reset (without animation) any internal states; sometimes called before.
-
-    It is usually called before a build in, by the parent view.
-  */
-  resetBuild: function() {
-
-  },
-
-  /**
-    Implement this if you need to do anything special when cancelling build out;
-    note that buildIn will subsequently be called, so you usually won't need to do
-    anything.
-
-    This is basically called whenever build in happens.
-  */
-  buildOutDidCancel: function() {
-
-  },
-
-  /**
-    Implement this if you need to do anything special when cancelling build in.
-    You probably won't be able to do anything. I mean, what are you gonna do?
-
-    If build in was cancelled, it means build out is probably happening.
-    So, any timers or anything you had going, you can cancel.
-    Then buildOut will happen.
-  */
-  buildInDidCancel: function() {
-
-  },
-
-  /**
-    Call this when you have built in.
-  */
-  buildInDidFinish: function() {
-    this.isBuildingIn = NO;
-    this._buildingInTo.buildInDidFinishFor(this);
-    this._buildingInTo = null;
-  },
-
-  /**
-    Call this when you have finished building out.
-  */
-  buildOutDidFinish: function() {
-    this.isBuildingOut = NO;
-    this._buildingOutFrom.buildOutDidFinishFor(this);
-    this._buildingOutFrom = null;
-  },
-
-  /**
-    Usually called by parentViewDidChange, this resets the build state (calling resetBuild in the process).
-  */
-  resetBuildState: function() {
-    if (this.isBuildingIn) {
-      this.buildInDidCancel();
-      this.isBuildingIn = NO;
-    }
-    if (this.isBuildingOut) {
-      this.buildOutDidCancel();
-      this.isBuildingOut = NO;
-    }
-
-    // finish cleaning up
-    this.buildingInTo = null;
-    this.buildingOutFrom = null;
-
-    this.resetBuild();
-  },
-
-  /**
-    @private (semi)
-    Called by building parent view's buildInChild method. This prepares
-    to build in, but unlike buildInToView, this is called _before_ the child
-    is appended.
-
-    Mostly, this cancels any build out _before_ the view is removed through parent change.
-  */
-  willBuildInToView: function(view) {
-    // stop any current build outs (and if we need to, we also need to build in again)
-    if (this.isBuildingOut) {
-      this.buildOutDidCancel();
-    }
-  },
-
-  /**
-    @private (semi)
-    Called by building parent view's buildInChild method.
-  */
-  buildInToView: function(view) {
-    // if we are already building in, do nothing.
-    if (this.isBuildingIn) return;
-
-    this._buildingInTo = view;
-    this.isBuildingOut = NO;
-    this.isBuildingIn = YES;
-    this.buildIn();
-  },
-
-  /**
-    @private (semi)
-    Called by building parent view's buildOutChild method.
-
-    The supplied view should always be the parent view.
-  */
-  buildOutFromView: function(view) {
-    // if we are already building out, do nothing.
-    if (this.isBuildingOut) return;
-
-    // cancel any build ins
-    if (this.isBuildingIn) {
-      this.buildInDidCancel();
-    }
-
-    // in any case, we need to build out
-    this.isBuildingOut = YES;
-    this.isBuildingIn = NO;
-    this._buildingOutFrom = view;
-    this.buildOut();
-  }
 });
 
 SC.View.mixin(/** @scope SC.View */ {
