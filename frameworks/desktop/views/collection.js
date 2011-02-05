30473136edb5d8ec088c324f5097d490afc89b39
Peter Wagenet <peter.wagenet@gmail.com>
2011-02-05 14:44:22 -0800

    Syntax Error

--- frameworks/desktop/views/collection.js
+++ frameworks/desktop/views/collection.js
@@ -2342,7 +2342,7 @@ SC.CollectionView = SC.View.extend(
 
     if (this.get('useToggleSelection')) {
       var sel = this.get('selection');
-      isSelected = sel && sel.containsObject(itemView.get('content')); }
+      isSelected = sel && sel.containsObject(itemView.get('content'));
     }
 
     if (isSelected) {
