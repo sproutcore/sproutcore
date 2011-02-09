cfc5bb1be29a351dfc27006c580986d30da2190c
Sunny <sthakwani@apple.com>
2011-02-09 17:34:40 +0530

    ARIA attribute for - SC.LabelView

--- frameworks/foundation/views/label.js
+++ frameworks/foundation/views/label.js
@@ -33,6 +33,16 @@ SC.LabelView = SC.View.extend(SC.Control, SC.InlineEditorDelegate, SC.InlineEdit
   classNames: ['sc-label-view'],
 
   displayProperties: 'displayTitle textAlign fontWeight icon escapeHTML needsEllipsis hint'.w(),
+
+  /**
+    The WAI-ARIA attribute for the label view. This property is assigned to
+    'aria-labelledby' attribute, which defines a string value that labels the
+    element. Used to support voiceover. It should be assigned a non-empty string,
+    if the 'aria-labelledby' attribute has to be set for the element.
+
+    @property {String}
+  */
+  ariaLabeledBy: "",
   
   isEditable: NO,
   
