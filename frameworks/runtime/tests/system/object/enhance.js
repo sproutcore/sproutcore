82d21f8c4a96d05167fd5cc18c243c09ac10da39
tomhuda <tomhuda@strobecorp.com>
2011-02-10 14:08:19 -0800

    Fix reopen so that methods defined inside a reopened class can call sc_super()

--- frameworks/runtime/tests/system/object/enhance.js
+++ frameworks/runtime/tests/system/object/enhance.js
@@ -47,3 +47,35 @@ test("subclassing and then enhancing the parent", function() {
   var obj = SubKlass.create();
   equals(obj.loudly("foo"), "ZOMG OHAI: FOO!");
 });
+
+test("calling sc_super inside a reopened class", function() {
+  var Klass = SC.Object.extend({
+    loudly: function(string) {
+      return string + this.get('exclaim');
+    },
+    exclaim: "!"
+  });
+
+  Klass.reopen({
+    loudly: function(original, string) {
+      return original(string.toUpperCase());
+    }.enhance()
+  });
+
+  SubKlass = Klass.extend({});
+
+  SubKlass.reopen({
+    loudly: function(string) {
+      return "ZOMG " + sc_super() // Klass.prototype.loudly.call(this, string); // sc_super();
+    }
+  });
+
+  SubKlass.reopen({
+    loudly: function(original, string) {
+      return "OHAI: " + original(string);
+    }.enhance()
+  });
+
+  var obj = SubKlass.create();
+  equals(obj.loudly("foo"), "OHAI: ZOMG FOO!");
+});
