ea03a8e8b0b0f53f083bc896b916be393036a595
tomhuda <tomhuda@strobecorp.com>
2011-02-10 21:31:38 -0800

    Convert tabs to spaces.

--- frameworks/core_foundation/tests/views/view/layoutDidChange.js
+++ frameworks/core_foundation/tests/views/view/layoutDidChange.js
@@ -10,120 +10,119 @@ module("SC.View#layoutDidChange");
 
 test("notifies layoutStyle & frame change", function() {
 
-	var view = SC.View.create();
-	var callCount = 0;
-	
-	view.addObserver('layoutStyle', function() { callCount++; });
-	view.addObserver('frame', function() { callCount++; });
+  var view = SC.View.create();
+  var callCount = 0;
 
-	view.layoutDidChange();
-	equals(callCount,2,'should trigger observers for layoutStyle + frame');
+  view.addObserver('layoutStyle', function() { callCount++; });
+  view.addObserver('frame', function() { callCount++; });
+
+  view.layoutDidChange();
+  equals(callCount,2,'should trigger observers for layoutStyle + frame');
 });
 
 test("invokes layoutDidChangeFor() on layoutView each time it is called", function() {
 
-	var callCount = 0 ;
-	var layoutView = SC.View.create({
-		layoutDidChangeFor: function(changedView){
-			equals(this.get('childViewsNeedLayout'), YES, 'should set childViewsNeedLayout to YES before calling layoutDidChangeFor()');
-			
-			equals(view, changedView, 'should pass view');
-			callCount++;
-		}
-	});
-	
-	var view = SC.View.create({ layoutView: layoutView });
+  var callCount = 0 ;
+  var layoutView = SC.View.create({
+    layoutDidChangeFor: function(changedView){
+      equals(this.get('childViewsNeedLayout'), YES, 'should set childViewsNeedLayout to YES before calling layoutDidChangeFor()');
 
-	view.layoutDidChange();
-	view.layoutDidChange();
-	view.layoutDidChange();
-	
-	equals(callCount, 3, 'should call layoutView.layoutDidChangeFor each time');
+      equals(view, changedView, 'should pass view');
+      callCount++;
+    }
+  });
+
+  var view = SC.View.create({ layoutView: layoutView });
+
+  view.layoutDidChange();
+  view.layoutDidChange();
+  view.layoutDidChange();
+
+  equals(callCount, 3, 'should call layoutView.layoutDidChangeFor each time');
 });
 
 test("invokes layoutChildViewsIfNeeded() on layoutView once per runloop", function() {
 
-	var callCount = 0 ;
-	var layoutView = SC.View.create({
-		layoutChildViewsIfNeeded: function(){
-			callCount++;
-		}
-	});
-	
-	var view = SC.View.create({ layoutView: layoutView });
+  var callCount = 0 ;
+  var layoutView = SC.View.create({
+    layoutChildViewsIfNeeded: function(){
+      callCount++;
+    }
+  });
 
-	SC.RunLoop.begin();
-	view.layoutDidChange();
-	view.layoutDidChange();
-	view.layoutDidChange();
-	SC.RunLoop.end();
-	
-	equals(callCount, 1, 'should call layoutView.layoutChildViewsIfNeeded one time');
+  var view = SC.View.create({ layoutView: layoutView });
+
+  SC.RunLoop.begin();
+  view.layoutDidChange();
+  view.layoutDidChange();
+  view.layoutDidChange();
+  SC.RunLoop.end();
+
+  equals(callCount, 1, 'should call layoutView.layoutChildViewsIfNeeded one time');
 });
 
 
 test("should not invoke layoutChildViewsIfNeeded() if layoutDidChangeFor() sets childViewsNeedLayout to NO each time", function() {
 
-	var callCount = 0 ;
-	var layoutView = SC.View.create({
-		layoutDidChangeFor: function() {
-			this.set('childViewsNeedLayout', NO);
-		},
-		
-		layoutChildViewsIfNeeded: function(){
-			callCount++;
-		}
-	});
-	
-	var view = SC.View.create({ layoutView: layoutView });
+  var callCount = 0 ;
+  var layoutView = SC.View.create({
+    layoutDidChangeFor: function() {
+      this.set('childViewsNeedLayout', NO);
+    },
 
-	SC.RunLoop.begin();
-	view.layoutDidChange();
-	view.layoutDidChange();
-	view.layoutDidChange();
-	SC.RunLoop.end();
-	
-	equals(callCount, 0, 'should not call layoutView.layoutChildViewsIfNeeded');
+    layoutChildViewsIfNeeded: function(){
+      callCount++;
+    }
+  });
+
+  var view = SC.View.create({ layoutView: layoutView });
+
+  SC.RunLoop.begin();
+  view.layoutDidChange();
+  view.layoutDidChange();
+  view.layoutDidChange();
+  SC.RunLoop.end();
+
+  equals(callCount, 0, 'should not call layoutView.layoutChildViewsIfNeeded');
 });
 
 test('returns receiver', function() {
-	var view = SC.View.create();
-	equals(view.layoutDidChange(), view, 'should return receiver');
+  var view = SC.View.create();
+  equals(view.layoutDidChange(), view, 'should return receiver');
 });
 
 test("is invoked whenever layout property changes", function() {
 
-	var callCount = 0 ;
-	var layoutView = SC.View.create({
-		layoutDidChangeFor: function(changedView){
-			callCount++;
-		}
-	});
-	
-	var view = SC.View.create({ layoutView: layoutView });
+  var callCount = 0 ;
+  var layoutView = SC.View.create({
+    layoutDidChangeFor: function(changedView){
+      callCount++;
+    }
+  });
 
-	view.set('layout', { top: 0, left: 10 });
-	equals(callCount, 1, 'should call layoutDidChangeFor when setting layout of child view');
+  var view = SC.View.create({ layoutView: layoutView });
 
-	
+  view.set('layout', { top: 0, left: 10 });
+  equals(callCount, 1, 'should call layoutDidChangeFor when setting layout of child view');
+
+
 });
 
 test("is invoked on parentView if no layoutView whenever layout property changes", function() {
 
-	var callCount = 0 ;
-	var parentView = SC.View.create({
-		layoutDidChangeFor: function(changedView){
-			callCount++;
-		}
-	});
-	
-	var view = SC.View.create({});
-	view.set('parentView', parentView);
+  var callCount = 0 ;
+  var parentView = SC.View.create({
+    layoutDidChangeFor: function(changedView){
+      callCount++;
+    }
+  });
 
-	view.set('layout', { top: 0, left: 10 });
-	equals(callCount, 1, 'should call layoutDidChangeFor when setting layout of child view');
+  var view = SC.View.create({});
+  view.set('parentView', parentView);
 
-	
+  view.set('layout', { top: 0, left: 10 });
+  equals(callCount, 1, 'should call layoutDidChangeFor when setting layout of child view');
+
 });
 
 test("sets rotateX when rotate is set", function() {
