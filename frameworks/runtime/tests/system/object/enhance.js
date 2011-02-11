bf1c75532bc884015052d1f31857c2e48619c7b0
tomhuda <tomhuda@strobecorp.com>
2011-02-10 16:09:22 -0800

    Fix a number of additional edge-cases involving sc_super. These edge-cases were not all caused by reopen, but reopen exposed the issue (most of them were just the result of sc_super not being truly dynamic).

--- frameworks/runtime/tests/system/object/enhance.js
+++ frameworks/runtime/tests/system/object/enhance.js
@@ -66,7 +66,7 @@ test("calling sc_super inside a reopened class", function() {
 
   SubKlass.reopen({
     loudly: function(string) {
-      return "ZOMG " + sc_super() // Klass.prototype.loudly.call(this, string); // sc_super();
+      return "ZOMG " + sc_super();
     }
   });
 
@@ -76,6 +76,62 @@ test("calling sc_super inside a reopened class", function() {
     }.enhance()
   });
 
+  Klass.reopen({
+    loudly: function(original, string) {
+      return "HAHA " + original(string);
+    }.enhance()
+  });
+
   var obj = SubKlass.create();
-  equals(obj.loudly("foo"), "OHAI: ZOMG FOO!");
+  equals(obj.loudly("foo"), "OHAI: ZOMG HAHA FOO!");
+});
+
+test("calling sc_super inside a reopened class", function() {
+  var Klass = SC.Object.extend();
+
+  var object = Klass.create({
+    loudly: function(string) {
+      return sc_super() + "!";
+    }
+  });
+
+  Klass.reopen({
+    loudly: function(string) {
+      return string.toUpperCase();
+    }
+  });
+
+  equals(object.loudly("foo"), "FOO!");
+});
+
+test("sc_super to a non-method", function() {
+  var Klass = SC.Object.extend({
+    wot: function() {
+      return sc_super();
+    }
+  });
+
+  var object = Klass.create(), error;
+
+  try {
+    object.wot();
+  } catch(e) {
+    error = e;
+  }
+
+  ok(error, "sc_super throws an error if there is no superclass method");
+});
+
+test("__sc_proto__ semantics", function() {
+  var Klass = SC.Object.extend({});
+  ok(Klass.__sc_super__ === SC.Object.prototype, "SproutCore remembers the original begetted prototype for subclasses");
+
+  var object = Klass.create({});
+  ok(object.__sc_super__ === Klass.prototype, "SproutCore remembers the original prototype for new instances");
+
+  var SubKlass = Klass.extend({});
+  ok(SubKlass.__sc_super__ === Klass.prototype, "SproutCore remembers the original begetted prototype for custom subclasses");
+
+  SubKlass.reopen({});
+  ok(SubKlass.__sc_super__ === Klass.prototype, "Reopen doesn't break prototype recordkeeping");
 });
