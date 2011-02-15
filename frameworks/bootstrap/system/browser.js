8b82c38e3fc0d5376b5a537567520311f9af5fcf
Colin Campbell <colin@strobecorp.com>
2011-02-15 12:48:15 -0330

    Initial work at making our browser detection more robust. Unit tests now test for false positives (ie. every property of SC.browser is tested for true or false, instead of just true). Renamed SC.browser.safari to SC.browser.webkit and introduced SC.browser.iOS for iOS devices

--- frameworks/bootstrap/system/browser.js
+++ frameworks/bootstrap/system/browser.js
@@ -7,30 +7,41 @@
 
 var SC = SC || { MODULE_INFO: {}, LAZY_INSTANTIATION: {} };
 
-SC._detectBrowser = function(rawUserAgent, language) {
-  if (rawUserAgent === undefined) rawUserAgent = navigator.userAgent;
-  if (language === undefined) language = navigator.language || navigator.browserLanguage;
+SC._detectBrowser = function(userAgent, language) {
+  var version, webkitVersion, browser = {};
 
-  var userAgent = rawUserAgent.toLowerCase(),
-      // Gibberish at the end is to determine when the browser version is done
-      version = (userAgent.match( /.*(?:rv|chrome|webkit|opera|ie)[\/: ](.+?)([ \);]|$)/ ) || [])[1],
-      webkitVersion = (userAgent.match( /webkit\/(.+?) / ) || [])[1];
+  userAgent = (userAgent || navigator.userAgent).toLowerCase();
+  language = language || navigator.language || navigator.browserLanguage;
 
-  var browser = {
-    version:      version,
-    safari:       /webkit/.test(userAgent) ? webkitVersion : 0,
-    opera:        /opera/.test(userAgent) ? version : 0,
-    msie:         /msie/.test(userAgent) && !/opera/.test(userAgent) ? version : 0,
-    mozilla:      /mozilla/.test( userAgent ) && !/(compatible|webkit)/.test(userAgent) ? version : 0,
-    mobileSafari: /apple.*mobile.*safari/.test(userAgent) ? version : 0,
-    chrome:       /chrome/.test( userAgent ) ? version : 0,
-    windows:      !!/windows/.test(userAgent),
-    mac:          !!/macintosh/.test(userAgent) || (/mac os x/.test(userAgent) && !/like mac os x/.test(userAgent)),
-    android:      !!/android/.test(userAgent),
-    language:     language.split('-', 1)[0]
-  };
+  // Gibberish at the end is to determine when the browser version is done
+  version = browser.version = (userAgent.match( /.*(?:rv|chrome|webkit|opera|ie)[\/: ](.+?)([ \);]|$)/ ) || [])[1];
+  webkitVersion = (userAgent.match( /webkit\/(.+?) / ) || [])[1];
 
-  browser.current = browser.msie ? 'msie' : browser.mozilla ? 'mozilla' : browser.chrome ? 'chrome' : browser.safari ? 'safari' : browser.opera ? 'opera' : 'unknown' ;
+  // Platforms -- these should probably be moved to another object
+  // We test for these first because it is helpful when determining browsers below
+  browser.windows = !!/windows/.test(userAgent);
+  browser.mac = !!/macintosh/.test(userAgent) || (/mac os x/.test(userAgent) && !/like mac os x/.test(userAgent));
+  browser.iOS = !!/iphone|ipod|ipad/.test(userAgent);
+  browser.android = !!/android/.test(userAgent);
+
+  // IE-based browsers
+  browser.opera = /opera/.test(userAgent) ? version : 0;
+  browser.msie = /msie/.test(userAgent) && !browser.opera ? version : 0;
+
+  // Mozilla browsers
+  browser.mozilla = /mozilla/.test(userAgent) && !/(compatible|webkit|msie)/.test(userAgent) ? version : 0;
+
+  // Webkit-based browsers
+  browser.webkit = /webkit/.test(userAgent) ? webkitVersion : 0;
+  browser.chrome = /chrome/.test(userAgent) ? version: 0;
+  browser.mobileSafari = /apple.*mobile.*safari/.test(userAgent) && browser.iOS ? webkitVersion : 0;
+  // this is a stupid test -- anything that aren't the following doesn't necessarily mean Safari
+  browser.safari = browser.webkit && !browser.chrome && !browser.iOS && !browser.android ? webkitVersion : 0;
+
+  // Language
+  browser.language = language.split('-', 1)[0];
+
+  browser.current = browser.msie ? 'msie' : browser.mozilla ? 'mozilla' : browser.chrome ? 'chrome' : browser.safari ? 'safari' : browser.opera ? 'opera' : browser.mobileSafari ? 'mobile-safari' : 'unknown' ;
   return browser ;
 };
 
