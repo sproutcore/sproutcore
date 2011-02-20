9fd9ed93ab5e4405c621b71a4b990a9b5cd41cf7
wycats <wycats@gmail.com>
2011-02-19 16:50:16 -0800

    Property notification in a jQuery callback must be inside a run loop

--- frameworks/core_foundation/views/template/checkbox_support.js
+++ frameworks/core_foundation/views/template/checkbox_support.js
@@ -1,7 +1,9 @@
 SC.CheckboxSupport = {
   didCreateLayer: function() {
     this.$('input').change(jQuery.proxy(function() {
+      SC.RunLoop.begin();
       this.notifyPropertyChange('value');
+      SC.RunLoop.end();
     }, this));
   },
 
