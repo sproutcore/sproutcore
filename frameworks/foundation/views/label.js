cf5cdbb71c32f30ebcf51943713e755d92e519e6
Tom Dale <tom@sproutcore.com>
2011-03-04 10:58:55 -0800

    Restore label view opacity after inline editing.

--- frameworks/foundation/views/label.js
+++ frameworks/foundation/views/label.js
@@ -186,23 +186,16 @@ SC.LabelView = SC.View.extend(SC.Control, SC.InlineEditorDelegate, SC.InlineEdit
     
   */
   doubleClick: function( evt ) { return this.beginEditing(); },
-  
-  
-  /** @private 
-    Hide the label view while the inline editor covers it.
-  */
-  inlineEditorDidBeginEditing: function(inlineEditor) {
-    var layer = this.$();
-    this._oldOpacity = layer.css('opacity') ;
-    layer.css('opacity', 0.0);
-  },
-  
-  
+
   /** @private 
     Hide the label view while the inline editor covers it.
   */
   inlineEditorDidBeginEditing: function(editor) {
-    this._oldOpacity = this.get('layout').opacity ;
+    var layer = this.$();
+
+    // Cache the current opacity value
+    this._oldOpacity = layer.css('opacity');  //gets the opacity from the layer
+    // Hide the view by setting its opacity to 0
     this.adjust('opacity', 0);
   },
   
