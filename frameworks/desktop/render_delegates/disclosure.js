cb64d0cc4df77a91f03469a5100ef8394e4f801c
Alex Iskander <alexiskander@me.com>
2011-03-06 00:17:19 -0600

    Disclosure needs to give its label an id.

--- frameworks/desktop/render_delegates/disclosure.js
+++ frameworks/desktop/render_delegates/disclosure.js
@@ -27,7 +27,7 @@ SC.BaseTheme.disclosureRenderDelegate = SC.RenderDelegate.create({
     
     context.push('<img src = "' + SC.BLANK_IMAGE_URL + '" class = "disclosure button ' + state + '" />');
     
-    context = context.begin('span').addClass('sc-button-label');
+    context = context.begin('span').addClass('sc-button-label').id(labelId);
     theme.labelRenderDelegate.render(dataSource, context);
     context = context.end();
   },
