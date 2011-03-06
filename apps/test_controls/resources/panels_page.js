a191cc922c74ac5be0f6708f3b774b49313ec2ae
Alex Iskander <alexiskander@me.com>
2011-03-06 11:06:47 -0600

    Make alert test page of Test Controls test more cases.

--- apps/test_controls/resources/panels_page.js
+++ apps/test_controls/resources/panels_page.js
@@ -12,21 +12,50 @@ TestControls.panelsPage = SC.View.design({
     classNames: ["sample_controls"],
     layout: { left: 20, top: 40, right: 20, bottom: 40 },
 
-    childViews: "header normal".w(),
+    childViews: 'header buttonCount alertType show'.w(),
 
     header: SC.LabelView.design({
       layout: { width: 250, height: 18 },
-      value: "Panels",
+      value: "Alerts",
       classNames: "header".w()
     }),
 
-    normal: SC.FormView.row(SC.ButtonView.design({
+    buttonCount: SC.FormView.row(SC.RadioView.design({
+      layout: { height: 24 * 3, width: 200 },
+      items: [
+        { 'title': 'One Button', 'value': 1 },
+        { 'title': 'Two Buttons', 'value': 2 },
+        { 'title': 'Three Buttons', 'value': 3 }
+      ],
+
+      itemTitleKey: 'title',
+      itemValueKey: 'value',
+
+      valueBinding: 'TestControls.alertController.buttonCount'
+    })),
+
+    alertType: SC.FormView.row(SC.SelectView.design({
+      layout: { height: 24, width: 200 },
+      items: [
+        { title: 'Error', value: 'error' },
+        { title: 'Info', value: 'info' },
+        { title: 'Plain', value: 'plain' },
+        { title: 'Show', value: 'show' },
+        { title: 'Warn', value: 'warn' }
+      ],
+
+      itemTitleKey: 'title',
+      itemValueKey: 'value',
+
+      valueBinding: 'TestControls.alertController.alertType'
+    })),
+
+    show: SC.ButtonView.design({
       layout: { height: 24, width: 100 },
       title: 'Alert',
-      action: function() {
-        SC.AlertPane.show("Hello, world");
-      }
-    }))
+      target: TestControls.alertController,
+      action: 'showAlert'
+    })
 
   })
 });
