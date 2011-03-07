44738ad077bd233c48b1923463969dcd1e2464c9
Colin Campbell <colin@strobecorp.com>
2011-03-07 01:38:06 -0330

    When handling reducedProperty in Enumerable, check if the key is a String

--- frameworks/runtime/mixins/enumerable.js
+++ frameworks/runtime/mixins/enumerable.js
@@ -978,7 +978,7 @@ SC.Reducers = /** @lends SC.Enumerable */ {
   */
   reducedProperty: function(key, value, generateProperty) {
 
-    if (!key || key.charAt(0) !== '@') return undefined ; // not a reduced property
+    if (!key || typeof key !== SC.T_STRING || key.charAt(0) !== '@') return undefined ; // not a reduced property
 
     // get the reducer key and the reducer
     var matches = key.match(/^@([^(]*)(\(([^)]*)\))?$/) ;
