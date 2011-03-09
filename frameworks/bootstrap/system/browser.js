910ef770b9b93eac6be87f72f009014aa98a05ff
Colin Campbell <colin@strobecorp.com>
2011-03-09 15:55:22 -0330

    Moved most of SC.browser that was in CoreFoundation to bootstrap (simplify code). Added new SC.browser.iPhone/SC.browser.iPad/SC.browser.iPod variables. Unit tests updated and passing.

--- frameworks/bootstrap/system/browser.js
+++ frameworks/bootstrap/system/browser.js
@@ -17,38 +17,49 @@ SC._detectBrowser = function(userAgent, language) {
   version = browser.version = (userAgent.match( /.*(?:rv|chrome|webkit|opera|ie)[\/: ](.+?)([ \);]|$)/ ) || [])[1];
   webkitVersion = (userAgent.match( /webkit\/(.+?) / ) || [])[1];
 
-  // Platforms -- these should probably be moved to another object
-  // We test for these first because it is helpful when determining browsers below
-  browser.windows = !!/windows/.test(userAgent);
-  browser.mac = !!/macintosh/.test(userAgent) || (/mac os x/.test(userAgent) && !/like mac os x/.test(userAgent));
-  browser.iOS = !!/iphone|ipod|ipad/.test(userAgent);
-  browser.android = !!/android/.test(userAgent);
+  browser.windows = browser.isWindows = !!/windows/.test(userAgent);
+  browser.mac = browser.isMac = !!/macintosh/.test(userAgent) || (/mac os x/.test(userAgent) && !/like mac os x/.test(userAgent));
+  browser.iPhone = browser.isiPhone = !!/iphone/.test(userAgent);
+  browser.iPod = browser.isiPod = !!/ipod/.test(userAgent);
+  browser.iPad = browser.isiPad = !!/ipad/.test(userAgent);
+  browser.iOS = browser.isiOS = browser.iPhone || browser.iPod || browser.iPad;
+  browser.android = browser.isAndroid = !!/android/.test(userAgent);
 
-  // IE-based browsers
   browser.opera = /opera/.test(userAgent) ? version : 0;
+  browser.isOpera = !!browser.opera;
+
   browser.msie = /msie/.test(userAgent) && !browser.opera ? version : 0;
+  browser.isIE = !!browser.msie;
+  browser.isIE8OrLower = !!(browser.msie && parseInt(browser.msie, 10) <= 8);
 
-  // Mozilla browsers
   browser.mozilla = /mozilla/.test(userAgent) && !/(compatible|webkit|msie)/.test(userAgent) ? version : 0;
+  browser.isMozilla = !!browser.mozilla;
 
-  // Webkit-based browsers
   browser.webkit = /webkit/.test(userAgent) ? webkitVersion : 0;
-  browser.chrome = /chrome/.test(userAgent) ? version: 0;
-  browser.mobileSafari = /apple.*mobile.*safari/.test(userAgent) && browser.iOS ? webkitVersion : 0;
-  browser.iPadSafari = /ipad/.test(userAgent) && browser.iOS ? webkitVersion : 0;
-  browser.iPhoneSafari = /iphone/.test(userAgent) && browser.iOS ? webkitVersion : 0;
-  
-  // this is a stupid test -- anything that aren't the following doesn't necessarily mean Safari
-  browser.safari = browser.webkit && !browser.chrome && !browser.iOS && !browser.android ? webkitVersion : 0;
+  browser.isWebkit = !!browser.webkit;
 
-  // Language
+  browser.chrome = /chrome/.test(userAgent) ? version: 0;
+  browser.isChrome = !!browser.chrome;
+
+  browser.mobileSafari = /apple.*mobile.*safari/.test(userAgent) && browser.iOS ? webkitVersion : 0;
+  browser.isMobileSafari = !!browser.mobileSafari;
+
+  browser.iPadSafari = browser.iPad && browser.isMobileSafari ? webkitVersion : 0;
+  browser.isiPadSafari = !!browser.iPadSafari;
+
+  browser.iPhoneSafari = browser.iPhone && browser.isMobileSafari ? webkitVersion : 0;
+  browser.isiPhoneSafari = !!browser.iphoneSafari;
+
+  browser.iPodSafari = browser.iPod && browser.isMobileSafari ? webkitVersion : 0;
+  browser.isiPodSafari = !!browser.iPodSafari;
+
+  browser.safari = browser.webkit && !browser.chrome && !browser.iOS && !browser.android ? webkitVersion : 0;
+  browser.isSafari = !!browser.safari;
+
   browser.language = language.split('-', 1)[0];
   
-  
-  browser.isIE8OrLower = !!(browser.msie && parseInt(browser.msie,10) <= 8);
-  
-  browser.current = browser.msie ? 'msie' : browser.mozilla ? 'mozilla' : browser.chrome ? 'chrome' : browser.safari ? 'safari' : browser.opera ? 'opera' : browser.mobileSafari ? 'mobile-safari' : 'unknown' ;
-  return browser ;
+  browser.current = browser.msie ? 'msie' : browser.mozilla ? 'mozilla' : browser.chrome ? 'chrome' : browser.safari ? 'safari' : browser.opera ? 'opera' : browser.mobileSafari ? 'mobile-safari' : 'unknown';
+  return browser;
 };
 
 SC.browser = SC._detectBrowser();
