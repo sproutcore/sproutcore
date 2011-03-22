42cb8d1052fd67eb2b0c6fd739b11a15426b41c5
Sean Eidemiller <sean@halcyon.cc>
2011-03-22 16:24:54 -0400

    Fixed bug preventing null values from being set on SC.ChildAttributes.

--- frameworks/datastore/models/child_attribute.js
+++ frameworks/datastore/models/child_attribute.js
@@ -47,23 +47,33 @@ SC.ChildAttribute = SC.RecordAttribute.extend(
   },
   
   // Default fromType is just returning itself
-  fromType: function(record, key, value){
+  fromType: function(record, key, value) {
     var sk, store, ret;
-    if (record){
-      ret = record.registerNestedRecord(value, key, key);
-      if (ret) {
-        sk = ret.get('storeKey');
-        store = ret.get('store');
-        record.writeAttribute(key, store.readDataHash(sk));
-      }
-      else if (value) {
+
+    if (record) {
+      if (SC.none(value)) {
+        // Handle null value.
         record.writeAttribute(key, value);
+        ret = value;
+      } else {
+        // Register the nested record with this record (the parent).
+        ret = record.registerNestedRecord(value, key);
+
+        if (ret) {
+          // Write the data hash of the nested record to the store.
+          sk = ret.get('storeKey');
+          store = ret.get('store');
+          record.writeAttribute(key, store.readDataHash(sk));
+        } else if (value) {
+          // If registration failed, just write the value.
+          record.writeAttribute(key, value);
+        }
       }
-    } 
-    
+    }
+
     return ret;
   },
-    
+ 
   /**
     The core handler.  Called from the property.
     @param {SC.Record} record the record instance
