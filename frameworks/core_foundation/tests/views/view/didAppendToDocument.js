1448aaba5681960e3c26753d0b475c38bb16f4ff
Public Keating <publickeating@me.com>
2011-04-01 16:55:45 -0600

    Add failing test to illustrate potential bug if a non-fixed layout view is embedded within a static layout view.  Found the bug by accident, but the fix is straightforward so added it (in next commit)

--- frameworks/core_foundation/tests/views/view/didAppendToDocument.js
+++ frameworks/core_foundation/tests/views/view/didAppendToDocument.js
@@ -8,44 +8,76 @@
 /*global module test htmlbody ok equals same stop start Q$ */
 
 
-var pane, view;
+var counter, pane, view, additionalView;
 
-module("SC.View#didAppendToDocument");
+module("SC.View#didAppendToDocument", {
+  setup: function() {
+    counter = 0;
+
+    pane = SC.MainPane.create({
+      childViews: [
+        SC.View.extend({
+          render: function (context, firstTime) {
+            context.push('new string');
+          },
+          didAppendToDocument: function(){
+            counter++;
+          }
+        })
+      ]
+    });
+    view = pane.childViews[0];
+
+    additionalView = SC.View.create({
+      didAppendToDocument: function(){
+        counter++;
+      }
+    });
+  },
+
+  teardown: function() {
+    pane.remove().destroy();
+    pane = null;
+  }
+});
 
 test("Check that didAppendToDocument gets called at the right moment", function() {
-   var counter=0;
-   pane = SC.MainPane.create({
-     childViews: [
-       SC.View.extend({
-         render: function (context, firstTime) {
-           context.push('new string');
-         },
-         didAppendToDocument: function(){
-           counter++;
-         }
-       })
-     ]
-   });
-   equals(counter, 0, "precond - has not been called yet");
-   pane.append(); // make sure there is a layer...
-   equals(counter, 1, "didAppendToDocument was called once");
-   view  = pane.childViews[0];
 
-   SC.run(function() {
-     view.updateLayer();
-   });
+  equals(counter, 0, "precond - has not been called yet");
+  pane.append(); // make sure there is a layer...
+  equals(counter, 1, "didAppendToDocument was called once");
 
-   equals(counter, 2, "didAppendToDocument is called every time a new DOM element is created");
+  SC.run(function() {
+    view.updateLayer();
+  });
 
-   var additionalView = SC.View.extend({
-     didAppendToDocument: function(){
-       counter++;
-     }
-   });
-   additionalView = additionalView.create();
-   pane.appendChild(additionalView);
+  equals(counter, 2, "didAppendToDocument is called every time a new DOM element is created");
 
-   SC.RunLoop.begin().end();
-   equals(counter, 3, "");
-   pane.remove();
+  pane.appendChild(additionalView);
+
+  SC.RunLoop.begin().end();
+  equals(counter, 3, "");
+});
+
+// Test for bug: when a childView has a non-fixed layout and we request its frame before the parentView has
+// a layer and the parentView uses static layout, then the frame returned will be {x: 0, y:0, width: 0, height: 0}
+// and any further requests for the childView's frame will not return a new value unless the parentViewDidChange
+// or parentViewDidResize.  A weird case, but we prevent it from failing by checking if we don't have
+test("Check that childView is updated if we have static layout and they don't have a fixed layout", function() {
+  var childFrame,
+      wrongFrame = {x:0, y:0, width: 0, height: 0},
+      correctFrame;
+
+  pane.set('useStaticLayout', YES);
+
+  childFrame = view.get('frame');
+  same(childFrame, wrongFrame, 'getting frame before layer exists on non-fixed layout childView should return an empty frame');
+
+  SC.run(function() {
+    pane.append(); // make sure there is a layer...
+  });
+  childFrame = view.get('frame');
+  correctFrame = pane.get('frame');
+
+  same(childFrame, correctFrame, 'getting frame before layer exists on non-fixed layout childView should return an empty frame');
 });
