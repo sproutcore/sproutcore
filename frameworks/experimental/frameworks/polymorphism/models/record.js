64ee1b3ff3f39ef98dac387244a58945a8f0f068
Colin Campbell <colin@strobecorp.com>
2011-04-12 15:09:39 -0230

    Experimental polymorphism: Add ability to pass isPolymorphic in SC.Record.extend hash instead of needing it set it afterwards, with unit test.

--- frameworks/experimental/frameworks/polymorphism/models/record.js
+++ frameworks/experimental/frameworks/polymorphism/models/record.js
@@ -5,93 +5,108 @@
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
 
-SC.mixin(SC.Record,
-/** @scope SC.Record */{
+(function() {
+  var oldExtend = SC.Record.extend;
 
-  // ..........................................................
-  // Properties
-  // 
+  SC.mixin(SC.Record,
+  /** @scope SC.Record */ {
 
-  /**
-    If YES, then searches for records of this type will return
-    subclass instances. For example:
+    // ..........................................................
+    // Properties
+    // 
 
-    {{{
-      Person = SC.Record.extend();
-      Person.isPolymorphic = YES;
+    /**
+      If YES, then searches for records of this type will return
+      subclass instances. For example:
 
-      Male = Person.extend();
-      Female = Person.extend();
-    }}}
+      {{{
+        Person = SC.Record.extend();
+        Person.isPolymorphic = YES;
 
-    Using SC.Store#find, or a toOne or toMany relationship on
-    Person will then return records of type Male and Female.
-    Polymorphic record types must have unique GUIDs for all
-    subclasses.
+        Male = Person.extend();
+        Female = Person.extend();
+      }}}
 
-    @property {Boolean}
-    @default NO
-  */
-  isPolymorphic: NO,
+      Using SC.Store#find, or a toOne or toMany relationship on
+      Person will then return records of type Male and Female.
+      Polymorphic record types must have unique GUIDs for all
+      subclasses.
 
-
-  // ..........................................................
-  // Propagation Support
-  // 
-
-  /**
-    Like the original SC.Record.storeKeyFor,
-    but when a storeKey is generated, it will
-    propagate it to superclasses.
-    
-    @see SC.Record.storeKeyFor
-  */
-  storeKeyFor: function(id) {
-    var storeKeys = this.storeKeysById(),
-        ret = storeKeys[id];
-    
-    if (!ret) {
-      ret = SC.Store.generateStoreKey();
-      SC.Store.idsByStoreKey[ret] = id;
-      SC.Store.recordTypesByStoreKey[ret] = this;
-      storeKeys[id] = ret;
-      this._propagateIdForStoreKey(id, ret);
-    }
-    
-    return ret ;
-  },
-
-
-  // ..........................................................
-  // Internal Support
-  // 
-
-  /** @private */
-  _propagateIdForStoreKey: function(id, storeKey) {
-    var superclass = this.superclass;
-    if (this.isPolymorphic) {
-      while (superclass.isPolymorphic && superclass !== SC.Record) {
-        superclass._storeKeyForId(storeKey, id);
-        superclass = superclass.superclass;
-      }
-    }
-  },
-
-  /** @private */
-  _storeKeyForId: function(storeKey, id) {
-    var storeKeys;
-    if (!this.isPolymorphic) return;
-
-    /*
-      TODO [CC] SC.Store.recordTypesByStoreKey will be broken
+      @property {Boolean}
+      @default NO
     */
+    isPolymorphic: NO,
 
-    storeKeys = this.storeKeysById();
-    if (storeKeys[id]) {
-      throw "A store key (%@) already existing for %@ on %@".fmt(storeKey, id, this);
+
+    // ..........................................................
+    // Propagation Support
+    // 
+
+    /**
+      Like the original SC.Record.storeKeyFor,
+      but when a storeKey is generated, it will
+      propagate it to superclasses.
+
+      @see SC.Record.storeKeyFor
+    */
+    storeKeyFor: function(id) {
+      var storeKeys = this.storeKeysById(),
+          ret = storeKeys[id];
+
+      if (!ret) {
+        ret = SC.Store.generateStoreKey();
+        SC.Store.idsByStoreKey[ret] = id;
+        SC.Store.recordTypesByStoreKey[ret] = this;
+        storeKeys[id] = ret;
+        this._propagateIdForStoreKey(id, ret);
+      }
+
+      return ret ;
+    },
+
+
+    // ..........................................................
+    // Internal Support
+    // 
+
+    extend: function() {
+      var ret = oldExtend.apply(this, arguments);
+
+      if (ret.prototype.hasOwnProperty('isPolymorphic')) {
+        ret.isPolymorphic = ret.prototype.isPolymorphic;
+        delete ret.prototype.isPolymorphic;
+      }
+
+      return ret;
+    },
+
+    /** @private */
+    _propagateIdForStoreKey: function(id, storeKey) {
+      var superclass = this.superclass;
+      if (this.isPolymorphic) {
+        while (superclass.isPolymorphic && superclass !== SC.Record) {
+          superclass._storeKeyForId(storeKey, id);
+          superclass = superclass.superclass;
+        }
+      }
+    },
+
+    /** @private */
+    _storeKeyForId: function(storeKey, id) {
+      var storeKeys;
+      if (!this.isPolymorphic) return;
+
+      /*
+        TODO [CC] SC.Store.recordTypesByStoreKey will be broken
+      */
+
+      storeKeys = this.storeKeysById();
+      if (storeKeys[id]) {
+        throw "A store key (%@) already existing for %@ on %@".fmt(storeKey, id, this);
+      }
+
+      storeKeys[id] = storeKey;
     }
 
-    storeKeys[id] = storeKey;
-  }
-
-});
+  });
+})();
