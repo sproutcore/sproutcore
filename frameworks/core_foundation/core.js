c7393e87f4c3669e784016d93cac49c50f33c9b8
Colin Campbell <colin@strobecorp.com>
2011-04-11 22:37:27 -0230

    Move SC string localization support to Core Foundation (where it belongs) and small code shuffle in CF

--- frameworks/core_foundation/core.js
+++ frameworks/core_foundation/core.js
@@ -159,37 +159,31 @@ SC.mixin(/** @lends SC */ {
     }
     
     return ret ;
-  }
-}) ;
+  },
 
-SC.mixin(Function.prototype, /** @scope Function.prototype */ {
+
+  // ..........................................................
+  // LOCALIZATION SUPPORT
+  //
+
   /**
-    Creates a timer that will execute the function after a specified 
-    period of time.
-    
-    If you pass an optional set of arguments, the arguments will be passed
-    to the function as well.  Otherwise the function should have the 
-    signature:
-    
-        function functionName(timer)
+    Known loc strings
 
-    @param target {Object} optional target object to use as this
-    @param interval {Number} the time to wait, in msec
-    @returns {SC.Timer} scheduled timer
+    @type Hash
   */
-  invokeLater: function(target, interval) {
-    if (interval === undefined) interval = 1 ;
-    var f = this;
-    if (arguments.length > 2) {
-      var args = SC.$A(arguments).slice(2,arguments.length);
-      args.unshift(target);
-      // f = f.bind.apply(f, args) ;
-      var func = f ;
-      // Use "this" in inner func because it get its scope by 
-      // outer func f (=target). Could replace "this" with target for clarity.
-      f = function() { return func.apply(this, args.slice(1)); } ;
-    }
-    return SC.Timer.schedule({ target: target, action: f, interval: interval });
-  }    
+  STRINGS: {},
 
-});
+  /**
+    This is a simplified handler for installing a bunch of strings.  This
+    ignores the language name and simply applies the passed strings hash.
+
+    @param {String} lang the language the strings are for
+    @param {Hash} strings hash of strings
+    @returns {SC} The receiver, useful for chaining calls to the same object.
+  */
+  stringsFor: function(lang, strings) {
+    SC.mixin(SC.STRINGS, strings);
+    return this ;
+  }
+
+}) ;
