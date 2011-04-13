48ec6deba3f16426c191547fcfb23dbd1bba4384
Peter Wagenet <peter.wagenet@gmail.com>
2011-04-13 11:38:41 -0700

    Added icon examples to TestControls

--- apps/test_controls/resources/radio_page.js
+++ apps/test_controls/resources/radio_page.js
@@ -11,7 +11,7 @@ TestControls.radioPage = SC.View.design({
   form: SC.FormView.design({
     classNames: ["sample_controls"],
     layout: { left: 20, top: 40, right: 20, bottom: 40 },
-    childViews: "header normal disabled multiple horizontal".w(),
+    childViews: "header normal disabled multiple horizontal icon".w(),
     // Plain Views
     header: SC.LabelView.design({
       fillWidth: YES,
@@ -43,6 +43,12 @@ TestControls.radioPage = SC.View.design({
       layoutDirection: SC.LAYOUT_HORIZONTAL,
       items: [ { title: "One", value: "one" },{ title: "Two", value: "two" },{ title: "Three", value: "three" } ],
       itemTitleKey: "title", itemValueKey: "value",
+      value: null
+    })),
+    icon: SC.FormView.row(SC.RadioView.design({
+      layout: { width: 150, height: 60},
+      items: [ { title: "One", value: "one", icon: "sc-icon-info-16" },{ title: "Two", value: "two" },{ title: "Three", value: "three" } ],
+      itemTitleKey: "title", itemValueKey: "value", itemIconKey: "icon",
       value: null
     }))
   })
