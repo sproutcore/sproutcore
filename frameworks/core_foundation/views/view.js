67ff3bbe8d22b161a073bf8e75925d060e27b5dd
Colin Campbell <colin@strobecorp.com>
2011-04-15 17:00:57 -0230

    Move some necessary updateLayerLocation code from SC.View to SC.CoreView

--- frameworks/core_foundation/views/view.js
+++ frameworks/core_foundation/views/view.js
@@ -494,6 +494,31 @@ SC.CoreView.reopen(
   },
 
   /**
+    Set to YES when the view's layer location is dirty.  You can call
+    updateLayerLocationIfNeeded() to clear this flag if it is set.
+
+    @property {Boolean}
+  */
+  layerLocationNeedsUpdate: NO,
+
+  /**
+    Calls updateLayerLocation(), but only if the view's layer location
+    currently needs to be updated.  This method is called automatically at
+    the end of a run loop if you have called parentViewDidChange() at some
+    point.
+
+    @property {Boolean} force This property is ignored.
+    @returns {SC.View} receiver
+    @test in updateLayerLocation
+  */
+  updateLayerLocationIfNeeded: function(force) {
+    if (this.get('layerLocationNeedsUpdate')) {
+      this.updateLayerLocation() ;
+    }
+    return this ;
+  },
+
+  /**
     This method is called when a view changes its location in the view
     hierarchy.  This method will update the underlying DOM-location of the
     layer so that it reflects the new location.
