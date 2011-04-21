59ed39a1e208f86bf120c79bfa28d52a5e99f2d9
Michael Cohen <michael.lee.cohen@gmail.com>
2011-04-20 22:58:36 -0700

    Refactored statechart code so that classes and class extensions are placed into their own respective files

--- frameworks/statechart/debug/monitor.js
+++ frameworks/statechart/debug/monitor.js
@@ -1,6 +1,7 @@
 // ==========================================================================
 // Project:   SC.Statechart - A Statechart Framework for SproutCore
 // Copyright: ©2010, 2011 Michael Cohen, and contributors.
+//            Portions @2011 Apple Inc. All rights reserved.
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
 
@@ -85,71 +86,6 @@ SC.StatechartMonitor = SC.Object.extend({
     seq += "]";
 
     return seq;
-  }
-  
-});
-
-SC.StatechartSequenceMatcher = SC.Object.extend({
-  
-  statechartMonitor: null,
-  
-  position: 0,
-  
-  match: YES,
-  
-  begin: function() {
-    this.position = -1;
-    this.match = YES;
-    return this;
-  },
-  
-  end: function() {
-    return this.match;
-  },
-  
-  entered: function() {
-    return this._doCheck('entered', arguments);
-  },
-  
-  exited: function() {
-    return this._doCheck('exited', arguments);
-  },
-  
-  _doCheck: function(event, args) {
-    var i = 0,
-        len = args.length,
-        seqItem = null,
-        arg = null,
-        seq = this.statechartMonitor.sequence;
-        
-    for (; i < len; i += 1) {
-      this.position += 1;
-  
-      if (this.position >= seq.length) {
-        this.match = NO;
-        return this;
-      }
-      
-      seqItem = seq[this.position];
-      if (!seqItem[event]) {
-        this.match = NO;
-        return this;
-      }
-      
-      arg = args[i];
-      if (SC.typeOf(arg) === SC.T_OBJECT) {
-        if (seqItem[event] !== arg) {
-          this.match = NO;
-          return this;
-        }
-      } 
-      else if (seqItem[event].get('name') !== arg) {
-        this.match = NO;
-        return this;
-      }
-    }
-  
-    return this;
   }
   
 });
