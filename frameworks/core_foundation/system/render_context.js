95165929f38c08ffe16105251d30918456cad1d9
Sven Fuchs <svenfuchs@artweb-design.de>
2011-04-27 19:31:47 +0200

    fix RenderContext.element() to use SC.$ instead of a custom factory

--- frameworks/core_foundation/system/render_context.js
+++ frameworks/core_foundation/system/render_context.js
@@ -259,40 +259,7 @@ SC.RenderContext = SC.Builder.create(/** SC.RenderContext.fn */ {
     @returns {DOMElement} the element
   */
   element: function() {
-    if (this._elem) return this._elem;
-
-    // create factory div if needed
-    var K       = SC.RenderContext,
-        factory = K.factory,
-        ret, child;
-
-    if (!factory) {
-      factory = K.factory = document.createElement('div');
-    }
-    factory.innerHTML = this.join();
-
-    // In IE something weird happens when reusing the same element.
-    // After setting innerHTML, the innerHTML of the element in the previous
-    // view turns blank.  It seems that there is something weird with their
-    // garbage  collection algorithm. I tried just removing the nodes after
-    // keeping a  reference to the first child, but it didn't work.  I ended
-    // up cloning the first child.
-    if (SC.browser.msie) {
-      if (factory.innerHTML.length > 0) {
-        child = factory.firstChild.cloneNode(true);
-        factory.innerHTML = '';
-      }
-      else {
-        child = null;
-      }
-    }
-    else {
-      // Faster path (avoiding the unnecessary node clone) for non-IE
-      // browsers.
-      child = factory.firstChild;
-    }
-
-    return child ;
+    return this._elem ? this._elem : SC.$(this.join())[0]
   },
 
   /**
