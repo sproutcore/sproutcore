3a7cc56a3977e530bf59461c838fc26d8fca9107
Gregory Moeck <gmoeck@gmail.com>
2011-05-09 09:16:30 -0700

    Potential update to button view

--- frameworks/core_foundation/tests/controls/button.js
+++ frameworks/core_foundation/tests/controls/button.js
@@ -1,78 +1,78 @@
-var button;
+var button, wasTriggered, actionObject, rootResponder, pane, expectedAction;
+var calledAction, calledTarget, calledSender, calledPane, calledContext, calledFirstResponder;
 
 module("SC.Button", {
   setup: function() {
-    button = SC.Button.create();
-
-    pane = SC.MainPane.create({
-      childViews: [button]
+    expectedAction = 'myAction';
+    targetObject = SC.Object.create({
+      myAction: function() {
+      }
     });
-    pane.append();
-  },
 
-  teardown: function() {
-    pane.remove();
+    //This is ugly because we don't bundle a mocking framework
+    pane = SC.Object.create({
+      rootResponder: {
+        sendAction: function(action, target, sender, pane, context, firstResponder) {
+          calledAction         = action;
+          calledTarget         = target;
+          calledSender         = sender;
+          calledPane           = pane;
+          calledContext        = context;
+          calledFirstResponder = firstResponder;
+          wasTriggered = true;
+        }
+      }
+    });
+
+    button = SC.Button.create({
+      target: targetObject,
+      action: 'myAction',
+      pane: pane
+    });
+
+    wasTriggered = false;
+    calledAction = calledTarget = calledSender = calledContext = calledFirstResponder = undefined;
   }
 });
 
-function synthesizeEvent(type, view) {
-  var event = new SC.Event({
-    type: type,
-    target: view.get('layer')
-  });
-  SC.RootResponder.responder[type](event);
-}
+test("#mouseUp - triggers the action when mouse is over the button", function() {
+  button.set('isActive', true);
 
-test("should trigger an action when clicked", function() {
-  var wasClicked = false;
+  button.mouseUp();
 
-  var actionObject = SC.Object.create({
-    myAction: function() {
-      wasClicked = true;
-    }
-  });
-
-  button.target = actionObject;
-  button.action = 'myAction';
-
-  synthesizeEvent('mousedown', button);
-  synthesizeEvent('mouseup', button);
-
-  ok(wasClicked);
+  ok(wasTriggered, 'should trigger the action');
+  //this is ugly because we don't bundle a mocking framework
+  equals(calledAction,   expectedAction, 'root responder should be called with the expected action');
+  equals(calledTarget,   targetObject,   'root responder should be called with the expected target');
+  equals(calledSender,   button,         'root responder should be called with the button as the sender');
+  equals(calledPane,     pane,           'root responder should be called with the button\'s pane as the pane');
+  equals(calledContext,  null,           'root responder should be called with null for the context');
 });
 
-test("should not trigger action if mouse leaves area before mouseup", function() {
-  var wasClicked = false;
+test("#mouseUp - does not trigger action if mouse is not over the button", function() {
+  button.set('isActive', false);
 
-  var actionObject = SC.Object.create({
-    myAction: function() {
-      wasClicked = true;
-    }
-  });
-
-  var otherButton = SC.Button.create();
-  pane.appendChild(otherButton);
-
-  button.target = actionObject;
-  button.action = 'myAction';
-
-  synthesizeEvent('mousedown', button);
-  synthesizeEvent('mousemove', button);
-  ok(button.get('isActive'), "becomes active when hovered");
-  synthesizeEvent('mousemove', otherButton);
-  ok(!button.get('isActive'), "loses active state if mouse exits");
-  synthesizeEvent('mouseup', button);
-
-  ok(!wasClicked);
-
-  wasClicked = false;
-
-  synthesizeEvent('mousedown', button);
-  synthesizeEvent('mousemove', button);
-  synthesizeEvent('mousemove', otherButton);
-  synthesizeEvent('mousemove', button);
-  synthesizeEvent('mouseup', button);
-
-  ok(wasClicked);
+  button.mouseUp();
+  ok(!wasTriggered, 'should not trigger the action');
 });
 
+test('#mouseUp - makes the button no longer active', function () {
+  button.set('isActive', true);
+  button.mouseUp();
+
+  equals(button.get('isActive'), false, 'should set the button to not be active');
+});
+
+test('#mouseDown - makes the button active', function() {
+  button.set('isActive', false);
+  button.mouseDown();
+
+  equals(button.get('isActive'), true, 'should set the button to be active');
+});
+
+test('#mouseExited - makes but button no longer active', function() {
+  button.set('isActive', false);
+  button.mouseExited();
+
+  equals(button.get('isActive'), false, 'should set the button to no longer be active');
+});
