9b996b315786fa41f89d1badd59ba690ab862c42
Alex Iskander <alexiskander@me.com>
2011-05-11 23:39:14 -0700

    Add 'extends' to list of reserved words (Safari reserves it, apparently)

--- frameworks/handlebars/handlebars.js
+++ frameworks/handlebars/handlebars.js
@@ -1389,7 +1389,7 @@ Handlebars.JavaScriptCompiler = function() {};
 
   var reservedWords = ("break case catch continue default delete do else finally " +
                        "for function if in instanceof new return switch this throw " + 
-                       "try typeof var void while with null true false").split(" ");
+                       "try typeof var void while with null true false extends").split(" ");
 
   compilerWords = JavaScriptCompiler.RESERVED_WORDS = {};
 
