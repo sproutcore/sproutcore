20463e731dce4feabbdb85faed0967ca6f86497f
Alex Iskander <alexiskander@me.com>
2011-05-13 17:49:11 -0700

    Additional FormRowView tests.

--- frameworks/experimental/frameworks/forms/tests/views/form_row.js
+++ frameworks/experimental/frameworks/forms/tests/views/form_row.js
@@ -9,7 +9,54 @@ var pane = SC.Pane.create();
 module("Forms - Form Row", {
   setup: function() {
     pane.append();
+  },
+
+  teardown: function() {
+    pane.remove();
   }
+});
+
+// - binds content
+// - sets contentValueKey, respecting _singleField
+
+test("Binds content to child views", function() {
+  SC.RunLoop.begin();
+  var test = SC.FormRowView.create({
+    childViews: 'a b'.w(),
+    a: SC.View.create(),
+    b: SC.View.create({ content: "HI" })
+  });
+  var content = SC.Object.create();
+  test.set('content', content);
+  SC.RunLoop.end();
+
+  equals(test.a.get('content'), content, "Content was set on view without content");
+  equals(test.b.get('content'), "HI", "Content stayed the same on view with content");
+});
+
+test("Sets contentValueKey on children", function() {
+  SC.RunLoop.begin();
+  var test = SC.FormRowView.create({
+    childViews: 'a b'.w(),
+    a: SC.View.create(),
+    b: SC.View.create({ contentValueKey: "HI" })
+  });
+  SC.RunLoop.end();
+
+  equals(test.a.get('contentValueKey'), 'a', "contentValueKey was set on the view without one");
+  equals(test.b.get('contentValueKey'), "HI", "contentValueKey was not set on view with one");
+});
+
+test("Sets contentValueKey to own key for _singleField", function() {
+  SC.RunLoop.begin();
+  var test = SC.FormRowView.create({
+    formKey: "theFormKey",
+    childViews: '_singleField'.w(),
+    _singleField: SC.View.create()
+  });
+  SC.RunLoop.end();
+
+  equals(test._singleField.get('contentValueKey'), 'theFormKey', "contentValueKey is set to row's formKey if the view is a _singleField");
 });
 
 test("Measuring label width", function() {
@@ -29,4 +76,22 @@ test("Measuring label width", function() {
 
   var newSize = row.get('rowLabelMeasuredSize');
   ok(newSize > size, "Size grew when label text did");
+});
+
+test("Creating Form Rows", function() {
+  var ret = SC.FormRowView.row("the label", SC.View.extend({
+    aProperty: YES
+  }), { extendedRow: YES });
+  
+  ret = SC.FormRowView.create(ret);
+
+  equals(ret.extendedRow, YES, "Row was extended");
+  equals(ret._singleField.get('aProperty'), YES, "Property was defined on field");
+  equals(ret.get('label'), "the label");
+
+  ret = SC.FormRowView.row(SC.View.extend({ aProperty: YES }), { extendedRow: YES });
+  ret = SC.FormRowView.create(ret);
+
+  equals(ret.extendedRow, YES, "Row was extended");
+  equals(ret._singleField.get('aProperty'), YES, "Property was defined on field");
 });
