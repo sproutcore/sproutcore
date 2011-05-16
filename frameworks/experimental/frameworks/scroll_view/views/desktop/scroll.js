eef9562d19490a84e8bcc5d08ce350fdedc059e5
Tim Evans <tim.evans@junctionnetworks.com>
2011-05-16 15:25:25 -0400

    experimental/scroll_view: inverting scroll behavior for smoother scrolling

--- frameworks/experimental/frameworks/scroll_view/views/desktop/scroll.js
+++ frameworks/experimental/frameworks/scroll_view/views/desktop/scroll.js
@@ -28,6 +28,56 @@ SC.DesktopScrollView = SC.CoreScrollView.extend(
 
   verticalScrollerView: SC.ScrollerView,
 
+  /**
+    The current horizontal scroll offset.
+    Changing this value will update both the `contentView`
+    and the horizontal scroller, if there is one.
+
+    @field
+    @type Number
+    @default 0
+   */
+  horizontalScrollOffset: function (key, value) {
+    if (arguments.length === 2) {
+      var minOffset = this.minimumHorizontalScrollOffset(),
+          maxOffset = this.get('maximumHorizontalScrollOffset'),
+          layer = this.getPath('containerView.layer'),
+          offset = Math.max(minOffset, Math.min(maxOffset, value));
+
+      this._scroll_horizontalScrollOffset = offset;
+      if (layer && layer.scrollLeft !== offset) {
+        layer.scrollLeft = offset;
+      }
+    }
+
+    return this._scroll_horizontalScrollOffset || 0;
+  }.property().cacheable(),
+
+  /**
+    The current vertical scroll offset.
+    Changing this value will update both the `contentView`
+    and the vertical scroller, if there is one.
+
+    @field
+    @type Number
+    @default 0
+   */
+  verticalScrollOffset: function (key, value) {
+    if (arguments.length === 2) {
+      var minOffset = this.get('minimumVerticalScrollOffset'),
+          maxOffset = this.get('maximumVerticalScrollOffset'),
+          layer = this.getPath('containerView.layer'),
+          offset = Math.max(minOffset, Math.min(maxOffset, value));
+
+      this._scroll_verticalScrollOffset = offset;
+      if (layer && layer.scrollTop !== offset) {
+        layer.scrollTop = offset;
+      }
+    }
+
+    return this._scroll_verticalScrollOffset || 0;
+  }.property().cacheable(),
+
   // ..........................................................
   // SCROLL WHEEL SUPPORT
   //
@@ -35,43 +85,49 @@ SC.DesktopScrollView = SC.CoreScrollView.extend(
   containerView: SC.ContainerView.extend({
     classNames: ['sc-scrollable'],
 
+    /** @private
+      Remove the "scroll" event handler for the layer.
+     */
     willDestroyLayer: function () {
       SC.Event.remove(this.get('layer'), 'scroll', this, this.scroll);
     },
 
+    /** @private
+      Attach the "scroll" event handler for the layer.
+     */
     didCreateLayer: function () {
       SC.Event.add(this.get('layer'), 'scroll', this, this.scroll);
     },
 
+    /** @private
+      Notify the container that the scroll offsets have changed.
+     */
     scroll: function (evt) {
       var layer = this.get('layer'),
           scrollTop = layer.scrollTop,
           scrollLeft = layer.scrollLeft,
           parentView = this.get('parentView');
 
-      parentView.scrollTo(scrollLeft, scrollTop);
+      // I'm using `verticalScrollOffset` and `horizontalScrollOffset`
+      // as proxies for the the actual scroll offsets.
+
+      // Since we know what the offsets are (we got the event), this
+      // needs to set the cached value, and let properties know that
+      // the offset changed.
+      if (this._scroll_verticalScrollOffset !== scrollTop) {
+        parentView.propertyWillChange('verticalScrollOffset');
+        parentView._scroll_verticalScrollOffset = scrollTop;
+        parentView.propertyDidChange('verticalScrollOffset');
+      }
+
+      if (this._scroll_horizontalScrollOffset !== scrollLeft) {
+        parentView.propertyWillChange('horizontalScrollOffset');
+        parentView._scroll_horizontalScrollOffset = scrollLeft;
+        parentView.propertyDidChange('horizontalScrollOffset');
+      }
+
       return parentView.get('canScrollHorizontal') || parentView.get('canScrollVertical');
     }
-  }),
-
-  adjustElementScroll: function() {
-    var container = this.get('containerView'),
-        layer = container && container.get('layer'),
-        verticalScrollOffset = this.get('verticalScrollOffset'),
-        horizontalScrollOffset = this.get('horizontalScrollOffset');
-
-    if (layer) {
-      if (verticalScrollOffset !== this._verticalScrollOffset) {
-        layer.scrollTop = verticalScrollOffset;
-        this._verticalScrollOffset = verticalScrollOffset;
-      }
-
-      if (horizontalScrollOffset !== this._horizontalScrollOffset) {
-        layer.scrollLeft = horizontalScrollOffset;
-        this._horizontalScrollOffset = horizontalScrollOffset;
-      }
-    }
-    return sc_super();
-  }
+  })
 
 });
