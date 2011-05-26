1aa506097e9b58692438f63ad29d2080c45f9f86
Juan Pinzon <juanpin@me.com>
2011-05-26 14:29:06 -0700

    Fixin popup button render delegate

--- frameworks/desktop/render_delegates/popup_button.js
+++ frameworks/desktop/render_delegates/popup_button.js
@@ -9,13 +9,13 @@
 /**
  * Renders and updates the HTML representation of a popup button.
  */
-SC.BaseTheme.popupButtonRenderDelegate = SC.RenderDelegate.create({
+SC.BaseTheme.popupButtonRenderDelegate = SC.BaseTheme.buttonRenderDelegate.create({
   render: function(dataSource, context) {
     context.attr('aria-haspopup', 'true');
-    dataSource.get('theme').buttonRenderDelegate.render(dataSource, context);
+    sc_super();
   },
 
   update: function(dataSource, jQuery) {
-    dataSource.get('theme').buttonRenderDelegate.update(dataSource, jQuery);
+    sc_super();
   }
 });
