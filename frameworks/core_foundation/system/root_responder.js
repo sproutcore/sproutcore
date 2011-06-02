c781d3b19daf9a7f6d4ae29d20c214dc6c5f70b7
Alex Iskander <aiskander@apple.com>
2011-06-01 17:50:51 -0700

    Stop crashing when there is no main pane.

--- frameworks/core_foundation/system/root_responder.js
+++ frameworks/core_foundation/system/root_responder.js
@@ -351,11 +351,14 @@ SC.RootResponder = SC.Object.extend(
       // If the app is getting focus again set the first responder to the first
       // valid firstResponder view in the view's tree
       if(!SC.TABBING_ONLY_INSIDE_DOCUMENT){
-        var mainPane = this.get('mainPane'),
-        nextValidKeyView = mainPane.get('nextValidKeyView');
-        if (nextValidKeyView) mainPane.makeFirstResponder(nextValidKeyView);
+        var mainPane = this.get('mainPane');
+
+        if (mainPane) {
+          nextValidKeyView = mainPane.get('nextValidKeyView');
+          if (nextValidKeyView) mainPane.makeFirstResponder(nextValidKeyView);
+        }
       }
-      
+
       SC.run(function() {
         this.set('hasFocus', YES);
       }, this);
