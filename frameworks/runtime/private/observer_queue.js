7d45186ad4f04482964567c59c7d08c115d38f44
Piotr Steininger <piotr.steininger@threepillarglobal.com>
2011-06-06 17:02:02 -0400

    fixed a null pointer exception.

--- frameworks/runtime/private/observer_queue.js
+++ frameworks/runtime/private/observer_queue.js
@@ -74,7 +74,7 @@ SC.Observers = {
     idx = this.queue.length; queue = this.queue ;
     while(--idx >= 0) {
       item = queue[idx] ;
-      if ((item[0] === propertyPath) && (item[1] === target) && (item[2] == method) && (item[3] === pathRoot)) queue[idx] = null ;
+      if (item && (item[0] === propertyPath) && (item[1] === target) && (item[2] == method) && (item[3] === pathRoot)) queue[idx] = null ;
     }
   },
 
