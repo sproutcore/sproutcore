a9a32bc90e0ec520ea94a44241a95d640cd91970
Public Keating <publickeating@me.com>
2011-07-06 12:58:02 -0600

    Whitespace.

--- frameworks/core_foundation/panes/pane.js
+++ frameworks/core_foundation/panes/pane.js
@@ -260,8 +260,8 @@ SC.Pane = SC.View.extend(SC.ResponderContext,
     // if we are currently key pane, then notify key views of change also
     if (isKeyPane) {
       if (current) { current.tryToPerform('willLoseKeyResponderTo', view); }
-      if (view) { 
-        view.tryToPerform('willBecomeKeyResponderFrom', current); 
+      if (view) {
+        view.tryToPerform('willBecomeKeyResponderFrom', current);
       }
     }
 
@@ -282,10 +282,10 @@ SC.Pane = SC.View.extend(SC.ResponderContext,
 
     // and notify again if needed.
     if (isKeyPane) {
-      if (view) { 
+      if (view) {
         view.tryToPerform('didBecomeKeyResponderFrom', current); }
-      if (current) { 
-        current.tryToPerform('didLoseKeyResponderTo', view); 
+      if (current) {
+        current.tryToPerform('didLoseKeyResponderTo', view);
       }
     }
 
