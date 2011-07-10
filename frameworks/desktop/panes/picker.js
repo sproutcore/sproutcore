sproutcore/sproutcore

commit daa3c3cbb864c81a5c1511d4126644773cb5ecf3

Always recompute anchorElement, this fixes issues caused when the anchor is in layer that is regenerated

frameworks/desktop/panes/picker.js
@@ -357,6 +357,9 @@ SC.PickerPane = SC.PalettePane.extend(
   */
   repositionOnWindowResize: YES,
   
+  _anchorView: null,
+  _anchorHTMLElement: null,
+  
   /**
     Displays a new picker pane.
 
@@ -367,17 +370,15 @@ SC.PickerPane = SC.PalettePane.extend(
     @returns {SC.PickerPane} receiver
   */
   popup: function(anchorViewOrElement, preferType, preferMatrix, pointerOffset) {
-    var anchor;
     if(anchorViewOrElement){
       if (anchorViewOrElement.isView) {
-        anchor = anchorViewOrElement.get('layer');
+        this._anchorView = anchorViewOrElement;
         this._setupScrollObservers(anchorViewOrElement);
       } else {
-        anchor = anchorViewOrElement;
+        this._anchorHTMLElement = anchorViewOrElement;
       }
     }
     this.beginPropertyChanges();
-    this.set('anchorElement',anchor) ;
     if (preferType) this.set('preferType',preferType) ;
     if (preferMatrix) this.set('preferMatrix',preferMatrix) ;
     if (pointerOffset) this.set('pointerOffset',pointerOffset) ;
@@ -867,6 +868,13 @@ SC.PickerPane = SC.PalettePane.extend(
     this._removeScrollObservers();
     return sc_super();
   },
+  
+  
+  /** Figure out what is the anchor element */
+  anchorElement: function() {
+    return this._anchorView ? this._anchorView.get('layer') : this.anchorHTMLElement;
+  }.property('layer').cacheable(),
+  
   
   /** @private
     Internal method to hide the overflow on the body to make sure we don't 

frameworks/desktop/panes/menu.js
@@ -276,14 +276,13 @@ SC.MenuPane = SC.PickerPane.extend(
     this.beginPropertyChanges();
     if(anchorViewOrElement){
       if (anchorViewOrElement.isView) {
-        anchor = anchorViewOrElement.get('layer');
+        this._anchorView = anchorViewOrElement;
         this._setupScrollObservers(anchorViewOrElement);
       } else {
-        anchor = anchorViewOrElement;
+        this._anchorHTMLElement = anchorViewOrElement;
       }
     }
-    this.set('anchorElement',anchor) ;
-    this.set('anchor',anchorViewOrElement);
+   // this.set('anchor',anchorViewOrElement);
     if (preferMatrix) this.set('preferMatrix',preferMatrix) ;
 
     this.adjust('height', this.get('menuHeight'));

commit ca1c7f33a98ca6c19baff7cd82b6a0b2d4184ee2

Address regressions introduced in daa3c3cb.

frameworks/desktop/panes/picker.js
@@ -871,8 +871,28 @@ SC.PickerPane = SC.PalettePane.extend(
   
   
   /** Figure out what is the anchor element */
-  anchorElement: function() {
-    return this._anchorView ? this._anchorView.get('layer') : this.anchorHTMLElement;
+  anchorElement: function(key, value) {
+    var anchorView;
+
+    if (value === undefined) {
+      // Getting the value.
+      anchorView = this._anchorView;
+      return anchorView ? anchorView.get('layer') : this._anchorHTMLElement;
+    }
+    else {
+      // Setting the value.
+      if (!value) {
+        throw "You must set 'anchorElement' to either a view or a DOM element";
+      }
+      else if (value.isView) {
+        this._anchorView        = value;
+        this._anchorHTMLElement = null;
+      }
+      else {
+        this._anchorView        = null;
+        this._anchorHTMLElement = value;
+      }
+    }
   }.property('layer').cacheable(),
   
   