faf30c0d1b3e74f8b69e0199903658a9d73e122d
Alex Iskander <aiskander@apple.com>
2011-07-13 16:30:10 -0700

    mouseDown should call becomeFirstResponder so that if another text field is blurring we don't say "Hey! No-one is first responder anymore"

--- frameworks/foundation/views/text_field.js
+++ frameworks/foundation/views/text_field.js
@@ -1073,6 +1073,7 @@ SC.TextFieldView = SC.FieldView.extend(SC.StaticLayout, SC.Editable,
   mouseDown: function(evt) {
     var fieldValue = this.get('fieldValue'); // use 'fieldValue' since we want actual text
     this._txtFieldMouseDown=YES;
+    this.becomeFirstResponder();
     if (!this.get('isEnabled')) {
       evt.stop();
       return YES;
