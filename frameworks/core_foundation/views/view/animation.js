0067bde8fdc35123f4e9691f21e9be799b93831b
Juan Pinzon <juanpin@me.com>
2011-07-21 15:23:39 -0700

    Splitting support for accelarated layer from animation code

--- frameworks/core_foundation/views/view/animation.js
+++ frameworks/core_foundation/views/view/animation.js
@@ -136,47 +136,5 @@ SC.View.reopen(
   transitionDidEnd: function(evt){
     // WARNING: Sometimes this will get called more than once for a property. Not sure why.
     this.get('layoutStyleCalculator').transitionDidEnd(evt);
-  },
-
-  /**
-    Setting wantsAcceleratedLayer to YES will use transforms to move the
-    layer when available. On some platforms transforms are hardware accelerated.
-  */
-  wantsAcceleratedLayer: NO,
-
-  /**
-    Specifies whether transforms can be used to move the layer.
-  */
-  hasAcceleratedLayer: function(){
-    if (this.get('wantsAcceleratedLayer') && SC.platform.supportsAcceleratedLayers) {
-      var layout = this.get('layout'),
-          animations = layout.animate,
-          AUTO = SC.LAYOUT_AUTO,
-          key;
-
-      if (animations && (animations.top || animations.left)) {
-        for (key in animations) {
-          // If we're animating other transforms at different speeds, don't use acceleratedLayer
-          if (
-            SC.CSS_TRANSFORM_MAP[key] &&
-            ((animations.top && animations.top.duration !== animations[key].duration) ||
-             (animations.left && animations.left.duration !== animations[key].duration))
-          ) {
-            return NO;
-          }
-        }
-      }
-
-      // loose comparison used instead of (layout.X === null || layout.X === undefined)
-      if (
-        layout.left != null && !SC.isPercentage(layout.left) && layout.left !== AUTO &&
-        layout.top != null && !SC.isPercentage(layout.top) && layout.top !== AUTO &&
-        layout.width != null && !SC.isPercentage(layout.width) && layout.width !== AUTO &&
-        layout.height != null && !SC.isPercentage(layout.height) && layout.height !== AUTO
-      ) {
-       return YES;
-      }
-    }
-    return NO;
-  }.property('wantsAcceleratedLayer').cacheable()
+  }
 });
