e2a47d4630fae070b1c3d3679ed748e3c9a1a90c
Sudarshan Bhat <sudarshan.bhat@gmail.com>
2011-08-17 23:10:08 -0700

    Alex: add begin/endLiveResize so we know when resize begins and ends.

--- frameworks/experimental/frameworks/split_view/mixins/split_thumb.js
+++ frameworks/experimental/frameworks/split_view/mixins/split_thumb.js
@@ -176,6 +176,8 @@ SC.SplitThumb = {
     this._isDragging = YES;
     
     var splitView = this.get('splitView');
+    splitView.beginLiveResize();
+    
     this._scst_mouseStartPosition = splitView.get('layoutDirection') === SC.LAYOUT_HORIZONTAL ? 
       touch.pageX : touch.pageY;
     
@@ -194,7 +196,7 @@ SC.SplitThumb = {
         start = this._scst_childStartPosition;
     
     splitView.adjustPositionForChild(this.get('movesChild'), start + diff);
-    
+
     return YES;
   },
   
@@ -212,7 +214,7 @@ SC.SplitThumb = {
     splitView.adjustPositionForChild(this.get('movesChild'), start + diff);
     
     splitView.set('splitChildCursorStyle', null);
-    
+    splitView.endLiveResize();
     return YES;
   },
 
