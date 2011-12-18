50cef21f3b47bca17a61c3ed16e2cd8569cb589b
Thomas Stachl <thomas@stachl.me>
2011-12-18 17:30:15 +0000

    encoded body should lay more weight on the content type header than on isJSON or isXML.

--- frameworks/ajax/system/request.js
+++ frameworks/ajax/system/request.js
@@ -156,8 +156,12 @@ SC.Request = SC.Object.extend(SC.Copyable, SC.Freezable,
   */
   encodedBody: function() {
     // TODO: support XML
-    var ret = this.get('body');
-    if (ret && this.get('isJSON')) { ret = SC.json.encode(ret); }
+    var ret = this.get('body'),
+        ct = this.header('Content-Type');
+    if (ret && this.get('isJSON') && typeof(ret) !== 'string') {
+      if (ct.test(/urlencoded/)) { ret = $.param(ret); }
+      if (ct.test(/json/)) { ret = SC.json.encode(ret); }
+    }
     return ret;
   }.property('isJSON', 'isXML', 'body').cacheable(),
 
