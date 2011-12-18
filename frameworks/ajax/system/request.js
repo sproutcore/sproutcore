946b1795a2a80032e7bc7f53d44faf0b6abd7150
Thomas Stachl <thomas@stachl.me>
2011-12-18 19:22:03 +0000

    Fixed typo.

--- frameworks/ajax/system/request.js
+++ frameworks/ajax/system/request.js
@@ -159,8 +159,8 @@ SC.Request = SC.Object.extend(SC.Copyable, SC.Freezable,
     var ret = this.get('body'),
         ct = this.header('Content-Type');
     if (ret && this.get('isJSON') && typeof(ret) !== 'string') {
-      if (ct.test(/urlencoded/)) { ret = $.param(ret); }
-      if (ct.test(/json/)) { ret = SC.json.encode(ret); }
+      if (/urlencoded/.test(ct)) { ret = $.param(ret); }
+      if (/json/.test(ct)) { ret = SC.json.encode(ret); }
     }
     return ret;
   }.property('isJSON', 'isXML', 'body').cacheable(),
