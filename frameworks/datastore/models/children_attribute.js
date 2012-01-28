d8edeb4fe4a065e7d4a8545b608824c01af9678f
Michael Krotscheck <krotscheck@gmail.com>
2012-01-27 17:35:17 -0800

    If the defaultValue returned an array of childRecords, the resulting constructed instance would be an array, not a ChildArray. This fixes the issue. The unit test added to show this particular case is line #172 and #180, though I went ahead and verified the content of the record array while I was at it.

--- frameworks/datastore/models/children_attribute.js
+++ frameworks/datastore/models/children_attribute.js
@@ -68,7 +68,46 @@ SC.ChildrenAttribute = SC.ChildAttribute.extend(
     }
     
     return ret;
+  },
+  
+  /**
+    The core handler.  Called from the property.
+    @param {SC.Record} record the record instance
+    @param {String} key the key used to access this attribute on the record
+    @param {Object} value the property value if called as a setter
+    @returns {Object} property value
+  */
+  call: function(record, key, value) {
+    var attrKey = this.get('key') || key, cRef,
+        cacheKey = SC.keyFor('__kid__', SC.guidFor(this));
+    if (value !== undefined) {
+      // this.orphan(record, cacheKey, value);
+      value = this.fromType(record, key, value) ; // convert to attribute.
+      // record[cacheKey] = value;
+    } else {
+      value = record.readAttribute(attrKey);
+      if (SC.none(value) && (value = this.get('defaultValue'))) {
+        if (typeof value === SC.T_FUNCTION) {
+          value = this.defaultValue(record, key, this);
+          // write default value so it doesn't have to be executed again
+          if (record.attributes()) {
+            // Check for an array
+            if (value instanceof Array) {
+              // Instantiate the construct and replace all of the content.
+              console.log(value);
+              value = this.toType(record, key, value).replace(0, value.length, value);
+              console.log(value);
+            } else {
+              record.writeAttribute(attrKey, value, true);
+            }
+          }
+        }
+      } else value = this.toType(record, key, value);
+    }
+  
+    return value ;
   }
+    
 });
 
 
