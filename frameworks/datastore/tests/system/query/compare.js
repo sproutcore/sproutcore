95fcb48590fe2fc6731562b1e262c6215d359fc7
Public Keating <publickeating@me.com>
2012-05-10 15:45:07 -0700

    Fixes syntax error in tests.  Closes issue #777.

--- frameworks/datastore/tests/system/query/compare.js
+++ frameworks/datastore/tests/system/query/compare.js
@@ -4,22 +4,22 @@
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
 /*globals module ok equals same test MyApp */
- 
+
 // test parsing of query string
 var store, storeKey, rec1, rec2, rec3, rec4, rec5, MyApp, q;
 module("SC.Query comparison of records", {
   setup: function() {
-    
+
     SC.RunLoop.begin();
-    
+
     // setup dummy app and store
     MyApp = SC.Object.create({
       store: SC.Store.create()
     });
-    
+
     // setup a dummy model
     MyApp.Foo = SC.Record.extend({});
-    
+
     // load some data
     MyApp.store.loadRecords(MyApp.Foo, [
       { guid: 1, firstName: "John", lastName: "Doe", year: 1974 },
@@ -28,9 +28,9 @@ module("SC.Query comparison of records", {
       { guid: 4, firstName: "Johnny", lastName: "Cash", active: false },
       { guid: 5, firstName: "Bert", lastName: "Berthold", active: true }
     ]);
-    
+
     SC.RunLoop.end();
-    
+
     rec1 = MyApp.store.find(MyApp.Foo,1);
     rec2 = MyApp.store.find(MyApp.Foo,2);
     rec3 = MyApp.store.find(MyApp.Foo,3);
@@ -51,42 +51,42 @@ module("SC.Query comparison of records", {
     rec18 = MyApp.store.createRecord(MyApp.Foo, { firstName: "Amie", lastName: "Frontenac", active: true });
     rec19 = MyApp.store.createRecord(MyApp.Foo, { firstName: "Amelie", lastName: "Auguste", active: true });
     rec20 = MyApp.store.createRecord(MyApp.Foo, { firstName: "Percy", lastName: "Douglas", active: true });
-    
+
     q = SC.Query.create();
   }
 });
- 
- 
+
+
 // ..........................................................
 // TESTS
-// 
+//
 
 test("parse() should work with conditions = null", function(){
   q.parse();
 });
- 
+
 test("building the order", function() {
   // undefined orderBy
   q.orderBy = null;
   q.parse();
   equals(q._order.length, 0, 'order should be empty');
-  
+
   // empty orderBy
   q.orderBy = "";
   q.parse();
   equals(q._order.length, 0, 'order should be empty');
-  
+
   // single property
   q.orderBy = "firstName";
   q.parse();
   equals(q._order[0].propertyName,'firstName', 'propertyName should be firstName');
-  
+
   // more properties
   q.orderBy = "lastName, firstName";
   q.parse();
   equals(q._order[0].propertyName,'lastName', 'propertyName should be lastName');
   equals(q._order[1].propertyName,'firstName', 'propertyName should be firstName');
-  
+
   // more properties with direction
   q.orderBy = "lastName, firstName, year DESC";
   q.parse();
@@ -102,6 +102,8 @@ test("no order should result in comparison by guid", function() {
   q.orderBy = null;
   q.parse();
   equals(q.compare(rec1,rec2), -1, 'guid 1 should be before guid 2');
+});
+
 /**
   This test was added to prove new code that switched from ordering by guid
   if there is no orderBy property on the query to ordering by storeKey if
@@ -147,34 +149,34 @@ test("comparing non existent properties", function() {
   q.parse();
   equals(q.compare(rec5,rec1), -1, 'null should be before 1974');
 });
- 
+
 test("comparing null and boolean properties", function() {
   q.orderBy = "active";
   q.parse();
   equals(q.compare(rec3,rec4), -1, 'null should be before false');
   equals(q.compare(rec4,rec5), -1, 'false should be before true');
 });
- 
+
 test("comparing number properties", function() {
   q.orderBy = "year";
   q.parse();
   equals(q.compare(rec1,rec2), -1, '1974 should be before 1975');
-  
+
   q.orderBy = "year DESC";
   q.parse();
   equals(q.compare(rec1,rec2), 1, '1974 should be after 1975 with DESC');
-}); 
- 
- 
+});
+
+
 test("comparing string properties", function() {
   q.orderBy = "firstName";
   q.parse();
   equals(q.compare(rec1,rec2), 1, 'John should be after Jane');
-  
+
   q.orderBy = "firstName DESC";
   q.parse();
   equals(q.compare(rec1,rec2), -1, 'John should be before Jane with DESC');
-}); 
+});
 
 test("comparing by equal properties should use guid for order", function() {
   q.orderBy = "lastName";
