6bfd5052af6bc5f6d9bf8ff5833a1aa9238efd5d
Public Keating <publickeating@me.com>
2012-05-10 17:47:10 -0700

    Bumps SC.VERSION to 1.8.2.

--- frameworks/runtime/core.js
+++ frameworks/runtime/core.js
@@ -59,7 +59,7 @@ window.SproutCore = window.SproutCore || SC ;
 */
 SC = window.SC; // This is dumb but necessary for jsdoc to get it right
 
-SC.VERSION = '1.8.1';
+SC.VERSION = '1.8.2';
 
 /**
   @private
