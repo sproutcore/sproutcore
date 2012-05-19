50b204f03b7621f93ea925ce3de955c9c0e90e0e
Public Keating <publickeating@me.com>
2012-05-19 12:24:09 -0600

    Removes a bunch of comments that don't mean anything.

--- frameworks/desktop/tests/views/select/methods.js
+++ frameworks/desktop/tests/views/select/methods.js
@@ -53,12 +53,12 @@ module("SC.SelectView",{
           itemTitleKey: 'title',
           itemSortKey: 'pos'
         }),
-        
+
         //view5
         SC.SelectView.extend({
           items: ["My","New", "List"]
         }),
-        
+
         //view6
         SC.SelectView.extend({
           items: ["My","New", "List"],
@@ -74,7 +74,7 @@ module("SC.SelectView",{
     view4 = pane.childViews[3] ;
     view5 = pane.childViews[4] ;
     view6 = pane.childViews[5] ;
-    
+
     pane.append(); // make sure there is a layer...
     SC.RunLoop.end();
   },
@@ -91,8 +91,6 @@ test("Check if valueBinding works", function() {
   equals('World',view4.get('value'),'Value should be') ;
 });
 
-// the test that was here pretty much tested nothing.
-
 //test5
 test("sortObjects() sorts the items of the Drop Down component", function() {
   var obj = view1.get("items");
@@ -103,8 +101,6 @@ test("sortObjects() sorts the items of the Drop Down component", function() {
   equals("You",obj.get(2),'Third item should be') ;
 });
 
-// There was an awful test here. I can't even tell what it was supposed to do.
-
 //test7
 test("isEnabled=NO should add disabled class", function() {
   SC.RunLoop.begin() ;
@@ -113,9 +109,7 @@ test("isEnabled=NO should add disabled class", function() {
   ok(view1.$().hasClass('disabled'), 'should have disabled class') ;
 });
 
-// there is NO need to test binding.
-
-// I think this one is probably somewhat pointless as well, but perhaps some
+// I think this test is probably somewhat pointless, but perhaps some
 // buggy observers being called could break it or something...
 test("Check if setting a value actually changes the selection value", function() {
   SC.RunLoop.begin() ;
@@ -124,8 +118,6 @@ test("Check if setting a value actually changes the selection value", function()
 
   equals(view2.get('value'), 'Menu', 'value of Drop down should change to') ;
 }) ;
-
-// No need to test binding (was test10) (new test10 is way better)
 
 //test10
 test('Setting the view\'s items should not result in an error.', function() {
