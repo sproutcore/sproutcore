2a3a4a8076ea8607d0882da43385e058651b33dd
Public Keating <publickeating@me.com>
2012-05-25 22:29:01 -0600

    Adds unit tests proving that customized classNameBindings (ex. 'isActive:active') don't work.  Also fixes the other unit tests in this file that had misplaced parentheses.	.

--- frameworks/core_foundation/tests/views/view/class_name_bindings_test.js
+++ frameworks/core_foundation/tests/views/view/class_name_bindings_test.js
@@ -26,10 +26,11 @@ test("should apply bound class names to the element", function() {
 
 test("should add, remove, or change class names if changed after element is created", function() {
   var view = SC.View.create({
-    classNameBindings: ['priority', 'isUrgent', 'canIgnore'],
+    classNameBindings: ['priority', 'isUrgent', 'isClassified:classified', 'canIgnore'],
 
     priority: 'high',
     isUrgent: true,
+    isClassified: true,
     canIgnore: false
   });
 
@@ -37,26 +38,33 @@ test("should add, remove, or change class names if changed after element is crea
 
   view.set('priority', 'orange');
   view.set('isUrgent', false);
+  view.set('isClassified', false);
   view.set('canIgnore', true);
 
   ok(view.$().hasClass('orange'), "updates string values");
   ok(!view.$().hasClass('high'), "removes old string value");
 
-  ok(!view.$().hasClass('is-urgent', "removes dasherized class when changed from true to false"));
+  ok(!view.$().hasClass('is-urgent'), "removes dasherized class when changed from true to false");
+  ok(!view.$().hasClass('classified'), "removes customized class name when changed from true to false");
   ok(view.$().hasClass('can-ignore'), "adds dasherized class when changed from false to true");
 });
 
 test("should preserve class names applied via classNameBindings when view layer is updated",
 function(){
   var view = SC.View.create({
-    classNameBindings: ['isUrgent'],
+    classNameBindings: ['isUrgent', 'isClassified:classified'],
+    isClassified: true,
     isUrgent: false
   });
   view.createLayer();
-  ok(!view.$().hasClass('is-urgent', "removes dasherized class when changed from true to false"));
+  ok(!view.$().hasClass('can-ignore'), "does not add false Boolean values as class");
+  ok(view.$().hasClass('classified'), "supports customizing class name for Boolean values");
+  view.set('isClassified', false);
   view.set('isUrgent', true);
-  ok(view.$().hasClass('is-urgent', "adds dasherized class when changed from true to false"));
+  ok(view.$().hasClass('is-urgent'), "adds dasherized class when changed from false to true");
+  ok(!view.$().hasClass('classified'), "removes customized class name when changed from true to false");
   view.set('layerNeedsUpdate', YES);
   view.updateLayer();
-  ok(view.$().hasClass('is-urgent', "still has class when view display property is updated"));
+  ok(view.$().hasClass('is-urgent'), "still has class when view display property is updated");
+  ok(!view.$().hasClass('classified'), "still does not have customized class when view display property is updated");
 });
