6b65373d82d14572c55674f6f6b61cbd8ef45eb4
Public Keating <publickeating@me.com>
2012-08-28 13:16:05 -0600

    Adds test for SC.CollectionFastPath that shows that it throws an exception when requesting itemViewForContentIndex() prior to having reloadIfNeeded() called.

---
+++ frameworks/desktop/tests/mixins/collection_fast_path.js
@@ -0,0 +1,58 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Strobe Inc. and contributors.
+//            portions copyright @2011 Apple Inc.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+var view, del, content;
+
+module("SC.CollectionFastPath", {
+  setup: function() {
+
+    view = SC.CollectionView.create({
+      content: "a b c d e f".w().map(function(x) {
+        return SC.Object.create({ title: x });
+      }),
+      useFastPath: YES
+    });
+
+    exampleView = SC.View.extend({
+      isPoolable: YES,
+      layerIsCacheable: YES
+    });
+
+  },
+
+  teardown: function() {
+    view = null;
+  }
+});
+
+
+/**
+  There was a bug that if you called itemViewForContentIndex() on a Collection
+  with SC.CollectionFastPath mixed in BEFORE it was visible, it would throw an
+  exception (because this._mapView wasn't initialized properly in
+  CollectionFastPath).
+*/
+test("Calling itemViewForContentIndex() before the Collection is visible.", function() {
+  try {
+    var itemView = view.itemViewForContentIndex(0);
+    ok(true, 'Requesting itemViewForContentIndex() should not throw an exception prior to reloadIfNeeded being called.');
+  } catch (ex) {
+    ok(false, 'Requesting itemViewForContentIndex() should not throw an exception prior to reloadIfNeeded being called.');
+  }
+
+  // The next test just shows how that when isVisibleInWindow changes, causing
+  // reloadIfNeeded to be called, then the request would succeed.
+  try {
+    SC.RunLoop.begin();
+    view.set('isVisibleInWindow', YES);
+    SC.RunLoop.end();
+    itemView = view.itemViewForContentIndex(0);
+    ok(true, 'Requesting itemViewForContentIndex() should not throw an exception after reloadIfNeeded being called.');
+  } catch (ex) {
+    ok(false, 'Requesting itemViewForContentIndex() should not throw an exception after reloadIfNeeded being called.');
+  }
+});
