7af902e578b0841eee98931cfac23ff9ef10b917
Public Keating <publickeating@me.com>
2012-09-22 13:52:55 -0400

    Adds some comments to aid further developers a bit and removes the temporary flag on all other SC.TextFieldViews.  This closes #816.

--- frameworks/desktop/panes/modal.js
+++ frameworks/desktop/panes/modal.js
@@ -9,35 +9,35 @@
 /** @class
 
   A modal pane is used to capture mouse events inside a pane that is modal.
-  You normally will not work with modal panes directly, though you may set 
-  the modalPane property to a subclass of this pane when designing custom 
+  You normally will not work with modal panes directly, though you may set
+  the modalPane property to a subclass of this pane when designing custom
   panes.
-  
+
   A modal pane is automatically appended when a pane with isModal set to
   `YES` is made visible and removed when the same pane is hidden.  The only
   purpose of the `ModalPane` is to absorb mouse events so that they cannot
   filter through to the underlying content.
-  
+
   @extends SC.Pane
   @since SproutCore 1.0
 */
 SC.ModalPane = SC.Pane.extend(
 /** @scope SC.ModalPane.prototype */{
-  
+
   /**
     @type Array
     @default ['sc-modal']
     @see SC.View#classNames
   */
   classNames: 'sc-modal',
-  
+
   /** @private */
   _openPaneCount: 0,
-  
+
   /** @private
     Called by a pane just before it appends itself.   The modal pane can
     make itself visible first if needed.
-    
+
     @param {SC.Pane} pane the pane
     @returns {SC.ModalPane} receiver
   */
@@ -53,18 +53,18 @@ SC.ModalPane = SC.Pane.extend(
         this._hideShowTextfields(_tmpPane, NO);
       }
     }
-    return this ;    
-  },  
-  
+    return this ;
+  },
+
   /** @private
     Called by a pane just after it removes itself.  The modal pane can remove
-    itself if needed.   Modal panes only remove themselves when an equal 
+    itself if needed.   Modal panes only remove themselves when an equal
     number of `paneWillAppend()` and `paneDidRemove()` calls are received.
-  
+
     @param {SC.Pane} pane the pane
     @returns {SC.ModalPane} receiver
   */
-  paneDidRemove: function(pane) { 
+  paneDidRemove: function(pane) {
     var _tmpPane;
     this._openPaneCount--;
     var panes = SC.RootResponder.responder.panes;
@@ -80,8 +80,14 @@ SC.ModalPane = SC.Pane.extend(
       if (this.get('isVisibleInWindow')) this.remove();
     }
   },
-  
-  /** @private */
+
+  /** @private
+    If `focusable` is NO all SC.TextFieldViews not belonging to the given
+    pane will have isBrowserFocusable set to NO.  If `focusable` is YES, then
+    all SC.TextFieldViews not belonging to the given pane will have
+    isBrowserFocusable set to YES, unless they previously had it set explictly
+    to NO.
+  */
   _hideShowTextfields: function(pane, focusable){
     var view;
     for(view in SC.View.views){
@@ -92,6 +98,9 @@ SC.ModalPane = SC.Pane.extend(
           // value, use that instead.
           if (view._scmp_isBrowserFocusable !== undefined) {
             focusable = view._scmp_isBrowserFocusable;
+
+            // Clean up entirely.
+            delete view._scmp_isBrowserFocusable;
           }
         } else {
           // Cache the value of isBrowserFocusable. If the text field
@@ -103,13 +112,13 @@ SC.ModalPane = SC.Pane.extend(
       }
     }
   },
-  
+
   /** @private */
   mouseDown: function(evt) {
     var owner = this.get('owner');
     if (owner && owner.modalPaneDidClick) owner.modalPaneDidClick(evt);
   },
-  
+
   /** @private */
   touchStart: function(evt) {
     this.mouseDown(evt);
