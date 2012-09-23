98e7fe8f13f035d2d76c76075f8d3fb144b257e2
Public Keating <publickeating@me.com>
2012-09-23 14:09:09 -0400

    Fixes failing unit tests related to SC.imageQueue, which needs to execute in the context of a RunLoop and which tests weren't cleaning up themselves which caused problems with SC.ImageView tests if run later.

--- frameworks/foundation/tests/system/image_queue.js
+++ frameworks/foundation/tests/system/image_queue.js
@@ -7,14 +7,15 @@
 // ========================================================================
 // SC.imageQueue Test for queue stalling (https://github.com/sproutcore/sproutcore/pull/716)
 // ========================================================================
-/*globals module test ok isObj equals expects */
+/*globals module, test, ok, isObj, equals, expects, start, stop*/
 
+var guardTimeout, firstGoodImageURL, secondGoodImageURL, badImageURL;
 module("Image Queue", {
-    setup: function() {
-		this.guardTimeout = 10000;
-		this.firstGoodImageURL = sc_static('images/sproutcore.png');
-		this.secondGoodImageURL = sc_static('images/sproutcore-logo.png');
-		this.badImageURL = "http://www.sproutcore.com/images/foobar.png";
+  setup: function() {
+		guardTimeout = 30000;
+		firstGoodImageURL = sc_static('images/sproutcore.png');
+		secondGoodImageURL = sc_static('images/sproutcore-logo.png');
+		badImageURL = "http://www.sproutcore.com/images/foobar.png";
 	}
 });
 
@@ -24,61 +25,84 @@ test("Ensure queue is in known state.", function() {
 	SC.imageQueue._foregroundQueue = [];
 	SC.imageQueue._backgroundQueue = [];
 	SC.imageQueue.set('isLoading', NO);
-	
-    equals(SC.imageQueue.activeRequests, 0, "There should be no active requests");
+
+  equals(SC.imageQueue.activeRequests, 0, "There should be no active requests");
 });
 
 test("Attempt to load a non-existent image.", function() {
-	SC.imageQueue.loadImage(this.badImageURL, {action: function(imageUrl, imageOrError) {
-	    // verify request loaded OK
-	    ok(SC.typeOf(imageOrError) === "error", "Image retrieval should fail with error.");
-	    // resume executing tests
-	    start();
-	  }}, 'action', NO);	
+	SC.run(function() {
+		SC.imageQueue.loadImage(badImageURL, {
+			action: function(imageUrl, imageOrError) {
+		    // verify request loaded OK
+		    ok(SC.typeOf(imageOrError) === "error", "Image retrieval should fail with error.");
+		    // resume executing tests
+		    start();
+		  }}, 'action', NO);
+	});
 
-  	stop(this.guardTimeout);
+	stop(guardTimeout);
 });
 
 test("Load a valid image successfully.", function() {
-	SC.imageQueue.loadImage(this.firstGoodImageURL, {action: function(imageUrl, imageOrError) {
-	    // verify request loaded OK
-	    ok(SC.typeOf(imageOrError) !== "error", "Image should be retrieved successfully.");
-	    // resume executing tests
-	    start();
-	  }}, 'action', NO);	
-	  
-    stop(this.guardTimeout);
+	SC.run(function() {
+		SC.imageQueue.loadImage(firstGoodImageURL, {
+			action: function(imageUrl, imageOrError) {
+		    // verify request loaded OK
+		    ok(SC.typeOf(imageOrError) !== "error", "Image should be retrieved successfully.");
+		    // resume executing tests
+		    start();
+		  }}, 'action', NO);
+	});
+
+  stop(guardTimeout);
 });
 
 test("Attempt to reload previous non-existent image.", function() {
-	SC.imageQueue.loadImage(this.badImageURL, {action: function(imageUrl, imageOrError) {
-	    // verify request loaded OK
-	    ok(SC.typeOf(imageOrError) === "error", "Image retrieval should fail with error.");
-	    // resume executing tests
-	    start();
-	  }}, 'action', NO);	
+	SC.run(function() {
+		SC.imageQueue.loadImage(badImageURL, {
+			action: function(imageUrl, imageOrError) {
+		    // verify request loaded OK
+		    ok(SC.typeOf(imageOrError) === "error", "Image retrieval should fail with error.");
+		    // resume executing tests
+		    start();
+		  }}, 'action', NO);
+	});
 
-  	stop(this.guardTimeout);
+	stop(guardTimeout);
 });
 
 test("Reload previous valid image (now cached) successfully.", function() {
-	SC.imageQueue.loadImage(this.firstGoodImageURL, {action: function(imageUrl, imageOrError) {
-	    // verify request loaded OK
-	    ok(SC.typeOf(imageOrError) !== "error", "Image should be retrieved successfully.");
-	    // resume executing tests
-	    start();
-	  }}, 'action', NO);	
+	SC.run(function() {
+		SC.imageQueue.loadImage(firstGoodImageURL, {
+			action: function(imageUrl, imageOrError) {
+		    // verify request loaded OK
+		    ok(SC.typeOf(imageOrError) !== "error", "Image should be retrieved successfully.");
+		    // resume executing tests
+		    start();
+		  }}, 'action', NO);
+	});
 
-  	stop(this.guardTimeout);
+	stop(guardTimeout);
 });
 
 test("Load a second non-cached image successfully.", function() {
-	SC.imageQueue.loadImage(this.secondGoodImageURL, {action: function(imageUrl, imageOrError) {
-	    // verify request loaded OK
-	    ok(SC.typeOf(imageOrError) !== "error", "Image should be retrieved successfully.");
-	    // resume executing tests
-	    start();
-	  }}, 'action', NO);	
+	SC.run(function() {
+		SC.imageQueue.loadImage(secondGoodImageURL, {
+			action: function(imageUrl, imageOrError) {
+		    // verify request loaded OK
+		    ok(SC.typeOf(imageOrError) !== "error", "Image should be retrieved successfully.");
+		    // resume executing tests
+		    start();
+		  }}, 'action', NO);
+	});
 
-  	stop(this.guardTimeout);
+	stop(guardTimeout);
+});
+
+
+test("Release all images.", function() {
+	SC.run(function() {
+		SC.imageQueue.releaseImage(firstGoodImageURL);
+		SC.imageQueue.releaseImage(secondGoodImageURL);
+	});
 });
