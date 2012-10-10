337def276498e1a3779793897abd812d175a872d
Dave Porter <dcporter@gmail.com>
2012-10-09 21:48:42 -0400

    Added unit tests for SC.View#destroy.

---
+++ frameworks/core_foundation/tests/views/view/destroy.js
@@ -0,0 +1,34 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Apple Inc. and contributors.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+/* global module test equals context ok same */
+
+module("SC.View#destroy");
+
+test('isDestroyed works.', function() {
+  var v = SC.View.create();
+  ok(!v.get('isDestroyed'), 'undestroyed view\'s isDestroyed property is false.');
+  v.destroy();
+  ok(v.get('isDestroyed'), 'destroyed view\'s isDestroyed property is true.');
+});
+
+test('childViews specified as classes are also destroyed.', function() {
+  var v = SC.View.create({ childViews: [ SC.View ] }),
+      v2 = v.childViews[0];
+  v.destroy();
+  ok(v2.get('isDestroyed'), 'destroying a parent also destroys a child, mwaha.');
+  ok(!v2.get('parentView'), 'destroying a parent removes the parentView reference from the child.');
+  ok(v2.get('owner') === null, 'destroying a parent removes the owner reference from the child.');
+});
+
+test('childViews specified as instances are also destroyed.', function() {
+  var v2 = SC.View.create(),
+      v = SC.View.create({ childViews: [v2] });
+  v.destroy();
+  ok(v2.get('isDestroyed'), 'destroying a parent also destroys a child, mwaha.');
+  ok(!v2.get('parentView'), 'destroying a parent removes the parentView reference from the child.');
+  ok(v2.get('owner') === null, 'destroying a parent removes the owner reference from the child.');
+});
