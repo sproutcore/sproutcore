80e852d7a915ce7ff3caad24572c9c4b54584541
Public Keating <publickeating@me.com>
2012-10-30 14:23:09 -0600

    Fixes small memory leak in SC.ObserverSet.
    Each time that a new observer is added to an object the ObserverSet for the object will add a tracking hash for the target and the method.  As more methods are tracked for the target, they are added and as methods are no longer tracked for the target, they are removed.  However, even when no methods are tracked for the target, the tracking hash for the target still exists.  This creates a small leak of memory, because the target may have been destroyed and freed, but we are still maintaining an empty tracking hash for it.
    
    Here's an example that would create 3,000 empty hashes in coreOb._kvo_observers_key._members:
    
        obs = [];
        window.coreOb = SC.Object.create({ key: '1' });
        SC.run(function() {
          for (var i = 2999; i >= 0; i--) {
            obs.push(SC.Object.create({ myKeyBinding: SC.Binding.from('coreOb.key') }));
          }
        });
    
        SC.run(function() {
          for (var i = obs.length - 1; i >= 0; i--) {
            obs.pop().destroy();
          }
        });

--- frameworks/runtime/tests/system/observer_set.js
+++ frameworks/runtime/tests/system/observer_set.js
@@ -54,3 +54,65 @@ test("Observers that remove themselves should fire at least once, and shouldn't 
   ok(observerFired, "At least one observer should have fired.");
 });
 
+/**
+  This test highlights a problem with SC.ObserverSet.  It would clear out the
+  method tracking on the target tracking hash, but when the number of methods
+  tracked went to zero, it never cleared out the target.
+
+  This is bad if a core object is bound to repeatedly by dynamically created
+  objects.  Each new object tracking creates a new hash in the observer set
+  that is never reclaimed.
+
+  Here's an example that would create 3,000 empty hashes in
+  coreOb._kvo_observers_key._members:
+
+    obs = [];
+    window.coreOb = SC.Object.create({ key: '1' });
+    SC.run(function() {
+      for (var i = 2999; i >= 0; i--) {
+        obs.push(SC.Object.create({ myKeyBinding: SC.Binding.from('coreOb.key') }));
+      }
+    });
+
+    SC.run(function() {
+      for (var i = obs.length - 1; i >= 0; i--) {
+        obs.pop().destroy();
+      }
+    });
+
+
+  The fix is to have SC.ObserverSet remove the target hash when the number of
+  methods for the target goes to zero.
+*/
+test("Removing all the methods on a target should clear the internal tracking of that target", function() {
+  var methodGuid1, methodGuid2,
+    target = {},
+    method1 = function () { }, method2 = function () { },
+    observerSet,
+    targetGuid;
+
+  observerSet = SC.ObserverSet.create();
+
+  targetGuid = SC.guidFor(target);
+  methodGuid1 = SC.guidFor(method1);
+  methodGuid2 = SC.guidFor(method2);
+
+  equals(observerSet.members.length, 0, "The ObserverSet should have a members length of");
+
+  observerSet.add(target, method1);
+  observerSet.add(target, method2);
+  equals(observerSet.members.length, 2, "The ObserverSet should have a members length of");
+  ok(observerSet._members[targetGuid], "The ObserverSet should be tracking methods for the target: %@".fmt(targetGuid));
+  ok(!SC.none(observerSet._members[targetGuid][methodGuid1]), "The ObserverSet should be tracking method: %@ for the target: %@".fmt(methodGuid1, targetGuid));
+  ok(!SC.none(observerSet._members[targetGuid][methodGuid2]), "The ObserverSet should be tracking method: %@ for the target: %@".fmt(methodGuid2, targetGuid));
+
+  observerSet.remove(target, method1);
+  equals(observerSet.members.length, 1, "The ObserverSet should have a members length of");
+  ok(observerSet._members[targetGuid], "The ObserverSet should be tracking methods for the target: %@".fmt(targetGuid));
+  ok(SC.none(observerSet._members[targetGuid][methodGuid1]), "The ObserverSet should not be tracking method: %@ for the target: %@".fmt(methodGuid1, targetGuid));
+  ok(!SC.none(observerSet._members[targetGuid][methodGuid2]), "The ObserverSet should be tracking method: %@ for the target: %@".fmt(methodGuid2, targetGuid));
+
+  observerSet.remove(target, method2);
+  equals(observerSet.members.length, 0, "The ObserverSet should have a members length of");
+  ok(!observerSet._members[targetGuid], "The ObserverSet should not be tracking methods for the target: %@".fmt(targetGuid));
+});
