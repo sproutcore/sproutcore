3f5f125e1a5f90f25ccae52a9727229fc7c78d9e
Michael Krotscheck <krotscheck@gmail.com>
2012-11-15 11:59:36 -0800

    Fixed source formatting. Moved string formatting logic into datetime/localized as it depends on localization. Created stub method in datetime/core Added %E and %e to unit tests in datetime/core

--- frameworks/datetime/frameworks/localized/system/datetime.js
+++ frameworks/datetime/frameworks/localized/system/datetime.js
@@ -22,7 +22,51 @@ SC.DateTime.mixin(
     SC.DateTime.monthNames = SC.String.w(SC.String.loc('_SC.DateTime.monthNames'));
     SC.DateTime.abbreviatedMonthNames = SC.String.w(SC.String.loc('_SC.DateTime.abbreviatedMonthNames'));
     SC.DateTime.AMPMNames = SC.String.w(SC.String.loc('_SC.DateTime.AMPMNames'));
-  }
+  },
+  
+  /**
+    @private
+    
+    Elapsed string formatting override, as it depends on localization.
+    
+    @see SC.DateTime#toFormattedString
+   */
+  __getElapsedStringFormat: function(start, timezone) {
+    
+    var elapsed = Math.floor((+new Date() - this._date.getTime() - (timezone * 60000)) / 1000);
+    var future = !!(elapsed < 0) ? "In" : "Ago";
+    var interval = 0;
+    var intervalType = "now";
+    
+    if(Math.abs(elapsed) > 31536000) { // 60 * 60 * 24 * 365 - years
+      interval = Math.floor(Math.abs(elapsed / 31536000));
+      intervalType = 'year';
+    } else if(Math.abs(elapsed) > 2678400) { // 60 * 60 * 24 * 31 - months
+      interval = Math.floor(Math.abs(elapsed / 2678400));
+      intervalType = 'month';
+    } else if(Math.abs(elapsed) > 604800) { // 60 * 60 * 24 * 7 - weeks
+      interval = Math.floor(Math.abs(elapsed / 604800));
+      intervalType = 'week';
+    } else if(Math.abs(elapsed) > 86400) { // 60 * 60 * 24 - day
+      interval = Math.floor(Math.abs(elapsed / 86400));
+      intervalType = 'day';
+    } else if(Math.abs(elapsed) > 3600) { // 60 * 60 - hour
+      interval = Math.floor(Math.abs(elapsed / 3600));
+      intervalType = 'hour';
+    } else if(Math.abs(elapsed) > 60) { // 60 - minute
+      interval = Math.floor(Math.abs(elapsed / 60));
+      intervalType = 'minute';
+    } else if(Math.abs(elapsed) > 0) { // second
+      interval = Math.abs(elapsed);
+      intervalType = 'second';
+    }
+    
+    var formatString = '_SC.DateTime.now';
+    if(intervalType !== 'now') {
+      formatString = "_SC.DateTime.%@%@%@".fmt(intervalType, interval > 1 ? "s" : "", future);
+    }
+    return formatString.loc().replace('%e', interval);
+  },
 
 });
 
