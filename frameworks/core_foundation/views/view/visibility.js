cf9b1e678e7f8814e3ee0f05f3a5f277f174d670
Greg Fairbanks <fairbanksg@seapine.com>
2012-12-21 13:16:43 -0500

    Add null check in recomputeIsVisibleInWindow.
    There are some circumstances where a view could get destroyed while
    recomputeIsVisibleInWindow is happening, which could lead to childViews
    going away in the middle of this loop.

--- frameworks/core_foundation/views/view/visibility.js
+++ frameworks/core_foundation/views/view/visibility.js
@@ -61,10 +61,10 @@ SC.View.reopen(
     if (previous !== current) {
       this.set('isVisibleInWindow', current);
 
-      var childViews = this.get('childViews'), len = childViews.length, idx, view;
-      for(idx=0;idx<len;idx++) {
+      var childViews = this.get('childViews'), idx, view;
+      for(idx=0;idx<childViews.length;idx++) {
         view = childViews[idx];
-        if(view.recomputeIsVisibleInWindow) { view.recomputeIsVisibleInWindow(current); }
+        if(view && view.recomputeIsVisibleInWindow) { view.recomputeIsVisibleInWindow(current); }
       }
 
       // For historical reasons, we'll also layout the child views if
