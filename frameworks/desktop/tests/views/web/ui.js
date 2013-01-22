df875a4108ad70bd86eaf5bc7f1138bc07278fbe
Public Keating <publickeating@me.com>
2013-01-22 10:51:26 -0600

    Fixes debug images and test iframe.html being included in builds.  These files (one of which is 2.5MB) would get included into every build, because they were at the wrong path.  I removed the apple logo which appears unused in the tests and fixed the directory structure and file references so that 'a_sample_image.jpg' and 'iframe.html' don't get included in production builds.
    Note: this is especially a problem if you wanted to create an app manifest based on the contents of the built static directory.  The client would have to download a 2.5MB debug image that is never used.

--- frameworks/desktop/tests/views/web/ui.js
+++ frameworks/desktop/tests/views/web/ui.js
@@ -23,6 +23,7 @@
     }
   }).add("auto resize", SC.WebView, {
     value: sc_static('iframe'),
+    includeImage: sc_static('a_sample_image.jpg'), // this simply ensures that the image is added to the build
     shouldAutoResize: YES,
     layout: {
       width: 250,
@@ -107,4 +108,5 @@
       start();
     });
   });
+
 })();
