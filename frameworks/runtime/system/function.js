f2f2bd728c5baebb81dcd47e619efad1ada1a440
Joe Gaudet <joe@learndot.com>
2013-01-25 17:50:02 -0800

    Added partial function application to the function prototype

--- frameworks/runtime/system/function.js
+++ frameworks/runtime/system/function.js
@@ -71,6 +71,20 @@ SC.Function = /** @scope SC.Function.prototype */{
       }
     }
     return fn ;
+  },
+  
+  /**
+    @see Function.prototype.partial
+  */
+  partial: function(){
+    var fn = this, args = Array.prototype.slice.call(arguments);
+    return function(){
+      var arg = 0;
+      for ( var i = 0; i < args.length && arg < arguments.length; i++ )
+        if ( args[i] === undefined )
+          args[i] = arguments[arg++];
+      return fn.apply(this, args);
+    };
   }
 
 };
