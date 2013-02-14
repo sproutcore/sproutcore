ff01b27768caf0ea733b941805ebc234d16f8951
Public Keating <publickeating@me.com>
2013-02-14 08:36:35 -0600

    Adds unit test highlighting regression in SC.ContainerView if destroying before setting nowShowing.

--- frameworks/foundation/tests/views/container/methods.js
+++ frameworks/foundation/tests/views/container/methods.js
@@ -4,5 +4,44 @@
 //            portions copyright @2011 Apple Inc.
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
+/*global module, test, ok, equals*/
+var containerView,
+  pane;
 
-module("TODO: Test SC.ContainerView Methods");
+module("SC.ContainerView Methods", {
+  setup: function () {
+    SC.run(function () {
+
+      containerView = SC.ContainerView.create({
+        nowShowing: null
+      });
+
+      pane = SC.Pane.create({
+        layout: { width: 200, height: 200, left: 0, top: 0 },
+        childViews: [containerView]
+      }).append();
+    });
+
+    SC.run(function () {
+      containerView.awake();
+    });
+  },
+
+  teardown: function () {
+    pane.remove();
+    containerView = pane = null;
+  }
+});
+
+/**
+  There was a regression where destroying a ContainerView that had never had
+  a nowShowing view set would throw an exception.
+  */
+test("Test destroy method.", function () {
+  try {
+    containerView.destroy();
+    ok(true, "ContainerView.prototype.destroy should not fail to work if the ContainerView has no view showing.");
+  } catch (ex) {
+    ok(false, "ContainerView.prototype.destroy should not fail to work if the ContainerView has no view showing.  Ex: %@".fmt(ex));
+  }
+});
