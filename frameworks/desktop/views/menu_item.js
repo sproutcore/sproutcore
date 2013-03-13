b2c3dd623bab4285e84a8e23949cc2e0689df57b
Dave Porter <dcporter@gmail.com>
2013-03-13 08:25:24 -0400

    Made MenuItemView legacy action-as-function call consistent with target/action call. Legacy action calls will now be called with the menu item as the sender.

--- frameworks/desktop/views/menu_item.js
+++ frameworks/desktop/views/menu_item.js
@@ -410,7 +410,7 @@ SC.MenuItemView = SC.View.extend(SC.ContentDisplay,
 
     // Legacy support for actions that are functions
     if (SC.typeOf(action) === SC.T_FUNCTION) {
-      action.apply(target, [rootMenu]);
+      action.apply(target, [this]);
       //@if(debug)
       SC.Logger.warn('Support for menu item action functions has been deprecated. Please use target and action.');
       //@endif
