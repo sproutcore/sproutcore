d1f19f1cf26b4dde1d868fd14a78ff38d3aa8988
Tyler Keating <tyler@7x7software.com>
2013-04-19 08:26:07 +0800

    Adds child view layout plugin code (WIP).

---
+++ frameworks/core_foundation/protocols/child_view_layout_protocol.js
@@ -0,0 +1,17 @@
+// ==========================================================================
+// Project:   SproutCore
+// Copyright: @2013 7x7 Software, Inc.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+
+/** @namespace
+  This protocol defines the allowable child view layout plugin methods.
+
+  SC.View can use child view layout plugins to setup and adjust the layout
+  of child views and expects the given child view layout plugin object
+  to implement the methods in this protocol.
+*/
+SC.ChildViewLayoutProtocol = {
+
+};
