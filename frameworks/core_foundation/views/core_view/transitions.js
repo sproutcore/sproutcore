dc64e0b336d4f72056f2f53e369147210e61c98d
Tyler Keating <tyler@7x7software.com>
2013-04-26 16:01:10 -0600

    Adds the transitions properties to SC.CoreView along with docs.

---
+++ frameworks/core_foundation/views/core_view/transitions.js
@@ -0,0 +1,204 @@
+sc_require("views/view");
+
+
+SC.CoreView.reopen(
+  /** @scope SC.CoreView.prototype */ {
+
+  /**
+    The transition plugin to use when this view is appended to the DOM.
+
+    SC.CoreView uses a pluggable transition architecture where the transition setup,
+    execution and cleanup can be handled by a specified transition plugin.
+
+    There are a number of pre-built transition in plugins available in the
+    foundation framework:
+
+      SC.View.BOUNCE_IN
+      SC.View.FADE_IN
+      SC.View.MOVE_IN
+      SC.View.POP_IN
+      SC.View.SCALE_IN
+      SC.View.SPRING_IN
+
+    You can even provide your own custom transition plugins.  Just create a
+    transition object that conforms to the SC.TransitionProtocol protocol.
+
+    @type Object (SC.TransitionProtocol)
+    @default null
+    @since Version 1.10
+  */
+  transitionIn: null,
+
+  /**
+    The options for the given transition in plugin.
+
+    These options are specific to the current transition plugin used and are
+    used to modify the transition animation.  To determine what options
+    may be used for a given plugin and to see what the default options are,
+    see the documentation for the transition plugin being used.
+
+    Most transitions will accept a duration and timing option, but may
+    also use other options.  For example, SC.View.MOVE_IN accepts options
+    like:
+
+        transitionInOptions: {
+          direction: 'left',
+          duration: 0.25,
+          timing: 'ease-in-out'
+        }
+
+    @type Object
+    @default null
+    @since Version 1.10
+  */
+  transitionInOptions: null,
+
+  /**
+    The transition plugin to use when this view is removed from the DOM.
+
+    SC.View uses a pluggable transition architecture where the transition setup,
+    execution and cleanup can be handled by a specified transition plugin.
+
+    There are a number of pre-built transition out plugins available in the
+    foundation framework:
+
+      SC.View.BOUNCE_OUT
+      SC.View.FADE_OUT
+      SC.View.MOVE_OUT
+      SC.View.POP_OUT
+      SC.View.SCALE_OUT
+      SC.View.SPRING_OUT
+
+    You can even provide your own custom transition plugins.  Just create a
+    transition object that conforms to the SC.TransitionProtocol protocol.
+
+    @type Object (SC.TransitionProtocol)
+    @default null
+    @since Version 1.10
+  */
+  transitionOut: null,
+
+  /**
+    The options for the given transition out plugin.
+
+    These options are specific to the current transition plugin used and are
+    used to modify the transition animation.  To determine what options
+    may be used for a given plugin and to see what the default options are,
+    see the documentation for the transition plugin being used.
+
+    Most transitions will accept a duration and timing option, but may
+    also use other options.  For example, SC.View.MOVE_OUT accepts options
+    like:
+
+        transitionOutOptions: {
+          direction: 'right',
+          duration: 0.15,
+          timing: 'ease-in'
+        }
+
+    @type Object
+    @default null
+    @since Version 1.10
+  */
+  transitionOutOptions: null,
+
+  /**
+    The transition plugin to use when this view is made shown from being
+    hidden.
+
+    SC.CoreView uses a pluggable transition architecture where the transition setup,
+    execution and cleanup can be handled by a specified transition plugin.
+
+    There are a number of pre-built transition show plugins available in the
+    foundation framework:
+
+      SC.View.BOUNCE_IN
+      SC.View.FADE_IN
+      SC.View.MOVE_IN
+      SC.View.POP_IN
+      SC.View.SCALE_IN
+      SC.View.SPRING_IN
+
+    You can even provide your own custom transition plugins.  Just create a
+    transition object that conforms to the SC.TransitionProtocol protocol.
+
+    @type Object (SC.TransitionProtocol)
+    @default null
+    @since Version 1.10
+  */
+  transitionShow: null,
+
+  /**
+    The options for the given transition show plugin.
+
+    These options are specific to the current transition plugin used and are
+    used to modify the transition animation.  To determine what options
+    may be used for a given plugin and to see what the default options are,
+    see the documentation for the transition plugin being used.
+
+    Most transitions will accept a duration and timing option, but may
+    also use other options.  For example, SC.View.MOVE_IN accepts options
+    like:
+
+        transitionShowOptions: {
+          direction: 'left',
+          duration: 0.25,
+          timing: 'ease-in-out'
+        }
+
+    @type Object
+    @default null
+    @since Version 1.10
+  */
+  transitionShowOptions: null,
+
+  /**
+    The transition plugin to use when this view is hidden after being shown.
+
+    SC.View uses a pluggable transition architecture where the transition setup,
+    execution and cleanup can be handled by a specified transition plugin.
+
+    There are a number of pre-built transition hide plugins available in the
+    foundation framework:
+
+      SC.View.BOUNCE_OUT
+      SC.View.FADE_OUT
+      SC.View.MOVE_OUT
+      SC.View.POP_OUT
+      SC.View.SCALE_OUT
+      SC.View.SPRING_OUT
+
+    You can even provide your own custom transition plugins.  Just create a
+    transition object that conforms to the SC.TransitionProtocol protocol.
+
+    @type Object (SC.TransitionProtocol)
+    @default null
+    @since Version 1.10
+  */
+  transitionHide: null,
+
+  /**
+    The options for the given transition hide plugin.
+
+    These options are specific to the current transition plugin used and are
+    used to modify the transition animation.  To determine what options
+    may be used for a given plugin and to see what the default options are,
+    see the documentation for the transition plugin being used.
+
+    Most transitions will accept a duration and timing option, but may
+    also use other options.  For example, SC.View.MOVE_OUT accepts options
+    like:
+
+        transitionHideOptions: {
+          direction: 'right',
+          duration: 0.15,
+          timing: 'ease-in'
+        }
+
+    @type Object
+    @default null
+    @since Version 1.10
+  */
+  transitionHideOptions: null
+
+});
