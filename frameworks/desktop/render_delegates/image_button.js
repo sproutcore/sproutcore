3614ce0fb87908a3b881f3f8eb7511bbd1bb5611
Tyler Keating <tyler@7x7software.com>
2013-06-02 22:12:49 -0600

    Fixes regression in SC.ImageButtonView that caused the sc-view class to be removed.

--- frameworks/desktop/render_delegates/image_button.js
+++ frameworks/desktop/render_delegates/image_button.js
@@ -13,11 +13,13 @@ SC.BaseTheme.imageButtonRenderDelegate = SC.RenderDelegate.create({
     var image = dataSource.get('image'),
       toolTip = dataSource.get('toolTip');
 
+    // render controlSize
+    this.addSizeClassName(dataSource, context);
+
     if (toolTip) {
       context.setAttr('title', toolTip);
       context.setAttr('alt', toolTip);
     }
-
 
     if (image) {
       context.addClass(image);
@@ -27,25 +29,32 @@ SC.BaseTheme.imageButtonRenderDelegate = SC.RenderDelegate.create({
     }
   },
 
-  update: function (dataSource, $) {
+  update: function (dataSource, jqElement) {
     var image, toolTip;
+
+    this.updateSizeClassName(dataSource, jqElement);
 
     if (dataSource.didChangeFor('imageButtonRenderDelegate', 'toolTip')) {
       toolTip = dataSource.get('toolTip');
 
-      $.attr('title', toolTip);
-      $.attr('alt', toolTip);
+      jqElement.attr('title', toolTip);
+      jqElement.attr('alt', toolTip);
     }
 
     if (dataSource.didChangeFor('imageButtonRenderDelegate', 'image')) {
       image = dataSource.get('image');
 
-      // Remove the last image class and add the new one.
-      $.removeClass(dataSource.renderState._cachedImage);
-      $.addClass(image);
+      // Remove the last image class
+      if (dataSource.renderState._cachedImage) {
+        jqElement.removeClass(dataSource.renderState._cachedImage);
+      }
 
-      // Track the image class used so that we can remove it when it changes.
-      dataSource.renderState._cachedImage = image;
+      if (image) {
+        jqElement.addClass(image);
+
+        // Track the image class used so that we can remove it when it changes.
+        dataSource.renderState._cachedImage = image;
+      }
     }
   }
 });
