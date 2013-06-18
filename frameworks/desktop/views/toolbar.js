1bb313951d867069577d70b7e827e522fbbd3b9d
Tyler Keating <tyler@7x7software.com>
2013-06-18 12:47:08 -0600

    Makes SC.ToolbarView more intelligent about it's styling: not just styled appropriately for top alignment, layout adjusted to include a top or bottom border.  Also improves class documentation.

--- frameworks/desktop/views/toolbar.js
+++ frameworks/desktop/views/toolbar.js
@@ -7,7 +7,7 @@
 
 /**
   Layout properties needed to anchor a view to the top.
-  
+
   @static
   @constant
   @type Hash
@@ -17,7 +17,7 @@ SC.ANCHOR_TOP = { top: 0 };
 
 /**
   Layout properties needed to anchor a view to the left.
-  
+
   @static
   @constant
   @type Hash
@@ -27,7 +27,7 @@ SC.ANCHOR_LEFT = { left: 0 };
 
 /*
   Layout properties to anchor a view to the top left
-  
+
   @static
   @constant
   @type Hash
@@ -37,7 +37,7 @@ SC.ANCHOR_TOP_LEFT = { top: 0, left: 0 };
 
 /**
   Layout properties to anchoe view to the bottom.
-  
+
   @static
   @constant
   @type Hash
@@ -47,17 +47,17 @@ SC.ANCHOR_BOTTOM = { bottom: 0 };
 
 /**
   Layout properties to anchor a view to the right.
-  
+
   @static
   @constant
   @type Hash
   @default `{ right: 0 }`
 */
-SC.ANCHOR_RIGHT = { right: 0 } ;
+SC.ANCHOR_RIGHT = { right: 0 };
 
 /**
   Layout properties to anchor a view to the bottom right.
-  
+
   @static
   @constant
   @type Hash
@@ -67,12 +67,23 @@ SC.ANCHOR_BOTTOM_RIGHT = { bottom: 0, right: 0 };
 
 /** @class
 
-  A toolbar view can be anchored at the top or bottom of the window to contain
-  your main toolbar buttons.
+  SC.ToolbarView is a simple horizontal view that has been styled like a
+  toolbar and can be easily anchored at the top or bottom of a parent view.
 
-  You can also override the layout property yourself or simply set the
-  anchorLocation to `SC.ANCHOR_TOP` or `SC.ANCHOR_BOTTOM`.  This will configure
-  the layout of your toolbar automatically when it is created.
+  To anchor to the top of the parent view, set `anchorLocation` to
+  `SC.ANCHOR_TOP` or to anchor to the bottom, set `anchorLocation` to
+  `SC.ANCHOR_BOTTOM`.  The default layout of SC.Toolbar is
+  `{ left: 0, right: 0, height: 32, zIndex: 10 }` and so by setting the value of
+  `anchorLocation`, the layout will be modified to either:
+
+  `SC.ANCHOR_TOP:`
+      { borderBottom: 1, top: 0, left: 0, right: 0, height: 32, zIndex: 10 }
+
+  `SC.ANCHOR_BOTTOM:`
+      { borderTop: 1, bottom: 0, left: 0, right: 0, height: 32, zIndex: 10 }
+
+  Of course, you can always override the layout property yourself in order to
+  adjust the height, border and zIndex values.
 
   @extends SC.View
   @since SproutCore 1.0
@@ -105,14 +116,14 @@ SC.ToolbarView = SC.View.extend(
   /**
     Default anchor location.  This will be applied automatically to the
     toolbar layout if you set it. Possible values:
-    
+
       - SC.ANCHOR_TOP
       - SC.ANCHOR_LEFT
       - SC.ANCHOR_TOP_LEFT
       - SC.ANCHOR_BOTTOM
       - SC.ANCHOR_RIGHT
       - SC.ANCHOR_BOTTOM_RIGHT
-    
+
     @type String
     @default null
   */
@@ -123,14 +134,27 @@ SC.ToolbarView = SC.View.extend(
   //
 
   /** @private */
-  layout: { left: 0, height: 32, right: 0 },
+  layout: { left: 0, right: 0, height: 32, zIndex: 10 },
 
   /** @private */
-  init: function() {
+  init: function () {
     // apply anchor location before setting up the rest of the view.
     if (this.anchorLocation) {
       this.layout = SC.merge(this.layout, this.anchorLocation);
+
+      switch (this.anchorLocation) {
+      case SC.ANCHOR_TOP:
+      case SC.ANCHOR_TOP_LEFT:
+        this.layout.borderBottom = 1;
+        break;
+      case SC.ANCHOR_BOTTOM:
+      case SC.ANCHOR_BOTTOM_RIGHT:
+        this.layout.borderTop = 1;
+        break;
+      default:
+      }
     }
+
     sc_super();
   }
 
