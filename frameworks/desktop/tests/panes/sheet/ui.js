4841198b34739eed9ccfb5faaed3e951b835ec53
Tyler Keating <tyler@7x7software.com>
2013-06-22 10:47:13 -0600

    Fixes invalid unit test.

--- frameworks/desktop/tests/panes/sheet/ui.js
+++ frameworks/desktop/tests/panes/sheet/ui.js
@@ -11,15 +11,15 @@ module("SC.SheetPane UI");
 
 var slidePane;
 
-test("verify sheet pane slide down works", function() { 
+test("verify sheet pane slide down works", function() {
   slidePane = SC.SheetPane.create({
-    layout: { top: 60, width: 400, height: 200, centerX: 0 },
+    layout: { width: 400, height: 200, centerX: 0 },
     contentView: SC.LabelView.extend({
       escapeHTML: NO,
       value: '<h1>Slide Down!</h1>'
     })
   });
-  
+
   var layout = slidePane.get('layout');
   var pt = layout.top;
   var pl = layout.left;
