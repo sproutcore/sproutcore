19403c799dafa2c14aebe808fd397b54e670a492
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2013-07-23 09:30:29 +0200

    fix unit test, partially reverting fc86ca0ca9df73934ffc09c4539b6fe8e465acdf

--- frameworks/desktop/tests/views/segmented/ui.js
+++ frameworks/desktop/tests/views/segmented/ui.js
@@ -378,7 +378,7 @@ var pane;
       var overflowEl = segments[segments.length - 1];
       ok($(overflowEl).hasClass('sc-overflow-segment'), 'overflow segment should have .sc-overflow-segment class');
       var overflowLabelEl = $(overflowEl).find('label')[0];
-      equals(overflowLabelEl.innerText, "&raquo;", 'there should be "&raquo;" text in the overflow segment');
+      equals(overflowLabelEl.innerHTML, "»", 'there should be "»" text in the overflow segment');
     };
 
     var lastIsSegment = function(sv, text) {
@@ -388,7 +388,7 @@ var pane;
       var lastEl = segments[segments.length - 2];
       ok(!$(lastEl).hasClass('sc-overflow-segment'), 'last segment should not have .sc-overflow-segment class');
       var lastLabelEl = $(lastEl).find('label')[0];
-      equals(lastLabelEl.innerText, text, 'there should be "' + text + '" text in the last segment');
+      equals(lastLabelEl.innerHTML, text, 'there should be "' + text + '" text in the last segment');
     };
 
     // the last item should be an overflow segment (ie. has .sc-overflow-segment class and text "&raquo")
