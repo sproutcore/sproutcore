9ddcd4f27882129a1e8a61daba46d41b882d59b7
Umberto Nicoletti <umberto.nicoletti@gmail.com>
2013-09-02 04:49:39 -0400

    force notify on request

--- frameworks/ajax/tests/system/request.js
+++ frameworks/ajax/tests/system/request.js
@@ -378,7 +378,7 @@ test("Test Multiple listeners per single status response", function() {
   
   setTimeout(function() {
     // give observers a chance to fire again
-    SC.RunLoop.begin(); SC.RunLoop.end();
+    request.notify();
     
     if (numResponses === 2) { window.start(); }
   }, ((test_timeout*2) - 500));
