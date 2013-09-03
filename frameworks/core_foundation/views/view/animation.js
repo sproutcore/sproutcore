b3d08ffcbe70030f9cfcff07c1bce2447de71788
Dave Porter <dcporter@gmail.com>
2013-09-02 21:12:38 -0400

    Removes a developer warning that warns about a valid use case.
    Animating with a duration of zero is common when using the same code to set an initial (calculated) state and to animate back to that state.

--- frameworks/core_foundation/views/view/animation.js
+++ frameworks/core_foundation/views/view/animation.js
@@ -285,10 +285,6 @@ SC.View.reopen(
 
     // In the case of zero duration, just adjust and call the callback.
     if (options.duration === 0) {
-      //@if(debug)
-      // Provide a little developer support if they are doing something that should be considered wrong.
-      SC.warn("Developer Warning: SC.View:animate() was called with a duration of 0 seconds.  The view will be adjusted and the callback will fire immediately in the next run loop.");
-      //@endif
       this.adjust(hash);
       this.runAnimationCallback(options, null, NO);
       return this;
