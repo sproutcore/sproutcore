9f34e32c0035ce788d5c1a86ea74ffd720438ad2
Tyler Keating <tyler@7x7software.com>
2013-09-06 13:37:30 -0600

    Tidies the unit test file.

--- frameworks/desktop/tests/views/list/render.js
+++ frameworks/desktop/tests/views/list/render.js
@@ -4,74 +4,74 @@
 //            portions copyright @2011 Apple Inc.
 // License:   Licensed under MIT license (see license.js)
 // ==========================================================================
+/*global CoreTest, module, test */
 
-// SC.LOG_BINDINGS = YES ;
+// SC.LOG_BINDINGS = YES;
+// SC.LOG_DEFERRED_CALLS = YES;
 
-var view, content, pane ;
+var view, content, pane;
 
-var renderFunc = CoreTest.stub("render", function() {
-  SC.ListItemView.prototype.render.apply(this, arguments) ;
+var renderFunc = CoreTest.stub("render", function () {
+  SC.ListItemView.prototype.render.apply(this, arguments);
 });
 
 module("SC.ListView.render", {
-  
-  setup: function() {
-    SC.RunLoop.begin();
-    
-    content = "1 2 3 4 5 6 7 8 9 10".w().map(function(x) {
-      return SC.Object.create({ value: x });
+
+  setup: function () {
+    SC.run(function () {
+      content = "1 2 3 4 5 6 7 8 9 10".w().map(function (x) {
+        return SC.Object.create({ value: x });
+      });
+
+      view = SC.ListView.create({
+        content: content,
+
+        layout: { top: 0, left: 0, width: 300, height: 500 },
+
+        layoutForContentIndex: function (idx) {
+          return { left: 0, right: 0, top: idx * 50, height: 50 };
+        },
+
+        _cv_nowShowingDidChange: CoreTest.stub("_cv_nowShowingDidChange", function () {
+          SC.ListView.prototype._cv_nowShowingDidChange.apply(this, arguments);
+        }),
+
+        exampleView: SC.ListItemView.extend({
+          render: renderFunc
+        }),
+
+        // reset stubs
+        reset: function () {
+          this._cv_nowShowingDidChange.reset();
+          renderFunc.reset();
+        }
+
+      });
+
+      pane = SC.MainPane.create();
+      pane.appendChild(view);
+      pane.append();
     });
-    
-    view = SC.ListView.create({
-      content: content, 
-      
-      layout: { top: 0, left: 0, width: 300, height: 500 },
-      
-      layoutForContentIndex: function(idx) {
-        return { left: 0, right: 0, top: idx * 50, height: 50 };
-      },
-      
-      _cv_nowShowingDidChange: CoreTest.stub("_cv_nowShowingDidChange", function() {
-        SC.ListView.prototype._cv_nowShowingDidChange.apply(this, arguments) ;
-      }),
-      
-      exampleView: SC.ListItemView.extend({
-        render: renderFunc
-      }),
-      
-      // reset stubs
-      reset: function() {
-        this._cv_nowShowingDidChange.reset();
-        renderFunc.reset();
-      }
-      
-    });
-    
-    pane = SC.MainPane.create();
-    pane.appendChild(view);
-    pane.append();
-    
-    SC.RunLoop.end();
   },
-  
-  teardown: function() {
-    SC.RunLoop.begin();
-    view.reset();
-    pane.remove();
-    pane.destroy();
-    SC.RunLoop.end();
+
+  teardown: function () {
+    SC.run(function () {
+      view.reset();
+      pane.remove();
+      pane.destroy();
+    });
   }
-  
+
 });
 
 // ..........................................................
 // BASIC TESTS
-// 
+//
 
-test("list item render() should only be called once per item view a static content array", function() {
+test("list item render() should only be called once per item view a static content array", function () {
   renderFunc.expect(10);
 });
 
-// test("_cv_nowShowingDidChange() should only be called once with a static content array", function() {
-//   view._cv_nowShowingDidChange.expect(1); // currently is 3...
-// });
+test("_cv_nowShowingDidChange() should only be called once with a static content array", function () {
+  view._cv_nowShowingDidChange.expect(1); // currently is 3...
+});
