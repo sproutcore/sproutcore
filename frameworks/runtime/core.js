c56d3b2301624203560e2fb676d360cc8770fddf
Tyler Keating <tyler@7x7software.com>
2013-09-08 16:52:47 -0600

    Updates version for Edge.

--- frameworks/runtime/core.js
+++ frameworks/runtime/core.js
@@ -59,7 +59,7 @@ window.SproutCore = window.SproutCore || SC;
 */
 SC = window.SC; // This is dumb but necessary for jsdoc to get it right
 
-SC.VERSION = '1.10.0';
+SC.VERSION = 'Edge';
 
 /**
   @private
