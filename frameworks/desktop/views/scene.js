c9f03133905e0ecc38dac9b6e172b24a65ca50c6
Tyler Keating <tyler@7x7software.com>
2013-11-22 23:16:27 -0600

    Cleans whitespace and changes the default directions to match the direction of the scenes.

--- frameworks/desktop/views/scene.js
+++ frameworks/desktop/views/scene.js
@@ -8,24 +8,24 @@
 /** @class
 
   Displays several views as scenes that can slide on and off the screen.  The
-  scene view is a nice way to provide a simple effect of moving from a 
+  scene view is a nice way to provide a simple effect of moving from a
   higher level screen to a more detailed level screen.  You will be able to
-  optionally choose the kind of animation used to transition the two scenes 
+  optionally choose the kind of animation used to transition the two scenes
   as well if supported on the web browser.
-  
+
   # Using SC.SceneView
-  
-  To setup the scene view, you should define the 'scenes' property with an 
+
+  To setup the scene view, you should define the 'scenes' property with an
   array of scene names.  These will be the properties on the scene view that
   you can shift in an out of view as needed.  You can edit the scenes property
   at any time.  It will only be used when you start to transition from one
   scene to another.
-  
-  Next you should set your nowShowing property to the name of the scene you 
+
+  Next you should set your nowShowing property to the name of the scene you
   would like to display.  This will cause the view to transition scenes if it
-  is visible on screen.  Otherwise, it will simply make the new scene view 
+  is visible on screen.  Otherwise, it will simply make the new scene view
   the current content view and that's it.
-  
+
   @extends SC.ContainerView
   @since Version 1.0
 */
@@ -56,10 +56,10 @@ SC.SceneView = SC.ContainerView.extend(
     The options for the given transitionSwap plugin when animating backwards.
 
     @type Object
-    @default { duration: .25, direction: 'left' }
+    @default { duration: .25, direction: 'right' }
     @see SC.ContainerView#transitionBackwardOptions
   */
-  transitionBackwardOptions: { duration: .25, direction: 'left' },
+  transitionBackwardOptions: { duration: .25, direction: 'right' },
 
   /**
     The transitionSwap plugin to use when animating forwards.
@@ -74,10 +74,10 @@ SC.SceneView = SC.ContainerView.extend(
     The options for the given transitionSwap plugin when animating forwards.
 
     @type Object
-    @default { duration: .25, direction: 'right' }
+    @default { duration: .25, direction: 'left' }
     @see SC.ContainerView#transitionBackwardOptions
   */
-  transitionForwardOptions: { duration: .25, direction: 'right' },
+  transitionForwardOptions: { duration: .25, direction: 'left' },
 
   /** @private
     @param {SC.View} newContent the new content view or null.
@@ -85,9 +85,9 @@ SC.SceneView = SC.ContainerView.extend(
   */
   replaceContent: function(newContent) {
     var scenes = this.get('scenes'),
-        nowShowing = this.get('nowShowing'),
-        outIdx = scenes ? scenes.indexOf(this._lastNowShowingView) : -1,
-        inIdx = scenes ? scenes.indexOf(nowShowing) : -1;
+      nowShowing = this.get('nowShowing'),
+      outIdx = scenes ? scenes.indexOf(this._lastNowShowingView) : -1,
+      inIdx = scenes ? scenes.indexOf(nowShowing) : -1;
 
     this._lastNowShowingView = nowShowing;
 
