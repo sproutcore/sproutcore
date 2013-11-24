97328ecc353070c5cf40777ea2ba07d396d920e7
Tyler Keating <tyler@7x7software.com>
2013-11-24 14:08:22 -0600

    Actually fixes regression in SC.TextFieldView.

--- frameworks/foundation/views/text_field.js
+++ frameworks/foundation/views/text_field.js
@@ -732,7 +732,7 @@ SC.TextFieldView = SC.FieldView.extend(SC.Editable,
                       '" value="' + value + '"' + '/></div>');
       }
     } else {
-      var input = context.$input(),
+      var input = this.$input(),
         element = input[0],
         val = this.get('value');
 
