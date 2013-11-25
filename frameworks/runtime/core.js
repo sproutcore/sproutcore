ddd4dbc90f121b7e6122cff3419c0aae6a939819
Tyler Keating <tyler@7x7software.com>
2013-11-25 10:31:47 -0600

    Updates SC.VERSION for Edge.

--- frameworks/runtime/core.js
+++ frameworks/runtime/core.js
@@ -59,7 +59,7 @@ window.SproutCore = window.SproutCore || SC;
 */
 SC = window.SC; // This is dumb but necessary for jsdoc to get it right
 
-SC.VERSION = '1.10.1';
+SC.VERSION = 'Edge';
 
 /**
   @private
