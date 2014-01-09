dfe8956f18c06ab648cad4eec8d9ba0891fa24bf
Nicolas BADIA <badia.nicolas@gmail.com>
2014-01-09 19:28:35 +0100

    Adds ordinal function for the french language

---
+++ frameworks/core_foundation/french.lproj/ordinal.js
@@ -0,0 +1,13 @@
+// ==========================================================================
+// Project:   SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Strobe Inc. and contributors.
+//            Portions ©2008-2011 Apple Inc. All rights reserved.
+// License:   Licensed under MIT license (see license.js)
+// ==========================================================================
+
+/**
+ * Returns the ordinal associated for the current number.
+ */
+SC.methodForLocale('fr', 'ordinalForNumber', function(number) {
+  return number == 1 ? 'er' : 'e';
+});
