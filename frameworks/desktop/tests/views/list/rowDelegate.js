52e750cc6b03550abc1cdf8d494f6c569b331df6
Tyler Keating <tyler@7x7software.com>
2014-03-28 20:45:31 -0600

    Adds layoutDirection property to SC.ListView. Refactors SC.ListView and SC.CollectionRowDelegate to move away from height specific methods and properties to generic height or width methods and properties.
    Also fixes issue that rowSpacing set on the row delegate had no effect on the list item layout.

--- frameworks/desktop/tests/views/list/rowDelegate.js
+++ frameworks/desktop/tests/views/list/rowDelegate.js
@@ -9,23 +9,23 @@ var view, del, content;
 module("SC.ListView.rowDelegate", {
   setup: function() {
     del = SC.Object.create(SC.CollectionRowDelegate);
-    
+
     // fake empty array that implements delegate
     content = SC.Object.create(SC.CollectionRowDelegate, SC.Array, SC.Freezable, {
-      
+
       isFrozen: YES,
-      
+
       length: 0,
-      
+
       objectAt: function(idx) { return undefined; }
     });
-    
+
   }
 });
 
 // ..........................................................
 // BASIC CONFIGURATION
-// 
+//
 
 test("no delegate and no content", function() {
   view = SC.ListView.create();
@@ -61,18 +61,18 @@ test("with delegate and content is delegate", function() {
 
 // ..........................................................
 // CHANGING PROPERTIES
-// 
+//
 
 test("modifying delegate and content", function() {
   var callCount = 0 ;
-  
+
   view = SC.ListView.create();
   view.addObserver('rowDelegate', function() { callCount++; });
-  
+
   equals(view.get('delegate'), null, 'precond - delegate should be null');
   equals(view.get('content'), null, 'precond - content should be null');
   equals(view.get('rowDelegate'), view, 'default row delegate should view');
-  
+
   // test setting content
   callCount=0;
   SC.run(function() { view.set('content', content); });
@@ -96,41 +96,41 @@ test("modifying delegate and content", function() {
   SC.run(function() { view.set('delegate', null); });
   ok(callCount>0, 'rowDelegate should change when setting delegate');
   equals(view.get('rowDelegate'), view, 'rowDelegate should go back to view when delegate and content cleared or do not implement');
-  
+
 });
 
 // ..........................................................
 // NOTIFICATIONS
-// 
+//
 
 test("changing the rowHeight should invalidate all row heights", function() {
   var indexes = null;
   view = SC.ListView.create({
     content:  '1 2 3 4 5'.w(), // provide some content
     delegate: del,
-    
+
     // override for testing
-    rowHeightDidChangeForIndexes: function(passed) {
+    rowSizeDidChangeForIndexes: function(passed) {
       indexes = passed;
     }
   });
-  
+
   indexes = null;
   del.set('rowHeight', 30);
-  same(indexes, SC.IndexSet.create(0,5), 'changing row height should call rowHeightDidChangeForIndexes(0..5)');
-  
+  same(indexes, SC.IndexSet.create(0,5), 'changing row height should call rowSizeDidChangeForIndexes(0..5)');
+
   // remove del and try again to verify that it stops tracking changes
   view.set('delegate', null);
   indexes = null;
   del.set('rowHeight', 23);
   equals(indexes, null, 'once delegate is removed changed rowHeight should not impact anything');
-  
+
   // change row height on the view without a delegate to verify new observers
   // are setup.
   indexes = null;
   view.set('rowHeight', 14);
-  same(indexes, SC.IndexSet.create(0,5), 'changing row height should call rowHeightDidChangeForIndexes(0..5)');
-  
+  same(indexes, SC.IndexSet.create(0,5), 'changing row height should call rowSizeDidChangeForIndexes(0..5)');
+
 });
 
 
@@ -139,13 +139,13 @@ test("changing the customRowHeightIndexes should invalidate impacted row heights
   view = SC.ListView.create({
     content:  '1 2 3 4 5'.w(), // provide some content
     delegate: del,
-    
+
     // override for testing
-    rowHeightDidChangeForIndexes: function(passed) {
+    rowSizeDidChangeForIndexes: function(passed) {
       indexes = passed;
     }
   });
-  
+
   // try changing the index set
   indexes = null;
   var set = SC.IndexSet.create(2,2);
@@ -156,7 +156,7 @@ test("changing the customRowHeightIndexes should invalidate impacted row heights
   indexes = null ;
   set.add(1,3);
   same(indexes, set, 'modifying index set should invalidate all old and new indexes');
-  
+
   // changing to a new set
   indexes =null ;
   var set2 = SC.IndexSet.create(0,1);
@@ -179,5 +179,5 @@ test("changing the customRowHeightIndexes should invalidate impacted row heights
   indexes = null ;
   del.set('customRowHeightIndexes', set);
   equals(indexes, null, 'modifying delegate once removed should not change view');
-  
+
 });
