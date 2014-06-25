sproutcore/sproutcore

commit 50ddf31649ba9482f611a21ca9ef7c90201c9cd7

Only accept primary button clicks on controls. i.e. right clicking on a button shouldn't trigger the button

frameworks/desktop/views/checkbox.js
@@ -76,6 +76,9 @@ SC.CheckboxView = SC.ButtonView.extend(
 
   /** @private */
   mouseDown: function(evt) {
+    // Fast path, reject secondary clicks.
+    if (evt.which !== 1) return false;
+
     if(!this.get('isEnabledInPane')) return YES;
     this.set('isActive', YES);
     this._isMouseDown = YES;

frameworks/desktop/views/list_item.js
@@ -356,6 +356,9 @@ SC.ListItemView = SC.View.extend(SC.InlineEditable, SC.Control,
     button.
   */
   mouseDown: function (evt) {
+    // Fast path, reject secondary clicks.
+    if (evt.which !== 1) return false;
+
     // if content is not editable, then always let collection view handle the
     // event.
     if (!this.get('contentIsEditable')) return NO;

frameworks/desktop/views/popup_button.js
@@ -181,6 +181,9 @@ SC.PopupButtonView = SC.ButtonView.extend(
     @returns {Boolean}
   */
   mouseDown: function(evt) {
+    // Fast path, reject secondary clicks.
+    if (evt.which !== 1) return false;
+
     // If disabled, handle mouse down but ignore it.
     if (!this.get('isEnabledInPane')) return YES ;
 

frameworks/desktop/views/radio.js
@@ -347,6 +347,9 @@ SC.RadioView = SC.View.extend(SC.Control,
     mouseUp.
   */
   mouseDown: function(evt) {
+    // Fast path, reject secondary clicks.
+    if (evt.which !== 1) return false;
+
     if (!this.get('isEnabledInPane')) return YES;
 
     var delegate = this.get('renderDelegate'), proxy = this.get('renderDelegateProxy'),

frameworks/desktop/views/slider.js
@@ -149,6 +149,9 @@ SC.SliderView = SC.View.extend(SC.Control,
 
   /* @private */
   mouseDown: function(evt) {
+    // Fast path, reject secondary clicks.
+    if (evt.which !== 1) return false;
+
     if (!this.get('isEnabledInPane')) return YES; // nothing to do...
     this.set('isActive', YES);
     this._isMouseDown = YES ;

frameworks/desktop/views/button.js
@@ -636,6 +636,9 @@ SC.ButtonView = SC.View.extend(SC.Control,
     On mouse down, set active only if enabled.
   */
   mouseDown: function(evt) {
+    // Fast path, reject secondary clicks.
+    if (evt.which !== 1) return false;
+
     var buttonBehavior = this.get('buttonBehavior');
 
     if (!this.get('isEnabledInPane')) return YES ; // handled event, but do nothing

frameworks/desktop/views/collection.js
@@ -2086,6 +2086,9 @@ SC.CollectionView = SC.View.extend(SC.CollectionViewDelegate, SC.CollectionConte
     @returns {Boolean} Usually YES.
   */
   mouseDown: function (ev) {
+    // Fast path, reject secondary clicks.
+    if (ev.which !== 1) return false;
+
     var content = this.get('content');
 
     // Fast path!

frameworks/desktop/views/menu_item.js
@@ -454,7 +454,8 @@ SC.MenuItemView = SC.View.extend(SC.ContentDisplay,
 
   /** @private*/
   mouseDown: function (evt) {
-    return YES;
+    // Reject secondary clicks.
+    return evt.which !== 1;
   },
 
   /** @private */

commit 54f8f053fce483709a7f8e858b55d79ca1abc726

Un-breaks touch on several controls that broke after 50ddf31649ba9482f611a21ca9ef7c90201c9cd7 added right-button click rejection. This is a partial measure; the correct approach will be to separate out click and touch handling rather than simply proxying touches through the click handler; should also include testing. Resolves #1250.

frameworks/desktop/views/checkbox.js
@@ -77,7 +77,7 @@ SC.CheckboxView = SC.ButtonView.extend(
   /** @private */
   mouseDown: function(evt) {
     // Fast path, reject secondary clicks.
-    if (evt.which !== 1) return false;
+    if (evt.which && evt.which !== 1) return false;
 
     if(!this.get('isEnabledInPane')) return YES;
     this.set('isActive', YES);

frameworks/desktop/views/list_item.js
@@ -357,7 +357,7 @@ SC.ListItemView = SC.View.extend(SC.InlineEditable, SC.Control,
   */
   mouseDown: function (evt) {
     // Fast path, reject secondary clicks.
-    if (evt.which !== 1) return false;
+    if (evt.which && evt.which !== 1) return false;
 
     // if content is not editable, then always let collection view handle the
     // event.

frameworks/desktop/views/popup_button.js
@@ -182,7 +182,7 @@ SC.PopupButtonView = SC.ButtonView.extend(
   */
   mouseDown: function(evt) {
     // Fast path, reject secondary clicks.
-    if (evt.which !== 1) return false;
+    if (evt.which && evt.which !== 1) return false;
 
     // If disabled, handle mouse down but ignore it.
     if (!this.get('isEnabledInPane')) return YES ;

frameworks/desktop/views/radio.js
@@ -348,7 +348,7 @@ SC.RadioView = SC.View.extend(SC.Control,
   */
   mouseDown: function(evt) {
     // Fast path, reject secondary clicks.
-    if (evt.which !== 1) return false;
+    if (evt.which && evt.which !== 1) return false;
 
     if (!this.get('isEnabledInPane')) return YES;
 

frameworks/desktop/views/slider.js
@@ -208,7 +208,7 @@ SC.SliderView = SC.View.extend(SC.Control,
   /* @private */
   mouseDown: function(evt) {
     // Fast path, reject secondary clicks.
-    if (evt.which !== 1) return false;
+    if (evt.which && evt.which !== 1) return false;
 
     if (!this.get('isEnabledInPane')) return YES; // nothing to do...
     this.set('isActive', YES);