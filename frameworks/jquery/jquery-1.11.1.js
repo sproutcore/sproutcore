a51bfc0688f9eaa9325e92b0340f17dab4f98787
Tyler Keating <tyler@7x7software.com>
2014-08-29 22:08:34 -0600

    Removes comment that is being sniffed by the build tools as a require() command.

--- frameworks/jquery/jquery-1.11.1.js
+++ frameworks/jquery/jquery-1.11.1.js
@@ -20,7 +20,7 @@
 		// For environments that do not inherently posses a window with a document
 		// (such as Node.js), expose a jQuery-making factory as module.exports
 		// This accentuates the need for the creation of a real window
-		// e.g. var jQuery = require("jquery")(window);
+		//
 		// See ticket #14549 for more info
 		module.exports = global.document ?
 			factory( global, true ) :
