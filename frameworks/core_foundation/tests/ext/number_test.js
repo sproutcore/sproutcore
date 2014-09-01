9c35f75ea4cbae446e984aa781b7c8b57c7f4e84
Tyler Keating <tyler@7x7software.com>
2014-09-01 10:22:14 -0600

    Fixes failing unit tests, due to the locale being improperly cleaned up when changed.

--- frameworks/core_foundation/tests/ext/number_test.js
+++ frameworks/core_foundation/tests/ext/number_test.js
@@ -6,18 +6,34 @@
 // ==========================================================================
 /*globals module, test, start, stop, expect, ok, equals*/
 
-module("Number#ordinal");
+var defaultLocale;
+module("Number#ordinal", {
+  setup: function () {
+    // Cache the current locale.
+    defaultLocale = SC.Locale.currentLocale;
+  },
+
+  teardown: function () {
+    // Return the current locale.
+    SC.Locale.currentLocale = defaultLocale;
+  }
+});
 
 /**
  * Admitedly not exhaustive, but tests the numbers from 1-100
  */
 test("Properly Computes the Ordinal in english", function () {
 
+  // Force it to English
+  String.preferredLanguage = 'en';
+  SC.Locale.currentLocale = SC.Locale.createCurrentLocale();
+
   equals(SC.Locale.currentLocale.language, 'en');
 
   var sts = [1, 21, 31, 41, 51, 61, 71, 81, 91, 101],
     nds = [2, 22, 32, 42, 52, 62, 72, 82, 92, 102],
     rds = [3, 23, 33, 43, 53, 63, 73, 83, 93, 103];
+
   sts.forEach(function (number) {
     equals(number.ordinal(), 'st');
   });
@@ -47,9 +63,8 @@ test("Properly Computes the Ordinal in english", function () {
 
 test("Do not compute the ordinal in a language if no method is defined", function () {
 
-  // force it to japanese
+  // Force it to Japanese
   String.preferredLanguage = 'jp';
-
   SC.Locale.currentLocale = SC.Locale.createCurrentLocale();
 
   equals(SC.Locale.currentLocale.language, 'ja');
