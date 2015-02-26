98afd8284a072d68b5a3a992c853bab6a4cc40fd
Tyler Keating <tyler@7x7software.com>
2015-02-26 10:22:41 -0600

    Fixes regressions in SC.WellView

--- frameworks/desktop/views/well.js
+++ frameworks/desktop/views/well.js
@@ -17,23 +17,23 @@ SC.WELL_CONTAINER_PADDING = 15;
 
   A WellView is a ContainerView with a border. It's useful when you want to
   group a set of views. It allows you to easily switch its contents too.
-  
+
   It has a default contentLayout that will replace the layout of the contentView.
-  
+
   @extends SC.ContainerView
   @since SproutCore 1.0
   @test in progress
 */
 SC.WellView = SC.ContainerView.extend(
 /** @scope SC.WellView.prototype */ {
-  
+
   /**
     @type Array
     @default ['sc-well-view']
     @see SC.View#classNames
   */
   classNames: 'sc-well-view',
-  
+
   /**
     Layout for the content of the container view.
     @type Hash
@@ -44,38 +44,33 @@ SC.WellView = SC.ContainerView.extend(
     left: SC.WELL_CONTAINER_PADDING,
     right: SC.WELL_CONTAINER_PADDING
   },
-  
+
   /**
     @type String
     @default 'wellRenderDelegate'
   */
   renderDelegateName: 'wellRenderDelegate',
-  
+
   /** @private
      Overrides createChildViews and replaces the layout of the contentView
      with the one in contentLayout.
    */
-  createChildViews: function() {
-    // if contentView is defined, then create the content
-    var view = this.get('contentView') ;
-    if (view) {
-      view = this.contentView = this.createChildView(view) ;
-      view.set('layout', this.contentLayout);
-      this.childViews = [view] ;
-    } 
+  createChildView: function (view, attrs) {
+    view = sc_super();
+
+    view.set('layout', this.contentLayout);
+    return view;
   },
 
   /** @private
-     Invoked whenever the content property changes.  This method will simply
-     call replaceContent and set the contentLayout in the new contentView.
-     
-     Override replaceContent to change how the view is
-     swapped out.
+     Invoked whenever the content property changes.  This method will simply set the contentLayout
+     in the new contentView.
    */
-  contentViewDidChange: function() {
-    var view = this.get('contentView');
-    view.set('layout', this.contentLayout);
-    this.replaceContent(view);
-  }.observes('contentView')
-  
-}) ;
+  _sc_contentViewDidChange: function () {
+    var contentView = this.get('contentView');
+    contentView.set('layout', this.contentLayout);
+
+    sc_super();
+  }
+
+});
