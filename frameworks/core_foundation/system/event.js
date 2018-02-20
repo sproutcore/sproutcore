4a24386a4d3ab7c25593ceecce559e00f95b3a43
Nicolas Badia <badia.nicolas@gmail.com>
2018-02-20 10:36:24 +0100

    Removes some SC.FUNCTION_KEYS as they are conflicting with valid printable keys

--- frameworks/core_foundation/system/event.js
+++ frameworks/core_foundation/system/event.js
@@ -982,9 +982,7 @@ SC.FUNCTION_KEYS = {
   8: 'backspace',  9: 'tab',  13: 'return',  19: 'pause',  27: 'escape',
   33: 'pageup', 34: 'pagedown', 35: 'end', 36: 'home',
   37: 'left', 38: 'up', 39: 'right', 40: 'down', 44: 'printscreen',
-  45: 'insert', 46: 'delete', 112: 'f1', 113: 'f2', 114: 'f3', 115: 'f4',
-  116: 'f5', 117: 'f7', 119: 'f8', 120: 'f9', 121: 'f10', 122: 'f11',
-  123: 'f12', 144: 'numlock', 145: 'scrolllock'
+  144: 'numlock', 145: 'scrolllock'
 } ;
 
 SC.PRINTABLE_KEYS = {
