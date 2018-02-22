2c40327cdedc6d192d751be91ee7a99d02556530
Nicolas Badia <badia.nicolas@gmail.com>
2018-02-22 13:09:58 +0100

    Makes Aki the default theme

--- apps/statechart_routing/theme.js
+++ apps/statechart_routing/theme.js
@@ -11,11 +11,11 @@
 // app.
 //
 // You don't have to create the whole theme on your own, though:
-// your app's theme is based on SproutCore's Ace theme.
+// your app's theme is based on SproutCore's Aki theme.
 //
 // NOTE: if you want to change the theme this one is based on, don't
 // forget to change the :css_theme property in your buildfile.
-Test.Theme = SC.AceTheme.create({
+Test.Theme = SC.AkiTheme.create({
   name: 'test'
 });
 
