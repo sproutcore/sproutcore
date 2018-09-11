615719270540280f28d0bc6f5f35ba8d1e004116
Nicolas Badia <badia.nicolas@gmail.com>
2018-09-11 14:33:34 +0200

    Replaces mouse wheel specific code to use the new wheel event

--- frameworks/core_foundation/system/root_responder.js
+++ frameworks/core_foundation/system/root_responder.js
@@ -796,22 +796,7 @@ SC.RootResponder = SC.Object.extend(
       }
     }, this);
 
-    var mousewheel = 'mousewheel';
-
-    // Firefox emits different mousewheel events than other browsers
-    if (SC.browser.isMozilla) {
-      // For Firefox < 3.5, subscribe to DOMMouseScroll events
-      if (SC.browser.compare(SC.browser.engineVersion, '1.9.1') < 0) {
-        mousewheel = 'DOMMouseScroll';
-
-      // For Firefox 3.5 and greater, we can listen for MozMousePixelScroll,
-      // which supports pixel-precision scrolling devices, like MacBook
-      // trackpads.
-      } else {
-        mousewheel = 'MozMousePixelScroll';
-      }
-    }
-    SC.Event.add(document, mousewheel, this, this.mousewheel);
+    SC.Event.add(document, 'wheel', this, this.mousewheel);
 
     // Do some initial set up.
     this.set('currentWindowSize', this.computeWindowSize()) ;
