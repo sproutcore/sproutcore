534ee56a5363849073380ad903d9383fad0c3a60
Maurits Lamers <maurits@weidestraat.nl>
2022-04-27 19:13:08 +0200

    Add a loader implementing a webpack safe way for @theme use in sproutcore themed (s)css

--- webpack.common.js
+++ webpack.common.js
@@ -110,6 +110,9 @@ module.exports = {
             }
           },
           {
+            loader: path.join(__dirname, './lib/sc_attheme_loader.js'),
+          },
+          {
             loader: path.resolve('./lib/sc_static_url_loader.js'),
           },
 
