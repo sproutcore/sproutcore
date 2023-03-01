f49ae4658e5b35ec77c1a8db2c9ca74141dffff5
Nicolas BADIA <nicolas@gestixi.com>
2023-03-01 02:57:10 +0100

    Add translations in Spanish and German

---
+++ frameworks/datetime/german.lproj/strings.js
@@ -0,0 +1,45 @@
+// ==========================================================================
+// Project: SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Strobe Inc. and contributors.
+// Portions ©2008-2011 Apple Inc. All rights reserved.
+// License: Licensed under MIT license (see license.js)
+// ==========================================================================
+
+SC.stringsFor('de', {
+  '_SC.DateTime.dayNames' : 'Sonntag Montag Dienstag Mittwoch Donnerstag Freitag Samstag',
+  '_SC.DateTime.abbreviatedDayNames' : 'So Mo Di Mi Do Fr Sa',
+  '_SC.DateTime.monthNames' : 'Januar Februar März April Mai Juni Juli August September Oktober November Dezember',
+  '_SC.DateTime.abbreviatedMonthNames' : 'Jan Feb Mär Apr Mai Jun Jul Aug Sep Okt Nov Dez',
+  '_SC.DateTime.AMPMNames' : 'AM PM',
+
+  '_SC.DateTime.now' : 'Jetzt gerade',
+  '_SC.DateTime.secondIn' : 'Gleich',
+  '_SC.DateTime.secondsIn' : 'In %e Sekunden',
+  '_SC.DateTime.minuteIn' : 'In einer Minute',
+  '_SC.DateTime.minutesIn' : 'In %e Minuten',
+  '_SC.DateTime.hourIn' : 'In einer Stunde ab jetzt',
+  '_SC.DateTime.hoursIn' : 'In ungefähr %e Stunden',
+  '_SC.DateTime.dayIn' : 'Morgen um %i:%M %p',
+  '_SC.DateTime.daysIn' : '%A um %i:%M %p',
+  '_SC.DateTime.weekIn' : 'Nächste Woche',
+  '_SC.DateTime.weeksIn' : 'In %e Wochen',
+  '_SC.DateTime.monthIn' : 'Nächster Monat',
+  '_SC.DateTime.monthsIn' : 'In %e Monaten',
+  '_SC.DateTime.yearIn' : 'Nächstes Jahr',
+  '_SC.DateTime.yearsIn' : 'In %e Jahren',
+
+  '_SC.DateTime.secondAgo' : 'Vor einem Augenblick',
+  '_SC.DateTime.secondsAgo' : 'Vor %e Sekunden',
+  '_SC.DateTime.minuteAgo' : 'Vor einer Minute',
+  '_SC.DateTime.minutesAgo' : 'vor %e Minuten',
+  '_SC.DateTime.hourAgo' : 'Vor einer Stunde',
+  '_SC.DateTime.hoursAgo' : 'Vor etwa %e Stunden',
+  '_SC.DateTime.dayAgo' : 'Gestern um %i:%M %p',
+  '_SC.DateTime.daysAgo' : '%A um %i:%M %p',
+  '_SC.DateTime.weekAgo' : 'Vor ungefähr einer Woche',
+  '_SC.DateTime.weeksAgo' : 'vor %e Wochen',
+  '_SC.DateTime.monthAgo' : 'Vor ungefähr einem Monat',
+  '_SC.DateTime.monthsAgo' : 'vor %e Monaten',
+  '_SC.DateTime.yearAgo' : 'Letztes Jahr',
+  '_SC.DateTime.yearsAgo' : 'vor %e Jahren'
+});
---
+++ frameworks/datetime/spanish.lproj/strings.js
@@ -0,0 +1,45 @@
+// ==========================================================================
+// Project: SproutCore - JavaScript Application Framework
+// Copyright: ©2006-2011 Strobe Inc. and contributors.
+// Portions ©2008-2011 Apple Inc. All rights reserved.
+// License: Licensed under MIT license (see license.js)
+// ==========================================================================
+
+SC.stringsFor('es', {
+  '_SC.DateTime.dayNames' : 'Domingo Lunes Martes Miércoles Jueves Viernes Sábado',
+  '_SC.DateTime.abbreviatedDayNames' : 'Dom Lun Tue Wed Thu Vie Sáb',
+  '_SC.DateTime.monthNames' : 'Enero Febrero Marzo Abril Mayo Junio Julio Agosto Septiembre Octubre Noviembre Diciembre',
+  '_SC.DateTime.abbreviatedMonthNames': 'Ene Feb Mar Abr May Jun Jul Ago Sep Oct Nov Dec',
+  '_SC.DateTime.AMPMNames' : 'AM PM',
+
+  '_SC.DateTime.now' : 'Ahora mismo',
+  '_SC.DateTime.secondIn' : 'En un momento',
+  '_SC.DateTime.secondsIn' : 'En %e segundos',
+  '_SC.DateTime.minuteIn' : 'En un minuto',
+  '_SC.DateTime.minutesIn' : 'En %e minutos',
+  '_SC.DateTime.hourIn' : 'Dentro de una hora',
+  '_SC.DateTime.hoursIn' : 'En aproximadamente %e horas',
+  '_SC.DateTime.dayIn' : 'Mañana a las %i:%M %p',
+  '_SC.DateTime.daysIn' : '%A a las %i:%M %p',
+  '_SC.DateTime.weekIn' : 'Próxima semana',
+  '_SC.DateTime.weeksIn' : 'En %e semanas',
+  '_SC.DateTime.monthIn' : 'Próximo mes',
+  '_SC.DateTime.monthsIn' : 'En %e meses',
+  '_SC.DateTime.yearIn' : 'Próximo año',
+  '_SC.DateTime.yearsIn' : 'En %e años',
+
+  '_SC.DateTime.secondAgo' : 'Hace un momento',
+  '_SC.DateTime.secondsAgo' : '%e segundos atrás',
+  '_SC.DateTime.minuteAgo' : 'Hace un minuto',
+  '_SC.DateTime.minutesAgo' : '%e minutos atrás',
+  '_SC.DateTime.hourAgo' : 'Hace una hora',
+  '_SC.DateTime.hoursAgo' : 'Hace unas %e horas',
+  '_SC.DateTime.dayAgo' : 'Ayer a las %i:%M %p',
+  '_SC.DateTime.daysAgo' : '%A a las %i:%M %p',
+  '_SC.DateTime.weekAgo' : 'Hace aproximadamente una semana',
+  '_SC.DateTime.weeksAgo' : '%e semanas atrás',
+  '_SC.DateTime.monthAgo' : 'Hace aproximadamente un mes',
+  '_SC.DateTime.monthsAgo' : '%e meses atrás',
+  '_SC.DateTime.yearAgo' : 'El año pasado',
+  '_SC.DateTime.yearsAgo' : '%e años atrás'
+});
